fd1ec98cfb3636745b5028ddc18635e9cfd75c4e
hrasbas <hrasbasji@gmail.com>
2026-07-01 03:40:06 +0530

    Add new entries for Afghan shops in afghanistan.js

--- afghanistan.js
+++ afghanistan.js
@@ -33,5 +33,16 @@
     c: "fashion, clothing, women, men, kids, jewellery, gifts",
     u: "https://afghanfashion.com",
     a: ""
-  }
+  },
+  { id: 6, n: "Click.af", c: "general, marketplace, shopping, electronics, mobile, fashion, home, beauty, kids, books", u: "https://click.af", a: "" },
+
+{ id: 7, n: "Afghan Wireless Shop", c: "general, electronics, mobile, telecom, accessories, gadgets", u: "https://shop.awcc.com.af", a: "" },
+
+{ id: 8, n: "Roshan Shop", c: "general, electronics, mobile, telecom, accessories", u: "https://www.roshan.af", a: "" },
+
+{ id: 9, n: "MTN Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://mtn.com.af", a: "" },
+
+{ id: 10, n: "Etisalat Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://www.etisalat.af", a: "" }
+
+  
 ];
