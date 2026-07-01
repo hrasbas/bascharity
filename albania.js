5f05d76fbfd00551fad22a2b2fedc7a50f313068
hrasbas <hrasbasji@gmail.com>
2026-07-01 08:34:13 +0530

    Add shops data for Albania

---
+++ albania.js
@@ -0,0 +1,212 @@
+countryShops.albania = [
+  { id: 1, n: "Big Market", c: "general, supermarket, grocery, food, home", u: "https://bigmarket.al", a: "" },
+  { id: 2, n: "SPAR Albania", c: "general, supermarket, grocery, food, beverages", u: "https://spar.al", a: "" },
+  { id: 3, n: "Conad Albania", c: "general, supermarket, grocery, food", u: "https://conad.al", a: "" },
+  { id: 4, n: "Neptun", c: "general, electronics, mobile, computers, appliances", u: "https://neptun.al", a: "" },
+  { id: 5, n: "Globe", c: "general, electronics, mobile, computers", u: "https://globe.al", a: "" },
+  { id: 6, n: "AZTech", c: "general, electronics, computers, gaming", u: "https://aztech.al", a: "" },
+  { id: 7, n: "Megatek", c: "general, home, furniture, tools, garden", u: "https://megateksa.com", a: "" },
+  { id: 8, n: "Jumbo Albania", c: "general, toys, kids, gifts, home", u: "https://jumbo.al", a: "" },
+  { id: 9, n: "Fashion Group Albania", c: "general, fashion, shoes, beauty", u: "https://fashiongroup.al", a: "" },
+  { id: 10, n: "LC Waikiki Albania", c: "general, fashion, clothing, kids", u: "https://www.lcwaikiki.com", a: "" },
+  { id: 11, n: "Mango", c: "general, fashion, clothing, women", u: "https://shop.mango.com", a: "" },
+  { id: 12, n: "Zara", c: "general, fashion, clothing, kids", u: "https://www.zara.com", a: "" },
+  { id: 13, n: "Bershka", c: "general, fashion, clothing, shoes", u: "https://www.bershka.com", a: "" },
+  { id: 14, n: "Pull&Bear", c: "general, fashion, clothing", u: "https://www.pullandbear.com", a: "" },
+  { id: 15, n: "Stradivarius", c: "general, fashion, clothing, accessories", u: "https://www.stradivarius.com", a: "" },
+  { id: 16, n: "adidas", c: "general, sports, shoes, fashion", u: "https://www.adidas.com", a: "" },
+  { id: 17, n: "Nike", c: "general, sports, shoes, fashion", u: "https://www.nike.com", a: "" },
+  { id: 18, n: "Intersport", c: "general, sports, fitness, outdoor", u: "https://www.intersport.al", a: "" },
+  { id: 19, n: "Rossmann & Lala", c: "general, pharmacy, beauty, health", u: "https://rossmann-lala.al", a: "" },
+  { id: 20, n: "Farmanet", c: "general, pharmacy, health, beauty", u: "https://farmanet.al", a: "" },
+  { id: 21, n: "MerrJep", c: "general, marketplace, electronics, cars, home", u: "https://www.merrjep.al", a: "" },
+  { id: 22, n: "Gjirafa50", c: "general, marketplace, electronics, fashion, home", u: "https://gjirafa50.com", a: "" },
+  { id: 23, n: "Booking.com", c: "general, hotels, travel, tourism", u: "https://www.booking.com", a: "" },
+  { id: 24, n: "Airbnb", c: "general, travel, hotels, apartments", u: "https://www.airbnb.com", a: "" },
+  { id: 25, n: "Glovo", c: "general, food, grocery, restaurant, delivery", u: "https://glovoapp.com", a: "" },
+
+{ id: 26, n: "Wolt Albania", c: "general, food, grocery, restaurant, delivery", u: "https://wolt.com", a: "" },
+
+{ id: 27, n: "Burger King Albania", c: "general, food, restaurant, fast food", u: "https://www.burgerking.al", a: "" },
+
+{ id: 28, n: "KFC Albania", c: "general, food, restaurant, fast food", u: "https://kfc.al", a: "" },
+
+{ id: 29, n: "Pizza Hut Albania", c: "general, food, restaurant, pizza", u: "https://pizzahut.al", a: "" },
+
+{ id: 30, n: "Domino's Pizza", c: "general, food, restaurant, pizza", u: "https://www.dominos.com", a: "" },
+
+{ id: 31, n: "Mon Cheri", c: "general, bakery, cafe, food, desserts", u: "https://moncheri.al", a: "" },
+
+{ id: 32, n: "Mulliri Vjeter", c: "general, cafe, coffee, food", u: "https://mullirivjeter.al", a: "" },
+
+{ id: 33, n: "Vodafone Albania", c: "general, mobile, telecom, internet", u: "https://www.vodafone.al", a: "" },
+
+{ id: 34, n: "ONE Albania", c: "general, mobile, telecom, internet", u: "https://www.one.al", a: "" },
+
+{ id: 35, n: "ALBtelecom", c: "general, telecom, internet, tv", u: "https://www.albtelecom.al", a: "" },
+
+{ id: 36, n: "Raiffeisen Bank Albania", c: "general, banking, finance, payments", u: "https://www.raiffeisen.al", a: "" },
+
+{ id: 37, n: "BKT", c: "general, banking, finance, payments", u: "https://www.bkt.com.al", a: "" },
+
+{ id: 38, n: "Credins Bank", c: "general, banking, finance, loans", u: "https://www.bankacredins.com", a: "" },
+
+{ id: 39, n: "OTP Bank Albania", c: "general, banking, finance", u: "https://www.otpbank.al", a: "" },
+
+{ id: 40, n: "Abi Bank", c: "general, banking, finance", u: "https://www.abi.al", a: "" },
+
+{ id: 41, n: "DHL Albania", c: "general, courier, logistics, shipping", u: "https://www.dhl.com", a: "" },
+
+{ id: 42, n: "FedEx", c: "general, courier, logistics, shipping", u: "https://www.fedex.com", a: "" },
+
+{ id: 43, n: "UPS", c: "general, courier, logistics, shipping", u: "https://www.ups.com", a: "" },
+
+{ id: 44, n: "Aramex", c: "general, courier, logistics, shipping", u: "https://www.aramex.com", a: "" },
+
+{ id: 45, n: "Air Albania", c: "general, travel, flights, airlines", u: "https://www.airalbania.com.al", a: "" },
+
+{ id: 46, n: "Tirana International Airport", c: "general, travel, airport, flights", u: "https://www.tirana-airport.com", a: "" },
+
+{ id: 47, n: "Blue Taxi", c: "general, taxi, transport, travel", u: "https://bluetaxi.al", a: "" },
+
+{ id: 48, n: "Booking.com", c: "general, hotels, travel, tourism", u: "https://www.booking.com", a: "" },
+
+{ id: 49, n: "Agoda", c: "general, hotels, travel, tourism", u: "https://www.agoda.com", a: "" },
+
+{ id: 50, n: "Trip.com", c: "general, hotels, flights, travel", u: "https://www.trip.com", a: "" },
+
+{ id: 51, n: "eSky Albania", c: "general, travel, flights, hotels, tourism", u: "https://www.esky.al", a: "" },
+
+{ id: 52, n: "FlixBus", c: "general, travel, bus, transport, tickets", u: "https://www.flixbus.com", a: "" },
+
+{ id: 53, n: "Rent From Locals Albania", c: "general, car rental, travel, transport", u: "https://rentfromlocals.al", a: "" },
+
+{ id: 54, n: "Eurocar Albania", c: "general, car rental, travel, transport", u: "https://www.eurocar.al", a: "" },
+
+{ id: 55, n: "Europcar", c: "general, car rental, travel, transport", u: "https://www.europcar.com", a: "" },
+
+{ id: 56, n: "Shell Albania", c: "general, fuel, automotive, services", u: "https://www.shell.al", a: "" },
+
+{ id: 57, n: "Kastrati", c: "general, fuel, automotive, services", u: "https://kastrati.al", a: "" },
+
+{ id: 58, n: "Neptun Mobile", c: "general, electronics, mobile, accessories", u: "https://neptun.al", a: "" },
+
+{ id: 59, n: "GoTech", c: "general, electronics, computers, mobile", u: "https://gotech.al", a: "" },
+
+{ id: 60, n: "OfficeLine", c: "general, office, stationery, electronics", u: "https://officeline.al", a: "" },
+
+{ id: 61, n: "Adrion Bookstore", c: "general, books, education, stationery", u: "https://adrionltd.com", a: "" },
+
+{ id: 62, n: "Libraria Albania", c: "general, books, education, gifts", u: "https://librarialbania.al", a: "" },
+
+{ id: 63, n: "Toys4Kids Albania", c: "general, toys, kids, games", u: "https://toys4kids.al", a: "" },
+
+{ id: 64, n: "Baby Center Albania", c: "general, baby, kids, clothing", u: "https://babycenter.al", a: "" },
+
+{ id: 65, n: "Chic Shoes", c: "general, shoes, fashion, accessories", u: "https://chicshoes.al", a: "" },
+
+{ id: 66, n: "Optika Roma", c: "general, eyewear, health, fashion", u: "https://optikaroma.com", a: "" },
+
+{ id: 67, n: "DentalTurism Albania", c: "general, dental, health, medical", u: "https://dentalturismalbania.com", a: "" },
+
+{ id: 68, n: "American Hospital Albania", c: "general, hospital, medical, health", u: "https://americanhospital.al", a: "" },
+
+{ id: 69, n: "Hygeia Hospital", c: "general, hospital, medical, health", u: "https://hygeia.al", a: "" },
+
+{ id: 70, n: "Aladini", c: "general, marketplace, shopping, home", u: "https://aladini.al", a: "" },
+
+{ id: 71, n: "Shpresa AL", c: "general, furniture, home, decor", u: "https://shpresa.al", a: "" },
+
+{ id: 72, n: "CoinCasa Albania", c: "general, furniture, home, decor", u: "https://coincasa.al", a: "" },
+
+{ id: 73, n: "Mr. Chicken Albania", c: "general, food, restaurant, takeaway", u: "https://mrchicken.al", a: "" },
+
+{ id: 74, n: "Proper Pizza", c: "general, food, pizza, restaurant", u: "https://properpizza.al", a: "" },
+
+{ id: 75, n: "SushiCo Albania", c: "general, food, restaurant, sushi", u: "https://sushico.al", a: "" },
+
+{ id: 76, n: "Rossmann Albania", c: "general, pharmacy, health, beauty, personal care", u: "https://rossmann-lala.al", a: "" },
+
+{ id: 77, n: "Venere Pharmacy", c: "general, pharmacy, health, beauty", u: "https://farmacivenere.al", a: "" },
+
+{ id: 78, n: "Pegasus Hospital", c: "general, hospital, medical, health", u: "https://pegasusmedical.al", a: "" },
+
+{ id: 79, n: "Intermedica", c: "general, medical, laboratory, health", u: "https://intermedica.al", a: "" },
+
+{ id: 80, n: "Dental Center Albania", c: "general, dental, medical, health", u: "https://dentalcenteralbania.com", a: "" },
+
+{ id: 81, n: "Mango Outlet", c: "general, fashion, clothing, accessories", u: "https://www.mangooutlet.com", a: "" },
+
+{ id: 82, n: "Deichmann", c: "general, shoes, fashion, accessories", u: "https://www.deichmann.com", a: "" },
+
+{ id: 83, n: "Skechers", c: "general, shoes, sports, fashion", u: "https://www.skechers.com", a: "" },
+
+{ id: 84, n: "Pandora", c: "general, jewelry, gifts, fashion", u: "https://www.pandora.net", a: "" },
+
+{ id: 85, n: "Swarovski", c: "general, jewelry, gifts, fashion", u: "https://www.swarovski.com", a: "" },
+
+{ id: 86, n: "Pandora Albania", c: "general, jewelry, fashion, gifts", u: "https://fashiongroup.al", a: "" },
+
+{ id: 87, n: "Miniso", c: "general, home, gifts, toys, beauty", u: "https://miniso.com", a: "" },
+
+{ id: 88, n: "Flying Tiger Copenhagen", c: "general, gifts, home, toys, stationery", u: "https://flyingtiger.com", a: "" },
+
+{ id: 89, n: "The Body Shop", c: "general, beauty, cosmetics, skincare", u: "https://www.thebodyshop.com", a: "" },
+
+{ id: 90, n: "Sephora", c: "general, beauty, cosmetics, skincare", u: "https://www.sephora.com", a: "" },
+
+{ id: 91, n: "AIA Albania", c: "general, insurance, finance, health", u: "https://www.aia.al", a: "" },
+
+{ id: 92, n: "SIGAL", c: "general, insurance, finance, travel", u: "https://www.sigal.com.al", a: "" },
+
+{ id: 93, n: "INSIG", c: "general, insurance, finance", u: "https://www.insig.com.al", a: "" },
+
+{ id: 94, n: "University of Tirana", c: "general, education, university, books", u: "https://unitir.edu.al", a: "" },
+
+{ id: 95, n: "Epoka University", c: "general, education, university", u: "https://epoka.edu.al", a: "" },
+
+{ id: 96, n: "Agricultural University of Tirana", c: "general, education, university", u: "https://ubt.edu.al", a: "" },
+
+{ id: 97, n: "Albania e-Albania", c: "general, government, services, documents", u: "https://e-albania.al", a: "" },
+
+{ id: 98, n: "Albanian Post", c: "general, post, courier, logistics", u: "https://www.postashqiptare.al", a: "" },
+
+{ id: 99, n: "Pet Center Albania", c: "general, pets, food, accessories", u: "https://petcenter.al", a: "" },
+
+{ id: 100, n: "Interflora", c: "general, flowers, gifts, delivery", u: "https://www.interflora.com", a: "" },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
