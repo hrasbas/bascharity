bc493d137b48679d94f54ec5ade0d96d2a47cf31
hrasbas <hrasbasji@gmail.com>
2026-07-01 08:54:14 +0530

    Add Algeria shop and service listings
    Added a list of shops and services in Algeria with details such as name, category, and URL.

---
+++ algeria.js
@@ -0,0 +1,227 @@
+countryShops.algeria = [
+{ id: 1, n: "Ouedkniss", c: "general, marketplace, electronics, fashion, vehicles, home", u: "https://www.ouedkniss.com", a: "" },
+
+{ id: 2, n: "Jumia Algeria", c: "general, marketplace, electronics, fashion, home, beauty", u: "https://www.jumia.dz", a: "" },
+
+{ id: 3, n: "Ardis", c: "general, supermarket, grocery, food, bakery", u: "https://ardis.dz", a: "" },
+
+{ id: 4, n: "Uno Hypermarket", c: "general, supermarket, grocery, food, home", u: "https://uno.dz", a: "" },
+
+{ id: 5, n: "Batolis", c: "general, supermarket, grocery, food", u: "https://batolis.com", a: "" },
+
+{ id: 6, n: "Condor", c: "general, electronics, mobile, computers, appliances", u: "https://www.condor.dz", a: "" },
+
+{ id: 7, n: "Bomare Company", c: "general, electronics, appliances, mobile", u: "https://www.bomarecompany.com", a: "" },
+
+{ id: 8, n: "Stream", c: "general, electronics, mobile, computers", u: "https://streamsystem.dz", a: "" },
+
+{ id: 9, n: "SLC", c: "general, electronics, computers, accessories", u: "https://slc-dz.com", a: "" },
+
+{ id: 10, n: "Mobilis", c: "general, telecom, mobile, internet", u: "https://www.mobilis.dz", a: "" },
+
+{ id: 11, n: "Djezzy", c: "general, telecom, mobile, internet", u: "https://www.djezzy.dz", a: "" },
+
+{ id: 12, n: "Ooredoo Algeria", c: "general, telecom, mobile, internet", u: "https://www.ooredoo.dz", a: "" },
+
+{ id: 13, n: "Algérie Télécom", c: "general, telecom, internet, fiber", u: "https://www.algerietelecom.dz", a: "" },
+
+{ id: 14, n: "Air Algérie", c: "general, travel, airlines, flights", u: "https://airalgerie.dz", a: "" },
+
+{ id: 15, n: "Tassili Airlines", c: "general, travel, airlines, flights", u: "https://www.tassiliairlines.dz", a: "" },
+
+{ id: 16, n: "Booking.com", c: "general, hotels, travel, tourism", u: "https://www.booking.com", a: "" },
+
+{ id: 17, n: "Airbnb", c: "general, hotels, apartments, travel", u: "https://www.airbnb.com", a: "" },
+
+{ id: 18, n: "Yassir", c: "general, taxi, food, grocery, delivery", u: "https://yassir.com", a: "" },
+
+{ id: 19, n: "Temtem", c: "general, taxi, transport, delivery", u: "https://temtem.one", a: "" },
+
+{ id: 20, n: "Banxy", c: "general, banking, finance, payments", u: "https://banxybank.com", a: "" },
+
+{ id: 21, n: "CPA Banque", c: "general, banking, finance, payments", u: "https://www.cpa-bank.dz", a: "" },
+
+{ id: 22, n: "BADR Bank", c: "general, banking, finance", u: "https://www.badr-bank.dz", a: "" },
+
+{ id: 23, n: "Saidal", c: "general, pharmacy, health, medical", u: "https://www.saidalgroup.dz", a: "" },
+
+{ id: 24, n: "Algeria Post", c: "general, post, courier, logistics", u: "https://www.poste.dz", a: "" },
+
+{ id: 25, n: "DHL Algeria", c: "general, courier, logistics, shipping", u: "https://www.dhl.com", a: "" },
+
+{ id: 26, n: "FedEx Algeria", c: "general, courier, logistics, shipping", u: "https://www.fedex.com", a: "" },
+
+{ id: 27, n: "UPS Algeria", c: "general, courier, logistics, shipping", u: "https://www.ups.com", a: "" },
+
+{ id: 28, n: "Aramex Algeria", c: "general, courier, logistics, shipping", u: "https://www.aramex.com", a: "" },
+
+{ id: 29, n: "Noon Algeria", c: "general, marketplace, electronics, fashion, home", u: "https://www.noon.com", a: "" },
+
+{ id: 30, n: "Decathlon Algeria", c: "general, sports, fitness, outdoor, fashion", u: "https://www.decathlon.dz", a: "" },
+
+{ id: 31, n: "Orca", c: "general, fashion, shoes, clothing", u: "https://www.orca.dz", a: "" },
+
+{ id: 32, n: "LC Waikiki Algeria", c: "general, fashion, clothing, kids", u: "https://www.lcwaikiki.com", a: "" },
+
+{ id: 33, n: "DeFacto Algeria", c: "general, fashion, clothing, accessories", u: "https://www.defacto.com", a: "" },
+
+{ id: 34, n: "Koton Algeria", c: "general, fashion, clothing, accessories", u: "https://koton.com", a: "" },
+
+{ id: 35, n: "Adidas Algeria", c: "general, sports, shoes, fashion", u: "https://www.adidas.com", a: "" },
+
+{ id: 36, n: "Nike Algeria", c: "general, sports, shoes, fashion", u: "https://www.nike.com", a: "" },
+
+{ id: 37, n: "Puma Algeria", c: "general, sports, shoes, fashion", u: "https://www.puma.com", a: "" },
+
+{ id: 38, n: "Yves Rocher Algeria", c: "general, beauty, cosmetics, skincare", u: "https://www.yves-rocher.dz", a: "" },
+
+{ id: 39, n: "Farmavans", c: "general, pharmacy, health, medical", u: "https://farmavans.dz", a: "" },
+
+{ id: 40, n: "Mustapha Pacha Hospital", c: "general, hospital, medical, health", u: "https://www.chu-mustapha.dz", a: "" },
+
+{ id: 41, n: "CHU Oran", c: "general, hospital, medical, health", u: "https://www.sante-oran.dz", a: "" },
+
+{ id: 42, n: "Hotel El Aurassi", c: "general, hotel, travel, accommodation", u: "https://www.elaurassi.com", a: "" },
+
+{ id: 43, n: "Sheraton Club des Pins", c: "general, hotel, travel, accommodation", u: "https://www.marriott.com", a: "" },
+
+{ id: 44, n: "Sofitel Algiers", c: "general, hotel, travel, accommodation", u: "https://all.accor.com", a: "" },
+
+{ id: 45, n: "Mercure Alger", c: "general, hotel, travel, accommodation", u: "https://all.accor.com", a: "" },
+
+{ id: 46, n: "Ibis Alger", c: "general, hotel, travel, accommodation", u: "https://all.accor.com", a: "" },
+
+{ id: 47, n: "Hôtel Saint Eugène", c: "general, hotel, travel, accommodation", u: "https://hotel-sainteugene.com", a: "" },
+
+{ id: 48, n: "Auto Leader Company", c: "general, automotive, cars, service", u: "https://www.autoleader.dz", a: "" },
+
+{ id: 49, n: "Renault Algérie", c: "general, automotive, cars, service", u: "https://www.renault.dz", a: "" },
+
+{ id: 50, n: "Toyota Algérie", c: "general, automotive, cars, service", u: "https://www.toyota.dz", a: "" },
+
+{ id: 51, n: "BNA Bank", c: "general, banking, finance, payments", u: "https://www.bna.dz", a: "" },
+
+{ id: 52, n: "BEA Bank", c: "general, banking, finance, business", u: "https://www.bea.dz", a: "" },
+
+{ id: 53, n: "BDL Bank", c: "general, banking, finance, loans", u: "https://www.bdl.dz", a: "" },
+
+{ id: 54, n: "CNEP Banque", c: "general, banking, finance, housing", u: "https://www.cnepbanque.dz", a: "" },
+
+{ id: 55, n: "AGB Bank", c: "general, banking, finance, payments", u: "https://www.agb.dz", a: "" },
+
+{ id: 56, n: "Société Générale Algérie", c: "general, banking, finance", u: "https://www.societegenerale.dz", a: "" },
+
+{ id: 57, n: "AXA Algeria", c: "general, insurance, health, automotive", u: "https://www.axa.dz", a: "" },
+
+{ id: 58, n: "CAAT", c: "general, insurance, travel, automotive", u: "https://www.caat.dz", a: "" },
+
+{ id: 59, n: "CAAR", c: "general, insurance, finance, automotive", u: "https://www.caar.dz", a: "" },
+
+{ id: 60, n: "Alliance Assurances", c: "general, insurance, health, finance", u: "https://www.allianceassurances.com.dz", a: "" },
+
+{ id: 61, n: "Bab Ezzouar Shopping Mall", c: "general, shopping, fashion, food, entertainment", u: "https://www.bab-ezzouar.com", a: "" },
+
+{ id: 62, n: "City Center Oran", c: "general, shopping, fashion, food, entertainment", u: "https://citycenteroran.com", a: "" },
+
+{ id: 63, n: "Ardis Mall", c: "general, shopping, supermarket, food, fashion", u: "https://ardis.dz", a: "" },
+
+{ id: 64, n: "Carrefour Algeria", c: "general, supermarket, grocery, food, home", u: "https://www.carrefour.dz", a: "" },
+
+{ id: 65, n: "UNO City Mall", c: "general, shopping, supermarket, food", u: "https://uno.dz", a: "" },
+
+{ id: 66, n: "Samsung Algeria", c: "general, electronics, mobile, appliances", u: "https://www.samsung.com/dz", a: "" },
+
+{ id: 67, n: "LG Algeria", c: "general, electronics, appliances, home", u: "https://www.lg.com/dz", a: "" },
+
+{ id: 68, n: "Huawei Algeria", c: "general, electronics, mobile, computers", u: "https://consumer.huawei.com/dz", a: "" },
+
+{ id: 69, n: "OPPO Algeria", c: "general, electronics, mobile, accessories", u: "https://www.oppo.com/dz", a: "" },
+
+{ id: 70, n: "Xiaomi Algeria", c: "general, electronics, mobile, smart home", u: "https://www.mi.com/dz", a: "" },
+
+{ id: 71, n: "Paul Bakery Algeria", c: "general, bakery, cafe, food", u: "https://www.paul-dz.com", a: "" },
+
+{ id: 72, n: "Pizza Hut Algeria", c: "general, food, pizza, restaurant", u: "https://www.pizzahut.dz", a: "" },
+
+{ id: 73, n: "Burger King Algeria", c: "general, food, restaurant, fast food", u: "https://www.burgerking.dz", a: "" },
+
+{ id: 74, n: "Taj Mahal Restaurant", c: "general, food, restaurant, indian", u: "https://tajmahalalger.com", a: "" },
+
+{ id: 75, n: "La Maison du Café", c: "general, cafe, coffee, food", u: "https://lamaisonducafe.dz", a: "" },
+
+{ id: 76, n: "Maison de la Presse", c: "general, books, stationery, magazines", u: "https://mdp.dz", a: "" },
+
+{ id: 77, n: "Librairie El Ijtihad", c: "general, books, education, stationery", u: "https://elijtihad.dz", a: "" },
+
+{ id: 78, n: "Office National des Publications Scolaires", c: "general, books, education, school", u: "https://www.onps.dz", a: "" },
+
+{ id: 79, n: "Saidal Pharmacies", c: "general, pharmacy, health, medical", u: "https://www.saidalgroup.dz", a: "" },
+
+{ id: 80, n: "Biopharm", c: "general, pharmacy, medicine, health", u: "https://biopharmdz.com", a: "" },
+
+{ id: 81, n: "Clinique Chahrazed", c: "general, hospital, medical, health", u: "https://cliniquechahrazed.dz", a: "" },
+
+{ id: 82, n: "Clinique Al Azhar", c: "general, hospital, medical, health", u: "https://cliniquealazhar.dz", a: "" },
+
+{ id: 83, n: "Air Express Algeria", c: "general, courier, logistics, shipping", u: "https://airexpress.dz", a: "" },
+
+{ id: 84, n: "EMS Algeria", c: "general, courier, express, logistics", u: "https://www.ems.dz", a: "" },
+
+{ id: 85, n: "Yalidine Express", c: "general, courier, logistics, ecommerce", u: "https://yalidine.com", a: "" },
+
+{ id: 86, n: "Guiddini", c: "general, furniture, home, decor", u: "https://guiddini.com", a: "" },
+
+{ id: 87, n: "IKEA Algeria", c: "general, furniture, home, decor", u: "https://www.ikea.com", a: "" },
+
+{ id: 88, n: "Home Box Algeria", c: "general, furniture, home, decor", u: "https://www.homeboxstores.com", a: "" },
+
+{ id: 89, n: "Centre Commercial Garden City", c: "general, shopping, fashion, food", u: "https://gardencity.dz", a: "" },
+
+{ id: 90, n: "Pull&Bear Algeria", c: "general, fashion, clothing, accessories", u: "https://www.pullandbear.com", a: "" },
+
+{ id: 91, n: "Bershka Algeria", c: "general, fashion, clothing, accessories", u: "https://www.bershka.com", a: "" },
+
+{ id: 92, n: "Stradivarius Algeria", c: "general, fashion, clothing, accessories", u: "https://www.stradivarius.com", a: "" },
+
+{ id: 93, n: "Zara Algeria", c: "general, fashion, clothing, kids", u: "https://www.zara.com", a: "" },
+
+{ id: 94, n: "Mango Algeria", c: "general, fashion, clothing, women", u: "https://shop.mango.com", a: "" },
+
+{ id: 95, n: "Intersport Algeria", c: "general, sports, fitness, outdoor", u: "https://www.intersport.dz", a: "" },
+
+{ id: 96, n: "Decathlon City", c: "general, sports, fitness, outdoor", u: "https://www.decathlon.dz", a: "" },
+
+{ id: 97, n: "Go Sport Algeria", c: "general, sports, fitness, clothing", u: "https://www.go-sport.com", a: "" },
+
+{ id: 98, n: "Royal Hotel Oran", c: "general, hotel, travel, accommodation", u: "https://royalhoteloran.com", a: "" },
+
+{ id: 99, n: "Constantine Marriott Hotel", c: "general, hotel, travel, accommodation", u: "https://www.marriott.com", a: "" },
+
+{ id: 100, n: "Renaissance Tlemcen Hotel", c: "general, hotel, travel, accommodation", u: "https://www.marriott.com", a: "" },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
