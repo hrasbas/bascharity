8dc3d02bc00bc4d5866659801a77fbe65aee8569
hrasbas <hrasbasji@gmail.com>
2026-07-01 06:37:27 +0530

    Update afghanistan.js

--- afghanistan.js
+++ afghanistan.js
@@ -1,40 +1,41 @@
  countryShops.afghanistan = [
-  {
-    id: 1,
-    n: "E-Tohfa",
-    c: "general, marketplace, shopping, fashion, electronics, mobile, home, beauty, health, books, gifts, flowers, kids",
-    u: "https://www.e-tohfa.com",
-    a: ""
-  },
-  {
-    id: 2,
-    n: "Sale.af",
-    c: "general, marketplace, shopping, fashion, electronics, home, beauty, health, books, sports, kids",
-    u: "https://sale.af",
-    a: ""
-  },
-  {
-    id: 3,
-    n: "4Sough",
-    c: "general, marketplace, shopping, fashion, electronics, mobile, home, vehicles, services",
-    u: "https://4sough.com/en",
-    a: ""
-  },
-  {
-    id: 4,
-    n: "BaranMart",
-    c: "general, shopping, fashion, electronics, grocery, home, beauty, kids",
-    u: "https://baranmart.com",
-    a: ""
-  },
-  {
-    id: 5,
-    n: "Afghan Fashion",
-    c: "fashion, clothing, women, men, kids, jewellery, gifts",
-    u: "https://afghanfashion.com",
-    a: ""
-  },
-  { id: 6, n: "Click.af", c: "general, marketplace, shopping, electronics, mobile, fashion, home, beauty, kids, books", u: "https://click.af", a: "" },
+{
+id: 1,
+n: "E-Tohfa",
+c: "general, marketplace, shopping, fashion, electronics, mobile, home, beauty, health, books, gifts, flowers, kids",
+u: "https://www.e-tohfa.com",
+a: ""
+},
+{
+id: 2,
+n: "Sale.af",
+c: "general, marketplace, shopping, fashion, electronics, home, beauty, health, books, sports, kids",
+u: "https://sale.af",
+a: ""
+},
+{
+id: 3,
+n: "4Sough",
+c: "general, marketplace, shopping, fashion, electronics, mobile, home, vehicles, services",
+u: "https://4sough.com/en",
+a: ""
+},
+{
+id: 4,
+n: "BaranMart",
+c: "general, shopping, fashion, electronics, grocery, home, beauty, kids",
+u: "https://baranmart.com",
+a: ""
+},
+{
+id: 5,
+n: "Afghan Fashion",
+c: "fashion, clothing, women, men, kids, jewellery, gifts",
+u: "https://afghanfashion.com",
+a: ""
+}
+
+{ id: 6, n: "Click.af", c: "general, marketplace, shopping, electronics, mobile, fashion, home, beauty, kids, books", u: "https://click.af", a: "" },
 
 { id: 7, n: "Afghan Wireless Shop", c: "general, electronics, mobile, telecom, accessories, gadgets", u: "https://shop.awcc.com.af", a: "" },
 
@@ -42,7 +43,128 @@
 
 { id: 9, n: "MTN Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://mtn.com.af", a: "" },
 
-{ id: 10, n: "Etisalat Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://www.etisalat.af", a: "" }
+{ id: 10, n: "Etisalat Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://www.etisalat.af", a: "" },{ id: 11, n: "Noorjahan Market", c: "general, fashion, clothing, home, kids", u: "https://noorjahanmarket.com", a: "" },
 
-  
+{ id: 12, n: "Khanjan Market", c: "general, groceries, fashion, electronics, home", u: "https://khanjanmarket.com", a: "" },
+
+{ id: 13, n: "Maiwand Market", c: "general, electronics, mobile, accessories, home", u: "https://maiwandmarket.com", a: "" },
+
+{ id: 14, n: "Kabul Star Market", c: "general, fashion, beauty, kids, gifts", u: "https://kabulstarmarket.com", a: "" },
+
+{ id: 15, n: "Aryan Shopping", c: "general, marketplace, electronics, fashion, home, books", u: "https://aryanshopping.com", a: "" },
+
+{ id: 16, n: "Afghan Wireless (AWCC)", c: "general, electronics, mobile, telecom, services", u: "https://www.awcc.com.af", a: "" },
+
+{ id: 17, n: "Roshan", c: "general, electronics, mobile, telecom, services", u: "https://www.roshan.af", a: "" },
+
+{ id: 18, n: "Etisalat Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://www.etisalat.af", a: "" },
+
+{ id: 19, n: "MTN Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://afghanistan.mtn.com", a: "" },
+
+{ id: 20, n: "Ubuy Afghanistan", c: "general, electronics, fashion, books, kids, home, beauty, sports", u: "https://www.afghanistan.ubuy.com", a: "" },{ id: 21, n: "Ariana Afghan Airlines", c: "general, travel, flights, services, tickets", u: "https://www.flyariana.com", a: "" },
+
+{ id: 22, n: "Kam Air", c: "general, travel, flights, services, tickets", u: "https://www.kamair.com", a: "" },
+
+{ id: 23, n: "Afghan Post", c: "general, logistics, courier, services", u: "https://afghanpost.gov.af", a: "" },
+
+{ id: 24, n: "E-Tohfa", c: "general, gifts, fashion, beauty, home", u: "https://e-tohfa.com", a: "" },
+
+{ id: 25, n: "Click Afghanistan", c: "general, marketplace, electronics, fashion, home", u: "https://click.af", a: "" },{ id: 26, n: "Aseel", c: "general, gifts, crafts, fashion, home", u: "https://aseelapp.com", a: "" },{ id: 27, n: "Afghan Wireless", c: "general, electronics, mobile, telecom, services", u: "https://www.awcc.com.af", a: "" },{ id: 28, n: "Roshan", c: "general, electronics, mobile, telecom, services", u: "https://www.roshan.af", a: "" },{ id: 29, n: "Etisalat Afghanistan", c: "general, electronics, mobile, telecom, services", u: "https://www.etisalat.af", a: "" },{ id: 30, n: "Kam Air", c: "general, travel, flights, tickets, services", u: "https://www.kamair.com", a: "" },{ id: 31, n: "Ariana Telecom", c: "general, electronics, mobile, telecom, services", u: "https://www.atra.gov.af", a: "" },{ id: 32, n: "Afghan Logistics", c: "general, logistics, courier, services", u: "https://www.afghanlogistics.com", a: "" },{ id: 33, n: "Daraz Global", c: "general, electronics, fashion, home, kids, beauty", u: "https://www.daraz.com", a: "" },{ id: 34, n: "AliExpress", c: "general, electronics, fashion, home, tools, sports, toys", u: "https://www.aliexpress.com", a: "" },{ id: 35, n: "eBay", c: "general, electronics, fashion, books, home, collectibles, toys", u: "https://www.ebay.com", a: "" },{ id: 36, n: "Amazon", c: "general, electronics, fashion, books, kids, home, beauty, grocery", u: "https://www.amazon.com", a: "" },{ id: 37, n: "Temu", c: "general, electronics, fashion, home, beauty, sports, toys", u: "https://www.temu.com", a: "" },{ id: 38, n: "Banggood", c: "general, electronics, gadgets, tools, home, toys", u: "https://www.banggood.com", a: "" },{ id: 39, n: "DHgate", c: "general, electronics, fashion, home, sports, beauty", u: "https://www.dhgate.com", a: "" },{ id: 40, n: "LightInTheBox", c: "general, fashion, electronics, home, beauty, wedding", u: "https://www.lightinthebox.com", a: "" },{ id: 41, n: "Etsy", c: "general, handmade, crafts, fashion, home, gifts", u: "https://www.etsy.com", a: "" },{ id: 42, n: "iHerb", c: "general, health, beauty, supplements, groceries", u: "https://www.iherb.com", a: "" },{ id: 43, n: "Book Depository", c: "general, books, education, kids", u: "https://www.bookdepository.com", a: "" },{ id: 44, n: "Better World Books", c: "general, books, education, charity", u: "https://www.betterworldbooks.com", a: "" },{ id: 45, n: "AbeBooks", c: "general, books, education, collectibles", u: "https://www.abebooks.com", a: "" },{ id: 46, n: "Newegg", c: "general, electronics, computers, gaming, accessories", u: "https://www.newegg.com", a: "" },{ id: 47, n: "B&H Photo Video", c: "general, electronics, cameras, computers, audio", u: "https://www.bhphotovideo.com", a: "" },{ id: 48, n: "ASOS", c: "general, fashion, clothing, shoes, beauty", u: "https://www.asos.com", a: "" },{ id: 49, n: "YesStyle", c: "general, fashion, beauty, skincare, lifestyle", u: "https://www.yesstyle.com", a: "" },{ id: 50, n: "Vitacost", c: "general, health, beauty, groceries, vitamins", u: "https://www.vitacost.com", a: "" },{ id: 51, n: "DHL Afghanistan", c: "general, courier, logistics, shipping, services", u: "https://www.dhl.com/af-en/home.html", a: "" },
+
+{ id: 52, n: "FedEx Afghanistan", c: "general, courier, logistics, shipping, services", u: "https://www.fedex.com/en-af/home.html", a: "" },
+
+{ id: 53, n: "Aramex", c: "general, courier, logistics, shipping, services", u: "https://www.aramex.com", a: "" },
+
+{ id: 54, n: "Western Union Afghanistan", c: "general, finance, money transfer, services", u: "https://www.westernunion.com", a: "" },
+
+{ id: 55, n: "MoneyGram", c: "general, finance, money transfer, services", u: "https://www.moneygram.com", a: "" },
+
+{ id: 56, n: "Wise", c: "general, finance, money transfer, services", u: "https://wise.com", a: "" },
+
+{ id: 57, n: "Payoneer", c: "general, finance, payments, business, services", u: "https://www.payoneer.com", a: "" },
+
+{ id: 58, n: "Booking.com", c: "general, travel, hotels, tourism, services", u: "https://www.booking.com", a: "" },
+
+{ id: 59, n: "Agoda", c: "general, travel, hotels, tourism, services", u: "https://www.agoda.com", a: "" },
+
+{ id: 60, n: "Trip.com", c: "general, travel, flights, hotels, tourism, services", u: "https://www.trip.com", a: "" },{ id: 61, n: "Expedia", c: "general, travel, flights, hotels, tourism, services", u: "https://www.expedia.com", a: "" },
+
+{ id: 62, n: "Kiwi.com", c: "general, travel, flights, tickets, tourism, services", u: "https://www.kiwi.com", a: "" },
+
+{ id: 63, n: "Skyscanner", c: "general, travel, flights, hotels, tourism, services", u: "https://www.skyscanner.com", a: "" },
+
+{ id: 64, n: "KAYAK", c: "general, travel, flights, hotels, car rental, services", u: "https://www.kayak.com", a: "" },
+
+{ id: 65, n: "Rentalcars.com", c: "general, travel, car rental, transport, services", u: "https://www.rentalcars.com", a: "" },
+{ id: 66, n: "Hotels.com", c: "general, travel, hotels, tourism, booking", u: "https://www.hotels.com", a: "" },
+
+{ id: 67, n: "Hostelworld", c: "general, travel, hostels, tourism, booking", u: "https://www.hostelworld.com", a: "" },
+
+{ id: 68, n: "Omio", c: "general, travel, transport, booking, services", u: "https://www.omio.com", a: "" },
+
+{ id: 69, n: "Rome2Rio", c: "general, travel, transport, routes, services", u: "https://www.rome2rio.com", a: "" },
+
+{ id: 70, n: "12Go", c: "general, travel, transport, tickets, booking", u: "https://12go.asia", a: "" },
+
+{ id: 71, n: "Klook", c: "general, travel, attractions, tours, booking", u: "https://www.klook.com", a: "" },
+
+{ id: 72, n: "GetYourGuide", c: "general, travel, tours, activities, booking", u: "https://www.getyourguide.com", a: "" },
+
+{ id: 73, n: "Viator", c: "general, travel, tours, activities, booking", u: "https://www.viator.com", a: "" },
+
+{ id: 74, n: "WorldRemit", c: "general, finance, money transfer, services", u: "https://www.worldremit.com", a: "" },
+
+{ id: 75, n: "XE Money Transfer", c: "general, finance, money transfer, currency, services", u: "https://www.xe.com", a: "" },
+
+{ id: 76, n: "Airbnb", c: "general, travel, stays, booking, tourism", u: "https://www.airbnb.com", a: "" },
+
+{ id: 77, n: "Travelocity", c: "general, travel, flights, hotels, booking", u: "https://www.travelocity.com", a: "" },
+
+{ id: 78, n: "Orbitz", c: "general, travel, flights, hotels, booking", u: "https://www.orbitz.com", a: "" },
+
+{ id: 79, n: "Priceline", c: "general, travel, hotels, flights, booking", u: "https://www.priceline.com", a: "" },
+
+{ id: 80, n: "eDreams", c: "general, travel, flights, hotels, booking", u: "https://www.edreams.com", a: "" },
+
+{ id: 81, n: "Opodo", c: "general, travel, flights, hotels, booking", u: "https://www.opodo.com", a: "" },
+
+{ id: 82, n: "Lastminute.com", c: "general, travel, holidays, booking", u: "https://www.lastminute.com", a: "" },
+
+{ id: 83, n: "CheapOair", c: "general, travel, flights, booking", u: "https://www.cheapoair.com", a: "" },
+
+{ id: 84, n: "OneTravel", c: "general, travel, flights, booking", u: "https://www.onetravel.com", a: "" },
+
+{ id: 85, n: "Traveloka", c: "general, travel, hotels, flights, booking", u: "https://www.traveloka.com", a: "" },
+
+{ id: 86, n: "KiwiTaxi", c: "general, travel, taxi, airport transfer", u: "https://kiwitaxi.com", a: "" },
+
+{ id: 87, n: "Discover Cars", c: "general, travel, car rental, booking", u: "https://www.discovercars.com", a: "" },
+
+{ id: 88, n: "Auto Europe", c: "general, travel, car rental, booking", u: "https://www.autoeurope.com", a: "" },
+
+{ id: 89, n: "Dollar Car Rental", c: "general, travel, car rental", u: "https://www.dollar.com", a: "" },
+
+{ id: 90, n: "Hertz", c: "general, travel, car rental", u: "https://www.hertz.com", a: "" },
+
+{ id: 91, n: "Avis", c: "general, travel, car rental", u: "https://www.avis.com", a: "" },
+
+{ id: 92, n: "Budget", c: "general, travel, car rental", u: "https://www.budget.com", a: "" },
+
+{ id: 93, n: "Enterprise", c: "general, travel, car rental", u: "https://www.enterprise.com", a: "" },
+
+{ id: 94, n: "National Car Rental", c: "general, travel, car rental", u: "https://www.nationalcar.com", a: "" },
+
+{ id: 95, n: "Alamo", c: "general, travel, car rental", u: "https://www.alamo.com", a: "" },
+
+{ id: 96, n: "RentalCover", c: "general, travel, insurance, car rental", u: "https://www.rentalcover.com", a: "" },
+
+{ id: 97, n: "InsureMyTrip", c: "general, travel, insurance", u: "https://www.insuremytrip.com", a: "" },
+
+{ id: 98, n: "SafetyWing", c: "general, travel, insurance, nomads", u: "https://safetywing.com", a: "" },
+
+{ id: 99, n: "World Nomads", c: "general, travel, insurance", u: "https://www.worldnomads.com", a: "" },
+
+{ id: 100, n: "VisitorsCoverage", c: "general, travel, insurance", u: "https://www.visitorscoverage.com", a: "" }
 ];
+
+
