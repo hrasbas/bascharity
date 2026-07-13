0488d746d711f0ef4ead6e11ac283127ccde46fe
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:54:26 +0530

    Add country shops data for Panama

---
+++ panama.js
@@ -0,0 +1,14 @@
+// panama.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["panama"] = [
+  {
+    i:1,
+    n:"ubuy panama",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.pa/en/",
+    d:"ubuy panama is an international online shopping website where customers in panama can buy imported electronics, mobiles and tablets, home goods, fashion, beauty and health products, books, toys and games, sports gear, travel and outdoor accessories, groceries, jewellery and automotive accessories from global brands at low prices.[web:1373][web:1376][web:1377][web:1378][web:1380][web:1382]",
+    k:"ubuy panama,international online shopping panama,imported electronics mobiles tablets home goods,fashion beauty health products toys games,books sports gear travel outdoor accessories,groceries jewellery automotive accessories worldwide shipping panama",
+    a:""
+  }
+];
