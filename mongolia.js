4a1748d7140df3d0c1b519db2f6904aa488e48a9
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:13:55 +0530

    Add Ubuy Mongolia shop details
    Added shop information for Ubuy Mongolia including categories and description.

---
+++ mongolia.js
@@ -0,0 +1,14 @@
+// mongolia.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["mongolia"] = [
+  {
+    i:1,
+    n:"ubuy mongolia",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,business",
+    u:"https://www.mongolia.ubuy.com/",
+    d:"ubuy mongolia is an international online shopping website where customers in mongolia can order over one hundred million imported products from global brands including electronics, computers and tablets, clothing, mobile phones, beauty items, home and kitchen goods, books, groceries and automotive accessories at low prices.[web:1194][web:1196][web:1199][web:1201][web:1202]",
+    k:"ubuy mongolia,international online shopping mongolia,imported electronics computers tablets mobiles,clothing beauty home kitchen goods online store,books groceries and automotive accessories cross border shopping mongolia",
+    a:""
+  }
+];
