1b1acbd0825673ecd1a9471792c91ff1f29b3d63
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:20:59 +0530

    Add online shopping platforms for Romania
    Added a list of online shopping platforms available in Romania, including Ubuy and eMAG, with details on categories, URLs, and descriptions.

---
+++ romania.js
@@ -0,0 +1,23 @@
+// romania.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["romania"] = [
+  {
+    i:1,
+    n:"ubuy romania",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.ro/en/",
+    d:"ubuy romania is an international online shopping website where customers in romania can buy imported electronics, mobiles and tablets, clothing and shoes, beauty and personal care products, home and kitchen goods, toys and games, office and school supplies, gardening tools, baby care items, pet supplies, travel and outdoors gear and automotive accessories from global brands at low prices.[web:1451][web:1453][web:1455][web:1456][web:1457][web:1458][web:1459][web:1460]",
+    k:"ubuy romania,international online shopping romania,imported electronics mobiles tablets clothing shoes,beauty personal care home kitchen goods,toys games office school supplies gardening tools,baby care pet supplies travel outdoors automotive accessories",
+    a:""
+  },
+  {
+    i:2,
+    n:"emag romania online marketplace",
+    c:"electronics,home,fashion,beauty,food,sports,books,software,automotive,business",
+    u:"https://www.emag.ro/",
+    d:"emag romania online marketplace is one of the largest ecommerce platforms in romania where thousands of sellers list products across categories such as electronics and gadgets, home and kitchen, fashion, beauty and care, books, software, car parts, sports equipment and groceries for customers nationwide.[web:1454]",
+    k:"emag romania online marketplace,largest ecommerce platform in romania,sell and buy electronics gadgets home kitchen,fashion beauty care books software,car parts sports equipment groceries nationwide",
+    a:""
+  }
+];
