c05a7a584654cf4ba93d0532098b8774565899c0
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:01:58 +0530

    Add north_macedonia.js for country shops data

---
+++ north_macedonia.js
@@ -0,0 +1,14 @@
+// north_macedonia.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["north_macedonia"] = [
+  {
+    i:1,
+    n:"ubuy north macedonia",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.mk/mk/",
+    d:"ubuy north macedonia is an international online shopping website where customers in north macedonia can buy imported electronics, computers, mobile phones, fashion, beauty and grooming products, home and kitchen goods, books, sports gear, travel accessories, groceries, jewellery and automotive parts from global brands at low prices.[web:1326][web:1328][web:1329][web:1331][web:1333]",
+    k:"ubuy north macedonia,international online shopping north macedonia,imported electronics computers mobiles fashion,beauty grooming home kitchen goods books,sports gear travel accessories groceries jewellery automotive parts",
+    a:""
+  }
+];
