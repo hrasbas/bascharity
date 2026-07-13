36930422a82ed7ead5a87a68f855a8b102f1b65d
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:19:52 +0530

    Add Mauritius country shops data

---
+++ mauritius.js
@@ -0,0 +1,23 @@
+// mauritius.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["mauritius"] = [
+  {
+    i:1,
+    n:"ubuy mauritius",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,business",
+    u:"https://www.ubuy.mu/en/",
+    d:"ubuy mauritius is an international online shopping website where customers across mauritius can buy imported electronics, clothing, mobile phones, beauty products, groceries, home and kitchen goods and automotive parts from global brands at low prices.[web:1143][web:1144][web:1145][web:1146]",
+    k:"ubuy mauritius,international online shopping mauritius,imported electronics clothing mobiles beauty,grocery home kitchen goods online store,automotive parts cross border shopping mauritius",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy mauritius mobile shopping app",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://www.ubuy.mu/en/download-app",
+    d:"ubuy mauritius mobile shopping app lets android and ios users browse millions of products, complete quick checkout and track orders on the go for electronics, fashion, beauty items and home essentials.[web:1151][web:1143]",
+    k:"ubuy mauritius mobile shopping app,android ios shopping app mauritius,browse products quick checkout order tracking,electronics fashion beauty home essentials app",
+    a:""
+  }
+];
