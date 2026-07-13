67b143bbb1f5da270a6587495e500e68985ec01a
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:40:26 +0530

    Add online shops data for Cyprus
    This file contains a list of online shops in Cyprus, including their categories, URLs, descriptions, and keywords for better searchability.

---
+++ cyprus.js
@@ -0,0 +1,95 @@
+// cyprus.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["cyprus"] = [
+  {
+    i:1,
+    n:"ubuy cyprus",
+    c:"electronics,home,fashion,beauty,health,sports,books,business",
+    u:"https://www.ubuy.cy/en/",
+    d:"ubuy cyprus is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands at low prices.[web:524][web:525]",
+    k:"ubuy cyprus,cross border shopping,imported electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store cyprus",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy cyprus local language site",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.ubuy.cy/el/",
+    d:"ubuy cyprus local language site offers the same international shopping experience in greek for electronics, clothing, mobile phones, beauty products and home goods.[web:525][web:524]",
+    k:"ubuy cyprus local language site,greek online shopping,electronics and mobiles,clothing and fashion,beauty products and cosmetics,home goods delivery cyprus",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy cyprus online stores brand page",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.ubuy.cy/en/brand/online-stores",
+    d:"ubuy cyprus online stores brand page curates international online store products across categories like electronics, fashion, beauty and home goods for shoppers across cyprus cities.[web:528][web:524]",
+    k:"ubuy cyprus online stores brand page,curated international products,electronics fashion and beauty,home goods for cyprus cities",
+    a:""
+  },
+  {
+    i:4,
+    n:"ubuy cyprus cisco networking store",
+    c:"electronics,software,business",
+    u:"https://www.ubuy.cy/en/brand/cisco",
+    d:"ubuy cyprus cisco networking store sells routers, switches, firewalls and communication equipment for businesses and tech users needing cisco hardware online.[web:529][web:524]",
+    k:"ubuy cyprus cisco networking store,cisco routers and switches,firewalls and communication equipment,business networking hardware cyprus",
+    a:""
+  },
+  {
+    i:5,
+    n:"ubuy cyprus panoxyl moisturizer page",
+    c:"beauty,health,business",
+    u:"https://www.ubuy.cy/en/product/1COWH9OZI-panoxyl-oil-control-moisturizer-1-75-oz",
+    d:"ubuy cyprus panoxyl moisturizer page offers imported oil control skincare products for customers with acne prone or oily skin.[web:530][web:524]",
+    k:"ubuy cyprus panoxyl moisturizer,oil control skincare,acne prone skin products,imported beauty and health items cyprus",
+    a:""
+  },
+  {
+    i:6,
+    n:"ubuy cyprus app",
+    c:"electronics,software,business",
+    u:"https://www.ubuy.cy/en/download-app",
+    d:"ubuy cyprus app lets users browse products, check out quickly and track orders on android and ios devices for convenient mobile shopping.[web:532][web:524]",
+    k:"ubuy cyprus app,mobile shopping app,browse and order products,quick checkout and order tracking,android and ios ecommerce cyprus",
+    a:""
+  },
+  {
+    i:7,
+    n:"skroutz cyprus",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.skroutz.cy/?lang=en",
+    d:"skroutz cyprus is a price comparison and shopping platform that aggregates products from many online stores in cyprus, helping users find deals on smartphones, fashion and homeware.[web:523]",
+    k:"skroutz cyprus,price comparison platform,shop from many online stores,smartphones fashion and homeware deals,online shopping cyprus",
+    a:""
+  },
+  {
+    i:8,
+    n:"brandnav ecommerce list cyprus",
+    c:"business",
+    u:"https://www.brandnav.io/reports/countries/cyprus/",
+    d:"brandnav ecommerce list cyprus provides downloadable reports of ecommerce websites with metrics and contacts to help b2b vendors find online retailers and marketplaces.[web:526]",
+    k:"brandnav ecommerce list cyprus,ecommerce websites report,b2b vendor prospecting,online retailers and marketplaces contacts cyprus",
+    a:""
+  },
+  {
+    i:9,
+    n:"appscrip top 5 ecommerce websites cyprus article",
+    c:"business",
+    u:"https://appscrip.com/blog/top-5-ecommerce-websites-in-cyprus/",
+    d:"appscrip top 5 ecommerce websites cyprus article discusses leading platforms like alphamega hypermarkets, stephanis electronics chain and other online stores shaping the local ecommerce scene.[web:527][web:531]",
+    k:"appscrip top 5 ecommerce websites cyprus,alphamega hypermarkets online,stephanis electronics chain,ecommerce platforms cyprus article",
+    a:""
+  },
+  {
+    i:10,
+    n:"aftership cyprus online stores directory",
+    c:"electronics,home,fashion,beauty,food,sports,travel,business,health",
+    u:"https://ecommerce.aftership.com/countries/cy/stores",
+    d:"aftership cyprus online stores directory showcases major ecommerce sites such as stephanis electronics, alphamega hypermarkets, superhome center, beautyline and others with traffic and revenue metrics.[web:531]",
+    k:"aftership cyprus online stores directory,stephanis electronics online,alphamega hypermarkets groceries,superhome center home goods,beautyline beauty products,traffic and revenue metrics cyprus",
+    a:""
+  }
+];
