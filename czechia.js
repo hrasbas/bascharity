94aa10a2a70a9e9ee25e5703ac79550dc5f5e23f
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:41:46 +0530

    Add Czechia online shopping resources

---
+++ czechia.js
@@ -0,0 +1,86 @@
+// czechia.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["czechia"] = [
+  {
+    i:1,
+    n:"ubuy czech republic",
+    c:"electronics,home,fashion,beauty,health,sports,books,business",
+    u:"https://www.ubuy.cz/en/",
+    d:"ubuy czech republic is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands at low prices.[web:534][web:533][web:536]",
+    k:"ubuy czech republic,cross border shopping,imported electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store czechia",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy czech republic local language site",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.ubuy.cz/cs/",
+    d:"ubuy czech republic local language site offers the same international shopping experience in czech for electronics, clothing, mobile phones, beauty products and home goods.[web:533][web:534]",
+    k:"ubuy czech republic local language site,czech online shopping,electronics and mobiles,clothing and fashion,beauty products and cosmetics,home goods delivery czechia",
+    a:""
+  },
+  {
+    i:3,
+    n:"similarweb top ecommerce sites czech republic 2025",
+    c:"business",
+    u:"https://www.similarweb.com/top-websites/czech-republic/e-commerce-and-shopping/",
+    d:"similarweb top ecommerce sites czech republic 2025 ranks major platforms such as temu, alza, bazos, heureka and allegro by traffic and engagement.[web:535]",
+    k:"similarweb top ecommerce sites czech republic 2025,temu alza bazos heureka allegro,online shopping traffic ranking,ecommerce market insights czechia",
+    a:""
+  },
+  {
+    i:4,
+    n:"aftership top 100 online stores czech republic",
+    c:"electronics,home,fashion,food,books,business,health,automotive",
+    u:"https://www.aftership.com/store-list/top-100-cz-ecommerces-stores",
+    d:"aftership top 100 online stores czech republic lists leading ecommerce brands like alza.cz, mountfield, pilulka pharmacy, footshop and automobilist with revenue and category data.[web:540]",
+    k:"aftership top 100 online stores czech republic,alza.cz mountfield pilulka footshop automobilist,ecommerce brands revenue data,czech online retail ranking",
+    a:""
+  },
+  {
+    i:5,
+    n:"aftership top 100 online internet stores czech republic",
+    c:"electronics,software,business",
+    u:"https://www.aftership.com/store-list/top-100-cz-ecommerces-internet-stores",
+    d:"aftership top 100 online internet stores czech republic focuses on internet and web services shops such as sewio rtls, nej.cz, 524wifi and esd shop with platform and kpi benchmarks.[web:537]",
+    k:"aftership top 100 online internet stores czech republic,web services shops list,sewio rtls nej.cz 524wifi esd shop,internet sector kpi benchmarks",
+    a:""
+  },
+  {
+    i:6,
+    n:"brandnav ecommerce list czech republic",
+    c:"business",
+    u:"https://www.brandnav.io/reports/countries/czech-republic/",
+    d:"brandnav ecommerce list czech republic provides downloadable reports of ecommerce websites with metrics and contacts to help b2b vendors find online retailers and marketplaces.[web:539]",
+    k:"brandnav ecommerce list czech republic,ecommerce websites report,b2b vendor prospecting,online retailers and marketplaces contacts czechia",
+    a:""
+  },
+  {
+    i:7,
+    n:"shopbycountries best shopping platforms czech republic",
+    c:"electronics,home,fashion,beauty,food,sports,books,business,automotive",
+    u:"https://www.shopbycountries.com/en/country/cz",
+    d:"shopbycountries best shopping platforms czech republic compares marketplaces like temu, kosik grocery, zalando fashion, shein, wolt food delivery, vinted second hand and sauto vehicles.[web:541]",
+    k:"shopbycountries best shopping platforms czech republic,temu kosik zalando shein wolt vinted sauto,online marketplace comparison,grocery fashion food delivery vehicles",
+    a:""
+  },
+  {
+    i:8,
+    n:"semrush top retail websites czech republic",
+    c:"business",
+    u:"https://www.semrush.com/trending-websites/cz/retail",
+    d:"semrush top retail websites czech republic report tracks traffic trends for leading retail and ecommerce domains in the country.[web:538]",
+    k:"semrush top retail websites czech republic,retail traffic trends,ecommerce domains ranking,czech online shopping data",
+    a:""
+  },
+  {
+    i:9,
+    n:"expats.cz online shopping guide czech republic",
+    c:"electronics,home,fashion,books,business",
+    u:"https://www.expats.cz/czech-news/article/online-shopping",
+    d:"expats.cz online shopping guide czech republic lists international sites like amazon and ebay plus local options including alza, zbozi, aukro and korunka for electronics, books and household goods.[web:542]",
+    k:"expats.cz online shopping guide czech republic,amazon ebay alza zbozi aukro korunka,electronics and books shopping,household goods online czechia",
+    a:""
+  }
+];
