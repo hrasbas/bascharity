e46b7ff30cfbb7dc1e98d99875bde2d46d1a4e22
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:03:24 +0530

    Add Nigeria country shops data to nigeria.js

---
+++ nigeria.js
@@ -0,0 +1,32 @@
+// nigeria.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["nigeria"] = [
+  {
+    i:1,
+    n:"ubuy nigeria",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.u-buy.com.ng/",
+    d:"ubuy nigeria is an international online shopping website where customers in nigeria can buy imported electronics, mobiles and tablets, home goods, fashion, jewellery, beauty and health items, books, sports gear, travel accessories, groceries and automotive products from premium global brands at low prices.[web:1302][web:1313][web:1232]",
+    k:"ubuy nigeria,international online shopping nigeria,imported electronics mobiles tablets home goods,fashion jewellery beauty health items books,sports gear travel accessories groceries automotive products",
+    a:""
+  },
+  {
+    i:2,
+    n:"jumia nigeria online shopping",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://www.jumia.com.ng/",
+    d:"jumia nigeria online shopping is one of the largest ecommerce marketplaces in nigeria where users buy smartphones, tablets, electronics, home appliances, clothing, shoes, beauty products and groceries with nationwide delivery and pay on delivery options.[web:1310][web:1311][web:1314]",
+    k:"jumia nigeria online shopping,largest ecommerce marketplace in nigeria,buy smartphones tablets electronics home appliances,clothing shoes beauty products groceries,pay on delivery nationwide nigeria",
+    a:""
+  },
+  {
+    i:3,
+    n:"konga nigeria online shopping",
+    c:"electronics,home,fashion,beauty,food,sports,books,automotive,business",
+    u:"https://www.konga.com/",
+    d:"konga nigeria online shopping is a major nigerian ecommerce site offering a wide range of products including electronics, phones, computers, fashion, home and kitchen items, groceries, sports and fitness gear, books, automotive and industrial supplies with fast shipping and local payment methods.[web:1312][web:1315]",
+    k:"konga nigeria online shopping,major nigerian ecommerce site,buy electronics phones computers fashion,home kitchen groceries sports fitness gear,books automotive and industrial supplies fast shipping",
+    a:""
+  }
+];
