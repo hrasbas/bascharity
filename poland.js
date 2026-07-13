c81fff42659d535ffa04a31af8e402293278981a
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:28:28 +0530

    Add online shops data for Poland
    This file contains a list of online shops available in Poland, including details such as name, categories, URL, description, and keywords.

---
+++ poland.js
@@ -0,0 +1,23 @@
+// poland.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["poland"] = [
+  {
+    i:1,
+    n:"ubuy poland",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.pl/en/",
+    d:"ubuy poland is an international online shopping website where customers in poland can buy imported electronics, mobiles and tablets, clothing, shoes and accessories, beauty and skincare products, home and kitchen goods, toys and games, pet supplies, books, sports gear, travel accessories, groceries and automotive replacement parts from global brands at low prices.[web:1421][web:1422][web:1423][web:1424][web:1425][web:1426][web:1427][web:1429][web:1430]",
+    k:"ubuy poland,international online shopping poland,imported electronics mobiles tablets clothing shoes,beauty skincare home kitchen goods toys games,pet supplies books sports gear travel accessories,groceries automotive replacement parts global brands",
+    a:""
+  },
+  {
+    i:2,
+    n:"allegro poland online marketplace",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://allegro.pl/",
+    d:"allegro poland online marketplace is one of the biggest ecommerce platforms in poland where individuals and businesses buy and sell electronics, home and garden items, fashion, beauty, books, software, car parts, sports gear, food products and more with local shipping and convenient payments.[web:1428]",
+    k:"allegro poland online marketplace,largest ecommerce platform in poland,buy and sell electronics home garden fashion,beauty books software car parts sports gear,food products local shipping convenient payments",
+    a:""
+  }
+];
