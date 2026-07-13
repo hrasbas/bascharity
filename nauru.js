72833dbad2fc35fbdb9e4b3f1545865aca9b1862
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:08:39 +0530

    Add nauru.js for country-specific shopping data

---
+++ nauru.js
@@ -0,0 +1,14 @@
+// nauru.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["nauru"] = [
+  {
+    i:1,
+    n:"ubuy nauru",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.nauru.ubuy.com/",
+    d:"ubuy nauru is an international online shopping website where customers in nauru can order over one hundred million imported products from global brands including electronics, fashion, beauty and health items, home goods, books, groceries, jewellery and automotive accessories at low prices.[web:1252][web:1254][web:1257]",
+    k:"ubuy nauru,international online shopping nauru,imported electronics fashion beauty health,home goods books groceries online store,jewellery and automotive accessories cross border shopping nauru",
+    a:""
+  }
+];
