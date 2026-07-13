8e174c037479b6cad20eb9dd09b21f083a108f4a
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:39:36 +0530

    Add Cuba country shops data to cuba.js

---
+++ cuba.js
@@ -0,0 +1,68 @@
+// cuba.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["cuba"] = [
+  {
+    i:1,
+    n:"cuba market",
+    c:"electronics,home,fashion,food,business",
+    u:"https://cubamarketplace.shop/",
+    d:"cuba market (mercadocuba) is an online marketplace in cuba where users buy and sell products such as clothing, technology items, groceries and everyday goods in one place.[web:515]",
+    k:"cuba market mercadocuba,online marketplace cuba,buy and sell clothing,technology products cuba,grocery and everyday goods online",
+    a:""
+  },
+  {
+    i:2,
+    n:"top 10 ecommerce marketplaces in cuba article",
+    c:"business",
+    u:"https://stateglobe.com/blog/top-10-ecommerce-marketplaces-in-cuba",
+    d:"top 10 ecommerce marketplaces in cuba article reviews platforms like cubamarket, la tienda cubana, cubaexpress and shopcuba, explaining market share and growth trends for local online shopping.[web:514]",
+    k:"top 10 ecommerce marketplaces cuba,cubamarket la tienda cubana,cubaexpress shopcuba review,online shopping market share cuba,growth trends article",
+    a:""
+  },
+  {
+    i:3,
+    n:"rest of world cuba ecommerce boom story",
+    c:"business",
+    u:"https://restofworld.org/2023/cuba-e-commerce-boom/",
+    d:"rest of world cuba ecommerce boom story describes how relatives abroad drive online purchases through platforms like porlalivre and revolico for cuban shoppers.[web:513]",
+    k:"rest of world cuba ecommerce boom,porlalivre revolico online classifieds,cuban online shopping story,relatives abroad driving ecommerce",
+    a:""
+  },
+  {
+    i:4,
+    n:"aftership top 100 online stores cuba",
+    c:"travel,business,food,automotive,health",
+    u:"https://www.aftership.com/store-list/top-100-cu-ecommerces-stores",
+    d:"aftership top 100 online stores cuba ranks major ecommerce sites including rentcarcuba car rental, holiplus holidays, mallcubano cultural store, miscota pet shop and medical services platforms by traffic and revenue.[web:516][web:520]",
+    k:"aftership top 100 online stores cuba,rentcarcuba car rental online,holiplus cuba holidays booking,mallcubano cultural products store,miscota cuba pet shop,medical services cubanos ecommerce ranking",
+    a:""
+  },
+  {
+    i:5,
+    n:"cuba web directory commerce section",
+    c:"business",
+    u:"http://www.cubawebdirectory.com/index.php?module=site/cuba_directory/cuba-business-commerce-ecommerce",
+    d:"cuba web directory commerce section lists cuban business and ecommerce websites to help users find online shops and commercial services.[web:518]",
+    k:"cuba web directory commerce,cuban business websites list,ecommerce sites directory cuba,find online shops and services",
+    a:""
+  },
+  {
+    i:6,
+    n:"aftership cuba online stores directory",
+    c:"business,travel,food,health,books,automotive",
+    u:"https://ecommerce.aftership.com/countries/cu/stores",
+    d:"aftership cuba online stores directory showcases cuba ecommerce sites across categories like car rental, travel booking, cultural shops, pet supplies and medical services with traffic and revenue metrics.[web:520]",
+    k:"aftership cuba online stores directory,cuba ecommerce sites list,car rental and travel booking online,mallcubano cultural shop,miscota pet supplies,medical services cubanos,traffic and revenue metrics",
+    a:""
+  },
+  {
+    i:7,
+    n:"goxip cuba fashion shopping",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/cu/en",
+    d:"goxip cuba fashion shopping is an online destination aggregating thousands of fashion and beauty brands for customers who want to browse and buy clothing, shoes and cosmetics online.[web:522]",
+    k:"goxip cuba fashion shopping,online clothing and shoes cuba,beauty and cosmetics store,aggregated fashion brands marketplace",
+    a:""
+  }
+];
