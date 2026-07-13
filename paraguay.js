e980c1ef9f6cd2505afeb1a71e30ff78256a7f93
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:52:43 +0530

    Add paraguay.js with country shops data

---
+++ paraguay.js
@@ -0,0 +1,14 @@
+// paraguay.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["paraguay"] = [
+  {
+    i:1,
+    n:"ubuy paraguay",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.py/es/en/",
+    d:"ubuy paraguay is an international online shopping website where customers in paraguay can buy imported electronics, mobiles and tablets, fashion and footwear, beauty and personal care products, home and kitchen goods, books, toys, sports gear, travel accessories, groceries, jewellery and automotive parts from global brands at low prices.[web:1392][web:1393][web:1394][web:1396][web:1397][web:1398][web:1399][web:1401]",
+    k:"ubuy paraguay,international online shopping paraguay,imported electronics mobiles tablets fashion footwear,beauty personal care home kitchen goods books,toys sports gear travel accessories groceries jewellery automotive parts",
+    a:""
+  }
+];
