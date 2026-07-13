c43b5399a79bef6c65f10439d82cf9f9b9254f40
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:05:14 +0530

    Add country shops data for Nicaragua

---
+++ nicaragua.js
@@ -0,0 +1,14 @@
+// nicaragua.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["nicaragua"] = [
+  {
+    i:1,
+    n:"ubuy nicaragua",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.ni/en/",
+    d:"ubuy nicaragua is an international online shopping website where customers in nicaragua can buy imported electronics, mobiles and laptops, fashion, beauty and health products, home and kitchen goods, books, sports and outdoor gear, groceries, jewellery and automotive accessories from global brands at low prices.[web:1291][web:1293][web:1295][web:1296][web:1300]",
+    k:"ubuy nicaragua,international online shopping nicaragua,imported electronics mobiles laptops fashion beauty,home kitchen goods books sports outdoor gear,groceries jewellery and automotive accessories cross border shopping nicaragua",
+    a:""
+  }
+];
