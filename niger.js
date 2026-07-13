b65c34e354fb1c95be693ac4c5791099512cc1f3
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:04:22 +0530

    Add country shops data for Niger

---
+++ niger.js
@@ -0,0 +1,14 @@
+// niger.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["niger"] = [
+  {
+    i:1,
+    n:"ubuy niger",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.ne/en/",
+    d:"ubuy niger is an international online shopping website where customers in niger can buy imported electronics, mobiles and tablets, home goods, fashion, beauty and health items, books, sports and outdoor gear, groceries, jewellery and automotive accessories from global brands at low prices.[web:1301][web:1303][web:1307]",
+    k:"ubuy niger,international online shopping niger,imported electronics mobiles tablets home goods,fashion beauty health items books sports gear,groceries jewellery and automotive accessories cross border shopping niger",
+    a:""
+  }
+];
