e8aa0215a57216f9c630602e05eb4beb0ed693de
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:37:00 +0530

    Add online shops data for Côte d'Ivoire
    This file contains a list of online shops in Côte d'Ivoire, including their details such as name, category, URL, description, and keywords.

---
+++ cote_divoire.js
@@ -0,0 +1,140 @@
+// cote_divoire.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["cote_d'ivoire"] = [
+  {
+    i:1,
+    n:"ubuy cote d'ivoire",
+    c:"electronics,home,fashion,beauty,health,sports,books,business",
+    u:"https://www.ubuy.ci/fr/",
+    d:"ubuy cote d'ivoire is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands at low prices.[web:493][web:502]",
+    k:"ubuy cote d'ivoire,cross border shopping,imported electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store cote d'ivoire",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy cote d'ivoire books store",
+    c:"books,business",
+    u:"https://www.ubuy.ci/fr/category/books-10180",
+    d:"ubuy cote d'ivoire books store offers a large catalog of bestsellers, new releases and classics across genres for readers looking to buy books online.[web:500][web:493]",
+    k:"ubuy cote d'ivoire books store,online bookstore cote d'ivoire,bestsellers and new releases,classic literature and study books",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy cote d'ivoire playstation 3 shop",
+    c:"electronics,business",
+    u:"https://www.ubuy.ci/fr/category/electronics/video-games/playstation3-14210751",
+    d:"ubuy cote d'ivoire playstation 3 shop sells consoles and accessories with fast delivery and special offers for gamers.[web:501][web:493]",
+    k:"ubuy cote d'ivoire playstation 3 shop,playstation 3 consoles and accessories,video games online store,fast delivery for gamers cote d'ivoire",
+    a:""
+  },
+  {
+    i:4,
+    n:"jiji cote d'ivoire app",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://play.google.com/store/apps/details?id=ci.jiji.app&hl=en",
+    d:"jiji cote d'ivoire app is a classifieds marketplace where users buy and sell cars, electronics, furniture, clothing and services across ivory coast.[web:498]",
+    k:"jiji cote d'ivoire app,classifieds marketplace ivory coast,buy and sell cars,electronics and phones,furniture and clothing listings",
+    a:""
+  },
+  {
+    i:5,
+    n:"jumia cote d'ivoire",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.jumia.ci",
+    d:"jumia cote d'ivoire is one of the most visited ecommerce sites in the country, offering electronics, fashion, beauty products, home appliances and groceries with delivery options.[web:495][web:497][web:494]",
+    k:"jumia cote d'ivoire,leading ecommerce site,cote d'ivoire online shopping,electronics and fashion,beauty products and appliances,grocery delivery abidjan",
+    a:""
+  },
+  {
+    i:6,
+    n:"afrimarket cote d'ivoire",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.afrimarket.ci",
+    d:"afrimarket cote d'ivoire is an ecommerce site of proximity where people in europe can order electronics, clothing and food items that are delivered to relatives in ivory coast.[web:495][web:497]",
+    k:"afrimarket cote d'ivoire,ecommerce of proximity,order for relatives in ivory coast,electronics clothing and food delivery,cross border shopping",
+    a:""
+  },
+  {
+    i:7,
+    n:"africashop cote d'ivoire",
+    c:"fashion,beauty,home,business",
+    u:"https://www.africashop.ci",
+    d:"africashop cote d'ivoire is an online store linked to playce marcory that sells parisian fashion brands, cosmetics and home products to shoppers in abidjan.[web:495][web:497]",
+    k:"africashop cote d'ivoire,parisian fashion brands online,cosmetics and beauty products,home products playce marcory,shopping in abidjan",
+    a:""
+  },
+  {
+    i:8,
+    n:"yaatoo cote d'ivoire",
+    c:"food,home,business",
+    u:"https://www.yaatoo.ci",
+    d:"yaatoo cote d'ivoire is a drive style ecommerce site from groupe prosuma where customers order groceries and household essentials online for pickup or delivery.[web:495][web:497]",
+    k:"yaatoo cote d'ivoire,grocery drive ecommerce,groupe prosuma online,order groceries and household essentials,pickup and delivery ivory coast",
+    a:""
+  },
+  {
+    i:9,
+    n:"librairie de france online",
+    c:"books,business",
+    u:"https://www.librairiedefrance.net",
+    d:"librairie de france online is the web store of librairie de france, letting customers buy books and study materials from a virtual bookstore with home delivery.[web:495][web:497]",
+    k:"librairie de france online,virtual bookstore cote d'ivoire,buy books and study materials,home delivery for readers",
+    a:""
+  },
+  {
+    i:10,
+    n:"ivoiremobiles",
+    c:"electronics,business",
+    u:"https://www.ivoiremobiles.net",
+    d:"ivoiremobiles is a mobile and electronics ecommerce site in cote d'ivoire focused on phones and related accessories.[web:495][web:502]",
+    k:"ivoiremobiles cote d'ivoire,mobile phones online,electronics and accessories,phone shop ivory coast",
+    a:""
+  },
+  {
+    i:11,
+    n:"yaatoo deals and other local ecommerce list article",
+    c:"business",
+    u:"https://aboukam.net/abukam/2016/12/21/top-10-sites-e-commerces-en-cote-divoire-pour-faire-des-bonnes-affaires/",
+    d:"yaatoo deals and other local ecommerce list article reviews major ivoirian sites like jumia, afrimarket, africashop and yaatoo, helping users discover where to find good online deals.[web:497]",
+    k:"top 10 sites e commerces cote d'ivoire,jumia and afrimarket review,africashop and yaatoo deals,guide to good online offers ivory coast",
+    a:""
+  },
+  {
+    i:12,
+    n:"top 10 most visited ecommerce sites cote d'ivoire article",
+    c:"business",
+    u:"https://contactexpo.net/top10-sites-e-commerce-plus-visites-cote-divoire/",
+    d:"top 10 most visited ecommerce sites cote d'ivoire article lists platforms like jumia, afrimarket, africashop, ivoiremobiles and yaatoo with visitor numbers to show market leaders.[web:495]",
+    k:"top 10 most visited ecommerce sites cote d'ivoire,jumia visitor numbers,afrimarket and africashop stats,ivoiremobiles and yaatoo ranking",
+    a:""
+  },
+  {
+    i:13,
+    n:"top 22 online sales sites in cote d'ivoire article",
+    c:"business",
+    u:"https://gaynako.com/technology/marketing-digital/top-9-des-sites-de-vente-en-ligne-en-cote-divoire/",
+    d:"top 22 online sales sites in cote d'ivoire article showcases a curated list of popular online stores and marketplaces, giving merchants and buyers an overview of the digital retail landscape.[web:496]",
+    k:"top 22 online sales sites cote d'ivoire,popular online stores list,marketplaces overview,digital retail landscape ivory coast",
+    a:""
+  },
+  {
+    i:14,
+    n:"trade.gov cote d'ivoire ecommerce guide",
+    c:"business",
+    u:"https://www.trade.gov/country-commercial-guides/cote-divoire-ecommerce",
+    d:"trade.gov cote d'ivoire ecommerce guide explains the growth of online shopping in the country and highlights opportunities and challenges for foreign exporters and local platforms.[web:494]",
+    k:"trade.gov cote d'ivoire ecommerce guide,ecommerce market overview,opportunities for exporters,online shopping growth cote d'ivoire",
+    a:""
+  },
+  {
+    i:15,
+    n:"aftership top 100 online stores cote d'ivoire",
+    c:"business",
+    u:"https://www.aftership.com/store-list/top-100-ci-ecommerces-stores",
+    d:"aftership top 100 online stores cote d'ivoire ranks leading ecommerce brands such as ubuy ci, ivoiremobiles and decathlon cote d'ivoire with platform and category data for benchmarking.[web:502][web:499]",
+    k:"aftership top 100 online stores cote d'ivoire,ecommerce ranking and kpi,ubuy ci and ivoiremobiles,decathlon cote d'ivoire sports store,benchmarking online retailers",
+    a:""
+  }
+];
