156296494c9283957c613ee678756f1b904399c6
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:19:53 +0530

    Create russia.js with country shop data
    Added data for online shopping platforms in Russia.

---
+++ russia.js
@@ -0,0 +1,23 @@
+// russia.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["russia"] = [
+  {
+    i:1,
+    n:"ubuy russia",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.russia.ubuy.com/",
+    d:"ubuy russia is an international online shopping website where customers in russia can buy imported electronics, mobiles and tablets, clothing, shoes and accessories, beauty and grooming products, home and kitchen goods, toys and games, books, sports equipment, travel gear, groceries, jewellery and automotive parts from premium global brands at low prices.[web:1461][web:1462][web:1463][web:1464][web:1467][web:1232]",
+    k:"ubuy russia,international online shopping russia,imported electronics mobiles tablets clothing shoes,beauty grooming home kitchen goods toys games,books sports equipment travel gear groceries jewellery automotive parts",
+    a:""
+  },
+  {
+    i:2,
+    n:"ozon russia online marketplace",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://www.ozon.ru/",
+    d:"ozon russia online marketplace is one of the largest ecommerce platforms in russia offering millions of products including electronics and appliances, clothing and shoes, books and software, toys, baby goods, food, health and beauty items, home and garden, sports gear and car accessories with fast delivery, discounts and flexible payment options.[web:1465][web:1468]",
+    k:"ozon russia online marketplace,largest ecommerce platform in russia,millions of products electronics appliances clothing shoes,books software toys baby goods food,health beauty home garden sports gear car accessories fast delivery",
+    a:""
+  }
+];
