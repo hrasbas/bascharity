262173cb851cce880a589109756716eb71a32e40
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:35:39 +0530

    Add online shops data for Costa Rica
    This file contains a list of online shops available in Costa Rica, including their categories, URLs, descriptions, and keywords for better searchability.

---
+++ costa_rica.js
@@ -0,0 +1,86 @@
+// costa_rica.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["costa_rica"] = [
+  {
+    i:1,
+    n:"ubuy costa rica",
+    c:"electronics,home,fashion,beauty,health,sports,books,business",
+    u:"https://www.ubuy.cr/es/",
+    d:"ubuy costa rica is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands at low prices.[web:483][web:489]",
+    k:"ubuy costa rica,cross border shopping,imported electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store costa rica",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy costa rica luxury shopping deals",
+    c:"fashion,beauty,business",
+    u:"https://www.ubuy.cr/es/deals/luxury-shopping-store",
+    d:"ubuy costa rica luxury shopping deals page showcases premium fashion, accessories and beauty collections for customers seeking luxury brands online.[web:490][web:483]",
+    k:"ubuy costa rica luxury shopping,premium fashion brands,luxury accessories and beauty,online luxury store costa rica",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy costa rica japan imports",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.ubuy.cr/en/shop-import-products-from-japan",
+    d:"ubuy costa rica japan imports page focuses on genuine japanese electronics, beauty items, home goods and fashion products shipped to costa rica.[web:492][web:483]",
+    k:"ubuy costa rica japan imports,japanese electronics store,imported beauty items japan,home goods and fashion from japan,cross border shopping costa rica",
+    a:""
+  },
+  {
+    i:4,
+    n:"ubuy costa rica cayenne pepper supplements",
+    c:"health,food,business",
+    u:"https://www.ubuy.cr/en/product/TTWUEE36Y-kyrebcc-elorita-cayenne-pepper-softgels-elorita-s-cayenne-pepper-softgels-1pcs",
+    d:"ubuy costa rica cayenne pepper supplements page sells elorita cayenne pepper softgels and similar nutrition products that are not easily found locally.[web:484][web:483]",
+    k:"ubuy costa rica cayenne pepper supplements,elorita cayenne pepper softgels,nutrition supplements online,health and wellness products costa rica",
+    a:""
+  },
+  {
+    i:5,
+    n:"ubuy costa rica body shop fuji green tea butter",
+    c:"beauty,health,business",
+    u:"https://www.ubuy.cr/en/product/7TZ8NL5ZC-new-listing-the-body-shop-fuji-green-tea-replenishing-body-butter-6-9-oz-196-g-new-x-2",
+    d:"ubuy costa rica body shop fuji green tea butter page offers hydrating body butter from the body shop for costa rican shoppers looking for imported skincare.[web:486][web:483]",
+    k:"ubuy costa rica body shop fuji green tea butter,imported skincare products,body shop body butter,costa rica beauty shopping online",
+    a:""
+  },
+  {
+    i:6,
+    n:"ubuy costa rica cafe leyenda coffee",
+    c:"food,business",
+    u:"https://www.ubuy.cr/en/product/1AQS2T8-cafe-leyenda-gourmet-costa-rica-dark-roast-coffee-4-pack-8-8oz-ground-100-arabica-coffee",
+    d:"ubuy costa rica cafe leyenda coffee page sells gourmet costa rica dark roast coffee made from 100 percent arabica beans for international and local buyers.[web:488][web:483]",
+    k:"ubuy costa rica cafe leyenda coffee,gourmet costa rica dark roast,100 percent arabica beans,coffee lovers online shop",
+    a:""
+  },
+  {
+    i:7,
+    n:"ubuy costa rica app",
+    c:"electronics,software,business",
+    u:"https://www.ubuy.cr/es/download-app",
+    d:"ubuy costa rica app lets users browse products, check out quickly and track orders on android and ios devices for easier mobile shopping.[web:485][web:491]",
+    k:"ubuy costa rica app,mobile shopping app,browse and order products,quick checkout and order tracking,android and ios ecommerce costa rica",
+    a:""
+  },
+  {
+    i:8,
+    n:"aftership top 100 online stores costa rica",
+    c:"business",
+    u:"https://www.aftership.com/store-list/top-100-cr-ecommerces-stores",
+    d:"aftership top 100 online stores costa rica lists leading ecommerce brands such as gollo costa rica, tienda monge and automercado with platform and revenue details for market benchmarking.[web:489]",
+    k:"aftership top 100 online stores costa rica,ecommerce ranking list,gollo costa rica tienda monge,automercado online,platform and revenue benchmarks",
+    a:""
+  },
+  {
+    i:9,
+    n:"semrush most visited retail sites costa rica",
+    c:"business",
+    u:"https://www.semrush.com/trending-websites/cr/retail",
+    d:"semrush most visited retail sites costa rica report shows aliexpress, ebay and walmart costa rica among the top retail sites by traffic for the country.[web:487]",
+    k:"semrush most visited retail sites costa rica,aliexpress ebay walmart costa rica,retail traffic report,ecommerce trends costa rica",
+    a:""
+  }
+];
