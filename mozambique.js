683de83c536334320d50a08aaf4a012a15ab2a24
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:11:26 +0530

    Add Mozambique country shops data
    Added country shops data for Mozambique including details for Ubuy.

---
+++ mozambique.js
@@ -0,0 +1,14 @@
+// mozambique.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["mozambique"] = [
+  {
+    i:1,
+    n:"ubuy mozambique",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,business",
+    u:"https://www.ubuy.co.mz/en/",
+    d:"ubuy mozambique is an international online shopping website where customers in mozambique can buy imported electronics, apparel, mobile phones, beauty products, home and kitchen goods, books, groceries, jewellery and automotive accessories from global brands at low prices.[web:1226][web:1225][web:1228][web:1230][web:1231]",
+    k:"ubuy mozambique,international online shopping mozambique,imported electronics apparel mobiles beauty,home kitchen goods books groceries online store,jewellery and automotive accessories cross border shopping mozambique",
+    a:""
+  }
+];
