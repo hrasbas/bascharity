e67ef3129d970491d3ee298d8cf351064c14af79
hrasbas <hrasbasji@gmail.com>
2026-07-13 18:34:21 +0530

    Add country shops data for Congo

---
+++ congo.js
@@ -0,0 +1,95 @@
+// congo.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["congo"] = [
+  {
+    i:1,
+    n:"ubuy republic of the congo",
+    c:"electronics,home,fashion,beauty,health,sports,books,business",
+    u:"https://www.ubuy.cg/en/",
+    d:"ubuy republic of the congo is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands.[web:476][web:477][web:475]",
+    k:"ubuy republic of the congo,cross border shopping,electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store congo",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy congo internet products store",
+    c:"electronics,business",
+    u:"https://www.ubuy.cg/fr/brand/internet",
+    d:"ubuy congo internet products store highlights discounted internet related devices and accessories for customers in brazzaville and other major cities.[web:475][web:476]",
+    k:"ubuy congo internet products,discount devices and accessories,internet gadgets store,buy online in brazzaville and cities",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy congo health supplements page",
+    c:"health,business",
+    u:"https://www.ubuy.cg/en/productuk/ICAYCM8NM-lumarejebo",
+    d:"ubuy congo health supplements page sells imported liposomal sulforaphane and similar antioxidant supplements for wellness and immune support.[web:481][web:476]",
+    k:"ubuy congo health supplements,liposomal sulforaphane,antioxidant supplements online,wellness and immune support congo",
+    a:""
+  },
+  {
+    i:4,
+    n:"nasomba marketplace",
+    c:"electronics,home,fashion,beauty,books,business",
+    u:"https://nasomba.com",
+    d:"nasomba marketplace is a congo brazzaville online shopping site offering categories like electronics, fashion, cosmetics, appliances, computing, perfumes, video games and books.[web:474]",
+    k:"nasomba marketplace congo,online shopping site brazzaville,electronics and appliances,fashion and cosmetics,computing and video games,perfumes and books store",
+    a:""
+  },
+  {
+    i:5,
+    n:"mboteshop",
+    c:"electronics,home,fashion,food,business",
+    u:"https://mboteshop.com",
+    d:"mboteshop is a vente et achat platform in congo that delivers orders in about 90 minutes via tindabox, covering electronics, fashion and grocery items across several cities.[web:474]",
+    k:"mboteshop congo,vente et achat platform,90 minute delivery tindabox,electronics and fashion shopping,grocery items in congo cities",
+    a:""
+  },
+  {
+    i:6,
+    n:"congobonmarche",
+    c:"electronics,home,fashion,food,business",
+    u:"https://play.google.com/store/apps/details?id=org.congobonmarche&hl=en_US",
+    d:"congobonmarche app is promoted as one of the largest online marketplaces in congo, helping users buy and sell products and find deals on electronics, clothing, household goods and groceries.[web:480]",
+    k:"congobonmarche app,largest online marketplace congo,buy and sell products,deals on electronics and clothing,household goods and groceries",
+    a:""
+  },
+  {
+    i:7,
+    n:"onlineshop congo",
+    c:"electronics,home,fashion,business",
+    u:"https://www.onlineshopcongo.com/",
+    d:"onlineshop congo is a classified ads and ecommerce platform where users in the democratic republic of the congo sell and buy products and services such as electronics, fashion and home goods.[web:482]",
+    k:"onlineshop congo,classified ads and ecommerce,sell and buy products and services,electronics and fashion,home goods marketplace drc",
+    a:""
+  },
+  {
+    i:8,
+    n:"top 10 ecommerce marketplaces in republic of congo article",
+    c:"business",
+    u:"https://stateglobe.com/blog/top-10-ecommerce-marketplaces-in-republic-of-congo",
+    d:"top 10 ecommerce marketplaces in republic of congo article reviews platforms like jumia congo, congomall and kongoshop, giving businesses insight into local online market share and growth.[web:473]",
+    k:"top 10 ecommerce marketplaces congo,jumia congo market share,congomall and kongoshop review,online market insights republic of congo",
+    a:""
+  },
+  {
+    i:9,
+    n:"myleadfox congo ecommerce data",
+    c:"business",
+    u:"https://myleadfox.com/blog/platform/e-commerce/cg",
+    d:"myleadfox congo ecommerce data page lists active online stores in congo with platform and category details for lead generation and market research.[web:474]",
+    k:"myleadfox congo ecommerce data,active online stores list,platform and category info,lead generation and market research congo",
+    a:""
+  },
+  {
+    i:10,
+    n:"aftership top 100 online stores congo drc",
+    c:"business",
+    u:"https://www.aftership.com/store-list/top-100-cd-ecommerces-stores",
+    d:"aftership top 100 online stores congo drc ranks leading ecommerce sites including food, sports and auto parts retailers, helping brands benchmark performance in the democratic republic of the congo.[web:479]",
+    k:"aftership top 100 online stores congo drc,ecommerce ranking list,food and sports retailers,auto parts online,drc performance benchmark",
+    a:""
+  }
+];
