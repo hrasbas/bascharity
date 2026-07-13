fc21c42ac5fe224f3fb3c1a81716fbb499eefa85
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:49:55 +0530

    Create philippines.js with online shopping data
    Added an array of online shopping platforms in the Philippines with details.

---
+++ philippines.js
@@ -0,0 +1,32 @@
+// philippines.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["philippines"] = [
+  {
+    i:1,
+    n:"ubuy philippines",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.ph/",
+    d:"ubuy philippines is an international online shopping website where customers in the philippines can buy imported electronics, mobiles and tablets, home and kitchen goods, fashion and jewellery, beauty and personal care products, books, toys, sports gear, travel accessories, groceries and automotive parts from global brands at low prices.[web:1412][web:1413][web:1414][web:1416][web:1232]",
+    k:"ubuy philippines,international online shopping philippines,imported electronics mobiles tablets home kitchen,fashion jewellery beauty personal care products,books toys sports gear travel accessories groceries automotive parts",
+    a:""
+  },
+  {
+    i:2,
+    n:"shopee philippines online marketplace",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,automotive,business",
+    u:"https://shopee.ph/",
+    d:"shopee philippines online marketplace is a leading mobile first ecommerce platform where users across the philippines can buy and sell a wide range of products including electronics, home essentials, clothing, shoes, beauty items, groceries, books and automotive accessories with free shipping deals and secure payment.[web:1415][web:1418][web:1420]",
+    k:"shopee philippines online marketplace,buy and sell products online philippines,mobile first ecommerce with free shipping deals,electronics home essentials clothing shoes beauty,grocery books automotive accessories secure payment",
+    a:""
+  },
+  {
+    i:3,
+    n:"lazada philippines online shopping",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,automotive,business",
+    u:"https://www.lazada.com.ph/",
+    d:"lazada philippines online shopping is a major ecommerce site offering diverse electronics, home appliances, fashion, beauty essentials, toys, groceries and automotive goods with fast delivery, regular promotions and convenient payment options nationwide.[web:1417][web:1419]",
+    k:"lazada philippines online shopping,major ecommerce site in philippines,diverse electronics home appliances fashion beauty,toys groceries automotive goods fast delivery,promotions convenient payment options nationwide",
+    a:""
+  }
+];
