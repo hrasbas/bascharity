113289c0703e98ebe29858e51202afdd18fbcf3c
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:06:08 +0530

    Add new_zealand.js with country shops data
    This file contains a list of online shops available in New Zealand, including their details such as name, categories, URL, description, and keywords.

---
+++ new_zealand.js
@@ -0,0 +1,32 @@
+// new_zealand.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["new_zealand"] = [
+  {
+    i:1,
+    n:"ubuy new zealand",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.u-buy.co.nz/",
+    d:"ubuy new zealand is an international online shopping website where customers in new zealand can buy imported electronics, mobiles, fashion, beauty and health products, home and living goods, books, sports gear, travel accessories, groceries, jewellery and automotive parts from global brands at low prices.[web:1290][web:1282][web:1287]",
+    k:"ubuy new zealand,international online shopping new zealand,imported electronics mobiles fashion beauty health,home living goods books sports travel gear,groceries jewellery and automotive parts cross border shopping nz",
+    a:""
+  },
+  {
+    i:2,
+    n:"trade me new zealand online marketplace",
+    c:"electronics,home,fashion,beauty,food,sports,travel,books,automotive,business",
+    u:"https://www.trademe.co.nz/",
+    d:"trade me new zealand online marketplace is one of nz's biggest auction and classifieds platforms where users buy and sell new and used items across categories such as property, jobs, motors, electronics, fashion, books, home and garden, sports gear and travel services.[web:1283][web:1284][web:1286][web:1289]",
+    k:"trade me new zealand online marketplace,nz auction and classifieds site,buy and sell new and used items,property jobs motors electronics fashion,home garden books sports gear travel services",
+    a:""
+  },
+  {
+    i:3,
+    n:"themarket new zealand shopping destination",
+    c:"electronics,home,fashion,beauty,health,food,sports,business",
+    u:"https://www.themarket.com/",
+    d:"themarket new zealand shopping destination is an online marketplace from the warehouse group offering millions of products from thousands of nz and international brands across fashion, footwear, accessories, health and beauty, home and living, sports, toys, electronics and pantry items.[web:1285][web:1288]",
+    k:"themarket new zealand shopping destination,online marketplace from the warehouse group,millions of products thousands of brands nz,fashion footwear accessories health beauty,home living sports toys electronics pantry",
+    a:""
+  }
+];
