f728adc69df65c0542f32ef83508fc74716ecc39
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:02:41 +0530

    Add North Korea online shops data

---
+++ north_korea.js
@@ -0,0 +1,23 @@
+// north_korea.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["north_korea"] = [
+  {
+    i:1,
+    n:"okryu north korea online shop",
+    c:"food,beauty,health,fashion,home,business",
+    u:"http://okryu.example.kp/",
+    d:"okryu north korea online shop is a state-run ecommerce website on the domestic intranet where residents with local smartphones can order food, medicines, cosmetics, clothing, bags, shoes and household items from north korean producers and pay using national debit cards.[web:1318][web:1319][web:1320][web:1325]",
+    k:"okryu north korea online shop,state run ecommerce website north korea,order food medicines cosmetics clothing,bags shoes and household items online,pay with national debit cards domestic intranet shopping",
+    a:""
+  },
+  {
+    i:2,
+    n:"manmulsang north korea ecommerce platform",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"http://manmulsang.example.kp/",
+    d:"manmulsang north korea ecommerce platform is an online marketplace on the national network where factories, shops and producers list many different products, from everyday goods and electronics to clothing, food and cosmetics, so buyers can browse economic information and purchase directly.[web:1317][web:1320]",
+    k:"manmulsang north korea ecommerce platform,online marketplace on national network,factories shops and producers list products,everyday goods electronics clothing food cosmetics,browse economic information and purchase directly",
+    a:""
+  }
+];
