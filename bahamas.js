90f7fe1366fb1a2feb12629cdb0c7feef44cb3df
hrasbas <hrasbasji@gmail.com>
2026-07-13 12:35:38 +0530

    Refactor shop entries in bahamas.js
    Updated descriptions and keywords for various shops in the Bahamas. Added new entries and removed some outdated ones.

--- bahamas.js
+++ bahamas.js
@@ -1,12 +1,12 @@
-window.countryShops = window.countryShops || {}; 
+ window.countryShops = window.countryShops || {};
 window.countryShops["bahamas"] = [
   {
     i:1,
     n:"mall242",
     c:"electronics,home,fashion,beauty,food,business",
     u:"https://www.mall242.com",
-    d:"mall242 is a virtual mall and b2b marketplace in the bahamas where shoppers can buy electronics, fashion, home goods, beauty products and groceries from trusted local stores.",
-    k:"mall242 bahamas,virtual mall marketplace,local stores online,electronics and fashion shopping,home goods and groceries",
+    d:"mall242 is a virtual mall and marketplace in the bahamas where shoppers buy electronics, fashion, home goods, beauty products and groceries from local stores.",
+    k:"mall242 bahamas,virtual mall bahamas,online shopping bahamas,local stores marketplace,electronics fashion home food",
     a:""
   },
   {
@@ -15,7 +15,7 @@ window.countryShops["bahamas"] = [
     c:"food,business",
     u:"https://instagopher.com",
     d:"instagopher is an online grocery delivery service in the bahamas that brings supermarket food, drinks and household essentials to homes and vacation rentals.",
-    k:"instagopher bahamas,online grocery delivery,nassau food shopping,supermarket delivery service,household essentials",
+    k:"instagopher bahamas,grocery delivery bahamas,online supermarket bahamas,food and drinks delivery,nassau grocery service",
     a:""
   },
   {
@@ -23,8 +23,8 @@ window.countryShops["bahamas"] = [
     n:"foodstore2go",
     c:"food,business",
     u:"https://foodstore2go.com",
-    d:"foodstore2go offers convenient grocery and beverage delivery across the bahamas, supplying fresh food, pantry items and drinks to residents and visitors.",
-    k:"foodstore2go bahamas,grocery and beverage delivery,online food shopping,nassau grocery service,pantry items and drinks",
+    d:"foodstore2go offers grocery and beverage delivery across the bahamas, supplying fresh food, pantry items and drinks to residents and visitors.",
+    k:"foodstore2go bahamas,grocery delivery bahamas,beverage delivery bahamas,online food shopping,pantry items and drinks",
     a:""
   },
   {
@@ -32,120 +32,102 @@ window.countryShops["bahamas"] = [
     n:"central supercenter",
     c:"food,business",
     u:"https://central242.com",
-    d:"central supercenter is an online grocery store and delivery service in the bahamas providing brand name food, drinks and household products at low prices.",
-    k:"central supercenter bahamas,online grocery store,food and drinks delivery,brand name products,household supplies",
+    d:"central supercenter is an online grocery and household store in the bahamas providing food, drinks and home essentials at low prices.",
+    k:"central supercenter bahamas,online grocery store bahamas,food and drinks bahamas,household essentials,low price supermarket",
     a:""
   },
   {
     i:5,
-    n:"desertcart bahamas",
-    c:"electronics,home,fashion,beauty,health,sports,business",
-    u:"https://bahamas.desertcart.com",
-    d:"desertcart bahamas is an international online shopping platform delivering health care, beauty, baby items, toys, sports gear, electronics and fashion from overseas.",
-    k:"desertcart bahamas,international online shopping,imported electronics and fashion,health and beauty products,baby toys and sports gear",
+    n:"cbs bahamas",
+    c:"home,business",
+    u:"https://www.cbsbahamas.com",
+    d:"cbs bahamas is a home improvement retailer offering hardware, tools, paint, appliances and other home and building supplies.",
+    k:"cbs bahamas,home improvement bahamas,hardware store bahamas,tools and paint,appliances and building supplies",
     a:""
   },
   {
     i:6,
-    n:"ubuy bahamas",
-    c:"electronics,home,fashion,beauty,health,sports,business",
-    u:"https://www.bahamas.ubuy.com",
-    d:"ubuy bahamas is an online marketplace that ships premium electronics, fashion, beauty, health products and home goods from global brands to bahamas customers.",
-    k:"ubuy bahamas,online shopping website,premium electronics and fashion,beauty and health products,home goods delivery",
+    n:"sysco bahamas food services",
+    c:"food,business",
+    u:"https://www.syscobahamas.com",
+    d:"sysco bahamas food services supplies food and beverages to restaurants, hotels and institutions and offers bulk ordering for businesses.",
+    k:"sysco bahamas,food service supplier bahamas,bulk groceries for hotels,restaurant food supplier,beverage distribution",
     a:""
   },
   {
     i:7,
-    n:"cbs bahamas",
-    c:"home,business",
-    u:"https://www.cbsbahamas.com",
-    d:"cbs bahamas is a home improvement and building supplies retailer offering hardware, tools, paint, appliances and home essentials.",
-    k:"cbs bahamas,home improvement store,hardware and tools,building supplies bahamas,appliances and home essentials",
+    n:"the dash deal",
+    c:"food,business",
+    u:"https://www.thedashdeal.com",
+    d:"the dash deal is an online grocery store in nassau where customers order food and household items for delivery or pickup.",
+    k:"the dash deal nassau,online grocery bahamas,food delivery bahamas,household items shopping,same day groceries",
     a:""
   },
   {
     i:8,
-    n:"sysco bahamas food services",
-    c:"food,business",
-    u:"https://www.syscobahamas.com",
-    d:"sysco bahamas food services supplies food and beverages to restaurants, hotels and institutions and offers bulk grocery ordering for businesses.",
-    k:"sysco bahamas food services,food service supplier,bulk groceries for hotels,restaurant food supplier,business food delivery",
-    a:""
-  },
-  {
-    i:9,
-    n:"the dash deal",
-    c:"food,business",
-    u:"https://www.thedashdeal.com",
-    d:"the dash deal is an online grocery store in nassau where customers can order food and household items for same day delivery or in store pickup.",
-    k:"the dash deal nassau,online grocery store bahamas,same day delivery,in store pickup groceries,food and household shopping",
-    a:""
-  },
-  {
-    i:10,
     n:"aliv online shop",
     c:"electronics,business",
     u:"https://www.bealiv.com",
     d:"aliv is a mobile network operator in the bahamas and its online shop sells smartphones, devices, plans and accessories for wireless customers.",
-    k:"aliv bahamas online shop,mobile phones and plans,wireless devices and accessories,mobile operator store,smartphones in bahamas",
+    k:"aliv bahamas,online phone shop bahamas,mobile network bahamas,smartphones and devices,phone accessories and plans",
+    a:""
+  },
+  {
+    i:9,
+    n:"batelco online store",
+    c:"electronics,business",
+    u:"https://www.btcbahamas.com",
+    d:"batelco online store offers internet, tv and phone services along with routers, modems and related telecom equipment for homes and businesses.",
+    k:"batelco bahamas,online telecom store,internet and tv bahamas,routers and modems,home and business telecom services",
+    a:""
+  },
+  {
+    i:10,
+    n:"baha mar resort shop",
+    c:"travel,food,business",
+    u:"https://www.bahamar.com",
+    d:"baha mar resort runs an online portal for hotel booking, dining, activities and gift items at its resort complex in the bahamas.",
+    k:"baha mar bahamas,online hotel booking bahamas,resort dining and activities,gift shop resort,travel and vacation packages",
     a:""
   },
   {
     i:11,
-    n:"batelco online store",
-    c:"electronics,business",
-    u:"https://www.btcbahamas.com",
-    d:"batelco online store offers internet, tv and phone services along with routers, modems and related telecom equipment for bahamas households and businesses.",
-    k:"batelco bahamas online,internet and tv packages,telecom equipment routers,modems and devices,home and business telecom",
-    a:""
-  },
-  {
-    i:12,
-    n:"baha mar resort shop",
-    c:"travel,food,business",
-    u:"https://www.bahamar.com",
-    d:"baha mar resort operates an online booking and shopping portal for hotel stays, dining, activities and gift items at its bahamas resort complex.",
-    k:"baha mar bahamas,online hotel booking,resort dining and activities,gift shop resort,travel and vacation packages",
-    a:""
-  },
-  {
-    i:13,
     n:"abaco club shop",
     c:"travel,food,business",
     u:"https://www.theabacoclub.com",
     d:"abaco club is a private resort and golf club in the bahamas with online booking for stays, golf, dining and membership services.",
-    k:"abaco club bahamas,private resort booking,golf club stays,dining and membership,golf vacation bahamas",
+    k:"abaco club bahamas,golf resort bahamas,online stay booking,dining and membership,golf vacation bahamas",
     a:""
   },
   {
-    i:14,
+    i:12,
     n:"john watling's distillery",
     c:"food,business",
     u:"https://johnwatlings.com",
-    d:"john watling's distillery sells bahamian rum and spirits online along with tours and merchandise from its historic distillery in nassau.",
-    k:"john watling's distillery,rums and spirits bahamas,online liquor shop,distillery tours nassau,bahamian rum merchandise",
+    d:"john watling's distillery sells bahamian rum and spirits online along with tours and merchandise from its distillery in nassau.",
+    k:"john watling's distillery,rums and spirits bahamas,online liquor shop bahamas,distillery tours nassau,bahamian rum merchandise",
     a:""
   },
   {
-    i:15,
+    i:13,
     n:"bristol wines and spirits",
     c:"food,business",
     u:"https://www.bristolbahamas.com",
     d:"bristol wines and spirits is a liquor retailer in the bahamas offering wine, beer and spirits with online ordering and delivery.",
-    k:"bristol bahamas,wine and spirits retailer,online liquor ordering,beer and wine shop,duty free drinks bahamas",
+    k:"bristol bahamas,wine and spirits bahamas,online liquor ordering,beer and wine shop,duty free drinks bahamas",
     a:""
   },
   {
-    i:16,
+    i:14,
     n:"the sports center bahamas",
     c:"sports,fashion,business",
     u:"https://www.sportscenter.com.bs",
-    d:"the sports center bahamas sells athletic apparel, footwear, sports equipment and fitness accessories for men, women and kids.",
-    k:"the sports center bahamas,athletic apparel store,sports footwear bahamas,sports equipment and gear,fitness accessories",
+    d:"the sports center bahamas sells athletic apparel, sports footwear, equipment and fitness accessories for men, women and kids.",
+    k:"sports center bahamas,athletic apparel bahamas,sports shoes store,sports equipment and gear,fitness accessories shop",
     a:""
   },
   {
-    i:17,
+    i:15,
     n:"bahari bahamas",
     c:"fashion,business",
     u:"https://bahari.co",
@@ -154,25 +136,16 @@ window.countryShops["bahamas"] = [
     a:""
   },
   {
-    i:18,
-    n:"bahamas largest department store",
+    i:16,
+    n:"bahamas department store online",
     c:"electronics,home,fashion,beauty,food,business",
     u:"https://www.bahamasdepartmentstore.com",
-    d:"bahamas largest department store is a multi category retailer selling clothing, appliances, electronics, beauty products and household items online.",
-    k:"bahamas largest department store,online department store,multi category retailer,clothing and appliances,electronics and beauty",
+    d:"bahamas department store online is a multi category retailer selling clothing, appliances, electronics, beauty products and household items.",
+    k:"bahamas department store,online department store bahamas,multi category retailer,clothing and appliances,electronics and beauty products",
     a:""
   },
   {
-    i:19,
-    n:"chain debrief shop",
-    c:"books,business",
-    u:"https://chaindebrief.com/shop",
-    d:"chain debrief shop offers educational resources, books and merchandise focused on investing and financial literacy.",
-    k:"chain debrief shop,investing education store,financial literacy books,educational resources online,business and finance merchandise",
-    a:""
-  },
-  {
-    i:20,
+    i:17,
     n:"nagb online shop",
     c:"home,business",
     u:"https://nagb.org.bs",
@@ -181,7 +154,7 @@ window.countryShops["bahamas"] = [
     a:""
   },
   {
-    i:21,
+    i:18,
     n:"privileged shoes bahamas",
     c:"fashion,business",
     u:"https://bs-privilegedshoes.glopalstore.com",
@@ -190,17 +163,66 @@ window.countryShops["bahamas"] = [
     a:""
   },
   {
-    i:22,
+    i:19,
     n:"cosmostore bahamas",
     c:"beauty,health,business",
     u:"https://bs.cosmostore.org",
-    d:"cosmostore bahamas is an online beauty shop selling hair care, skincare, makeup and cosmetics with free shipping to the bahamas.",
-    k:"cosmostore bahamas,online beauty shop,hair and skincare products,makeup and cosmetics,free shipping bahamas",
+    d:"cosmostore bahamas is an online beauty shop selling hair care, skincare, makeup and cosmetics with delivery to the bahamas.",
+    k:"cosmostore bahamas,online beauty shop,hair and skincare products,makeup and cosmetics,beauty products delivery bahamas",
+    a:""
+  },
+  {
+    i:20,
+    n:"ubuy bahamas",
+    c:"electronics,home,fashion,beauty,health,sports,business",
+    u:"https://www.bahamas.ubuy.com",
+    d:"ubuy bahamas is an online marketplace that ships electronics, fashion, beauty, health products, sports gear and home goods from global brands.",
+    k:"ubuy bahamas,online shopping bahamas,imported electronics and fashion,beauty and health products,home goods and sports gear",
+    a:""
+  },
+  {
+    i:21,
+    n:"desertcart bahamas",
+    c:"electronics,home,fashion,beauty,health,sports,business",
+    u:"https://bahamas.desertcart.com",
+    d:"desertcart bahamas is an international shopping platform delivering health care, beauty, baby items, toys, sports gear, electronics and fashion to bahamas customers.",
+    k:"desertcart bahamas,international shopping bahamas,imported products online,health and beauty items,baby toys sports electronics",
+    a:""
+  },
+  {
+    i:22,
+    n:"chain debrief shop",
+    c:"books,business",
+    u:"https://chaindebrief.com/shop",
+    d:"chain debrief shop offers books, merchandise and educational resources focused on investing and financial literacy.",
+    k:"chain debrief shop,investing education store,financial literacy books,educational resources online,business and finance merchandise",
     a:""
   },
   {
     i:23,
+    n:"carethy wellness",
+    c:"health,beauty,business",
+    u:"https://www.carethy.com",
+    d:"carethy is an online wellness store selling nutrition products, supplements, hygiene and health items with international delivery options.",
+    k:"carethy wellness store,nutrition and supplements,online health products,hygiene and wellness items,beauty and fitness products",
+    a:""
+  },
+  {
+    i:24,
     n:"biuky beauty",
     c:"beauty,business",
     u:"https://www.biuky.co.uk",
-    d:"biuky offers cosmetics, perfumes
+    d:"biuky offers cosmetics, perfumes and beauty products online and ships selected items to international customers.",
+    k:"biuky beauty online,cosmetics and perfumes,beauty products store,makeup and fragrance shop,international beauty shipping",
+    a:""
+  },
+  {
+    i:25,
+    n:"miscota pet shop",
+    c:"food,health,business",
+    u:"https://www.miscota.com",
+    d:"miscota is an online pet shop offering pet food, accessories and supplies for dogs, cats, birds, fish and small animals.",
+    k:"miscota pet shop,online pet food store,pet accessories and supplies,dog and cat food,pet shop online",
+    a:""
+  }
+];
