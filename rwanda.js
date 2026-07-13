89eff68ac8d4c251051fa5413d26451f9fa475ad
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:19:03 +0530

    Create rwanda.js with country shops data
    Added a list of online shops available in Rwanda with details.

---
+++ rwanda.js
@@ -0,0 +1,23 @@
+// rwanda.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["rwanda"] = [
+  {
+    i:1,
+    n:"ubuy rwanda",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.rw/fr/",
+    d:"ubuy rwanda is an international online shopping website where customers in rwanda can buy imported electronics, mobiles and tablets, fashion and clothing, beauty and personal care products, home and kitchen goods, toys and games, office and school supplies, gardening tools, baby care items, pet supplies, travel and outdoor gear, snacks and groceries and automotive replacement parts from global brands at low prices.[web:1470][web:1471][web:1473][web:1474][web:1475][web:1476][web:1477]",
+    k:"ubuy rwanda,international online shopping rwanda,imported electronics mobiles tablets fashion clothing,beauty personal care home kitchen goods,toys games office school supplies gardening tools,baby care pet supplies travel outdoor gear snacks groceries automotive replacement parts",
+    a:""
+  },
+  {
+    i:2,
+    n:"kimironko market online rwanda grocery",
+    c:"food,business",
+    u:"https://kimironkomarket.rw/",
+    d:"kimironko market online rwanda grocery is a local ecommerce platform that brings kigali’s kimironko market to the web, letting shoppers order fruits, vegetables, grains, meat, dairy and everyday groceries with online checkout and delivery.[web:1472][web:1478]",
+    k:"kimironko market online rwanda grocery,online grocery shopping kigali rwanda,order fruits vegetables grains meat dairy,everyday groceries from kimironko market with delivery",
+    a:""
+  }
+];
