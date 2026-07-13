f09b209c9f883baaaf8332dc062ced66dfb1da4d
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:51:09 +0530

    Add Peru country shops information

---
+++ peru.js
@@ -0,0 +1,23 @@
+// peru.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["peru"] = [
+  {
+    i:1,
+    n:"ubuy peru",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.pe/sp/",
+    d:"ubuy peru is an international online shopping website where customers in peru can buy imported electronics, mobiles and tablets, home and kitchen goods, fashion and jewellery, beauty and personal care products, books, toys, sports gear, travel accessories, groceries and automotive parts from global brands at low prices.[web:1402][web:1403][web:1404][web:1406][web:1407][web:1409][web:1410]",
+    k:"ubuy peru,international online shopping peru,imported electronics mobiles tablets home kitchen,fashion jewellery beauty personal care products,books toys sports gear travel accessories groceries automotive parts",
+    a:""
+  },
+  {
+    i:2,
+    n:"falabella.com peru marketplace",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,automotive,business",
+    u:"https://www.falabella.com.pe/falabella-pe",
+    d:"falabella.com peru marketplace is a large online shopping platform that brings together products from falabella, sodimac, tottus, linio and many peruvian brands, offering categories such as fashion, beauty, electronics, appliances, groceries, pet items, books and automotive goods in one site.[web:1405][web:1408][web:1411]",
+    k:"falabella.com peru marketplace,online shopping platform peru,fashion beauty electronics appliances groceries,pet items books automotive goods one site,group falabella marketplace sellers all sizes peru",
+    a:""
+  }
+];
