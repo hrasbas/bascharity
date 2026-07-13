28f6a023f43ca88b5705b263864a377617a2a0a8
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:14:34 +0530

    Add country shops data for Monaco

---
+++ monaco.js
@@ -0,0 +1,14 @@
+// monaco.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["monaco"] = [
+  {
+    i:1,
+    n:"ubuy monaco",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,business",
+    u:"https://www.ubuy.mc/en/",
+    d:"ubuy monaco is an international online shopping website where customers in monaco can buy premium and luxury imported products including electronics, clothing, watches, mobile phones, beauty items, groceries, home and kitchen goods and automotive accessories from global brands.[web:1184][web:1190]",
+    k:"ubuy monaco,international online shopping monaco,premium and luxury imported electronics clothing,watches mobiles beauty groceries home goods,automotive accessories cross border shopping monaco",
+    a:""
+  }
+];
