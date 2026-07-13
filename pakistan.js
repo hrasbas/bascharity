2066451d3a39cb8cc658fca6f44b22d989f4def9
hrasbas <hrasbasji@gmail.com>
2026-07-13 22:56:59 +0530

    Add country shops data for Pakistan

---
+++ pakistan.js
@@ -0,0 +1,23 @@
+// pakistan.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["pakistan"] = [
+  {
+    i:1,
+    n:"ubuy pakistan",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.ubuy.com.pk/en/",
+    d:"ubuy pakistan is an international online shopping website where customers in pakistan can buy imported electronics, mobiles and tablets, home goods, fashion, beauty and health products, books, sports gear, travel accessories, groceries, jewellery and automotive accessories from global brands at low prices.[web:1355][web:1357][web:1358][web:1361][web:1364]",
+    k:"ubuy pakistan,international online shopping pakistan,imported electronics mobiles tablets home goods,fashion beauty health products books,sports gear travel accessories groceries jewellery automotive accessories",
+    a:""
+  },
+  {
+    i:2,
+    n:"daraz pakistan online shopping",
+    c:"electronics,home,fashion,beauty,food,sports,books,business",
+    u:"https://www.daraz.pk/",
+    d:"daraz pakistan online shopping is one of the biggest ecommerce marketplaces in pakistan offering more than fifteen million products including mobiles, electronics, home appliances, clothing, shoes, fashion accessories, beauty items, groceries and books with nationwide cash on delivery and frequent discount deals.[web:1359][web:1362][web:1363]",
+    k:"daraz pakistan online shopping,biggest ecommerce marketplace in pakistan,buy mobiles electronics home appliances online,clothing shoes fashion accessories beauty items,grocery and books cash on delivery pakistan",
+    a:""
+  }
+];
