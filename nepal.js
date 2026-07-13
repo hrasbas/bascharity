939bfcc7fffb3ded05f1881ffb0c7fb1ef76bd73
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:07:55 +0530

    Add nepal.js with country shops data
    This file contains information about online shopping options in Nepal, including Ubuy and Daraz.

---
+++ nepal.js
@@ -0,0 +1,23 @@
+// nepal.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["nepal"] = [
+  {
+    i:1,
+    n:"ubuy nepal",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.nepal.ubuy.com/en/",
+    d:"ubuy nepal is an international online shopping website where customers in nepal can buy imported electronics, fashion, beauty and health supplements, home and kitchen goods, books, sports equipment, travel gear, groceries, jewellery and automotive accessories from global brands at low prices.[web:1262][web:1263][web:1267][web:1270]",
+    k:"ubuy nepal,international online shopping nepal,imported electronics fashion beauty health supplements,home kitchen goods books sports travel gear,groceries jewellery and automotive accessories cross border shopping nepal",
+    a:""
+  },
+  {
+    i:2,
+    n:"daraz nepal online shopping",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.daraz.com.np/?keep_url=true",
+    d:"daraz nepal online shopping is one of the largest ecommerce marketplaces in nepal where users shop mobiles, electronics, fashion, home appliances, beauty products and daily essentials with nationwide delivery, cash on delivery and frequent discounts.[web:1264][web:1269]",
+    k:"daraz nepal online shopping,largest ecommerce marketplace in nepal,buy mobiles electronics fashion home appliances,beauty products and daily essentials online,cash on delivery nationwide delivery nepal",
+    a:""
+  }
+];
