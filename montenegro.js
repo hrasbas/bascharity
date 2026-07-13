1a1c52d23a9ce0dc8cd615ee2a4f2d5861d419e9
hrasbas <hrasbasji@gmail.com>
2026-07-13 23:13:14 +0530

    Create montenegro.js

---
+++ montenegro.js
@@ -0,0 +1,14 @@
+// montenegro.js
+
+window.countryShops = window.countryShops || {};
+window.countryShops["montenegro"] = [
+  {
+    i:1,
+    n:"ubuy montenegro",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,jewellery,automotive,business",
+    u:"https://www.montenegro.ubuy.com/",
+    d:"ubuy montenegro is an international online shopping website where customers in montenegro can buy imported electronics, mobiles, gaming laptops, fashion, beauty products, home goods, books, travel gear and automotive accessories from global brands at discount prices.[web:1204][web:1207][web:1209][web:1213]",
+    k:"ubuy montenegro,international online shopping montenegro,imported electronics mobiles gaming laptops,fashion beauty home goods online store,books travel gear and automotive accessories montenegro",
+    a:""
+  }
+];
