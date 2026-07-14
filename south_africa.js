0c0df06fe17679bd3e6e5183d495dd175140754a
hrasbas <hrasbasji@gmail.com>
2026-07-14 13:07:44 +0530

    Create south_africa.js

---
+++ south_africa.js
@@ -0,0 +1,229 @@
+// south_africa.js
+window.countryShops = window.countryShops || {};
+window.countryShops["south_africa"] = [
+  {
+    i:1,
+    n:"takealot",
+    c:"electronics,home,fashion,beauty,food,sports,books,business,automotive",
+    u:"https://www.takealot.com/",
+    d:"takealot is the largest online marketplace in south africa where customers can shop for electronics, home appliances, fashion, beauty, books, groceries, sports gear and automotive products with nationwide delivery.[web:683][web:686][web:699]",
+    k:"takealot south africa,online marketplace south africa,electronics home appliances and gadgets,fashion beauty books and groceries,nationwide delivery ecommerce site",
+    a:""
+  },
+  {
+    i:2,
+    n:"makro",
+    c:"electronics,home,food,business",
+    u:"https://www.makro.co.za/",
+    d:"makro online in south africa offers bulk and retail shopping for electronics, appliances, office supplies, groceries and home products with click and collect and delivery options.[web:683][web:685][web:686]",
+    k:"makro south africa,online bulk and retail shopping,electronics appliances and office supplies,groceries and home products,click and collect ecommerce platform",
+    a:""
+  },
+  {
+    i:3,
+    n:"checkers sixty60",
+    c:"food,home,business",
+    u:"https://www.checkers.co.za/sixty60",
+    d:"checkers sixty60 is a grocery delivery app and online service in south africa that lets customers order supermarket items and household essentials for rapid delivery in many urban areas.[web:683][web:685]",
+    k:"checkers sixty60 south africa,online grocery delivery service,supermarket items and household essentials,rapid delivery app based shopping,checkers online platform",
+    a:""
+  },
+  {
+    i:4,
+    n:"pick n pay online",
+    c:"food,home,business",
+    u:"https://www.pnp.co.za/",
+    d:"pick n pay online offers south africa shoppers groceries, fresh produce and household products from pick n pay stores with home delivery and pickup options.[web:683][web:691]",
+    k:"pick n pay online south africa,supermarket groceries and fresh produce,household products and daily essentials,home delivery and pickup,ecommerce grocery platform",
+    a:""
+  },
+  {
+    i:5,
+    n:"shoprite online",
+    c:"food,home,business",
+    u:"https://www.shoprite.co.za/",
+    d:"shoprite online in south africa provides affordable groceries and basic household essentials that can be ordered via web or app for collection or delivery.[web:685][web:691]",
+    k:"shoprite online south africa,affordable groceries and essentials,web and app ordering supermarket,collection and delivery services,ecommerce for budget shoppers",
+    a:""
+  },
+  {
+    i:6,
+    n:"spar online",
+    c:"food,home,business",
+    u:"https://www.spar.co.za/",
+    d:"spar online lets south africa customers shop groceries, bakery items and home products from spar stores with digital promotions and delivery in selected areas.[web:683]",
+    k:"spar online south africa,online groceries and bakery,home products and promotions,digital supermarket shopping,delivery in selected regions",
+    a:""
+  },
+  {
+    i:7,
+    n:"superbalist",
+    c:"fashion,home,beauty,business",
+    u:"https://superbalist.com/",
+    d:"superbalist is a leading fashion and lifestyle ecommerce site in south africa selling clothing, shoes, accessories and home decor from local and international brands.[web:683][web:684][web:694]",
+    k:"superbalist south africa,online fashion and lifestyle store,clothing shoes and accessories,home decor and branded products,ecommerce fashion platform",
+    a:""
+  },
+  {
+    i:8,
+    n:"zando",
+    c:"fashion,beauty,business",
+    u:"https://www.zando.co.za/",
+    d:"zando is an online fashion retailer in south africa offering shoes, clothing, accessories and selected beauty products for men, women and kids.[web:684][web:694]",
+    k:"zando south africa,online fashion retailer,shoes clothing and accessories,beauty products and brands,ecommerce style shopping site",
+    a:""
+  },
+  {
+    i:9,
+    n:"mr price online",
+    c:"fashion,home,business",
+    u:"https://www.mrp.com/",
+    d:"mr price online store in south africa sells value fashion for the whole family along with homeware and decor items through its ecommerce platform.[web:683][web:691]",
+    k:"mr price online south africa,value fashion for family,homeware and decor items,affordable clothing ecommerce,mrp online shopping site",
+    a:""
+  },
+  {
+    i:10,
+    n:"woolworths online",
+    c:"food,fashion,beauty,home,business",
+    u:"https://www.woolworths.co.za/",
+    d:"woolworths online in south africa provides premium groceries, fashion, beauty and home products using a sophisticated ecommerce site and delivery network.[web:683][web:686][web:694]",
+    k:"woolworths online south africa,premium groceries and fresh food,fashion beauty and home products,quality retail ecommerce,delivery and collection service",
+    a:""
+  },
+  {
+    i:11,
+    n:"clicks",
+    c:"beauty,health,business",
+    u:"https://www.clicks.co.za/",
+    d:"clicks online is a pharmacy and beauty ecommerce store in south africa offering medication, health supplements, skincare, haircare and personal care products.[web:683][web:691]",
+    k:"clicks south africa,online pharmacy and health store,skincare haircare and personal care,medication and supplements ecommerce,health and beauty retailer",
+    a:""
+  },
+  {
+    i:12,
+    n:"dis-chem",
+    c:"health,beauty,business",
+    u:"https://www.dischem.co.za/",
+    d:"dis-chem online in south africa sells prescription and otc medicines, health and wellness products, baby care and beauty items via its ecommerce platform.[web:683]",
+    k:"dis-chem south africa,online pharmacy and wellness store,medicines health and baby care,beauty items ecommerce platform,health retailer with delivery",
+    a:""
+  },
+  {
+    i:13,
+    n:"builders",
+    c:"home,business",
+    u:"https://www.builders.co.za/",
+    d:"builders is a home improvement and diy online store in south africa offering building materials, tools, garden supplies and home decor items.[web:683]",
+    k:"builders south africa,online home improvement store,diy tools building materials and garden supplies,decor items for homes,ecommerce renovation retailer",
+    a:""
+  },
+  {
+    i:14,
+    n:"game online",
+    c:"electronics,home,food,business",
+    u:"https://www.game.co.za/",
+    d:"game online in south africa offers electronics, appliances, groceries and general merchandise with promotions and delivery from game stores.[web:683][web:691]",
+    k:"game online south africa,electronics appliances groceries and general merchandise,promotions and specials ecommerce,delivery from game stores,hypermarket shopping site",
+    a:""
+  },
+  {
+    i:15,
+    n:"everyshop",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.everyshop.co.za/",
+    d:"everyshop is a multi category ecommerce platform in south africa selling tech, appliances, fashion, beauty and home products from leading brands.[web:690]",
+    k:"everyshop south africa,multi category online shopping,tech appliances fashion and beauty,home products from leading brands,ecommerce marketplace everyshop",
+    a:""
+  },
+  {
+    i:16,
+    n:"loot",
+    c:"books,electronics,home,business",
+    u:"https://www.loot.co.za/",
+    d:"loot is an online retailer in south africa focused on books, electronics, toys and home items with regular deals and nationwide delivery.[web:694][web:689]",
+    k:"loot south africa,online books and electronics store,toys and home items retailer,regular deals and discounts,ecommerce site with nationwide delivery",
+    a:""
+  },
+  {
+    i:17,
+    n:"amazon south africa segment",
+    c:"electronics,books,fashion,home,business",
+    u:"https://www.amazon.com/",
+    d:"amazon's global site serves south africa customers with international online shopping for electronics, books, fashion, home goods and many other categories shipped from abroad.[web:684][web:686][web:701]",
+    k:"amazon south africa,international online shopping south africa,electronics books fashion and home goods,cross border ecommerce marketplace,global retailer serving south africa",
+    a:""
+  },
+  {
+    i:18,
+    n:"shein south africa",
+    c:"fashion,beauty,business",
+    u:"https://www.shein.com/",
+    d:"shein is a fast fashion ecommerce platform widely used in south africa offering low priced clothing, shoes, accessories and beauty items from global warehouses.[web:684][web:683][web:695]",
+    k:"shein south africa,online fast fashion store,cheap clothing shoes and accessories,beauty items global warehouses,cross border fashion ecommerce",
+    a:""
+  },
+  {
+    i:19,
+    n:"temu south africa",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.temu.com/",
+    d:"temu serves south africa as a cross border marketplace where users can buy low cost electronics, home goods, fashion and beauty products directly from overseas suppliers.[web:684][web:686]",
+    k:"temu south africa,cross border marketplace south africa,low cost electronics and home goods,fashion and beauty products online,global discount ecommerce app",
+    a:""
+  },
+  {
+    i:20,
+    n:"mr price home",
+    c:"home,business",
+    u:"https://www.mrphome.com/",
+    d:"mr price home is an online homeware retailer in south africa selling furniture, soft furnishings, decor and kitchen items at affordable prices.[web:688][web:700]",
+    k:"mr price home south africa,online homeware retailer,furniture soft furnishings and decor,kitchen items at affordable prices,ecommerce home decor shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"pep online",
+    c:"fashion,home,business",
+    u:"https://www.pepstores.com/",
+    d:"pep online in south africa showcases affordable clothing, footwear and household items targeted at budget conscious families with selected ecommerce options.[web:693][web:700]",
+    k:"pep online south africa,affordable clothing and footwear,household items for budget families,low cost retail brand,ecommerce presence pep stores",
+    a:""
+  },
+  {
+    i:22,
+    n:"adidas south africa",
+    c:"sports,fashion,business",
+    u:"https://www.adidas.co.za/",
+    d:"adidas south africa online store sells sportswear, sneakers and athletic accessories for men, women and kids with delivery nationwide.[web:683][web:691]",
+    k:"adidas south africa,online sportswear and sneakers,athletic accessories for men women and kids,brand store ecommerce platform,nationwide delivery adidas.co.za",
+    a:""
+  },
+  {
+    i:23,
+    n:"mr price sport",
+    c:"sports,fashion,business",
+    u:"https://www.mrpsport.com/",
+    d:"mr price sport is a south african ecommerce site selling affordable sporting goods, outdoor equipment and activewear for families and teams.[web:688]",
+    k:"mr price sport south africa,online sporting goods store,outdoor equipment and activewear,affordable sports gear ecommerce,family and team sports retailer",
+    a:""
+  },
+  {
+    i:24,
+    n:"travelstart south africa",
+    c:"travel,business",
+    u:"https://www.travelstart.co.za/",
+    d:"travelstart south africa is an online travel agency offering flight bookings, hotels and holiday packages for local and international trips.[web:700]",
+    k:"travelstart south africa,online flight bookings and hotels,holiday packages and travel deals,travel agency ecommerce platform,book trips from south africa",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy south africa",
+    c:"electronics,fashion,home,beauty,books,sports,automotive,business",
+    u:"https://www.ubuy.co.za/",
+    d:"ubuy south africa is an international shopping marketplace that lets customers order imported electronics, fashion, home and beauty products, books, sports gear and automotive items online.[web:688][web:702]",
+    k:"ubuy south africa,international online shopping south africa,imported electronics fashion and home goods,beauty books sports gear and automotive products,cross border ecommerce marketplace",
+    a:""
+  }
+];
