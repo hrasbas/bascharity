8ee135309b118415c72aae6c0d8ecdd51ec63c5d
hrasbas <hrasbasji@gmail.com>
2026-07-14 12:32:42 +0530

    Add list of online shops in Seychelles
    This file contains a list of online shops available in Seychelles, including their names, categories, URLs, descriptions, and keywords for search optimization.

---
+++ seychelles.js
@@ -0,0 +1,229 @@
+// seychelles.js
+window.countryShops = window.countryShops || {};
+window.countryShops["seychelles"] = [
+  {
+    i:1,
+    n:"ubuy seychelles",
+    c:"electronics,fashion,home,beauty,books,sports,automotive,business",
+    u:"https://www.ubuy.sc/en/",
+    d:"ubuy seychelles is an international online marketplace where customers in seychelles can shop for imported electronics, mobiles, fashion, beauty products, books, sports gear and automotive items with home delivery.[web:789][web:797]",
+    k:"ubuy seychelles,international online shopping seychelles,imported electronics mobiles and gadgets,fashion beauty home kitchen products,books sports gear and automotive items marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"seybuy marketplace",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.seybuy.com/",
+    d:"seybuy is a leading online marketplace in seychelles where users can buy and sell electronics, fashion, beauty products, groceries and home essentials with secure payment and delivery.[web:790][web:792]",
+    k:"seybuy seychelles,online marketplace seychelles,electronics fashion and beauty shopping,groceries and home essentials delivery,local ecommerce platform",
+    a:""
+  },
+  {
+    i:3,
+    n:"desertcart seychelles",
+    c:"electronics,fashion,beauty,home,books,sports,business",
+    u:"https://www.desertcart.sc/",
+    d:"desertcart seychelles is a cross border shopping site offering international products in health care, beauty, baby care, toys, sports, electronics, fashion and more for customers in seychelles.[web:793]",
+    k:"desertcart seychelles,international online shopping seychelles,health care beauty and baby products,toys sports electronics and fashion,cross border ecommerce platform",
+    a:""
+  },
+  {
+    i:4,
+    n:"shopdecor seychelles",
+    c:"home,business",
+    u:"https://sc.shopdecor.com/",
+    d:"shopdecor seychelles is an online home interior decor store offering designer furniture, decor pieces and stylish home accessories shipped to seychelles.[web:793][web:791]",
+    k:"shopdecor seychelles,online home decor store,furniture designer decor pieces,stylish home accessories ecommerce,interior design shopping seychelles",
+    a:""
+  },
+  {
+    i:5,
+    n:"kreol cars seychelles",
+    c:"automotive,business,travel",
+    u:"https://kreolcars-seychelles.com/",
+    d:"kreol cars seychelles is an online car rental service where visitors can reserve affordable vehicles like compact cars and suvs for travel around the seychelles islands.[web:793][web:791]",
+    k:"kreol cars seychelles,online car rental seychelles,affordable vehicles compact cars and suvs,island travel booking platform,automotive and travel ecommerce",
+    a:""
+  },
+  {
+    i:6,
+    n:"oceana fisheries",
+    c:"food,business",
+    u:"https://oceanafisheries.sc/",
+    d:"oceana fisheries is a seafood supplier in seychelles offering online ordering of fresh and frozen fish and seafood products for households and businesses.[web:791]",
+    k:"oceana fisheries seychelles,online seafood store seychelles,fresh and frozen fish and seafood,household and restaurant supply,ecommerce food supplier",
+    a:""
+  },
+  {
+    i:7,
+    n:"lavish imports",
+    c:"beauty,health,business",
+    u:"https://lavishimports.sc/",
+    d:"lavish imports is an online beauty and hair care store in seychelles providing imported hair products, skincare and wellness items for local customers.[web:791]",
+    k:"lavish imports seychelles,online beauty and hair care store,imported hair products and skincare,wellness items ecommerce,personal care shopping seychelles",
+    a:""
+  },
+  {
+    i:8,
+    n:"venture bay",
+    c:"electronics,home,fashion,business",
+    u:"https://www.venture-bay.com/",
+    d:"venture bay is a lifestyle and electronics store in seychelles with click and collect online ordering for tech gadgets, fashion accessories and home products.[web:795][web:791]",
+    k:"venture bay seychelles,click and collect online shopping,lifestyle electronics and gadgets,fashion accessories and home products,ecommerce store eden plaza",
+    a:""
+  },
+  {
+    i:9,
+    n:"ipanema seychelles",
+    c:"fashion,business",
+    u:"https://ipanema-seychelles.com/",
+    d:"ipanema seychelles is an online footwear shop offering brazilian beachwear inspired sandals and flip flops for women, men and kids in seychelles.[web:793]",
+    k:"ipanema seychelles,online sandals and flip flops store,beachwear inspired footwear for women men and kids,brazilian style shoes ecommerce,summer fashion shopping seychelles",
+    a:""
+  },
+  {
+    i:10,
+    n:"pigervita",
+    c:"fashion,business",
+    u:"https://pigervita.com/",
+    d:"pigervita is an accessories focused online store in seychelles selling clothing accessories, bags and fashion items through its ecommerce site.[web:793][web:791]",
+    k:"pigervita seychelles,online clothing accessories store,bags and fashion items ecommerce,style shopping platform seychelles,fashion accessories retailer",
+    a:""
+  },
+  {
+    i:11,
+    n:"abler",
+    c:"health,business",
+    u:"https://abler.com/",
+    d:"abler supplies equine health products and pet supplies online from seychelles, including ulcer treatments, dewormers and probiotics for horses.[web:793][web:791]",
+    k:"abler seychelles,online equine health products,ulcer treatments dewormers and probiotics,pet supplies ecommerce platform,horse medication shopping",
+    a:""
+  },
+  {
+    i:12,
+    n:"smart stb store",
+    c:"electronics,business",
+    u:"https://smart-stb.com/",
+    d:"smart stb operates from seychelles as an online store selling smart iptv set top box software licences and apps for smart tv devices.[web:791]",
+    k:"smart stb seychelles,online iptv set top box store,software licences and apps for smart tv,streaming electronics ecommerce,global tech shop",
+    a:""
+  },
+  {
+    i:13,
+    n:"asia coin",
+    c:"software,business",
+    u:"https://asiacoin.sc/",
+    d:"asia coin is a digital currency project with an online platform from seychelles offering token purchase and related digital services for investors.[web:791]",
+    k:"asia coin seychelles,online digital currency platform,token purchase and blockchain services,crypto investing ecommerce,software based finance site",
+    a:""
+  },
+  {
+    i:14,
+    n:"master shopping",
+    c:"electronics,home,fashion,business",
+    u:"https://mastershopping.sc/",
+    d:"master shopping is an online store in seychelles offering mixed categories such as electronics, home items and fashion goods for local buyers.[web:791]",
+    k:"master shopping seychelles,online mixed category store,electronics home items and fashion goods,local ecommerce shopping platform,general merchandise site",
+    a:""
+  },
+  {
+    i:15,
+    n:"venture bay click & collect",
+    c:"home,fashion,electronics,business",
+    u:"https://www.venture-bay.com/click-collect/",
+    d:"venture bay click & collect service allows customers in seychelles to reserve fashion, home and electronics items online and collect them in store with convenient parking.[web:795]",
+    k:"venture bay click and collect seychelles,online reservation and in store collection,fashion home and electronics products,eden plaza shopping convenience,ecommerce plus retail service",
+    a:""
+  },
+  {
+    i:16,
+    n:"offshore.sc",
+    c:"business,software",
+    u:"https://offshore.sc/",
+    d:"offshore.sc is a seychelles based online business services platform offering company formation, corporate support and digital tools for offshore businesses.[web:791]",
+    k:"offshore.sc seychelles,online business services platform,company formation and corporate support,digital tools for offshore businesses,b2b ecommerce services",
+    a:""
+  },
+  {
+    i:17,
+    n:"capital trading seychelles",
+    c:"food,business",
+    u:"https://capitaltrading.sc/",
+    d:"capital trading seychelles is a large fmcg distributor with an online wholesale ordering portal for branded food and consumer products in seychelles.[web:793]",
+    k:"capital trading seychelles,online fmcg distributor,wholesale food and consumer products,branded goods ordering portal,business to business ecommerce",
+    a:""
+  },
+  {
+    i:18,
+    n:"the island gallery seychelles",
+    c:"home,fashion,business",
+    u:"https://islandgallery.sc/",
+    d:"the island gallery seychelles is an online boutique showcasing art, decor and island inspired fashion accessories for customers and collectors.[web:791]",
+    k:"the island gallery seychelles,online art and decor boutique,island inspired fashion accessories,home style and gifts ecommerce,creative shopping platform",
+    a:""
+  },
+  {
+    i:19,
+    n:"om vapor seychelles",
+    c:"health,business",
+    u:"https://omvapor.sc/",
+    d:"om vapor seychelles is an online vape and wellness store selling e liquids, vape devices and related accessories for adult customers.[web:791]",
+    k:"om vapor seychelles,online vape store seychelles,e liquids devices and accessories,adult wellness ecommerce,smoking alternative products",
+    a:""
+  },
+  {
+    i:20,
+    n:"small shop via ubuy",
+    c:"home,electronics,fashion,business",
+    u:"https://www.ubuy.sc/en/brand/small-shop",
+    d:"small shop products on ubuy seychelles provide a range of home goods, electronics and fashion accessories shipped to major areas across seychelles.[web:794][web:789]",
+    k:"small shop ubuy seychelles,online home goods and electronics,fashion accessories shipped nationwide,brand collection on ubuy marketplace,seychelles online shopping",
+    a:""
+  },
+  {
+    i:21,
+    n:"aliexpress seychelles users",
+    c:"electronics,fashion,home,beauty,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress is a global marketplace heavily accessed from seychelles for low cost electronics, fashion, home items and beauty products shipped from overseas sellers.[web:796]",
+    k:"aliexpress seychelles,cross border shopping seychelles,cheap electronics gadgets and accessories,fashion home goods and beauty items,international ecommerce marketplace",
+    a:""
+  },
+  {
+    i:22,
+    n:"ebay global for seychelles",
+    c:"electronics,fashion,home,books,business",
+    u:"https://www.ebay.com/",
+    d:"ebay is a global auction and marketplace site used by shoppers in seychelles to buy electronics, fashion, home goods and books from international sellers.[web:796]",
+    k:"ebay seychelles,online auctions and marketplace,electronics fashion home goods and books,internet shopping from international sellers,cross border ecommerce platform",
+    a:""
+  },
+  {
+    i:23,
+    n:"microless international",
+    c:"electronics,business",
+    u:"https://www.microless.com/",
+    d:"microless is an international online computer and electronics retailer that ships laptops, pc components and gadgets to seychelles customers.[web:796]",
+    k:"microless seychelles,online computer and electronics store,laptops pc components and gadgets,cross border tech ecommerce,international delivery to seychelles",
+    a:""
+  },
+  {
+    i:24,
+    n:"ravelry patterns for seychelles",
+    c:"home,books,business",
+    u:"https://www.ravelry.com/",
+    d:"ravelry is a global knitting and crochet community site where users in seychelles can buy digital patterns and craft books online.[web:796]",
+    k:"ravelry seychelles,online knitting and crochet patterns,digital craft books marketplace,home craft and hobby ecommerce,global creative community shopping",
+    a:""
+  },
+  {
+    i:25,
+    n:"venture bay online shop",
+    c:"fashion,home,electronics,business",
+    u:"https://www.venture-bay.com/shop/",
+    d:"venture bay online shop offers a curated selection of fashion, homeware and electronics for seychelles customers with online payment and collection options.[web:795][web:791]",
+    k:"venture bay online shop seychelles,fashion homeware and electronics,curated lifestyle products ecommerce,online payment and collection options,seychelles retail webstore",
+    a:""
+  }
+];
