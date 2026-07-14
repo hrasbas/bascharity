4302c385d4784ced212b32457d5b9faed48c627c
hrasbas <hrasbasji@gmail.com>
2026-07-14 17:44:55 +0530

    Add online shops data for Ireland
    This file contains a list of online shops available in Ireland, including their categories, URLs, descriptions, and keywords for better searchability.

---
+++ ireland.js
@@ -0,0 +1,229 @@
+// ireland.js
+window.countryShops = window.countryShops || {};
+window.countryShops["ireland"] = [
+  {
+    i:1,
+    n:"amazon ireland",
+    c:"electronics,home,fashion,beauty,books,sports,business",
+    u:"https://www.amazon.ie/",
+    d:"amazon ireland is a major online marketplace where customers can buy electronics, books, sports equipment, fashion, home and garden products and more with low prices and fast delivery across ireland.[web:1494][web:1500]",
+    k:"amazon ireland,online shopping ireland,electronics books sports equipment,fashion home and garden products,amazon.ie ecommerce marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy ireland",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.ie/en/",
+    d:"ubuy ireland is an international online shopping store bringing imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and daily essentials to shoppers in ireland.[web:1492][web:1499]",
+    k:"ubuy ireland,international online shopping ireland,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books sports gear automotive accessories daily essentials,cross border ecommerce platform ireland",
+    a:""
+  },
+  {
+    i:3,
+    n:"very ireland",
+    c:"fashion,electronics,home,business",
+    u:"https://www.very.ie/",
+    d:"very ireland is one of the country's largest online department stores offering womens, mens and kids fashion along with electronics and furniture with fast delivery and free returns.[web:1493][web:1500]",
+    k:"very ireland,online department store ireland,womens mens kids fashion,electronics and furniture fast delivery free returns,very.ie ecommerce site",
+    a:""
+  },
+  {
+    i:4,
+    n:"next ireland",
+    c:"fashion,beauty,home,business",
+    u:"https://www.next.ie/en",
+    d:"next ireland is an official fashion and homeware site selling women's, men's and children's clothing plus homeware, beauty and lifestyle products with next day delivery and free returns.[web:1497]",
+    k:"next ireland,online fashion and homeware,womens mens childrens clothing,homeware beauty lifestyle products,next.ie next day delivery free returns",
+    a:""
+  },
+  {
+    i:5,
+    n:"dunnes stores online",
+    c:"food,fashion,home,business",
+    u:"https://www.dunnesstores.com/",
+    d:"dunnes stores online is a leading irish supermarket and fashion retailer site offering groceries, clothing, homeware and household products for delivery and collection.[web:1496][web:1500][web:1501]",
+    k:"dunnes stores online ireland,supermarket and fashion retailer site,groceries clothing homeware household products,delivery and collection shopping,dunnesstores.com ecommerce",
+    a:""
+  },
+  {
+    i:6,
+    n:"tesco ireland online",
+    c:"food,home,business",
+    u:"https://www.tesco.ie/groceries/",
+    d:"tesco ireland online groceries lets customers order food, drinks and everyday household items from tesco supermarkets with time slot delivery.[web:1496][web:1500]",
+    k:"tesco ireland online groceries,food drinks household items delivery,tesco supermarkets ireland,time slot online shopping tesco.ie",
+    a:""
+  },
+  {
+    i:7,
+    n:"aldi ireland online shop",
+    c:"food,home,business",
+    u:"https://www.aldi.ie/",
+    d:"aldi ireland online shop provides limited specialbuys, wine and selected grocery products that can be ordered online or checked before visiting stores.[web:1498][web:1500]",
+    k:"aldi ireland online shop,specialbuys wine selected grocery products,check offers and order online,aldi.ie discount supermarket ireland",
+    a:""
+  },
+  {
+    i:8,
+    n:"lidl ireland online",
+    c:"food,home,business",
+    u:"https://www.lidl.ie/",
+    d:"lidl ireland online highlights weekly offers and some non food items while linking to grocery ordering apps, helping shoppers plan supermarket purchases.[web:1498][web:1500]",
+    k:"lidl ireland online,weekly offers and non food items,grocery ordering apps support,plan supermarket purchases,lidl.ie ireland shopping",
+    a:""
+  },
+  {
+    i:9,
+    n:"currys ireland",
+    c:"electronics,home,business",
+    u:"https://www.currys.ie/",
+    d:"currys ireland is a major electronics and appliance retailer offering online shopping for tvs, laptops, phones, kitchen appliances and tech accessories.[web:1496][web:1498][web:1500]",
+    k:"currys ireland,online electronics and appliance retailer,tvs laptops phones kitchen appliances tech accessories,currys.ie ecommerce ireland",
+    a:""
+  },
+  {
+    i:10,
+    n:"harvey norman ireland",
+    c:"electronics,home,business",
+    u:"https://www.harveynorman.ie/",
+    d:"harvey norman ireland is a large online store selling furniture, bedding, electricals, computers and home entertainment products nationwide.[web:1496][web:1501]",
+    k:"harvey norman ireland,furniture bedding electricals computers home entertainment,nationwide online shopping,harveynorman.ie ireland ecommerce",
+    a:""
+  },
+  {
+    i:11,
+    n:"did electrical ireland",
+    c:"electronics,home,business",
+    u:"https://www.did.ie/",
+    d:"did electrical ireland is an irish chain offering online sales of household appliances, tvs, audio systems and small electricals with delivery and installation options.[web:1496][web:1498][web:1501]",
+    k:"did electrical ireland,online household appliances tvs audio systems small electricals,delivery and installation options,did.ie electronics shopping",
+    a:""
+  },
+  {
+    i:12,
+    n:"littlewoods ireland",
+    c:"fashion,home,electronics,business",
+    u:"https://www.littlewoodsireland.ie/",
+    d:"littlewoods ireland is an online department store that sells fashion, footwear, homeware, electricals and gifts with flexible payment options.[web:1496][web:1498][web:1500]",
+    k:"littlewoods ireland,online department store,fashion footwear homeware electricals gifts,flexible payment options,littlewoodsireland.ie ecommerce",
+    a:""
+  },
+  {
+    i:13,
+    n:"asos ireland",
+    c:"fashion,beauty,business",
+    u:"https://www.asos.com/ie/",
+    d:"asos ireland storefront offers global fashion with clothing, shoes and accessories for young men and women plus beauty brands delivered to ireland.[web:1496][web:1498][web:1500]",
+    k:"asos ireland,global fashion storefront,clothing shoes accessories for young men and women,beauty brands delivered to ireland,asos.com ie online shopping",
+    a:""
+  },
+  {
+    i:14,
+    n:"marks & spencer ireland",
+    c:"fashion,food,home,business",
+    u:"https://www.marksandspencer.com/ie/",
+    d:"marks & spencer ireland online store sells clothing, lingerie, food and homeware with click and collect and delivery services.[web:1496][web:1498]",
+    k:"marks & spencer ireland online,clothing lingerie food homeware,click and collect and delivery services,m&s ireland ecommerce site",
+    a:""
+  },
+  {
+    i:15,
+    n:"next directory ireland app",
+    c:"software,business",
+    u:"https://play.google.com/store/apps/details?id=com.allinone.shopping.all.ireland",
+    d:"online shopping in ireland app acts as a directory letting users access next, amazon.ie, dunnes, tesco, asos, currys and more stores from one place on android.[web:1496]",
+    k:"online shopping in ireland app,directory of irish and international stores,next amazon dunnes tesco asos currys access,android shopping app ireland",
+    a:""
+  },
+  {
+    i:16,
+    n:"online shopping ireland stores app",
+    c:"software,business",
+    u:"https://play.google.com/store/apps/details?id=com.online.shoppingireland.irelandshopping&hl=en_IE",
+    d:"online shopping ireland stores app bundles links to irish retailers such as loveshopping, currys, littlewoods, very, dunnes and ubuy for easy browsing.[web:1498]",
+    k:"online shopping ireland stores app,collection of irish retailer links,loveshopping currys littlewoods very dunnes ubuy access,one stop shopping app ireland",
+    a:""
+  },
+  {
+    i:17,
+    n:"etsy ireland access",
+    c:"fashion,home,jewellery,business",
+    u:"https://www.etsy.com/ie/",
+    d:"etsy ireland access connects local and global makers with irish buyers looking for handmade jewellery, clothing, art and home decor items.[web:1500][web:1501]",
+    k:"etsy ireland access,handmade jewellery clothing art home decor,local and global makers marketplace,etsy.com ie creative shopping ireland",
+    a:""
+  },
+  {
+    i:18,
+    n:"aliExpress ireland shoppers",
+    c:"electronics,fashion,home,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress is a global marketplace widely used by ireland shoppers to buy low cost electronics, fashion, home goods and accessories directly from overseas sellers.[web:1500]",
+    k:"aliexpress ireland shoppers,global marketplace low cost products,electronics fashion home goods accessories,overseas seller direct buying",
+    a:""
+  },
+  {
+    i:19,
+    n:"adverts.ie",
+    c:"electronics,home,automotive,fashion,business",
+    u:"https://www.adverts.ie/",
+    d:"adverts.ie is an irish classifieds and marketplace site where users buy and sell electronics, furniture, cars, clothing and other items through listings.[web:1498][web:1501]",
+    k:"adverts.ie ireland,classifieds and marketplace site,buy and sell electronics furniture cars clothing,listings based online trading ireland",
+    a:""
+  },
+  {
+    i:20,
+    n:"supervalu online",
+    c:"food,home,business",
+    u:"https://supervalu.ie/",
+    d:"supervalu online is a supermarket site in ireland offering grocery shopping, local produce and household items with delivery and click and collect.[web:1498][web:1501]",
+    k:"supervalu online ireland,supermarket grocery shopping site,local produce and household items,delivery and click and collect,supervalu.ie ecommerce",
+    a:""
+  },
+  {
+    i:21,
+    n:"john lewis ireland delivery",
+    c:"fashion,home,beauty,business",
+    u:"https://www.johnlewis.com/",
+    d:"john lewis offers delivery to ireland from its uk online store with fashion, homeware, electronics and beauty products.[web:1498][web:1501]",
+    k:"john lewis ireland delivery,uk online store shipping to ireland,fashion homeware electronics beauty products,johnlewis.com cross border shopping",
+    a:""
+  },
+  {
+    i:22,
+    n:"mandmdirect ireland",
+    c:"fashion,business",
+    u:"https://www.mandmdirect.ie/",
+    d:"mandmdirect ireland is a discount fashion ecommerce site selling branded sportswear, casual clothing and footwear for men, women and kids.[web:1498]",
+    k:"mandmdirect ireland,discount fashion ecommerce,branded sportswear casual clothing footwear,men women kids outlet shopping,mandmdirect.ie store",
+    a:""
+  },
+  {
+    i:23,
+    n:"lookfantastic ireland",
+    c:"beauty,health,business",
+    u:"https://www.lookfantastic.com/",
+    d:"lookfantastic ireland storefront offers international beauty and skincare brands, haircare and makeup with delivery to ireland and regular offers.[web:1498][web:1500]",
+    k:"lookfantastic ireland,international beauty and skincare brands,haircare and makeup delivery to ireland,online cosmetic shopping lookfantastic",
+    a:""
+  },
+  {
+    i:24,
+    n:"ubuy ireland ubay brand store",
+    c:"electronics,home,business",
+    u:"https://www.ubuy.ie/en/brand/ubay",
+    d:"ubuy ireland ubay brand store features selected ubay products shipped from international warehouses to cities like dublin, cork, limerick and galway.[web:1495]",
+    k:"ubuy ireland ubay brand store,selected ubay products from international warehouses,shipping to dublin cork limerick galway,ubay products on ubuy ireland",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy ireland curated chemical collection",
+    c:"business",
+    u:"https://www.ubuy.ie/en/",
+    d:"ubuy ireland curated chemical collection sells laboratory grade chemicals such as anhydrous acetone and specialist supplies to irish buyers online.[web:1499]",
+    k:"ubuy ireland curated chemical collection,laboratory grade chemicals anhydrous acetone,specialist scientific supplies online ireland,ubuy.ie business shopping",
+    a:""
+  }
+];
