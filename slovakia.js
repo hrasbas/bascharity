4d034b663ee8a9dc9986b4010bcc99abb1bd723b
hrasbas <hrasbasji@gmail.com>
2026-07-14 12:36:15 +0530

    Add online shops data for Slovakia
    Added a list of online shops in Slovakia with details including name, category, URL, description, and keywords.

---
+++ slovakia.js
@@ -0,0 +1,229 @@
+// slovakia.js
+window.countryShops = window.countryShops || {};
+window.countryShops["slovakia"] = [
+  {
+    i:1,
+    n:"alza.sk",
+    c:"electronics,home,business",
+    u:"https://www.alza.sk/",
+    d:"alza.sk is a major online electronics and general merchandise retailer in slovakia offering computers, mobiles, tvs, home appliances, toys and office supplies with fast delivery.[web:758][web:763]",
+    k:"alza.sk slovakia,online electronics store slovakia,computers mobiles tvs and gadgets,home appliances toys and office supplies,large ecommerce marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"mall.sk",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.mall.sk/",
+    d:"mall.sk is a popular online marketplace in slovakia where customers can shop for electronics, home goods, fashion, beauty, toys and sporting products in one place.[web:751][web:758]",
+    k:"mall.sk slovakia,online shopping slovakia,electronics home goods and fashion,beauty toys and sports products,multi category ecommerce marketplace",
+    a:""
+  },
+  {
+    i:3,
+    n:"kaufland.sk online",
+    c:"food,home,business",
+    u:"https://www.kaufland.sk/online.html",
+    d:"kaufland.sk online shop lets slovakia customers order groceries, fresh food, drinks and household essentials from kaufland hypermarkets with delivery or pickup.[web:759][web:763]",
+    k:"kaufland.sk online,online supermarket slovakia,groceries fresh food and drinks,household essentials delivery or pickup,hypermarket ecommerce store",
+    a:""
+  },
+  {
+    i:4,
+    n:"nay",
+    c:"electronics,home,business",
+    u:"https://www.nay.sk/",
+    d:"nay is a leading slovak electronics retailer with an online store for tvs, laptops, smartphones, small appliances and large home appliances.[web:753][web:760]",
+    k:"nay slovakia,online electronics retailer,tvs laptops smartphones and gadgets,small and large home appliances,ecommerce tech shop slovakia",
+    a:""
+  },
+  {
+    i:5,
+    n:"okay.sk",
+    c:"electronics,home,business",
+    u:"https://www.okay.sk/",
+    d:"okay.sk focuses on online sales of electronics and home appliances in slovakia including tvs, audio, white goods and furniture for living rooms.[web:751][web:758]",
+    k:"okay.sk slovakia,online electronics and appliances store,tvs audio and white goods,furniture for living rooms,ecommerce home and tech retailer",
+    a:""
+  },
+  {
+    i:6,
+    n:"bazos.sk",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.bazos.sk/",
+    d:"bazos.sk is a top classifieds marketplace in slovakia where users buy and sell used and new items such as electronics, furniture, cars, clothing and household goods online.[web:752][web:758]",
+    k:"bazos.sk slovakia,online classifieds marketplace,electronics furniture cars and clothing,buy and sell used and new items,community ecommerce platform",
+    a:""
+  },
+  {
+    i:7,
+    n:"modivo.sk",
+    c:"fashion,business",
+    u:"https://www.modivo.sk/",
+    d:"modivo.sk is a fashion and lifestyle ecommerce site in slovakia offering branded clothing, shoes and accessories for men, women and children.[web:758][web:752]",
+    k:"modivo.sk slovakia,online fashion and lifestyle store,branded clothing shoes and accessories,men women and children apparel,ecommerce fashion platform",
+    a:""
+  },
+  {
+    i:8,
+    n:"martinus.sk",
+    c:"books,business",
+    u:"https://www.martinus.sk/",
+    d:"martinus.sk is a leading online bookstore in slovakia selling slovak and international books, ebooks, audiobooks and gifts for readers of all ages.[web:758][web:753]",
+    k:"martinus.sk slovakia,online bookstore slovakia,slovak and international books,ebooks audiobooks and gifts,reading and education ecommerce",
+    a:""
+  },
+  {
+    i:9,
+    n:"drmax.sk",
+    c:"health,business",
+    u:"https://www.drmax.sk/",
+    d:"drmax.sk is an online pharmacy in slovakia offering prescription medicines, over the counter products, vitamins, cosmetics and health accessories.[web:758][web:763]",
+    k:"drmax.sk slovakia,online pharmacy medicines and vitamins,over the counter health products,cosmetics and health accessories,ecommerce drugstore slovakia",
+    a:""
+  },
+  {
+    i:10,
+    n:"tesco online slovakia",
+    c:"food,home,business",
+    u:"https://nakup.itesco.sk/groceries/sk-SK",
+    d:"tesco online slovakia lets customers order groceries, fresh produce, bakery, meat and household goods from tesco stores for home delivery or collection.[web:753]",
+    k:"tesco online slovakia,online groceries shopping,fruit vegetables meat and bakery,household goods delivery or pickup,supermarket ecommerce platform",
+    a:""
+  },
+  {
+    i:11,
+    n:"4home.sk",
+    c:"home,business",
+    u:"https://www.4home.sk/",
+    d:"4home.sk is an online home and decor store in slovakia offering bedding, kitchenware, decorative items and small furniture at affordable prices.[web:753][web:758]",
+    k:"4home.sk slovakia,online home and decor store,bedding kitchenware and decorative items,small furniture ecommerce,affordable home accessories shop",
+    a:""
+  },
+  {
+    i:12,
+    n:"hej.sk",
+    c:"electronics,home,business",
+    u:"https://www.hej.sk/",
+    d:"hej.sk is an online retailer in slovakia specialising in consumer electronics, computers, mobile phones and home appliances with nationwide delivery.[web:753]",
+    k:"hej.sk slovakia,online consumer electronics store,computers mobile phones and gadgets,home appliances ecommerce,nationwide delivery tech retailer",
+    a:""
+  },
+  {
+    i:13,
+    n:"sportisimo.sk",
+    c:"sports,fashion,business",
+    u:"https://www.sportisimo.sk/",
+    d:"sportisimo.sk is a sports and outdoor retailer in slovakia providing online shopping for sportswear, footwear, fitness equipment and gear for many activities.[web:758]",
+    k:"sportisimo.sk slovakia,online sportswear and footwear store,fitness equipment and outdoor gear,multi sport ecommerce retailer,sports shopping slovakia",
+    a:""
+  },
+  {
+    i:14,
+    n:"mtbiker.sk",
+    c:"sports,automotive,business",
+    u:"https://www.mtbiker.sk/shop",
+    d:"mtbiker.sk shop is a specialised online store in slovakia for mountain bikes, cycling parts, accessories and clothing aimed at cycling enthusiasts.[web:758]",
+    k:"mtbiker.sk shop slovakia,online mountain bike store,cycling parts accessories and clothing,enthusiast and pro cyclist ecommerce,sports bike retailer",
+    a:""
+  },
+  {
+    i:15,
+    n:"belenka",
+    c:"fashion,business",
+    u:"https://www.belenka.sk/",
+    d:"belenka is a slovak barefoot footwear brand selling shoes, sandals and kids shoes online to local and international customers.[web:758]",
+    k:"belenka slovakia,online barefoot footwear store,shoes sandals and kids shoes,natural walking style ecommerce,slovak fashion brand",
+    a:""
+  },
+  {
+    i:16,
+    n:"sos electronic",
+    c:"electronics,business",
+    u:"https://www.sos.sk/",
+    d:"sos electronic is a b2b and retail electronics distributor in slovakia with an online shop for electronic components, measuring instruments and industrial equipment.[web:758]",
+    k:"sos electronic slovakia,online electronic components store,measuring instruments and industrial equipment,b2b and retail electronics ecommerce,tech supplies shop",
+    a:""
+  },
+  {
+    i:17,
+    n:"eshop.sk",
+    c:"electronics,home,business",
+    u:"https://www.eshop.sk/",
+    d:"eshop.sk is a general online store in slovakia offering consumer electronics, computer accessories, office supplies and home products in one marketplace.[web:754]",
+    k:"eshop.sk slovakia,general online store slovakia,consumer electronics and computer accessories,office supplies and home products,ecommerce marketplace",
+    a:""
+  },
+  {
+    i:18,
+    n:"slovakcrafts.sk",
+    c:"home,fashion,business",
+    u:"https://www.slovakcrafts.sk/",
+    d:"slovakcrafts.sk supports local artisans by selling handmade crafts, jewellery, clothing and home decor items online to customers in slovakia and abroad.[web:754]",
+    k:"slovakcrafts.sk slovakia,online handmade crafts store,local artisan jewellery and clothing,home decor items ecommerce,support slovak creators marketplace",
+    a:""
+  },
+  {
+    i:19,
+    n:"shooos.sk",
+    c:"fashion,business",
+    u:"https://www.shooos.sk/",
+    d:"shooos.sk is a sneaker and streetwear focused online shop in slovakia offering branded shoes, apparel and accessories for urban style lovers.[web:758]",
+    k:"shooos.sk slovakia,online sneaker and streetwear store,branded shoes apparel and accessories,urban fashion ecommerce,style shop for youth",
+    a:""
+  },
+  {
+    i:20,
+    n:"moebelix.sk",
+    c:"home,business",
+    u:"https://www.moebelix.sk/",
+    d:"moebelix.sk is a furniture and home accessories online store in slovakia where customers can buy sofas, beds, storage, decor and kitchen furnishings.[web:758]",
+    k:"moebelix.sk slovakia,online furniture store,sofas beds storage and decor,kitchen furnishings ecommerce,affordable home interior shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"falco holsters",
+    c:"fashion,automotive,business",
+    u:"https://www.falcoholsters.com/sk",
+    d:"falco holsters is a slovak manufacturer selling gun holsters, belts and tactical gear online for civilian and professional users worldwide and locally.[web:758]",
+    k:"falco holsters slovakia,online gun holsters store,belts and tactical gear ecommerce,civilian and professional equipment,slovak leather products brand",
+    a:""
+  },
+  {
+    i:22,
+    n:"gymbeam.sk",
+    c:"sports,health,business",
+    u:"https://gymbeam.sk/",
+    d:"gymbeam.sk is an online sports nutrition shop in slovakia offering protein powders, supplements, fitness food and workout accessories for active customers.[web:760]",
+    k:"gymbeam.sk slovakia,online sports nutrition store,protein powders supplements and fitness food,workout accessories ecommerce,health and fitness shop",
+    a:""
+  },
+  {
+    i:23,
+    n:"lidl shop online slovakia",
+    c:"food,home,business",
+    u:"https://www.lidl.sk/c/online-shop/s10012458",
+    d:"lidl online shop in slovakia sells selected groceries, household goods, clothing and non food specials that shoppers can order online.[web:760][web:763]",
+    k:"lidl online shop slovakia,discount groceries and household goods,online clothing and non food specials,supermarket ecommerce platform,budget shopping site",
+    a:""
+  },
+  {
+    i:24,
+    n:"venuspuzzle",
+    c:"books,home,business",
+    u:"https://www.venuspuzzle.com/sk",
+    d:"venuspuzzle is an online shop in slovakia specialising in custom photo puzzles and jigsaw sets for gifts and family entertainment.[web:758]",
+    k:"venuspuzzle slovakia,online custom photo puzzles,jigsaw sets for gifts and family fun,personalised puzzle ecommerce,creative home entertainment shop",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy slovakia",
+    c:"electronics,fashion,home,beauty,books,sports,automotive,business",
+    u:"https://www.ubuy.sk/en/",
+    d:"ubuy slovakia is an international online marketplace where shoppers can import electronics, fashion, home goods, beauty products, books, sports gear and automotive items from global brands.[web:754][web:764]",
+    k:"ubuy slovakia,international online shopping slovakia,imported electronics mobiles and gadgets,fashion home goods and beauty products,books sports gear and automotive items marketplace",
+    a:""
+  }
+];
