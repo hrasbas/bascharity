2183ff548b659bf3632df57fb59891c436db7db5
hrasbas <hrasbasji@gmail.com>
2026-07-14 17:46:57 +0530

    Add list of online shops in Italy
    This file contains a list of online shops in Italy, including their names, categories, URLs, descriptions, keywords, and additional attributes.

---
+++ italy.js
@@ -0,0 +1,229 @@
+// italy.js
+window.countryShops = window.countryShops || {};
+window.countryShops["italy"] = [
+  {
+    i:1,
+    n:"amazon italy",
+    c:"electronics,home,fashion,beauty,food,sports,books,business",
+    u:"https://www.amazon.it/",
+    d:"amazon italy is a leading online marketplace where italian shoppers can buy electronics, books, fashion, music, dvds, videogames, appliances, groceries and many other products with fast delivery and prime benefits.[web:1475][web:1476][web:1479]",
+    k:"amazon italy,online shopping italy,electronics books fashion music dvds videogames,appliances groceries and more,amazon.it ecommerce marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy italy",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.co.it/en/",
+    d:"ubuy italy is an international online shopping store where customers can buy imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and gourmet food at low prices in italy.[web:1472][web:1473][web:1480]",
+    k:"ubuy italy,international online shopping italy,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books sports gear automotive accessories gourmet food,cross border ecommerce platform italy",
+    a:""
+  },
+  {
+    i:3,
+    n:"ebay italy",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.ebay.it/",
+    d:"ebay italy is a marketplace where users buy and sell electronics, fashion, collectibles, automotive parts and home goods via auctions and fixed price listings.[web:1476][web:1479]",
+    k:"ebay italy,marketplace for buying and selling,electronics fashion collectibles automotive parts home goods,auctions and fixed price listings,ebay.it online shopping italy",
+    a:""
+  },
+  {
+    i:4,
+    n:"zalando italy",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.it/",
+    d:"zalando italy is an online fashion store offering clothing, shoes and accessories for women, men and kids along with beauty products and free returns.[web:1477][web:1476]",
+    k:"zalando italy,online fashion store clothing shoes accessories,women men kids styles,beauty products and free returns,zalando.it ecommerce italy",
+    a:""
+  },
+  {
+    i:5,
+    n:"mediaworld",
+    c:"electronics,home,business",
+    u:"https://www.mediaworld.it/",
+    d:"mediaworld is a major electronics and appliance retailer in italy with an online store for tvs, smartphones, laptops, audio systems and household appliances.[web:1476][web:1479]",
+    k:"mediaworld italy,electronics and appliance retailer,tvs smartphones laptops audio systems,household appliances online shopping,mediaworld.it ecommerce",
+    a:""
+  },
+  {
+    i:6,
+    n:"unieuro",
+    c:"electronics,home,business",
+    u:"https://www.unieuro.it/",
+    d:"unieuro is an italian electronics chain offering online sales of consumer electronics, computers, phones and home appliances with store pickup and delivery.[web:1476][web:1479]",
+    k:"unieuro italy,electronics chain online sales,consumer electronics computers phones home appliances,store pickup and delivery,unieuro.it shopping",
+    a:""
+  },
+  {
+    i:7,
+    n:"euronics italy",
+    c:"electronics,home,business",
+    u:"https://www.euronics.it/",
+    d:"euronics italy is an electronics and appliance retailer providing online shopping for tvs, audio, smartphones, computers and kitchen appliances.[web:1476][web:1479]",
+    k:"euronics italy,electronics and appliance retailer,tvs audio smartphones computers kitchen appliances,euronics.it ecommerce italy",
+    a:""
+  },
+  {
+    i:8,
+    n:"giglio.com",
+    c:"fashion,business",
+    u:"https://www.giglio.com/en-us/",
+    d:"giglio.com is an italian luxury fashion ecommerce site offering designer clothing, shoes and accessories from hundreds of italian and international brands with worldwide shipping.[web:1474]",
+    k:"giglio.com italy,luxury fashion ecommerce site,designer clothing shoes accessories,italian and international brands worldwide shipping,giglio online boutique",
+    a:""
+  },
+  {
+    i:9,
+    n:"yoox",
+    c:"fashion,business",
+    u:"https://www.yoox.com/it",
+    d:"yoox is an online fashion and lifestyle store in italy offering designer brands, outlet deals and curated collections of clothing, shoes and accessories.[web:1476][web:1479]",
+    k:"yoox italy,online fashion and lifestyle store,designer brands outlet deals curated collections,clothing shoes accessories,yoox.com ecommerce",
+    a:""
+  },
+  {
+    i:10,
+    n:"OVS online shop",
+    c:"fashion,business",
+    u:"https://www.ovs.it/",
+    d:"ovs online shop is an italian clothing retailer providing affordable fashion for women, men and children with nationwide delivery.[web:1476]",
+    k:"ovs online shop italy,affordable fashion retailer,women men children clothing,nationwide delivery ovs.it,ecommerce fashion italy",
+    a:""
+  },
+  {
+    i:11,
+    n:"coin.it",
+    c:"fashion,beauty,home,business",
+    u:"https://shop.coin.it/",
+    d:"coin.it is the online store of coin department stores in italy selling fashion, beauty products and home decor items.[web:1476]",
+    k:"coin.it italy,department store online fashion beauty home decor,coin shop ecommerce,national brand shopping italy",
+    a:""
+  },
+  {
+    i:12,
+    n:"esselunga a casa",
+    c:"food,home,business",
+    u:"https://www.esselunga.it/",
+    d:"esselunga a casa is a grocery delivery service in italy where customers order food, drinks and household products from esselunga supermarkets online.[web:1476][web:1479]",
+    k:"esselunga a casa italy,grocery delivery service,food drinks household products from esselunga,online supermarket shopping,esselunga.it ecommerce",
+    a:""
+  },
+  {
+    i:13,
+    n:"coop online store italy",
+    c:"food,home,business",
+    u:"https://www.cooponline.it/",
+    d:"coop online store italy allows members and shoppers to buy groceries, personal care and household items from coop supermarkets with home delivery.[web:1476]",
+    k:"coop online store italy,groceries personal care household items,coop supermarkets home delivery,cooponline.it food ecommerce italy",
+    a:""
+  },
+  {
+    i:14,
+    n:"carrefour italy online",
+    c:"food,electronics,home,fashion,business",
+    u:"https://www.carrefour.it/",
+    d:"carrefour italy online is a supermarket and hypermarket ecommerce site offering groceries, household goods, electronics and clothing with click and collect.[web:1476]",
+    k:"carrefour italy online,supermarket and hypermarket ecommerce,groceries household goods electronics clothing,click and collect and delivery,carrefour.it shopping",
+    a:""
+  },
+  {
+    i:15,
+    n:"IKEA italy online",
+    c:"home,business",
+    u:"https://www.ikea.com/it/it/",
+    d:"ikea italy online store sells furniture, kitchenware and home accessories for italian homes with online planning tools and delivery options.[web:1476]",
+    k:"ikea italy online,furniture kitchenware home accessories,online planning tools and delivery,ikea.com it ecommerce italy",
+    a:""
+  },
+  {
+    i:16,
+    n:"leroy merlin italy",
+    c:"home,business",
+    u:"https://www.leroymerlin.it/",
+    d:"leroy merlin italy online shop offers diy tools, building materials, garden supplies and home improvement products for consumers and professionals.[web:1476]",
+    k:"leroy merlin italy online,diy tools building materials garden supplies,home improvement products for consumers professionals,leroymerlin.it ecommerce",
+    a:""
+  },
+  {
+    i:17,
+    n:"bricomano italy",
+    c:"home,business",
+    u:"https://www.bricoman.it/",
+    d:"bricomano italy is a b2b and retail home improvement store selling construction materials, tools and hardware online.[web:1479]",
+    k:"bricomano italy,home improvement b2b and retail store,construction materials tools hardware,online building supplies shopping,bricoman.it ecommerce",
+    a:""
+  },
+  {
+    i:18,
+    n:"mondadori store online",
+    c:"books,beauty,home,business",
+    u:"https://www.mondadoristore.it/",
+    d:"mondadori store online is an italian bookstore and multimedia ecommerce site offering books, ebooks, music, films, stationery and gifts.[web:1476]",
+    k:"mondadori store online italy,bookstore and multimedia ecommerce,books ebooks music films stationery gifts,mondadoristore.it shopping",
+    a:""
+  },
+  {
+    i:19,
+    n:"feltrinelli online",
+    c:"books,business",
+    u:"https://www.lafeltrinelli.it/",
+    d:"feltrinelli online is a major italian bookstore site where customers can buy books, ebooks, music and movies with frequent promotions.[web:1476]",
+    k:"feltrinelli online italy,major italian bookstore site,books ebooks music movies,promotions and deals,lafeltrinelli.it ecommerce",
+    a:""
+  },
+  {
+    i:20,
+    n:"farmacia online italia",
+    c:"health,beauty,business",
+    u:"https://www.farmaciaonline.it/",
+    d:"farmacia online italia sells medicines, supplements, skincare and health products with advice and delivery throughout italy.[web:1479]",
+    k:"farmacia online italia,online pharmacy medicines supplements,skincare and health products advice,delivery throughout italy,farmaciaonline.it ecommerce",
+    a:""
+  },
+  {
+    i:21,
+    n:"docmorris italy",
+    c:"health,beauty,business",
+    u:"https://www.docmorris.it/",
+    d:"docmorris italy is an online pharmacy platform offering otc medicines, wellness products and beauty items to italian customers.[web:1479]",
+    k:"docmorris italy,online pharmacy platform,otc medicines wellness products beauty items,italian customers ecommerce,docmorris.it health store",
+    a:""
+  },
+  {
+    i:22,
+    n:"decathlon italy online",
+    c:"sports,fashion,business",
+    u:"https://www.decathlon.it/",
+    d:"decathlon italy online sells sportswear, footwear and equipment for hundreds of sports including running, cycling, football and outdoor activities.[web:1476][web:1479]",
+    k:"decathlon italy online,sportswear footwear and equipment,hundreds of sports running cycling football outdoor,decathlon.it sports ecommerce italy",
+    a:""
+  },
+  {
+    i:23,
+    n:"privalia italy",
+    c:"fashion,home,business",
+    u:"https://it.privalia.com/",
+    d:"privalia italy is an online outlet offering flash sales on fashion, footwear and home products from popular brands at discounted prices.[web:1476]",
+    k:"privalia italy,online outlet flash sales,fashion footwear home products,popular brands discounted prices,it.privalia.com ecommerce",
+    a:""
+  },
+  {
+    i:24,
+    n:"ubuy italy ubay brand store",
+    c:"electronics,home,business",
+    u:"https://www.ubuy.co.it/en/brand/ubay",
+    d:"ubuy italy ubay brand store features curated international products from ubay shipped from global warehouses to cities like rome, milan, naples, turin and florence.[web:1481]",
+    k:"ubuy italy ubay brand store,curated international products shipped from global warehouses,delivery to rome milan naples turin florence,ubay products on ubuy italy,cross border brand shopping italy",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy italy ai voice recorder category",
+    c:"electronics,software,business",
+    u:"https://www.ubuy.co.it/en/",
+    d:"ubuy italy ai voice recorder category sells advanced ai voice recorders and digital gadgets with app control, transcription and multilingual support for italian customers.[web:1478]",
+    k:"ubuy italy ai voice recorder category,advanced ai voice recorders digital gadgets,app control transcription multilingual support,tech accessories online shopping italy",
+    a:""
+  }
+];
