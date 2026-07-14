1823202489ddcde7a6bc83ddbd704501edb751d1
hrasbas <hrasbasji@gmail.com>
2026-07-14 17:36:00 +0530

    Add online shopping platforms for Honduras

---
+++ honduras.js
@@ -0,0 +1,60 @@
+// honduras.js
+window.countryShops = window.countryShops || {};
+window.countryShops["honduras"] = [
+  {
+    i:1,
+    n:"ubuy honduras",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.hn/en/",
+    d:"ubuy honduras is an international online shopping store where customers in honduras can buy imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and grocery items from global brands.[web:1553][web:1560]",
+    k:"ubuy honduras,international online shopping honduras,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books sports gear automotive accessories groceries,cross border ecommerce platform honduras",
+    a:""
+  },
+  {
+    i:2,
+    n:"mercado libre honduras",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.mercadolibre.com.hn/",
+    d:"mercado libre honduras is a leading marketplace where users buy and sell electronics, home goods, fashion, auto parts and many other products with fast shipping options.[web:1554]",
+    k:"mercado libre honduras,online marketplace honduras,buy and sell electronics home goods fashion auto parts,fast shipping and secure payment,mercadolibre.com.hn ecommerce",
+    a:""
+  },
+  {
+    i:3,
+    n:"walmart honduras online",
+    c:"food,home,business",
+    u:"https://www.walmart.com.hn/",
+    d:"walmart honduras online lets customers shop groceries, fresh food, household products and home essentials with low prices and home delivery across honduras.[web:1552]",
+    k:"walmart honduras online,groceries fresh food household products home essentials,low prices home delivery honduras,walmart.com.hn supermarket shopping",
+    a:""
+  },
+  {
+    i:4,
+    n:"pricesmart honduras",
+    c:"food,home,business",
+    u:"https://www.pricesmart.com/en-HN",
+    d:"pricesmart honduras is a membership warehouse club offering online ordering of bulk groceries, household supplies and electronics for pickup and delivery.[web:1555]",
+    k:"pricesmart honduras,membership warehouse club online ordering,bulk groceries household supplies electronics,pickup and delivery shopping,pricesmart.com honduras",
+    a:""
+  },
+  {
+    i:5,
+    n:"desertcart honduras",
+    c:"electronics,fashion,home,beauty,health,sports,jewellery,automotive,food,business",
+    u:"https://honduras.desertcart.com/",
+    d:"desertcart honduras is a cross border shopping site that delivers international health care, beauty, baby care, toys, sports, electronics, fashion and many more products with secure payment and free returns.[web:1557]",
+    k:"desertcart honduras,cross border shopping honduras,international health care beauty baby care toys,sports electronics fashion and more,secure platform free delivery returns",
+    a:""
+  },
+  {
+    i:6,
+    n:"yaxa honduras",
+    c:"electronics,fashion,home,food,business",
+    u:"https://honduras.yaxa.co/",
+    d:"yaxa honduras is a local platform for buying and importing national and international products with trackable shipping and local support for honduras shoppers.[web:1559]",
+    k:"yaxa honduras,platform to buy and import products,national and international goods trackable shipping,local support online shopping honduras,yaxa.co ecommerce",
+    a:""
+  },
+  {
+    i:7,
+    n:"ubuy honduras spanish store",
