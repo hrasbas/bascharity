3a9df214b7ebeb00496bee45307609dfe6100144
hrasbas <hrasbasji@gmail.com>
2026-07-14 17:50:43 +0530

    Add kazakhstan.js with online shop listings
    This file contains a list of online shops in Kazakhstan, including their names, categories, URLs, descriptions, and keywords for each shop.

---
+++ kazakhstan.js
@@ -0,0 +1,229 @@
+// kazakhstan.js
+window.countryShops = window.countryShops || {};
+window.countryShops["kazakhstan"] = [
+  {
+    i:1,
+    n:"ubuy kazakhstan",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.com.kz/en/",
+    d:"ubuy kazakhstan is an international online shopping store where customers can buy imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and grocery items at low prices in kazakhstan.[web:1432][web:1433][web:1436]",
+    k:"ubuy kazakhstan,international online shopping kazakhstan,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books toys sports gear automotive products grocery,cross border ecommerce platform kazakhstan",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy.kz app",
+    c:"software,business",
+    u:"https://play.google.com/store/apps/details?id=com.megabait.ubuy&hl=tr",
+    d:"ubuy.kz app is a mobile shopping application in kazakhstan that lets users browse products, place orders and manage deliveries from their phones.[web:1438]",
+    k:"ubuy.kz app,mobile shopping application kazakhstan,browse products place orders manage deliveries,online marketplace in your phone,ubuy kazakhstan mobile ecommerce",
+    a:""
+  },
+  {
+    i:3,
+    n:"kaspi shop",
+    c:"electronics,home,fashion,beauty,books,sports,business",
+    u:"https://kaspi.kz/shop/",
+    d:"kaspi shop is a leading online marketplace in kazakhstan where customers can buy electronics, fashion, home and garden goods, books, sports items, health and beauty products with integrated kaspi pay and delivery.[web:1435][web:1439][web:1440]",
+    k:"kaspi shop kazakhstan,leading online marketplace,kaspi super app ecommerce,electronics fashion home garden books sports health beauty,integrated kaspi pay and delivery",
+    a:""
+  },
+  {
+    i:4,
+    n:"flip.kz",
+    c:"electronics,home,fashion,beauty,books,food,business",
+    u:"https://flip.kz/",
+    d:"flip.kz is an online store and marketplace in kazakhstan offering clothing, cosmetics, books, home and garden goods, children's items, household appliances and more with free delivery nationwide.[web:1434][web:1435]",
+    k:"flip.kz kazakhstan,online store and marketplace,clothing cosmetics books home and garden,children goods household appliances,free delivery throughout kazakhstan",
+    a:""
+  },
+  {
+    i:5,
+    n:"flip marketplace app",
+    c:"software,business",
+    u:"https://play.google.com/store/apps/details?id=kz.flip.mobile&hl=en_AU",
+    d:"flip marketplace app lets shoppers in kazakhstan access over one million products with free shipping and even one hour delivery in almaty and karaganda.[web:1434]",
+    k:"flip marketplace app,over one million products free shipping,one hour delivery in almaty karaganda,online shopping app kazakhstan,flip.kz mobile ecommerce",
+    a:""
+  },
+  {
+    i:6,
+    n:"mechta.kz",
+    c:"electronics,home,business",
+    u:"https://www.mechta.kz/",
+    d:"mechta.kz is a major electronics and home appliances retailer in kazakhstan with online shopping for tvs, smartphones, laptops, kitchen appliances and household goods.[web:1435][web:1440]",
+    k:"mechta.kz kazakhstan,major electronics and home appliances retailer,tvs smartphones laptops kitchen appliances,household goods online shopping,mechta ecommerce store",
+    a:""
+  },
+  {
+    i:7,
+    n:"sulpak.kz",
+    c:"electronics,home,business",
+    u:"https://www.sulpak.kz/",
+    d:"sulpak.kz is a well known electronics and appliance chain in kazakhstan offering online sales of consumer electronics, computers and home appliances.[web:1435][web:1440]",
+    k:"sulpak.kz kazakhstan,electronics and appliance chain,consumer electronics computers home appliances,online tech and appliance shopping,sulpak ecommerce platform",
+    a:""
+  },
+  {
+    i:8,
+    n:"technodom.kz",
+    c:"electronics,home,business",
+    u:"https://www.technodom.kz/",
+    d:"technodom.kz is an electronics and household appliances store in kazakhstan with online ordering for smartphones, laptops, tvs and kitchen devices.[web:1440]",
+    k:"technodom.kz kazakhstan,electronics and household appliances store,smartphones laptops tvs kitchen devices,online ordering and delivery,technodom ecommerce site",
+    a:""
+  },
+  {
+    i:9,
+    n:"shop.kz",
+    c:"electronics,business",
+    u:"https://shop.kz/",
+    d:"shop.kz is an online computer and electronics shop in kazakhstan selling pcs, components, networking gear and peripherals with loyalty cashback.[web:1440]",
+    k:"shop.kz kazakhstan,online computer and electronics shop,pcs components networking gear peripherals,loyalty cashback program,tech ecommerce platform",
+    a:""
+  },
+  {
+    i:10,
+    n:"dns-shop.kz",
+    c:"electronics,home,business",
+    u:"https://www.dns-shop.kz/",
+    d:"dns-shop.kz is an electronics and household goods retailer in kazakhstan providing online shopping for gadgets, appliances and accessories.[web:1440]",
+    k:"dns-shop.kz kazakhstan,electronics and household goods retailer,gadgets appliances and accessories,online tech shopping,dns-shop ecommerce site",
+    a:""
+  },
+  {
+    i:11,
+    n:"wildberries kazakhstan",
+    c:"fashion,home,beauty,business",
+    u:"https://kz.wildberries.ru/",
+    d:"wildberries kazakhstan is a cross border fashion and lifestyle marketplace offering clothing, shoes, home goods and beauty products with delivery across the country.[web:1435][web:1440]",
+    k:"wildberries kazakhstan,cross border fashion and lifestyle marketplace,clothing shoes home goods beauty products,delivery across kazakhstan,wildberries.ru ecommerce access",
+    a:""
+  },
+  {
+    i:12,
+    n:"ozon kazakhstan",
+    c:"electronics,home,fashion,books,business",
+    u:"https://www.ozon.kz/",
+    d:"ozon kazakhstan is an online marketplace connected to the russian ozon platform where users order books, electronics, fashion and home products.[web:1435][web:1440]",
+    k:"ozon kazakhstan,online marketplace books electronics fashion home,connected to russian ozon platform,cross border ecommerce kazakhstan,ozon.kz shopping",
+    a:""
+  },
+  {
+    i:13,
+    n:"lamoda kazakhstan",
+    c:"fashion,business",
+    u:"https://www.lamoda.kz/",
+    d:"lamoda kazakhstan is an online fashion store specializing in clothing, footwear and accessories for men, women and children.[web:1440]",
+    k:"lamoda kazakhstan,online fashion store clothing footwear accessories,men women children fashion shopping,lamoda.kz ecommerce,fashion marketplace kazakhstan",
+    a:""
+  },
+  {
+    i:14,
+    n:"aliExpress kazakhstan access",
+    c:"electronics,home,fashion,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress serves kazakhstan as a global marketplace where local shoppers order electronics, fashion, home goods and accessories using international shipping and local proxies.[web:1435][web:1440]",
+    k:"aliexpress kazakhstan,global marketplace electronics fashion home goods,international shipping and local proxies,cross border shopping platform,online deals for kazakhstan buyers",
+    a:""
+  },
+  {
+    i:15,
+    n:"olx kazakhstan",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.olx.kz/",
+    d:"olx kazakhstan is a classifieds marketplace where users buy and sell second hand electronics, home and garden items, vehicles and personal goods via online listings.[web:1439]",
+    k:"olx kazakhstan,classifieds marketplace second hand goods,electronics home and garden vehicles personal items,buy and sell online kazakhstan,olx.kz local marketplace",
+    a:""
+  },
+  {
+    i:16,
+    n:"kolesa.kz",
+    c:"automotive,business",
+    u:"https://kolesa.kz/",
+    d:"kolesa.kz is a dedicated automotive classifieds site in kazakhstan listing cars, bikes and vehicle services for sale across the country.[web:1439]",
+    k:"kolesa.kz kazakhstan,automotive classifieds site,cars bikes and vehicle services for sale,nationwide vehicle marketplace,online car shopping kazakhstan",
+    a:""
+  },
+  {
+    i:17,
+    n:"glovo kazakhstan",
+    c:"food,business",
+    u:"https://glovoapp.com/kz/",
+    d:"glovo kazakhstan is a quick delivery app and platform that brings food, beverages and groceries from local restaurants and stores to customers.[web:1439]",
+    k:"glovo kazakhstan,quick delivery app platform,food beverages and groceries from local stores,online food ordering and delivery,glovoapp.com ecommerce service",
+    a:""
+  },
+  {
+    i:18,
+    n:"magnum.kz online",
+    c:"food,home,business",
+    u:"https://magnum.kz/",
+    d:"magnum.kz online store allows shoppers in kazakhstan to order supermarket groceries, fresh produce and household goods from magnum chain outlets.[web:1437]",
+    k:"magnum.kz online,supermarket groceries fresh produce household goods,magnum chain outlets ecommerce,online grocery shopping kazakhstan,magnum delivery service",
+    a:""
+  },
+  {
+    i:19,
+    n:"small.kz",
+    c:"food,business",
+    u:"https://small.kz/",
+    d:"small.kz is a convenience and grocery delivery platform in kazakhstan providing everyday food items and essentials via online orders.[web:1437]",
+    k:"small.kz kazakhstan,convenience and grocery delivery platform,everyday food items and essentials,online orders and fast delivery,small.kz food ecommerce",
+    a:""
+  },
+  {
+    i:20,
+    n:"teez.kz",
+    c:"fashion,business",
+    u:"https://teez.kz/",
+    d:"teez.kz is a kazakh online fashion shop selling t-shirts, streetwear and branded apparel with local designs.[web:1440]",
+    k:"teez.kz kazakhstan,online fashion shop t-shirts streetwear,branded apparel with local designs,clothing ecommerce platform,teez.kz style store",
+    a:""
+  },
+  {
+    i:21,
+    n:"chocotravel",
+    c:"travel,business",
+    u:"https://www.chocotravel.com/",
+    d:"chocotravel is a kazakhstan based online travel agency providing flight tickets, hotel bookings and travel packages via its website.[web:1435]",
+    k:"chocotravel kazakhstan,online travel agency flight tickets hotels,travel packages booking platform,chocotravel.com ecommerce,travel services marketplace",
+    a:""
+  },
+  {
+    i:22,
+    n:"aviata.kz",
+    c:"travel,business",
+    u:"https://aviata.kz/",
+    d:"aviata.kz is an online service in kazakhstan for booking airline and railway tickets with digital payment options.[web:1435]",
+    k:"aviata.kz kazakhstan,online airline and railway ticket booking,digital payment options,travel ecommerce platform,aviata.kz ticket service",
+    a:""
+  },
+  {
+    i:23,
+    n:"detmir.kz",
+    c:"fashion,home,business",
+    u:"https://www.detmir.kz/",
+    d:"detmir.kz is a children's goods online store in kazakhstan selling toys, kids clothing, baby care products and nursery items.[web:1437]",
+    k:"detmir.kz kazakhstan,children goods online store,toys kids clothing baby care nursery items,family oriented ecommerce,detmir kids marketplace",
+    a:""
+  },
+  {
+    i:24,
+    n:"ubuy kazakhstan city delivery",
+    c:"business,home,food",
+    u:"https://www.ubuy.com.kz/en/",
+    d:"ubuy kazakhstan city delivery service ships products from international warehouses to almaty, karaganda, shymkent, taraz, nur sultan, pavlodar and other major cities.[web:1436]",
+    k:"ubuy kazakhstan city delivery,international warehouses to almaty karaganda shymkent taraz nur sultan pavlodar,major cities logistics network,online shopping delivery service,cross border ecommerce shipping",
+    a:""
+  },
+  {
+    i:25,
+    n:"online shopping kazakhstan all sites app",
+    c:"software,business",
+    u:"https://www.shopbycountries.com/en/country/kz",
+    d:"online shopping kazakhstan all sites app and directory lists top platforms like kaspi, glovo, olx and kolesa for quick comparison and access to trusted ecommerce sites.[web:1439]",
+    k:"online shopping kazakhstan all sites app,directory of top platforms kaspi glovo olx kolesa,quick comparison and access to trusted ecommerce sites,shopbycountries kazakhstan guide,aggregator for online marketplaces",
+    a:""
+  }
+];
