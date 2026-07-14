238b0cdfae0e9290a2e3a3f94e4bafecc1837d5b
hrasbas <hrasbasji@gmail.com>
2026-07-14 11:09:31 +0530

    Add online shops data for Greenland
    This file contains a list of online shops in Greenland, including their names, categories, URLs, descriptions, and keywords for search optimization.

---
+++ greenland.js
@@ -0,0 +1,229 @@
+// greenland.js
+window.countryShops = window.countryShops || {};
+window.countryShops["greenland"] = [
+  {
+    i:1,
+    n:"ubuy greenland",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://www.ubuy.gl/en/",
+    d:"ubuy greenland is an international online shopping marketplace in greenland delivering imported electronics, fashion, beauty, grocery, sports gear, books, software and automotive products from global brands.",
+    k:"ubuy greenland,international online shopping greenland,imported electronics mobiles tablets,fashion beauty home kitchen products,global brands groceries sports travel,books software automotive accessories",
+    a:""
+  },
+  {
+    i:2,
+    n:"greenlandcruises.net",
+    c:"travel,business",
+    u:"https://greenlandcruises.net/",
+    d:"greenlandcruises.net is an online travel shop focused on cruises and tours in greenland, letting visitors book expedition cruises and adventure trips around the arctic waters.",
+    k:"greenlandcruises.net,cruise booking greenland,expedition cruises and tours,arctic travel online shop,greenland travel marketplace",
+    a:""
+  },
+  {
+    i:3,
+    n:"diskobay tours",
+    c:"travel,business",
+    u:"https://www.diskobaytours.com/",
+    d:"diskobay tours offers online booking for boat trips, iceberg sightseeing and adventure tours in greenland, targeting tourists seeking unique arctic experiences.",
+    k:"diskobay tours,online tours booking greenland,boat trips and iceberg sightseeing,arctic adventure travel ecommerce,greenland excursions shop",
+    a:""
+  },
+  {
+    i:4,
+    n:"arctic excursions greenland",
+    c:"travel,business",
+    u:"https://arcticexcursions.com/",
+    d:"arctic excursions is an online travel agency in greenland offering excursions, tours and outdoor activities like hiking and sailing with online reservation.",
+    k:"arctic excursions greenland,online excursions booking,tours and outdoor activities,hiking and sailing trips,ecommerce travel shop greenland",
+    a:""
+  },
+  {
+    i:5,
+    n:"greenland ruby",
+    c:"jewellery,business",
+    u:"https://www.greenlandruby.gl/",
+    d:"greenland ruby is a jewellery brand selling ethically sourced ruby and sapphire gemstones from greenland, offering finished jewellery and loose stones through its online store.",
+    k:"greenland ruby,ruby jewellery online,greenland gemstones and sapphires,ethical gemstone shop,ecommerce jewellery store greenland",
+    a:""
+  },
+  {
+    i:6,
+    n:"qiviut.gl",
+    c:"fashion,home,business",
+    u:"https://qiviut.gl/",
+    d:"qiviut.gl specializes in products made from musk ox wool and sealskin in greenland, selling warm knitwear, accessories and home items via its webshop.",
+    k:"qiviut.gl,musk ox wool products,sealskin knitwear and accessories,greenland wool shop online,warm clothing and home items ecommerce",
+    a:""
+  },
+  {
+    i:7,
+    n:"navarana",
+    c:"fashion,business",
+    u:"https://navarana.com/",
+    d:"navarana is an online fashion boutique linked to greenland offering clothing and accessories inspired by nordic and inuit designs for global customers.",
+    k:"navarana,greenland fashion boutique,clothing and accessories online,nordic inuit inspired designs,fashion ecommerce greenland",
+    a:""
+  },
+  {
+    i:8,
+    n:"pikkori sport",
+    c:"sports,fashion,business",
+    u:"https://pikkorisport.com/",
+    d:"pikkori sport is an online sports and athletic apparel shop associated with greenland, selling sportswear, shoes and sporting goods for active customers.",
+    k:"pikkori sport,athletic apparel greenland,sportswear and shoes online,sporting goods ecommerce,online sports shop greenland",
+    a:""
+  },
+  {
+    i:9,
+    n:"fitnessgl",
+    c:"sports,health,business",
+    u:"https://fitnessgl.com/",
+    d:"fitnessgl is a nutrition and fitness ecommerce site in greenland offering supplements, workout products and health focused items for training enthusiasts.",
+    k:"fitnessgl,fitness nutrition greenland,supplements and workout products,health items ecommerce,online sports and health shop",
+    a:""
+  },
+  {
+    i:10,
+    n:"atlantic music greenland",
+    c:"books,business",
+    u:"https://atlanticmusic.gl/",
+    d:"atlantic music aps is a greenlandic music and media shop selling cds, vinyl, books and related merchandise from local artists through its online store.",
+    k:"atlantic music greenland,greenlandic music shop online,cds vinyl and books,local artists merchandise,ecommerce media store",
+    a:""
+  },
+  {
+    i:11,
+    n:"atuagkat boghandel",
+    c:"books,business",
+    u:"https://atuagkat.gl/",
+    d:"atuagkat boghandel is a leading bookstore in greenland with an online shop selling books, maps and educational materials in greenlandic, danish and other languages.",
+    k:"atuagkat boghandel,online bookstore greenland,books maps educational materials,greenlandic and danish titles,bookshop ecommerce gl",
+    a:""
+  },
+  {
+    i:12,
+    n:"igloo magazine shop",
+    c:"books,business",
+    u:"https://igloomagazine.com/",
+    d:"igloo magazine shop offers subscriptions and issues of igloo magazine about greenland along with books and printed materials via online ordering.",
+    k:"igloo magazine shop,greenland magazine online,subscriptions and back issues,books and printed materials,ecommerce media shop greenland",
+    a:""
+  },
+  {
+    i:13,
+    n:"sassuma sea salt",
+    c:"food,business",
+    u:"https://sassumaseasalt.com/",
+    d:"sassuma sea salt is an online shop selling handcrafted sea salt from greenland along with seasonings and gift sets for home cooks and food lovers.",
+    k:"sassuma sea salt,greenland sea salt online,handcrafted salt and seasonings,gift sets for home cooks,food ecommerce shop greenland",
+    a:""
+  },
+  {
+    i:14,
+    n:"perle's webshop",
+    c:"home,food,business",
+    u:"https://perleswebshop.gl/",
+    d:"perle's webshop is a homemade gift shop from greenland offering crafts, decor items and small food gifts that can be ordered online.",
+    k:"perle's webshop,homemade gifts greenland,crafts and decor items,small food gifts ecommerce,online souvenir shop gl",
+    a:""
+  },
+  {
+    i:15,
+    n:"allbuy.gl",
+    c:"electronics,home,fashion,business",
+    u:"https://allbuy.gl/",
+    d:"allbuy.gl is an online marketplace in greenland where customers can buy electronics, home goods, clothing and general merchandise from various sellers.",
+    k:"allbuy.gl,online marketplace greenland,electronics home goods clothing,general merchandise ecommerce,shopping site gl",
+    a:""
+  },
+  {
+    i:16,
+    n:"greenlands.biz",
+    c:"sports,home,business",
+    u:"https://greenlands.biz/",
+    d:"greenlands.biz is an outdoor gear ecommerce store offering backpacks, trekking equipment, caps, tents and adventure accessories to customers worldwide.",
+    k:"greenlands.biz,outdoor gear online,backpacks trekking equipment,caps tents and adventure accessories,greenland inspired outdoor shop",
+    a:""
+  },
+  {
+    i:17,
+    n:"arctic circle hike",
+    c:"sports,travel,business",
+    u:"https://arcticcirclehike.com/",
+    d:"arctic circle hike is an online greenland platform selling hiking tours, outdoor experiences and related gear booking for the arctic circle trail.",
+    k:"arctic circle hike,greenland hiking tours,arctic circle trail booking,outdoor experiences ecommerce,sports and travel shop gl",
+    a:""
+  },
+  {
+    i:18,
+    n:"greenland cruises shop",
+    c:"travel,business",
+    u:"https://greenland-cruises.com/",
+    d:"greenland cruises shop offers online booking for cruise packages, coastal voyages and arctic sailing trips around greenland's fjords and coastline.",
+    k:"greenland cruises shop,cruise packages greenland,coastal voyages and sailing trips,arctic cruise booking ecommerce,travel shop gl",
+    a:""
+  },
+  {
+    i:19,
+    n:"navarana jewellery",
+    c:"jewellery,business",
+    u:"https://navarana.com/collections/jewellery",
+    d:"navarana jewellery section sells designer jewellery pieces inspired by greenland motifs, offering earrings, necklaces and accessories online.",
+    k:"navarana jewellery,designer jewellery greenland,earrings necklaces accessories,greenland inspired jewellery online,jewellery ecommerce gl",
+    a:""
+  },
+  {
+    i:20,
+    n:"kaataq el aps",
+    c:"electronics,business",
+    u:"https://kaataqel.gl/",
+    d:"kaataq el aps is an electronics and electrical equipment shop in greenland providing lighting, cables and electronic components through online ordering.",
+    k:"kaataq el aps,electrical equipment greenland,lighting cables and components,online electronics shop gl,business supplies store",
+    a:""
+  },
+  {
+    i:21,
+    n:"fitness gl shop",
+    c:"sports,health,business",
+    u:"https://shop.fitnessgl.com/",
+    d:"fitness gl shop extends the fitnessgl brand with an ecommerce store selling gym accessories, sports clothing and health supplements in greenland.",
+    k:"fitness gl shop,gym accessories greenland,sports clothing and gear,health supplements ecommerce,online fitness store gl",
+    a:""
+  },
+  {
+    i:22,
+    n:"pajussi ilulissat",
+    c:"fashion,home,business",
+    u:"https://pajussi.gl/",
+    d:"pajussi ilulissat is a clothing and gift shop in greenland with online sales of apparel, souvenirs and home items featuring local designs.",
+    k:"pajussi ilulissat,clothing and gifts greenland,souvenirs and home items,local design products ecommerce,online shop gl",
+    a:""
+  },
+  {
+    i:23,
+    n:"nanoq media shop",
+    c:"books,business",
+    u:"https://nanoqmedia.gl/",
+    d:"nanoq media shop is an online store in greenland selling books, magazines and media products related to greenlandic culture and society.",
+    k:"nanoq media shop,books and magazines greenland,media products online,greenlandic culture ecommerce,reading materials gl",
+    a:""
+  },
+  {
+    i:24,
+    n:"greenland souvenirs online",
+    c:"home,fashion,business",
+    u:"https://greenlandsouvenirs.com/",
+    d:"greenland souvenirs online sells souvenirs, clothing, small decor items and accessories featuring arctic and inuit inspired designs for tourists and fans of greenland.",
+    k:"greenland souvenirs online,souvenirs shop greenland,clothing decor and accessories,arctic inuit inspired designs,online gift store gl",
+    a:""
+  },
+  {
+    i:25,
+    n:"permafrost records shop",
+    c:"books,business",
+    u:"https://permafrostrecords.com/",
+    d:"permafrost records shop offers cds, vinyl records and merchandise from greenlandic and nordic bands through its online music store.",
+    k:"permafrost records shop,cds and vinyl online,greenlandic bands merchandise,music ecommerce store,records shop greenland",
+    a:""
+  }
+];
