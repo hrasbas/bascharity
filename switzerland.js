f3636101017e9833ea781679277230c605a13eb7
hrasbas <hrasbasji@gmail.com>
2026-07-14 13:17:46 +0530

    Add switzerland.js with online shop data
    This file contains a list of online shops in Switzerland, including details such as name, category, URL, description, keywords, and additional attributes.

---
+++ switzerland.js
@@ -0,0 +1,229 @@
+// switzerland.js
+window.countryShops = window.countryShops || {};
+window.countryShops["switzerland"] = [
+  {
+    i:1,
+    n:"digitec",
+    c:"electronics,business",
+    u:"https://www.digitec.ch/en",
+    d:"digitec is a leading swiss online shop for consumer electronics where customers can buy smartphones, tvs, notebooks, pc components and many digital gadgets with fast delivery across switzerland.[web:583][web:578]",
+    k:"digitec switzerland,online electronics shop switzerland,smartphones tvs notebooks and pcs,consumer tech gadgets marketplace,swiss digital products ecommerce",
+    a:""
+  },
+  {
+    i:2,
+    n:"galaxus",
+    c:"electronics,home,fashion,beauty,food,sports,books,business",
+    u:"https://www.galaxus.ch/",
+    d:"galaxus is a major swiss online marketplace offering a broad range of electronics, home and garden products, fashion, beauty, sports, toys and books for everyday online shopping in switzerland.[web:574][web:584]",
+    k:"galaxus switzerland,online marketplace switzerland,electronics home and garden store,fashion beauty sports and toys,books and lifestyle products ecommerce",
+    a:""
+  },
+  {
+    i:3,
+    n:"migros online",
+    c:"food,home,fashion,business",
+    u:"https://www.migros.ch/de.html",
+    d:"migros online is the ecommerce arm of migros supermarkets in switzerland where shoppers can order groceries, fresh food, household items and selected fashion products with home delivery and pickup.[web:580][web:584]",
+    k:"migros online switzerland,online supermarket switzerland,grocery fresh food and beverages,household items and basic fashion,hypermarket ecommerce platform",
+    a:""
+  },
+  {
+    i:4,
+    n:"coop.ch",
+    c:"food,home,business",
+    u:"https://www.coop.ch/en/online-supermarket.html",
+    d:"coop.ch is the online supermarket of coop in switzerland providing groceries, fresh produce, household products and one of the largest online wine selections in the country with convenient delivery.[web:577][web:579][web:582]",
+    k:"coop.ch,online supermarket switzerland,groceries fresh produce and beverages,household products and cleaning supplies,switzerland online wine cellar marketplace",
+    a:""
+  },
+  {
+    i:5,
+    n:"manor e-shop",
+    c:"fashion,beauty,home,food,business",
+    u:"https://www.manor.ch/",
+    d:"manor e-shop is the online store of the manor department chain in switzerland selling fashion, beauty, homeware, toys and gourmet food with nationwide delivery and click and collect services.[web:580][web:581]",
+    k:"manor e-shop switzerland,online department store switzerland,fashion beauty and homeware,toys gifts and gourmet food,click and collect ecommerce platform",
+    a:""
+  },
+  {
+    i:6,
+    n:"brack.ch",
+    c:"electronics,home,sports,books,business",
+    u:"https://www.brack.ch/",
+    d:"brack.ch is a swiss online retailer specializing in electronics, it equipment, household goods, sports items and office supplies, serving both consumers and business buyers.[web:580][web:581]",
+    k:"brack.ch,online electronics and it store switzerland,household goods and sports items,office supplies and business products,swiss multi category ecommerce site",
+    a:""
+  },
+  {
+    i:7,
+    n:"microspot",
+    c:"electronics,home,business",
+    u:"https://www.microspot.ch/",
+    d:"microspot is an online shop in switzerland offering discounted electronics, home appliances, furniture and home improvement products for price conscious shoppers.[web:580][web:581]",
+    k:"microspot switzerland,discount electronics online shop,home appliances furniture and decor,home improvement products ecommerce,price conscious swiss marketplace",
+    a:""
+  },
+  {
+    i:8,
+    n:"ricardo",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.ricardo.ch/",
+    d:"ricardo is a popular swiss online auction and marketplace platform where users buy and sell electronics, fashion, home items, vehicles and collectibles via listings.[web:574][web:584]",
+    k:"ricardo switzerland,online auction marketplace switzerland,buy and sell electronics fashion and home,vehicles and collectibles listings,consumer to consumer ecommerce",
+    a:""
+  },
+  {
+    i:9,
+    n:"tutti.ch",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.tutti.ch/",
+    d:"tutti.ch is a swiss classifieds and marketplace site that lets individuals list and find electronics, furniture, clothing, cars and local services in different regions of switzerland.[web:574][web:576]",
+    k:"tutti.ch,swiss classifieds marketplace,electronics furniture clothing and cars,regional listings and local services,online buy and sell platform switzerland",
+    a:""
+  },
+  {
+    i:10,
+    n:"zalando switzerland",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.ch/",
+    d:"zalando switzerland is an online fashion and beauty store offering clothing, shoes, accessories and cosmetics from many brands with easy returns and delivery across switzerland.[web:584][web:586]",
+    k:"zalando switzerland,online fashion shop switzerland,clothing shoes and accessories,beauty and cosmetics from many brands,easy returns ecommerce platform",
+    a:""
+  },
+  {
+    i:11,
+    n:"amazon.de switzerland segment",
+    c:"electronics,home,fashion,books,software,business",
+    u:"https://www.amazon.de/",
+    d:"amazon.de serves swiss customers with cross border online shopping for electronics, books, fashion, homeware and software, shipping a wide range of products into switzerland.[web:574][web:584]",
+    k:"amazon.de switzerland,cross border online shopping switzerland,electronics books fashion and homeware,software and digital products marketplace,international ecommerce for swiss buyers",
+    a:""
+  },
+  {
+    i:12,
+    n:"fust",
+    c:"electronics,home,business",
+    u:"https://www.fust.ch/",
+    d:"fust is a swiss retailer specialising in home appliances, kitchen equipment, consumer electronics and household services, with an online shop for ordering and installation bookings.[web:580][web:581]",
+    k:"fust switzerland,online home appliances store,kitchen equipment and consumer electronics,household services and installation,ecommerce platform for swiss homes",
+    a:""
+  },
+  {
+    i:13,
+    n:"ikea switzerland",
+    c:"home,business",
+    u:"https://www.ikea.com/ch/en/",
+    d:"ikea switzerland online shop sells furniture, decor, kitchen products and home accessories and allows customers to order for delivery or click and collect in swiss ikea stores.[web:576][web:581]",
+    k:"ikea switzerland,online furniture shop switzerland,home decor kitchen and accessories,click and collect or delivery,european home goods ecommerce",
+    a:""
+  },
+  {
+    i:14,
+    n:"le shop by migros",
+    c:"food,home,business",
+    u:"https://www.migros.ch/en/leshop.html",
+    d:"le shop by migros is a swiss online grocery service focusing on fresh groceries and organic products, letting customers order food and household essentials online.[web:580]",
+    k:"le shop migros,online grocery service switzerland,fresh and organic food delivery,household essentials supermarket ecommerce,migros digital shopping platform",
+    a:""
+  },
+  {
+    i:15,
+    n:"coop brico+loisirs online",
+    c:"home,business",
+    u:"https://www.bricoetloisirs.ch/",
+    d:"coop brico+loisirs online store sells diy tools, garden equipment, building materials and home improvement products to swiss homeowners and hobbyists.[web:582][web:581]",
+    k:"coop brico loisirs online,diy tools and garden equipment,building materials and home improvement,swiss homeowners and hobbyists ecommerce,coop group hardware shop",
+    a:""
+  },
+  {
+    i:16,
+    n:"sportxx online",
+    c:"sports,fashion,business",
+    u:"https://www.sportxx.ch/",
+    d:"sportxx online is a swiss sports and outdoor shop offering sportswear, shoes, fitness gear and equipment for many activities with delivery throughout switzerland.[web:581]",
+    k:"sportxx online switzerland,sportswear shoes and fitness gear,outdoor equipment ecommerce,active lifestyle products swiss market,sport shop online platform",
+    a:""
+  },
+  {
+    i:17,
+    n:"dataquest",
+    c:"electronics,business",
+    u:"https://www.dataquest.ch/",
+    d:"dataquest is an apple focused and it retailer in switzerland with an online shop selling macs, iphones, ipads and accessories along with business it solutions.[web:581]",
+    k:"dataquest switzerland,apple products online shop,macs iphones ipads and accessories,it retailer with business solutions,swiss electronics ecommerce",
+    a:""
+  },
+  {
+    i:18,
+    n:"melectronics online",
+    c:"electronics,business",
+    u:"https://www.melectronics.ch/",
+    d:"melectronics is a swiss consumer electronics chain from migros that offers an online shop for tvs, audio systems, cameras, gaming and household electronics.[web:581][web:580]",
+    k:"melectronics switzerland,consumer electronics chain online,tvs audio cameras and gaming gear,household electronics ecommerce,migros group tech retailer",
+    a:""
+  },
+  {
+    i:19,
+    n:"siroop marketplace",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.siroop.ch/",
+    d:"siroop marketplace is an online platform in switzerland where different sellers offer electronics, fashion, home articles and selected food products to swiss consumers.[web:581]",
+    k:"siroop marketplace switzerland,multi seller online platform,electronics fashion home and food,swiss consumer ecommerce site,third party sellers marketplace",
+    a:""
+  },
+  {
+    i:20,
+    n:"otto's online shop",
+    c:"home,fashion,food,business",
+    u:"https://www.ottos.ch/",
+    d:"otto's online shop is a swiss discount retailer selling household items, furniture, fashion, food and beverages at attractive prices via its ecommerce website.[web:581]",
+    k:"ottos online shop switzerland,discount household and furniture retailer,fashion food and beverages deals,swiss bargain ecommerce,online discount store",
+    a:""
+  },
+  {
+    i:21,
+    n:"loccitane switzerland",
+    c:"beauty,business",
+    u:"https://ch.loccitane.com/",
+    d:"loccitane switzerland online boutique offers premium beauty and skincare products including body care, fragrances and face care for swiss customers.[web:581]",
+    k:"loccitane switzerland,online beauty boutique switzerland,skincare body care and fragrances,premium cosmetics ecommerce,brand beauty shop swiss market",
+    a:""
+  },
+  {
+    i:22,
+    n:"christ watches and jewellery",
+    c:"jewellery,business",
+    u:"https://www.christ-swiss.ch/",
+    d:"christ is a swiss online shop specialising in watches and jewellery, offering gold, silver and diamond pieces from many brands for shoppers across switzerland.[web:581]",
+    k:"christ switzerland,online watches and jewellery shop,gold silver and diamond pieces,branded swiss timepieces marketplace,luxury accessories ecommerce",
+    a:""
+  },
+  {
+    i:23,
+    n:"ochsner sport",
+    c:"sports,fashion,business",
+    u:"https://www.ochsner-sport.ch/",
+    d:"ochsner sport is a swiss sports retailer with an online store selling sportswear, shoes, equipment and outdoor gear for multiple disciplines.[web:581]",
+    k:"ochsner sport switzerland,online sports retailer,sportswear shoes and equipment,outdoor gear ecommerce,multi discipline sport shop",
+    a:""
+  },
+  {
+    i:24,
+    n:"medpex switzerland",
+    c:"health,beauty,business",
+    u:"https://www.medpex.ch/",
+    d:"medpex switzerland online pharmacy offers health products, medicines, wellness items and beauty care for swiss customers with discreet delivery.[web:581]",
+    k:"medpex switzerland,online pharmacy and wellness shop,medicines health products and beauty care,discreet delivery healthcare ecommerce,swiss digital drugstore",
+    a:""
+  },
+  {
+    i:25,
+    n:"petfriends",
+    c:"food,health,business",
+    u:"https://www.petfriends.ch/",
+    d:"petfriends is a swiss online shop for pet food and accessories that provides dog, cat and small animal nutrition, toys and care products with home delivery.[web:588][web:585][web:590]",
+    k:"petfriends switzerland,online pet food shop,dog cat and small animal nutrition,pet accessories toys and care,swiss pet ecommerce platform",
+    a:""
+  }
+];
