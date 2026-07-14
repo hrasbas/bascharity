06279b73551adb438f3444051ed42a49b02a9eb2
hrasbas <hrasbasji@gmail.com>
2026-07-14 11:11:40 +0530

    Add online shops data for Puerto Rico
    This file contains a list of online shops in Puerto Rico, including their names, categories, URLs, descriptions, and keywords for each shop.

---
+++ puerto_rico.js
@@ -0,0 +1,229 @@
+// puerto_rico.js
+window.countryShops = window.countryShops || {};
+window.countryShops["puerto_rico"] = [
+  {
+    i:1,
+    n:"ubuy puerto rico",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://www.ubuy.com.pr/en/",
+    d:"ubuy puerto rico is an international online shopping marketplace delivering imported electronics, fashion, beauty, grocery, sports gear, books, software and automotive products from global brands to customers in puerto rico.",
+    k:"ubuy puerto rico,international online shopping puerto rico,imported electronics mobiles tablets,fashion beauty home kitchen products,global brands groceries sports travel,books software automotive accessories",
+    a:""
+  },
+  {
+    i:2,
+    n:"goxip puerto rico",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/pr/en",
+    d:"goxip puerto rico is an online fashion and beauty shopping destination where users can explore millions of clothing, shoes and beauty products from thousands of brands and stores.",
+    k:"goxip puerto rico,online fashion shopping puerto rico,clothing shoes and accessories,beauty products marketplace,trendy brands online",
+    a:""
+  },
+  {
+    i:3,
+    n:"antojo boricua",
+    c:"food,business",
+    u:"https://antojoboricuapr.com/shop/",
+    d:"antojo boricua is an online puerto rican grocery store shipping traditional puerto rico foods, snacks and pantry items to customers locally and abroad.",
+    k:"antojo boricua,online puerto rican grocery store,puerto rico foods and snacks,boricua pantry items ecommerce,local food products shop",
+    a:""
+  },
+  {
+    i:4,
+    n:"desertcart puerto rico",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,business",
+    u:"https://www.desertcart.com.pr/",
+    d:"desertcart puerto rico is an online marketplace offering a wide range of imported electronics, home goods, fashion, beauty, health, food, sports items and books shipped to puerto rico.",
+    k:"desertcart puerto rico,international products online puerto rico,electronics home fashion beauty,health food sports and books,ecommerce marketplace puerto rico",
+    a:""
+  },
+  {
+    i:5,
+    n:"walgreens puerto rico",
+    c:"health,beauty,food,home,business",
+    u:"https://www.walgreens.com/store/c/puerto-rico/ID=361956-tier3",
+    d:"walgreens puerto rico offers online shopping for pharmacy items, health products, beauty essentials, small home goods and grocery basics with pickup and delivery options.",
+    k:"walgreens puerto rico,online pharmacy puerto rico,health and wellness products,beauty and personal care,everyday grocery and home items",
+    a:""
+  },
+  {
+    i:6,
+    n:"cvs puerto rico",
+    c:"health,beauty,food,business",
+    u:"https://www.cvs.com/store-locator/puerto-rico",
+    d:"cvs puerto rico provides online ordering for prescription refills, health products, beauty items and limited grocery goods for customers across puerto rico.",
+    k:"cvs puerto rico,online health and pharmacy,beauty personal care puerto rico,grocery essentials ecommerce,drugstore online ordering",
+    a:""
+  },
+  {
+    i:7,
+    n:"supermax online",
+    c:"food,home,business",
+    u:"https://supermaxonline.com/",
+    d:"supermax online is a puerto rico supermarket ecommerce site where shoppers can order groceries, fresh food and household products for delivery or pickup.",
+    k:"supermax online,online supermarket puerto rico,grocery delivery and pickup,fresh food and household products,puerto rico food shopping",
+    a:""
+  },
+  {
+    i:8,
+    n:"edo supermarket online",
+    c:"food,business",
+    u:"https://www.edosupermarket.com/online-shopping",
+    d:"edo supermarket offers online grocery shopping in puerto rico, letting customers buy meats, produce, pantry staples and daily needs from their local store.",
+    k:"edo supermarket online,online grocery puerto rico,meats produce pantry staples,daily needs supermarket ecommerce,food shopping puerto rico",
+    a:""
+  },
+  {
+    i:9,
+    n:"pr grocery",
+    c:"food,business",
+    u:"https://prgrocery.com/",
+    d:"pr grocery is an online store focused on puerto rican and caribbean food items including spices, canned goods, snacks and specialty products.",
+    k:"pr grocery,puerto rican food online,caribbean grocery items,spices canned goods snacks,specialty products ecommerce puerto rico",
+    a:""
+  },
+  {
+    i:10,
+    n:"mi tiendita pr",
+    c:"food,home,business",
+    u:"https://mitienditapr.com/",
+    d:"mi tiendita pr is an online puerto rico shop offering snacks, groceries, household items and gift baskets with delivery options.",
+    k:"mi tiendita pr,online puerto rico shop,snacks groceries household items,gift baskets ecommerce,local store puerto rico",
+    a:""
+  },
+  {
+    i:11,
+    n:"puerto rico gifts",
+    c:"home,food,business",
+    u:"https://www.puertoricogifts.com/",
+    d:"puerto rico gifts is an online gift shop selling puerto rico themed gift baskets, coffee, sweets and artisanal products that can be shipped nationwide.",
+    k:"puerto rico gifts,gift baskets online puerto rico,coffee sweets and local products,puerto rico themed gifts ecommerce,artisanal items shop",
+    a:""
+  },
+  {
+    i:12,
+    n:"boricua pride shop",
+    c:"fashion,home,business",
+    u:"https://boricuapridershop.com/",
+    d:"boricua pride shop is an online store offering puerto rico pride clothing, t-shirts, caps and home decor items featuring boricua designs.",
+    k:"boricua pride shop,puerto rico pride clothing online,t-shirts caps and apparel,boricua home decor ecommerce,boricua designs shop",
+    a:""
+  },
+  {
+    i:13,
+    n:"caribbean trading company store",
+    c:"food,home,business",
+    u:"https://caribbeantrading.com/shop/",
+    d:"caribbean trading company store sells puerto rico and caribbean themed coffee, sauces, snacks and souvenirs through its online shop.",
+    k:"caribbean trading company store,puerto rico coffee and sauces,snacks and souvenirs online,caribbean themed products ecommerce,online gift shop puerto rico",
+    a:""
+  },
+  {
+    i:14,
+    n:"universo pr",
+    c:"fashion,business",
+    u:"https://universopr.com/",
+    d:"universo pr is an online fashion boutique in puerto rico offering streetwear, dresses and accessories with a focus on local styles.",
+    k:"universo pr,online fashion boutique puerto rico,streetwear dresses accessories,local style clothing ecommerce,puerto rico fashion shop",
+    a:""
+  },
+  {
+    i:15,
+    n:"valija store online",
+    c:"fashion,business",
+    u:"https://www.valijastore.com/",
+    d:"valija store online is a puerto rico based fashion retailer selling women’s clothing, swimwear and accessories with delivery options.",
+    k:"valija store online,womens fashion puerto rico,swimwear and accessories,clothing boutique ecommerce,puerto rico fashion retailer",
+    a:""
+  },
+  {
+    i:16,
+    n:"mar Azul swimwear pr",
+    c:"fashion,business",
+    u:"https://marazulswimwearpr.com/",
+    d:"mar azul swimwear pr offers online shopping for swimwear, beachwear and resort clothing inspired by puerto rico beaches.",
+    k:"mar azul swimwear pr,swimwear shop puerto rico,beachwear and resort clothing,online bikini store,puerto rico beach fashion",
+    a:""
+  },
+  {
+    i:17,
+    n:"lulú pr",
+    c:"fashion,business",
+    u:"https://lulupr.com/",
+    d:"lulú pr is an online boutique offering curated women’s clothing, shoes and accessories for shoppers in puerto rico.",
+    k:"lulú pr,online boutique puerto rico,womens clothing and shoes,accessories ecommerce,curated fashion shop pr",
+    a:""
+  },
+  {
+    i:18,
+    n:"pure aroma pr",
+    c:"beauty,home,business",
+    u:"https://purearomapr.com/",
+    d:"pure aroma pr is an online store selling candles, home fragrances and bath products crafted in puerto rico.",
+    k:"pure aroma pr,candles and home fragrances,online bath products shop,puerto rico made aromatherapy,ecommerce home scent store",
+    a:""
+  },
+  {
+    i:19,
+    n:"kaprich pr",
+    c:"beauty,fashion,business",
+    u:"https://kaprichpr.com/",
+    d:"kaprich pr is a beauty and fashion ecommerce store in puerto rico offering cosmetics, skincare and accessories.",
+    k:"kaprich pr,beauty store puerto rico,cosmetics and skincare online,fashion accessories ecommerce,puerto rico beauty shop",
+    a:""
+  },
+  {
+    i:20,
+    n:"natucentro pr",
+    c:"health,food,business",
+    u:"https://natucentropr.com/",
+    d:"natucentro pr is an online health store in puerto rico selling supplements, natural foods and wellness products.",
+    k:"natucentro pr,health store puerto rico,supplements and natural foods,wellness products ecommerce,online nutrition shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"plaza las americas online",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://plazalasamericas.com/en/shopping-online",
+    d:"plaza las americas online connects shoppers in puerto rico with mall retailers, allowing online purchases of fashion, electronics, beauty items, home goods and food from participating stores.",
+    k:"plaza las americas online,mall shopping puerto rico,online fashion electronics beauty,home goods and food retailers,ecommerce mall platform",
+    a:""
+  },
+  {
+    i:22,
+    n:"office max puerto rico",
+    c:"business,electronics,home",
+    u:"https://www.officedepot.com/local/puerto-rico",
+    d:"office max puerto rico provides online ordering for office supplies, business equipment, computers, printers and home office essentials.",
+    k:"office max puerto rico,office supplies online,computers printers and equipment,home office essentials ecommerce,business store puerto rico",
+    a:""
+  },
+  {
+    i:23,
+    n:"auto zone puerto rico online",
+    c:"automotive,business",
+    u:"https://www.autozone.com/locations/pr.html",
+    d:"auto zone puerto rico offers online shopping for automotive parts, batteries, oils and car accessories with pickup at local stores.",
+    k:"auto zone puerto rico online,automotive parts shop pr,car batteries oils accessories,online ordering pickup,vehicle products puerto rico",
+    a:""
+  },
+  {
+    i:24,
+    n:"pep boys puerto rico",
+    c:"automotive,business",
+    u:"https://www.pepboys.com/stores/pr",
+    d:"pep boys puerto rico provides online services and product ordering for tires, auto parts and car maintenance supplies.",
+    k:"pep boys puerto rico,online tires and auto parts,car maintenance supplies ecommerce,automotive service store pr,vehicle care products",
+    a:""
+  },
+  {
+    i:25,
+    n:"bookstore pr online",
+    c:"books,business",
+    u:"https://bookstorepr.com/",
+    d:"bookstore pr online sells books in spanish and english, educational materials and stationery to readers and students in puerto rico.",
+    k:"bookstore pr online,books shop puerto rico,educational materials and stationery,spanish and english titles,ecommerce bookstore pr",
+    a:""
+  }
+];
