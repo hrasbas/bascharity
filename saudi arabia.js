cace942c91b48f5d3c240adcf0e2e719119f2667
hrasbas <hrasbasji@gmail.com>
2026-07-14 12:30:35 +0530

    Create saudi arabia.js with online shop listings
    Added a new JavaScript file containing a list of online shops in Saudi Arabia with details such as name, category, URL, description, and keywords.

---
+++ saudi arabia.js
@@ -0,0 +1,229 @@
+// saudi arabia.js
+window.countryShops = window.countryShops || {};
+window.countryShops["saudi arabia"] = [
+  {
+    i:1,
+    n:"amazon.sa",
+    c:"electronics,home,fashion,beauty,books,sports,food,business",
+    u:"https://www.amazon.sa/",
+    d:"amazon.sa is a leading online marketplace in saudi arabia offering a huge range of electronics, home goods, fashion, beauty, books, groceries and more for customers across the kingdom.[web:835][web:844]",
+    k:"amazon.sa ksa,online shopping saudi arabia,electronics mobiles and gadgets,fashion beauty home and books,groceries and everyday products marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"noon ksa",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://www.noon.com/saudi-en/",
+    d:"noon ksa is a major online shopping platform in saudi arabia where shoppers can buy mobiles, electronics, fashion, beauty, groceries and home appliances with same day delivery options.[web:840][web:835]",
+    k:"noon ksa,online shopping saudi arabia,mobiles electronics and gadgets,fashion beauty and home appliances,online grocery and daily needs marketplace",
+    a:""
+  },
+  {
+    i:3,
+    n:"haraj",
+    c:"electronics,home,automotive,fashion,business",
+    u:"https://haraj.com.sa/",
+    d:"haraj is one of the biggest classifieds marketplaces in saudi arabia where users buy and sell cars, real estate, electronics, furniture and fashion items online.[web:838][web:839]",
+    k:"haraj saudi arabia,online classifieds marketplace ksa,buy and sell cars and real estate,electronics furniture and fashion,second hand and new goods platform",
+    a:""
+  },
+  {
+    i:4,
+    n:"ubuy saudi arabia",
+    c:"electronics,fashion,home,beauty,books,sports,automotive,business",
+    u:"https://www.ubuy.com.sa/en/",
+    d:"ubuy saudi arabia is an international online marketplace offering imported electronics, apparel, mobile phones, beauty products, books, sports gear and automotive accessories to customers in ksa.[web:836][web:837]",
+    k:"ubuy saudi arabia,international online shopping ksa,imported electronics mobiles and gadgets,apparel beauty books and sports gear,automotive accessories cross border marketplace",
+    a:""
+  },
+  {
+    i:5,
+    n:"jarir bookstore online",
+    c:"electronics,books,home,business",
+    u:"https://www.jarir.com/sa-en/",
+    d:"jarir bookstore online in saudi arabia sells laptops, tablets, phones, office supplies, books and educational materials through its ecommerce platform.[web:839]",
+    k:"jarir bookstore online ksa,online electronics and books store,office supplies and stationery,education materials and textbooks,ecommerce retailer saudi arabia",
+    a:""
+  },
+  {
+    i:6,
+    n:"namshi ksa",
+    c:"fashion,beauty,business",
+    u:"https://www.namshi.com/saudi-arabia/",
+    d:"namshi ksa is an online fashion store in saudi arabia specializing in clothing, shoes and beauty products from regional and international brands.[web:839]",
+    k:"namshi ksa,online fashion shopping saudi arabia,clothing shoes and accessories,beauty products and fragrances,ecommerce style destination",
+    a:""
+  },
+  {
+    i:7,
+    n:"trendyol for saudi arabia",
+    c:"fashion,home,beauty,business",
+    u:"https://www.trendyol.com/",
+    d:"trendyol is a regional marketplace widely used in saudi arabia for affordable fashion, home products and beauty items shipped from turkey and other countries.[web:835][web:844]",
+    k:"trendyol saudi arabia,cross border fashion marketplace,affordable clothing shoes and accessories,home products and beauty items,online shopping ksa",
+    a:""
+  },
+  {
+    i:8,
+    n:"temu ksa",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.temu.com/",
+    d:"temu serves customers in saudi arabia as a bargain marketplace for low priced electronics, fashion, home goods and beauty products with app based shopping.[web:835][web:844]",
+    k:"temu ksa,bargain online shopping saudi arabia,cheap electronics gadgets and accessories,fashion home goods and beauty items,cross border ecommerce app",
+    a:""
+  },
+  {
+    i:9,
+    n:"extra stores",
+    c:"electronics,home,business",
+    u:"https://www.extra.com/en-sa/",
+    d:"extra is a leading electronics and appliance retailer in saudi arabia offering online shopping for tvs, smartphones, computers and household appliances.[web:838][web:843]",
+    k:"extra stores ksa,online electronics retailer saudi arabia,tvs smartphones computers and gadgets,household appliances ecommerce,tech and home store",
+    a:""
+  },
+  {
+    i:10,
+    n:"xcite ksa",
+    c:"electronics,home,business",
+    u:"https://www.xcite.com.sa/",
+    d:"xcite ksa is an online electronics and home appliance store where customers in saudi arabia can buy mobiles, laptops, tvs and kitchen appliances.[web:838]",
+    k:"xcite ksa,online electronics and appliances store,mobiles laptops tvs and gadgets,kitchen and home appliances shopping,tech retailer saudi arabia",
+    a:""
+  },
+  {
+    i:11,
+    n:"carrefour saudi arabia online",
+    c:"food,home,business",
+    u:"https://www.carrefourksa.com/maflko/en/",
+    d:"carrefour saudi arabia online offers supermarket grocery shopping, household essentials and non food items with delivery and pickup services across ksa.[web:839][web:843]",
+    k:"carrefour saudi arabia online,supermarket grocery shopping ksa,household essentials and non food items,delivery and pickup services,hypermarket ecommerce platform",
+    a:""
+  },
+  {
+    i:12,
+    n:"panda grocery online",
+    c:"food,home,business",
+    u:"https://www.panda.com.sa/",
+    d:"panda is a large supermarket chain in saudi arabia providing online ordering for groceries, fresh food, bakery and household products.[web:843]",
+    k:"panda grocery online ksa,online supermarket saudi arabia,groceries fresh food and bakery,household products ecommerce,hypermarket web store",
+    a:""
+  },
+  {
+    i:13,
+    n:"danube online",
+    c:"food,home,business",
+    u:"https://www.danube.sa/",
+    d:"danube online is a premium supermarket service in saudi arabia offering delivery of groceries, gourmet foods and household essentials from danube stores.[web:843]",
+    k:"danube online ksa,premium supermarket delivery,gourmet foods groceries and essentials,online shopping from danube,food ecommerce saudi arabia",
+    a:""
+  },
+  {
+    i:14,
+    n:"lulu hypermarket ksa online",
+    c:"food,home,business",
+    u:"https://www.luluhypermarket.com/en-sa/",
+    d:"lulu hypermarket ksa online sells groceries, home goods, electronics and fashion items, allowing customers to shop from hypermarkets through the website.[web:838][web:843]",
+    k:"lulu hypermarket ksa online,groceries home goods and electronics,hypermarket fashion and everyday items,online shopping saudi arabia,regional retail marketplace",
+    a:""
+  },
+  {
+    i:15,
+    n:"faces ksa",
+    c:"beauty,business",
+    u:"https://sa.faces.com/",
+    d:"faces ksa is a beauty ecommerce site in saudi arabia offering perfumes, makeup, skincare and luxury beauty brands.[web:843]",
+    k:"faces ksa,online beauty shop saudi arabia,perfumes makeup and skincare,luxury beauty brands ecommerce,fragrance and cosmetics retailer",
+    a:""
+  },
+  {
+    i:16,
+    n:"sephora saudi arabia",
+    c:"beauty,business",
+    u:"https://www.sephora.sa/en/",
+    d:"sephora saudi arabia online store sells international beauty brands, makeup, skincare and fragrances for shoppers in ksa.[web:843]",
+    k:"sephora saudi arabia,online beauty retailer ksa,international makeup skincare and fragrances,cosmetics ecommerce platform,luxury beauty shopping",
+    a:""
+  },
+  {
+    i:17,
+    n:"awwna pharmacy online",
+    c:"health,business",
+    u:"https://www.nahdi.sa/en/",
+    d:"nahdi is a major pharmacy chain in saudi arabia offering online orders for medicines, health products, personal care and wellness items.[web:843]",
+    k:"nahdi pharmacy online ksa,online medicines and health products,personal care and wellness items,pharmacy ecommerce saudi arabia,health and beauty store",
+    a:""
+  },
+  {
+    i:18,
+    n:"sigma sports ksa",
+    c:"sports,business",
+    u:"https://www.sigma-sport.com.sa/",
+    d:"sigma sports ksa is an online sports store selling fitness equipment, gym machines and accessories for home and commercial use in saudi arabia.[web:843]",
+    k:"sigma sports ksa,online fitness equipment store,gym machines and accessories,home and commercial sports gear,ecommerce sports retailer saudi arabia",
+    a:""
+  },
+  {
+    i:19,
+    n:"decathlon saudi arabia",
+    c:"sports,fashion,business",
+    u:"https://www.decathlon.sa/",
+    d:"decathlon saudi arabia online offers sporting goods, activewear and equipment for many sports including football, running, camping and fitness.[web:843]",
+    k:"decathlon saudi arabia,online sports and activewear store,sporting goods and equipment,football running camping and fitness gear,ecommerce sports marketplace",
+    a:""
+  },
+  {
+    i:20,
+    n:"home centre ksa",
+    c:"home,business",
+    u:"https://www.homecentre.com/sa/en/",
+    d:"home centre ksa is an online furniture and home decor store offering sofas, beds, dining sets, lighting and accessories for homes in saudi arabia.[web:843]",
+    k:"home centre ksa,online furniture and home decor store,sofas beds dining sets and lighting,home accessories ecommerce,interior shopping saudi arabia",
+    a:""
+  },
+  {
+    i:21,
+    n:"ikea saudi arabia",
+    c:"home,business",
+    u:"https://www.ikea.com/sa/en/",
+    d:"ikea saudi arabia online shop provides furniture, home accessories, kitchenware and storage solutions with delivery and click and collect options.[web:843]",
+    k:"ikea saudi arabia online,furniture and home accessories,kitchenware and storage solutions,delivery and click and collect,ecommerce home store ksa",
+    a:""
+  },
+  {
+    i:22,
+    n:"nice one ksa",
+    c:"beauty,business",
+    u:"https://niceonesa.com/",
+    d:"nice one ksa is a local beauty ecommerce platform selling perfumes, cosmetics, skincare and personal care products with fast delivery in saudi arabia.[web:843]",
+    k:"nice one ksa,local beauty ecommerce saudi arabia,perfumes cosmetics and skincare,personal care products fast delivery,online beauty shop ksa",
+    a:""
+  },
+  {
+    i:23,
+    n:"ajmal perfumes ksa",
+    c:"beauty,business",
+    u:"https://saudi.ajmalperfume.com/",
+    d:"ajmal perfumes ksa online store offers oriental fragrances, perfumes and gift sets tailored to customers in saudi arabia.[web:843]",
+    k:"ajmal perfumes ksa,online oriental fragrances store,perfumes and gift sets ecommerce,beauty and fragrance shopping saudi arabia,perfume retailer ksa",
+    a:""
+  },
+  {
+    i:24,
+    n:"booking.com saudi arabia",
+    c:"travel,business",
+    u:"https://www.booking.com/",
+    d:"booking.com is widely used in saudi arabia for online booking of hotels, apartments and travel accommodations in ksa and worldwide.[web:841]",
+    k:"booking.com saudi arabia,online hotel booking ksa,apartments and travel accommodations,reservations in ksa and worldwide,travel ecommerce platform",
+    a:""
+  },
+  {
+    i:25,
+    n:"carrefour travel ksa",
+    c:"travel,business",
+    u:"https://travel.carrefourksa.com/",
+    d:"carrefour travel ksa is an online travel service offering holiday packages, flights and hotel deals for saudi arabia residents.[web:843]",
+    k:"carrefour travel ksa,online holiday packages and flights,hotel deals for ksa residents,travel booking ecommerce,saudi arabia travel platform",
+    a:""
+  }
+];
