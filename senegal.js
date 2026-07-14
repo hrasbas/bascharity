6e2982e6afffcd4bd151e829ef3e122a4f9b5452
hrasbas <hrasbasji@gmail.com>
2026-07-14 12:31:49 +0530

    Add list of online shops in Senegal
    This file contains a list of online shops in Senegal, including their names, categories, URLs, descriptions, and keywords for each shop.

---
+++ senegal.js
@@ -0,0 +1,229 @@
+// senegal.js
+window.countryShops = window.countryShops || {};
+window.countryShops["senegal"] = [
+  {
+    i:1,
+    n:"jumia senegal",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://www.jumia.sn/",
+    d:"jumia senegal is a leading online marketplace in senegal where customers can buy electronics, fashion, home appliances, beauty items, groceries and more with nationwide delivery.[web:825][web:817]",
+    k:"jumia senegal,online shopping senegal,electronics mobiles and gadgets,fashion beauty and home appliances,groceries and everyday products marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"nova senegal",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.nova.sn/",
+    d:"nova senegal is a generalist online store offering a wide range of electronics, household goods, fashion and food products for customers in dakar and across senegal.[web:817]",
+    k:"nova senegal,generalist online store senegal,electronics household goods and fashion,food products and groceries,ecommerce marketplace dakar",
+    a:""
+  },
+  {
+    i:3,
+    n:"soumari",
+    c:"electronics,home,fashion,food,business",
+    u:"https://soumari.com/",
+    d:"soumari is a broad online marketplace in senegal selling electronics, home appliances, fashion, groceries and everyday items with delivery services.[web:817]",
+    k:"soumari senegal,online marketplace senegal,electronics and home appliances,fashion clothing and accessories,groceries and everyday products shopping",
+    a:""
+  },
+  {
+    i:4,
+    n:"promo.sn",
+    c:"electronics,home,fashion,business",
+    u:"https://promo.sn/",
+    d:"promo.sn is an online deals and shopping site in senegal where users can buy discounted electronics, home appliances and fashion items.[web:817]",
+    k:"promo.sn senegal,online deals and shopping,discounted electronics and appliances,fashion clothing and accessories,senegal ecommerce promotions",
+    a:""
+  },
+  {
+    i:5,
+    n:"shopmeaway senegal",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.shopmeaway.com/",
+    d:"shopmeaway enables customers in senegal to buy a wide range of products from international sites in local currency, including electronics, fashion, beauty and home goods.[web:820][web:817]",
+    k:"shopmeaway senegal,cross border online shopping senegal,international electronics and fashion,beauty and home products imported,amazon catalog localized marketplace",
+    a:""
+  },
+  {
+    i:6,
+    n:"dakar web store",
+    c:"electronics,home,fashion,business",
+    u:"https://dakarwebstore.com/",
+    d:"dakar web store is a general online shop in senegal offering electronics, home appliances, fashion and accessories for customers in dakar and beyond.[web:817]",
+    k:"dakar web store senegal,general online shop dakar,electronics and home appliances,fashion clothing and accessories,ecommerce store senegal",
+    a:""
+  },
+  {
+    i:7,
+    n:"taftaf",
+    c:"food,business",
+    u:"https://www.taftaf.sn/",
+    d:"taftaf is a fast delivery grocery and food marketplace in senegal that delivers fresh and packaged products to homes in dakar in around twenty minutes.[web:817]",
+    k:"taftaf senegal,fast grocery delivery dakar,online food marketplace senegal,fresh and packaged products to home,quick delivery ecommerce service",
+    a:""
+  },
+  {
+    i:8,
+    n:"fabella shop",
+    c:"beauty,fashion,business",
+    u:"https://www.fabellashop.com/",
+    d:"fabella shop is an online beauty and cosmetics store in senegal offering makeup, skincare, hair products and beauty accessories.[web:817]",
+    k:"fabella shop senegal,online beauty and cosmetics store,makeup skincare and hair products,beauty accessories ecommerce,maquillage leader dakar",
+    a:""
+  },
+  {
+    i:9,
+    n:"yumyum senegal",
+    c:"food,business",
+    u:"https://yumyum.sn/",
+    d:"yumyum senegal is an online food ordering site specializing in pizzas and italian dishes delivered across dakar.[web:817]",
+    k:"yumyum senegal,online pizza ordering dakar,italian food delivery senegal,restaurant delivery platform,ecommerce food service",
+    a:""
+  },
+  {
+    i:10,
+    n:"master office deco",
+    c:"home,electronics,business",
+    u:"https://masterofficedeco.sn/",
+    d:"master office deco is an online store in senegal selling office furniture, decor, computers and home appliances for businesses and households.[web:817]",
+    k:"master office deco senegal,online office furniture store,decor computers and home appliances,business and household products,ecommerce office and home shop",
+    a:""
+  },
+  {
+    i:11,
+    n:"jumia food senegal",
+    c:"food,business",
+    u:"https://food.jumia.sn/",
+    d:"jumia food senegal is an online food delivery platform that connects customers with restaurants and supermarkets for meals and grocery delivery.[web:817]",
+    k:"jumia food senegal,online restaurant delivery senegal,order meals and groceries online,dakar food delivery platform,ecommerce food marketplace",
+    a:""
+  },
+  {
+    i:12,
+    n:"discount senegal",
+    c:"home,business",
+    u:"https://discount-senegal.com/",
+    d:"discount senegal is a furniture and home goods ecommerce site offering affordable sofas, beds, tables and decor items for customers in dakar.[web:831]",
+    k:"discount senegal,online furniture store dakar,affordable sofas beds tables and decor,home goods ecommerce senegal,meuble en ligne senegal",
+    a:""
+  },
+  {
+    i:13,
+    n:"orcatrend",
+    c:"home,business",
+    u:"https://orcatrend.com/",
+    d:"orcatrend is a furniture and home decor retailer in senegal with an online catalog for living room, bedroom and office furnishings.[web:831]",
+    k:"orcatrend senegal,online furniture and decor retailer,living room bedroom and office furnishings,home interior ecommerce,dakar furniture shop",
+    a:""
+  },
+  {
+    i:14,
+    n:"awalebiz",
+    c:"fashion,home,business",
+    u:"https://awalebiz.com/",
+    d:"awalebiz is an online marketplace in senegal focused on high quality artisanal products including fashion, accessories and home decor items.[web:834]",
+    k:"awalebiz senegal,online artisanal products marketplace,handmade fashion and accessories,home decor crafts ecommerce,support local artisans senegal",
+    a:""
+  },
+  {
+    i:15,
+    n:"khouma et freres",
+    c:"electronics,home,business",
+    u:"https://khoumaetfreres.com/",
+    d:"khouma et freres is an online shop in senegal specializing in home appliances, electronics and smartphones with delivery in dakar.[web:817]",
+    k:"khouma et freres senegal,online home appliances store,electronics and smartphones retailer,dakar delivery ecommerce,electro menager senegal",
+    a:""
+  },
+  {
+    i:16,
+    n:"toubacom",
+    c:"electronics,home,business",
+    u:"https://toubacom.sn/",
+    d:"toubacom is an ecommerce site in senegal selling smartphones, electronics, appliances and accessories for local customers.[web:817]",
+    k:"toubacom senegal,online smartphones and electronics store,home appliances and accessories,local tech retailer ecommerce,telephone and electro menager dakar",
+    a:""
+  },
+  {
+    i:17,
+    n:"ambulant en ligne",
+    c:"electronics,home,business",
+    u:"https://ambulantenligne.com/",
+    d:"ambulant en ligne is a senegalese online shop offering home appliances, electronics and smartphone products with delivery options.[web:817]",
+    k:"ambulant en ligne senegal,online home appliances shop,electronics and smartphone products,dakar delivery options,ecommerce electro menager site",
+    a:""
+  },
+  {
+    i:18,
+    n:"electromenager dakar",
+    c:"electronics,home,business",
+    u:"https://electromenager-dakar.com/",
+    d:"electromenager dakar is a digital store focused on household appliances, kitchen equipment and electronics for homes in senegal.[web:817]",
+    k:"electromenager dakar senegal,online household appliances store,kitchen equipment and electronics,dakar home goods ecommerce,electro menager retailer",
+    a:""
+  },
+  {
+    i:19,
+    n:"konnectek",
+    c:"electronics,business",
+    u:"https://konnectek.sn/",
+    d:"konnectek is an online smartphone and electronics retailer in senegal that also offers selected home appliances.[web:817]",
+    k:"konnectek senegal,online smartphone retailer,electronics gadgets and accessories,selected home appliances ecommerce,tech shop dakar",
+    a:""
+  },
+  {
+    i:20,
+    n:"arc shop senegal",
+    c:"electronics,business",
+    u:"https://shop.arc.sn/",
+    d:"arc shop senegal is an online catalog for computers, networking equipment and office it supplies targeted at businesses and individual customers.[web:817]",
+    k:"arc shop senegal,online computer and networking equipment store,office it supplies ecommerce,business focused tech retailer,dakar informatics shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"kimia cosmetiques",
+    c:"beauty,business",
+    u:"https://kimiacosmetiques.com/",
+    d:"kimia cosmetiques is a beauty shop in senegal offering online sales of cosmetics, skincare and haircare products.[web:819]",
+    k:"kimia cosmetiques senegal,online cosmetics store,skincare and haircare products,beauty shop dakar,ecommerce beauty retailer",
+    a:""
+  },
+  {
+    i:22,
+    n:"afrik fashions",
+    c:"fashion,business",
+    u:"https://afrikfashions.com/",
+    d:"afrik fashions is an online clothing and accessories store in senegal featuring african inspired fashion for men and women.[web:819]",
+    k:"afrik fashions senegal,online african fashion store,clothing and accessories for men and women,traditional and modern styles ecommerce,fashion shop dakar",
+    a:""
+  },
+  {
+    i:23,
+    n:"chelectro",
+    c:"electronics,business",
+    u:"https://chelectro.sn/",
+    d:"chelectro is an electronics retailer in senegal with online ordering for appliances, tvs, fridges and other household electronics.[web:819]",
+    k:"chelectro senegal,online electronics retailer,appliances tvs fridges and devices,household electronics ecommerce,dakar electro shop",
+    a:""
+  },
+  {
+    i:24,
+    n:"electro senegal",
+    c:"electronics,business",
+    u:"https://electrosenegal.com/",
+    d:"electro senegal is an online store providing a variety of electronic devices and accessories for customers in senegal.[web:819]",
+    k:"electro senegal,online electronic devices store,gadgets and accessories retailer,tech ecommerce senegal,dakar electronics shop",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy senegal",
+    c:"electronics,fashion,home,beauty,books,sports,automotive,business",
+    u:"https://www.ubuy.sn/en/",
+    d:"ubuy senegal is an international online marketplace offering imported electronics, apparel, mobile phones, beauty products, books, sports equipment and automotive accessories in senegal.[web:816][web:823][web:818]",
+    k:"ubuy senegal,international online shopping senegal,imported electronics mobiles and gadgets,apparel beauty books and sports gear,automotive accessories cross border marketplace",
+    a:""
+  }
+];
