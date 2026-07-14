eab53c62eaf5ea1567a52fc6c6d6322a26b945ac
hrasbas <hrasbasji@gmail.com>
2026-07-14 13:27:47 +0530

    Create trinidad_and_tobago.js

---
+++ trinidad_and_tobago.js
@@ -0,0 +1,229 @@
+// trinidad_and_tobago.js
+window.countryShops = window.countryShops || {};
+window.countryShops["trinidad_and_tobago"] = [
+  {
+    i:1,
+    n:"ubuy trinidad and tobago",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://www.ubuy.tt/",
+    d:"ubuy trinidad and tobago is an international online shopping marketplace where customers can buy imported electronics, fashion, beauty, home goods, groceries, sports gear, books, software and automotive products from global brands with delivery to trinidad and tobago.",
+    k:"ubuy trinidad and tobago,international online shopping trinidad and tobago,imported electronics mobiles and gadgets,fashion beauty home kitchen and decor,groceries sports travel books,automotive parts and accessories marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"trinitrolley",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,business",
+    u:"https://trinitrolley.com/",
+    d:"trinitrolley is a large local online shopping site in trinidad and tobago offering cell phones, electronics, groceries, toys, household goods, games, books, clothing, shoes, health and beauty, jewelry, tools, sports gear and auto and industrial products for island wide customers.",
+    k:"trinitrolley,trinidad and tobago online shopping,cell phones electronics and computers,groceries toys household and games,clothing shoes health beauty and jewelry,tools sports gear and auto industrial products marketplace",
+    a:""
+  },
+  {
+    i:3,
+    n:"tootoolbay",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://tootoolbay.com/",
+    d:"tootoolbay is a trinidad online marketplace offering electronics, fashion, beauty, home and grocery items so shoppers in trinidad and tobago can order everyday products with delivery from local sellers.",
+    k:"tootoolbay,trinidad online marketplace,electronics and gadgets shopping,fashion clothing and accessories,beauty home and grocery products,trinidad and tobago ecommerce site",
+    a:""
+  },
+  {
+    i:4,
+    n:"massy stores trinidad online",
+    c:"food,home,business",
+    u:"https://www.massystorestt.com/",
+    d:"massy stores trinidad online lets customers in trinidad and tobago shop supermarket groceries, fresh produce, meat, dairy and household essentials from massy stores with pickup and delivery options.",
+    k:"massy stores trinidad online,supermarket grocery shopping trinidad,fresh produce meat and dairy,household essentials and cleaning products,online supermarket trinidad and tobago",
+    a:""
+  },
+  {
+    i:5,
+    n:"kfc trinidad and tobago online",
+    c:"food,business",
+    u:"https://kfctt.com/",
+    d:"kfc trinidad and tobago online ordering site allows customers to buy fried chicken meals, sandwiches and sides from kfc restaurants and get takeaway or delivery across trinidad and tobago.",
+    k:"kfc trinidad and tobago online,order fried chicken meals trinidad,fast food delivery and pickup,restaurant food ordering website,trinidad and tobago online food service",
+    a:""
+  },
+  {
+    i:6,
+    n:"go xip trinidad and tobago",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/tt/en",
+    d:"goxip trinidad and tobago is an online fashion and beauty shopping destination aggregating clothing, shoes, accessories and beauty products from thousands of global brands for local shoppers.",
+    k:"goxip trinidad and tobago,online fashion shopping tt,clothing shoes and accessories,beauty products and cosmetics,global brands marketplace for trinidad and tobago",
+    a:""
+  },
+  {
+    i:7,
+    n:"pin.tt classifieds marketplace",
+    c:"automotive,home,electronics,business",
+    u:"https://pin.tt/",
+    d:"pin.tt is a classifieds and marketplace site in trinidad and tobago where users can buy and sell cars, electronics, home items and other goods through online listings and ads.",
+    k:"pin.tt,trinidad and tobago classifieds,buy and sell cars and vehicles,electronics and home items listings,local marketplace for second hand goods",
+    a:""
+  },
+  {
+    i:8,
+    n:"marios pizza online",
+    c:"food,business",
+    u:"https://mymarios.com/",
+    d:"marios pizza online ordering site in trinidad and tobago lets customers order pizza, sides and drinks from marios locations with delivery or pickup services.",
+    k:"marios pizza online,trinidad pizza delivery website,order pizzas sides and drinks,fast food online ordering tt,restaurant ecommerce platform",
+    a:""
+  },
+  {
+    i:9,
+    n:"sports and games trinidad",
+    c:"sports,business",
+    u:"https://sportsandgames.co.tt/",
+    d:"sports and games trinidad is an online store selling fitness equipment, sporting goods, outdoor gear and related accessories to customers across trinidad and tobago.",
+    k:"sports and games trinidad,online sports store tt,fitness equipment and sporting goods,outdoor gear and accessories,trinidad and tobago sports ecommerce",
+    a:""
+  },
+  {
+    i:10,
+    n:"jds supercenter online",
+    c:"food,home,business",
+    u:"https://jdssupercenter.com/",
+    d:"jds supercenter online is a supermarket ecommerce site in trinidad and tobago offering groceries, packaged foods, household products and everyday essentials for delivery or pickup.",
+    k:"jds supercenter online,trinidad supermarket ecommerce,groceries and packaged foods,household products and daily essentials,online market shopping tt",
+    a:""
+  },
+  {
+    i:11,
+    n:"belgroves online store",
+    c:"home,business",
+    u:"https://belgroves.com/",
+    d:"belgroves online store in trinidad and tobago sells funeral related products, memorial items and selected home decor accessories through its ecommerce website.",
+    k:"belgroves online store,trinidad and tobago memorial products,funeral related items ecommerce,home decor accessories shopping,online specialty store tt",
+    a:""
+  },
+  {
+    i:12,
+    n:"blue waters online shop",
+    c:"food,business",
+    u:"https://shop.bluewatersproducts.com/",
+    d:"blue waters online shop lets trinidad and tobago customers order bottled water and beverages directly from blue waters products for home or office delivery.",
+    k:"blue waters online shop,trinidad bottled water delivery,order beverages online tt,home and office drinks supply,online beverage store trinidad and tobago",
+    a:""
+  },
+  {
+    i:13,
+    n:"agri world tt",
+    c:"food,home,business",
+    u:"https://agriworldtt.com/",
+    d:"agri world tt is an ecommerce site in trinidad and tobago selling agricultural supplies, seeds, gardening tools and some farm food products to farmers and home growers.",
+    k:"agri world tt,agricultural supplies online trinidad,seeds and gardening tools ecommerce,farm and food products marketplace,trinidad and tobago agri store",
+    a:""
+  },
+  {
+    i:14,
+    n:"charrans online store",
+    c:"fashion,home,business",
+    u:"https://charrans.com/",
+    d:"charrans online store in trinidad and tobago offers clothing, footwear, household goods, furniture and decor for family shopping through its ecommerce site.",
+    k:"charrans online store,trinidad clothing and footwear,household goods furniture and decor,family shopping ecommerce tt,department style store online",
+    a:""
+  },
+  {
+    i:15,
+    n:"destination trinidad and tobago shop",
+    c:"travel,business",
+    u:"https://destinationtnt.com/shop/",
+    d:"destination trinidad and tobago shop sells travel themed merchandise, local souvenirs and tour experiences online to visitors and residents of trinidad and tobago.",
+    k:"destination trinidad and tobago shop,online souvenirs and gifts,trinidad travel experiences booking,local merchandise ecommerce,travel themed shop tt",
+    a:""
+  },
+  {
+    i:16,
+    n:"ttpost hummingbird express shop",
+    c:"travel,business",
+    u:"https://ttpost.net/",
+    d:"ttpost hummingbird express shop supports online shopping logistics and package delivery services in trinidad and tobago, allowing customers to manage shipments and related products online.",
+    k:"ttpost hummingbird express,trinidad and tobago package delivery,online shipping services tt,manage skybox and deliveries,ecommerce logistics support",
+    a:""
+  },
+  {
+    i:17,
+    n:"life in trinidad and tobago store",
+    c:"home,fashion,business",
+    u:"https://lifeintrinidadandtobago.com/shop/",
+    d:"life in trinidad and tobago store sells local themed clothing, accessories, home decor and gift items celebrating trinidadian culture through online shopping.",
+    k:"life in trinidad and tobago store,local themed clothing and accessories,home decor and gifts ecommerce,trinidad culture merchandise,online lifestyle shop tt",
+    a:""
+  },
+  {
+    i:18,
+    n:"seecharan used cars online",
+    c:"automotive,business",
+    u:"https://seecharanusedcars.com/",
+    d:"seecharan used cars online in trinidad and tobago lists used vehicles for sale, letting buyers browse car inventory and submit purchase inquiries through the website.",
+    k:"seecharan used cars online,trinidad used car listings,buy vehicles online tt,automotive marketplace trinidad and tobago,second hand cars ecommerce",
+    a:""
+  },
+  {
+    i:19,
+    n:"let's shop tt",
+    c:"food,home,business",
+    u:"https://letsshoptt.com/",
+    d:"let's shop tt is a grocery deals platform in trinidad and tobago that helps shoppers find the best offers from nearby supermarkets and grocery stores through its website.",
+    k:"let's shop tt,trinidad grocery deals finder,best offers from supermarkets,online grocery comparison tt,food and household savings platform",
+    a:""
+  },
+  {
+    i:20,
+    n:"make it simple tt tech shop",
+    c:"electronics,business",
+    u:"https://www.makeitsimplett.com/",
+    d:"make it simple tt tech shop promotes curated laptops, cameras, kitchen tech and gadgets for trinidad and tobago consumers with online recommendations and purchase links.",
+    k:"make it simple tt tech shop,trinidad online tech deals,laptops cameras and gadgets,kitchen technology and devices,consumer electronics guidance ecommerce",
+    a:""
+  },
+  {
+    i:21,
+    n:"kv tv online store",
+    c:"electronics,business",
+    u:"https://kvtv.net/store/",
+    d:"kv tv online store sells media related products, tech accessories and branded items to viewers in trinidad and tobago through its ecommerce site.",
+    k:"kv tv online store,trinidad media merchandise shop,tech accessories and branded items,online sales to caribbean viewers,ecommerce for kv tv audience",
+    a:""
+  },
+  {
+    i:22,
+    n:"skybox ttpost online shopping service",
+    c:"travel,business",
+    u:"http://ttpost.net/index.php/business-solutions/hummingbird-express-online-shopping-service/",
+    d:"skybox ttpost hummingbird express online shopping service provides skybox and forwarding solutions so trinidad and tobago residents can buy from overseas websites and receive packages locally.",
+    k:"skybox ttpost online shopping,trinidad hummingbird express service,overseas shopping and forwarding,package delivery from international stores,ecommerce logistics trinidad and tobago",
+    a:""
+  },
+  {
+    i:23,
+    n:"goxip fashion trinidad women",
+    c:"fashion,business",
+    u:"https://www.goxip.com/tt/en/women",
+    d:"goxip fashion trinidad womens category features dresses, tops, shoes and accessories from many brands for online womens fashion shopping in trinidad and tobago.",
+    k:"goxip fashion trinidad womens,online womens clothing tt,dresses tops shoes and accessories,fashion marketplace for trinidad and tobago,style shopping website",
+    a:""
+  },
+  {
+    i:24,
+    n:"goxip beauty trinidad category",
+    c:"beauty,business",
+    u:"https://www.goxip.com/tt/en/beauty",
+    d:"goxip beauty trinidad category aggregates skincare, makeup, haircare and fragrance products from global brands for customers in trinidad and tobago.",
+    k:"goxip beauty trinidad,online beauty shopping tt,skincare makeup haircare and fragrance,global beauty brands marketplace,trinidad and tobago cosmetics ecommerce",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy trinidad temu brand store",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.ubuy.tt/brand/temu",
+    d:"ubuy trinidad temu brand store page showcases imported temu products including electronics, fashion, beauty, home decor and kitchen essentials for shoppers in trinidad and tobago.",
+    k:"ubuy trinidad temu brand store,temu products online tt,electronics fashion beauty and home decor,kitchen essentials imported via ubuy,trinidad and tobago global goods marketplace",
+    a:""
+  }
+];
