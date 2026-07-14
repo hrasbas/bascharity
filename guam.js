540be0809b31da4b8af58b2bd29145430bb5730c
hrasbas <hrasbasji@gmail.com>
2026-07-14 11:12:38 +0530

    Add online shops data for Guam

---
+++ guam.js
@@ -0,0 +1,229 @@
+// guam.js
+window.countryShops = window.countryShops || {};
+window.countryShops["guam"] = [
+  {
+    i:1,
+    n:"ubuy guam",
+    c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,automotive,business",
+    u:"https://www.guam.ubuy.com/en/",
+    d:"ubuy guam is an international online shopping marketplace in guam delivering imported electronics, fashion, beauty, grocery, sports gear, books, software and automotive products from global brands.",
+    k:"ubuy guam,international online shopping guam,imported electronics mobiles tablets,fashion beauty home kitchen products,global brands groceries sports travel,books software automotive accessories",
+    a:""
+  },
+  {
+    i:2,
+    n:"goxip guam",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/gu/en",
+    d:"goxip guam is an online fashion and beauty shopping destination where customers can browse millions of clothing, shoes and beauty products from thousands of brands and stores.",
+    k:"goxip guam,online fashion shopping guam,clothing shoes and accessories,beauty products marketplace,trend fashion brands online",
+    a:""
+  },
+  {
+    i:3,
+    n:"jp superstore guam",
+    c:"home,fashion,beauty,food,business",
+    u:"https://jpshoppingguam.com/",
+    d:"jp superstore is a popular shopping store in tumon guam offering guam products, brand name fashion, beauty items, home goods and souvenirs with online ordering.",
+    k:"jp superstore guam,shopping store tumon online,guam products and souvenirs,brand name fashion and beauty,home goods and gifts ecommerce",
+    a:""
+  },
+  {
+    i:4,
+    n:"guam desertcart",
+    c:"electronics,home,fashion,beauty,health,food,sports,books,business",
+    u:"https://guam.desertcart.com/",
+    d:"desertcart guam is an online marketplace shipping a wide range of international products including electronics, home items, fashion, beauty, health, food, sports gear and books to guam customers.",
+    k:"desertcart guam,international online shopping guam,imported electronics and gadgets,home fashion beauty health products,books sports and food delivery",
+    a:""
+  },
+  {
+    i:5,
+    n:"k-mart guam online",
+    c:"home,fashion,beauty,food,electronics,business",
+    u:"https://www.kmartguam.com/",
+    d:"k-mart guam online extends the large department store in guam with an ecommerce site offering home goods, apparel, electronics, beauty and grocery items for local customers.",
+    k:"k-mart guam online,department store guam ecommerce,home goods and furniture,clothing electronics beauty grocery,online shopping guam",
+    a:""
+  },
+  {
+    i:6,
+    n:"cost u less guam",
+    c:"food,home,business",
+    u:"https://www.costuless.com/guam-online-shopping",
+    d:"cost u less guam provides online grocery shopping and bulk food deals along with household items so residents of guam can order daily needs and have them ready for pickup or delivery.",
+    k:"cost u less guam,online grocery shopping guam,bulk food and daily needs,household items ecommerce,warehouse store guam online",
+    a:""
+  },
+  {
+    i:7,
+    n:"payless supermarkets guam",
+    c:"food,business",
+    u:"https://www.paylessmarkets.com/",
+    d:"payless supermarkets guam is a local grocery chain offering online ordering for supermarket products including fresh food, pantry staples and household essentials.",
+    k:"payless supermarkets guam,online supermarket guam,grocery delivery and pickup,fresh food and pantry staples,local grocery chain ecommerce",
+    a:""
+  },
+  {
+    i:8,
+    n:"mada marketplace guam",
+    c:"food,home,business",
+    u:"https://www.madamarketplace.com/",
+    d:"mada marketplace is an online store in guam featuring local food products, home items and island-inspired goods from guam and micronesia.",
+    k:"mada marketplace guam,local food products online,home and island inspired goods,guam micronesia marketplace,ecommerce store guam",
+    a:""
+  },
+  {
+    i:9,
+    n:"chocolate house guam",
+    c:"food,business",
+    u:"https://www.chocolatehouseguam.com/",
+    d:"chocolate house guam is an online chocolatier offering guam themed chocolates, truffles and confectionery gifts that can be ordered online for pickup or delivery.",
+    k:"chocolate house guam,online chocolate shop guam,guam themed chocolates and truffles,sweet gifts and confectionery,ecommerce dessert store",
+    a:""
+  },
+  {
+    i:10,
+    n:"kelaguen box guam",
+    c:"food,business",
+    u:"https://kelaguenbox.com/",
+    d:"kelaguen box guam provides online ordering for guam cuisine meal boxes and local dishes so customers can enjoy traditional flavors conveniently at home.",
+    k:"kelaguen box guam,online guam food boxes,local cuisine delivery guam,traditional meal kits ecommerce,chamorro food online",
+    a:""
+  },
+  {
+    i:11,
+    n:"guam gifts",
+    c:"home,food,business",
+    u:"https://www.guamgifts.com/",
+    d:"guam gifts is an online shop specializing in guam themed souvenirs, gift baskets, local snacks and island decor for residents and visitors.",
+    k:"guam gifts,guam themed souvenirs online,gift baskets and local snacks,island decor ecommerce,gifts shop guam",
+    a:""
+  },
+  {
+    i:12,
+    n:"island girl fashions guam",
+    c:"fashion,business",
+    u:"https://www.islandgirlfashions.com/",
+    d:"island girl fashions guam is an online boutique offering island-inspired dresses, resort wear and accessories for women shopping from guam.",
+    k:"island girl fashions guam,online boutique guam,island inspired dresses,resort wear and accessories,womens fashion ecommerce",
+    a:""
+  },
+  {
+    i:13,
+    n:"denanche guam",
+    c:"food,business",
+    u:"https://denancheguam.com/",
+    d:"denanche guam is an online hot sauce and condiments store selling guam style denanche, spicy sauces and related food products worldwide.",
+    k:"denanche guam,guam hot sauce online,spicy condiments store,local food products ecommerce,denanche bottles and sets",
+    a:""
+  },
+  {
+    i:14,
+    n:"proa bakery guam",
+    c:"food,business",
+    u:"https://proabakery.com/",
+    d:"proa bakery guam offers online ordering for breads, pastries, cakes and baked goods so customers can reserve sweet treats from home.",
+    k:"proa bakery guam,online bakery shop guam,breads pastries cakes,order baked goods online,local bakery ecommerce",
+    a:""
+  },
+  {
+    i:15,
+    n:"guam gift shop at jp",
+    c:"home,food,business",
+    u:"https://jpshoppingguam.com/collections/guam-gifts",
+    d:"guam gift shop at jp superstore is an online section focused on guam souvenirs, t-shirts, snacks and island themed products available to order from guam.",
+    k:"guam gift shop at jp,guam souvenirs online,t-shirts snacks and island products,gift items ecommerce guam,shop guam themed goods",
+    a:""
+  },
+  {
+    i:16,
+    n:"pacific island store guam",
+    c:"home,fashion,business",
+    u:"https://pacificislandstore.com/",
+    d:"pacific island store guam is an online retailer of island clothing, tropical decor and accessories inspired by pacific cultures including guam.",
+    k:"pacific island store guam,island clothing online,tropical decor and accessories,pacific inspired fashion ecommerce,guam island shop",
+    a:""
+  },
+  {
+    i:17,
+    n:"guam online mall",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://guamonlinemall.com/",
+    d:"guam online mall is a marketplace style ecommerce site bringing together local vendors selling electronics, home goods, fashion, beauty, food and sports items to guam shoppers.",
+    k:"guam online mall,local marketplace guam,electronics home fashion beauty,food and sports products online,vendors ecommerce platform",
+    a:""
+  },
+  {
+    i:18,
+    n:"guam coffee company",
+    c:"food,business",
+    u:"https://www.guamcoffee.com/",
+    d:"guam coffee company is an online shop offering guam roasted coffee beans, blends and related accessories for home and office use.",
+    k:"guam coffee company,guam roasted coffee online,coffee beans and blends,coffee accessories ecommerce,local coffee brand guam",
+    a:""
+  },
+  {
+    i:19,
+    n:"marianas cable online store",
+    c:"electronics,business",
+    u:"https://shop.marianascable.com/",
+    d:"marianas cable online store in guam sells electronics accessories, networking equipment and related tech products to local customers.",
+    k:"marianas cable online store,networking equipment guam,electronics accessories shop,tech products ecommerce,online store guam",
+    a:""
+  },
+  {
+    i:20,
+    n:"guam pet supplies",
+    c:"food,health,business",
+    u:"https://guampetsupplies.com/",
+    d:"guam pet supplies is an online pet store providing pet food, accessories and health products for dogs, cats and other pets in guam.",
+    k:"guam pet supplies,online pet store guam,pet food and accessories,pet health products ecommerce,pet supplies delivery guam",
+    a:""
+  },
+  {
+    i:21,
+    n:"guam toys",
+    c:"home,sports,business",
+    u:"https://guamtoys.com/",
+    d:"guam toys is an online toy shop offering kids toys, games and outdoor play products for families in guam.",
+    k:"guam toys,online toy shop guam,kids toys and games,outdoor play products ecommerce,children store guam",
+    a:""
+  },
+  {
+    i:22,
+    n:"guam car accessories online",
+    c:"automotive,business",
+    u:"https://guamcaraccessories.com/",
+    d:"guam car accessories online sells automotive accessories such as seat covers, mats and car care products to vehicle owners in guam.",
+    k:"guam car accessories online,automotive accessories shop,seat covers mats car care,vehicle products ecommerce guam,car owners online store",
+    a:""
+  },
+  {
+    i:23,
+    n:"guam sports outlet",
+    c:"sports,fashion,business",
+    u:"https://guamsportsoutlet.com/",
+    d:"guam sports outlet is an online store offering sportswear, footwear and basic sports equipment for active customers in guam.",
+    k:"guam sports outlet,online sportswear shop guam,footwear and sports equipment,sports clothing ecommerce,fitness gear guam",
+    a:""
+  },
+  {
+    i:24,
+    n:"guam health foods",
+    c:"food,health,business",
+    u:"https://guamhealthfoods.com/",
+    d:"guam health foods is an online retailer focusing on health food, supplements and natural grocery items for wellness minded shoppers in guam.",
+    k:"guam health foods,health food shop guam,supplements and natural grocery,wellness products ecommerce,online store guam",
+    a:""
+  },
+  {
+    i:25,
+    n:"guam bookshop online",
+    c:"books,business",
+    u:"https://guambookshop.com/",
+    d:"guam bookshop online sells books about guam, regional literature, educational materials and stationery for students and readers.",
+    k:"guam bookshop online,books about guam,regional literature and education,stationery shop ecommerce,online bookstore guam",
+    a:""
+  }
+];
