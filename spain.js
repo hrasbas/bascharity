3c604dd63d5216a9a3533e26cec05fdd3c02306d
hrasbas <hrasbasji@gmail.com>
2026-07-14 13:10:04 +0530

    Add online shops data for Spain

---
+++ spain.js
@@ -0,0 +1,229 @@
+// spain.js
+window.countryShops = window.countryShops || {};
+window.countryShops["spain"] = [
+  {
+    i:1,
+    n:"amazon.es",
+    c:"electronics,home,fashion,beauty,food,sports,books,software,automotive,business",
+    u:"https://www.amazon.es/",
+    d:"amazon.es is one of the largest online marketplaces in spain where customers can buy millions of products including electronics, home appliances, fashion, books, groceries, beauty and automotive items with fast delivery options.[web:654][web:655][web:656][web:660][web:661]",
+    k:"amazon.es spain,online marketplace spain,electronics mobiles computers and gadgets,home appliances fashion books and groceries,prime delivery ecommerce platform",
+    a:""
+  },
+  {
+    i:2,
+    n:"aliexpress spain",
+    c:"electronics,fashion,home,beauty,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress serves shoppers in spain with cross border online shopping for low cost electronics, fashion, home goods and beauty products from international sellers.[web:655][web:656][web:661]",
+    k:"aliexpress spain,cross border online shopping spain,cheap electronics gadgets and accessories,fashion home goods and beauty products,international marketplace ecommerce",
+    a:""
+  },
+  {
+    i:3,
+    n:"elcorteingles.es",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.elcorteingles.es/",
+    d:"elcorteingles.es is the online store of el corte inglés in spain offering department store shopping for fashion, electronics, home, beauty and gourmet food with delivery and click and collect.[web:654][web:655][web:656][web:658][web:661]",
+    k:"elcorteingles.es spain,online department store spain,fashion electronics home and beauty,gourmet food and supermarket shopping,click and collect ecommerce",
+    a:""
+  },
+  {
+    i:4,
+    n:"carrefour.es",
+    c:"food,home,electronics,business",
+    u:"https://www.carrefour.es/",
+    d:"carrefour.es is the online hypermarket for carrefour spain providing groceries, fresh food, household goods and electronics with home delivery and pickup from stores.[web:654][web:655][web:658][web:661]",
+    k:"carrefour.es spain,online hypermarket groceries,fresh food household goods and electronics,home delivery and store pickup,ecommerce supermarket spain",
+    a:""
+  },
+  {
+    i:5,
+    n:"mediamarkt.es",
+    c:"electronics,business",
+    u:"https://www.mediamarkt.es/",
+    d:"mediamarkt.es is a leading electronics online store in spain selling tvs, computers, mobiles, gaming and household appliances with competitive prices and local delivery.[web:654][web:655][web:658]",
+    k:"mediamarkt.es spain,online electronics store spain,tvs computers mobiles and gaming,household appliances retailer,ecommerce tech shop mediamarkt",
+    a:""
+  },
+  {
+    i:6,
+    n:"pccomponentes.com",
+    c:"electronics,business",
+    u:"https://www.pccomponentes.com/",
+    d:"pccomponentes.com is a specialist tech ecommerce site in spain offering pc components, laptops, gaming gear and consumer electronics to enthusiasts and businesses.[web:654][web:655][web:658][web:661]",
+    k:"pccomponentes.com spain,online pc components store,laptops gaming gear and electronics,tech enthusiasts ecommerce,b2c and b2b technology retailer",
+    a:""
+  },
+  {
+    i:7,
+    n:"decathlon.es",
+    c:"sports,home,business",
+    u:"https://www.decathlon.es/",
+    d:"decathlon.es is the sports and outdoor online shop for spain selling fitness equipment, sportswear, camping gear and accessories for many disciplines.[web:654][web:655]",
+    k:"decathlon.es spain,online sports and outdoor shop,fitness equipment sportswear and footwear,camping gear and accessories,ecommerce sporting goods retailer",
+    a:""
+  },
+  {
+    i:8,
+    n:"leroymerlin.es",
+    c:"home,business",
+    u:"https://www.leroymerlin.es/",
+    d:"leroymerlin.es is a home improvement and diy ecommerce store in spain offering furniture, decor, garden supplies, tools and building materials for homeowners and professionals.[web:654][web:655]",
+    k:"leroymerlin.es spain,online home improvement store,diy tools garden supplies and decor,furniture and building materials,ecommerce for renovation projects",
+    a:""
+  },
+  {
+    i:9,
+    n:"ikea spain online",
+    c:"home,business",
+    u:"https://www.ikea.com/es/es/",
+    d:"ikea spain online sells furniture, home decor, kitchenware and storage solutions, letting customers in spain shop ikea products and arrange delivery or pickup.[web:655]",
+    k:"ikea spain online,furniture and home decor store,kitchenware storage and organisers,online shopping ikea products,ecommerce home furnishings",
+    a:""
+  },
+  {
+    i:10,
+    n:"lidl.es",
+    c:"food,home,business",
+    u:"https://www.lidl.es/",
+    d:"lidl.es provides online access to lidl's weekly offers in spain including groceries, household goods and non food specials with information for ordering and store services.[web:655]",
+    k:"lidl.es spain,online groceries and weekly offers,household goods and non food specials,discount supermarket ecommerce,low price shopping spain",
+    a:""
+  },
+  {
+    i:11,
+    n:"zara.com spain",
+    c:"fashion,business",
+    u:"https://www.zara.com/es/",
+    d:"zara.com spain is the official online fashion store of zara offering womens, mens and kids clothing, shoes and accessories with home delivery and store returns.[web:654][web:655][web:658]",
+    k:"zara.com spain,online fashion store spain,womens mens and kids clothing,shoes and accessories ecommerce,zara official shop with delivery",
+    a:""
+  },
+  {
+    i:12,
+    n:"zalando.es",
+    c:"fashion,business",
+    u:"https://www.zalando.es/",
+    d:"zalando.es is a multi brand fashion ecommerce platform in spain where customers can buy shoes, clothing and accessories from international and local brands.[web:655][web:661]",
+    k:"zalando.es spain,online multi brand fashion store,shoes clothing and accessories marketplace,international and local fashion brands,ecommerce platform zalando",
+    a:""
+  },
+  {
+    i:13,
+    n:"bershka online spain",
+    c:"fashion,business",
+    u:"https://www.bershka.com/es/es/",
+    d:"bershka online spain sells trendy youth fashion including dresses, tops, jeans and streetwear aimed at young shoppers with easy online ordering.[web:661]",
+    k:"bershka online spain,youth fashion online store,dresses tops jeans and streetwear,trendy clothing ecommerce,inditex brand shop spain",
+    a:""
+  },
+  {
+    i:14,
+    n:"shein.com spain",
+    c:"fashion,beauty,business",
+    u:"https://es.shein.com/",
+    d:"shein.com spain offers low cost fast fashion, accessories and some beauty items shipped to customers across spain from its global online platform.[web:655][web:658]",
+    k:"shein spain,online fast fashion spain,cheap clothing accessories and beauty,global ecommerce platform shein,cross border fashion shopping",
+    a:""
+  },
+  {
+    i:15,
+    n:"wallapop.com",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://es.wallapop.com/",
+    d:"wallapop.com is a popular second hand marketplace in spain where users buy and sell used electronics, furniture, fashion and vehicles through listings and in app chat.[web:654][web:656]",
+    k:"wallapop spain,second hand marketplace spain,buy and sell used electronics and furniture,pre owned fashion and vehicles,classifieds style ecommerce platform",
+    a:""
+  },
+  {
+    i:16,
+    n:"chollometro.com",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.chollometro.com/",
+    d:"chollometro.com is a deals community in spain aggregating online offers for electronics, fashion, groceries and everyday products from many stores and marketplaces.[web:655]",
+    k:"chollometro.com spain,online deals and bargains community,electronics fashion groceries offers,ecommerce discounts aggregator,shopping deals platform spain",
+    a:""
+  },
+  {
+    i:17,
+    n:"druni.es",
+    c:"beauty,health,business",
+    u:"https://www.druni.es/",
+    d:"druni.es is a spanish beauty and cosmetics online store offering perfumes, makeup, skincare and personal care products from major brands.[web:654]",
+    k:"druni.es spain,online beauty and cosmetics store,perfumes makeup skincare and personal care,branded products ecommerce,beauty shop druni",
+    a:""
+  },
+  {
+    i:18,
+    n:"privalia spain",
+    c:"fashion,home,business",
+    u:"https://es.privalia.com/",
+    d:"privalia spain runs an online outlet and flash sale platform with discounted fashion, footwear, home textiles and decor from well known brands.[web:661]",
+    k:"privalia spain,online outlet and flash sales,discount fashion footwear and home textiles,brand clearance ecommerce,private sale shopping platform",
+    a:""
+  },
+  {
+    i:19,
+    n:"vinted spain",
+    c:"fashion,business",
+    u:"https://www.vinted.es/",
+    d:"vinted spain is a second hand clothing marketplace where individuals buy and sell used fashion, shoes and accessories through a secure app and website.[web:661]",
+    k:"vinted spain,second hand clothing marketplace,buy and sell used fashion shoes and accessories,peer to peer ecommerce platform,pre loved clothes shopping",
+    a:""
+  },
+  {
+    i:20,
+    n:"ebay spain",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.ebay.es/",
+    d:"ebay spain offers auctions and fixed price listings for electronics, fashion, collectibles, home items and vehicles, connecting spanish buyers with global sellers.[web:661]",
+    k:"ebay spain,online auctions and fixed price listings,electronics fashion collectibles and home items,vehicles and parts marketplace,global ecommerce platform",
+    a:""
+  },
+  {
+    i:21,
+    n:"sprinter.es",
+    c:"sports,fashion,business",
+    u:"https://www.sprinter.es/",
+    d:"sprinter.es is a spanish sportswear and footwear ecommerce store offering sneakers, gym clothing and equipment for men, women and kids.[web:662]",
+    k:"sprinter.es spain,online sportswear and footwear,sneakers gym clothing and equipment,sport fashion ecommerce,athletic gear store spain",
+    a:""
+  },
+  {
+    i:22,
+    n:"mango.com spain",
+    c:"fashion,business",
+    u:"https://shop.mango.com/es",
+    d:"mango.com spain is the official online shop for mango offering contemporary fashion for women, men and kids including clothing, shoes and accessories.[web:662]",
+    k:"mango.com spain,online contemporary fashion store,womens mens and kids clothing,shoes and accessories ecommerce,mango official shop spain",
+    a:""
+  },
+  {
+    i:23,
+    n:"pullandbear.com spain",
+    c:"fashion,business",
+    u:"https://www.pullandbear.com/es/",
+    d:"pullandbear.com spain sells casual fashion and streetwear for young people including jeans, hoodies, t shirts and accessories online.[web:662]",
+    k:"pullandbear spain,online casual fashion and streetwear,jeans hoodies t shirts and accessories,youth clothing ecommerce,inditex brand pullandbear shop",
+    a:""
+  },
+  {
+    i:24,
+    n:"sprinter outlet online",
+    c:"sports,fashion,business",
+    u:"https://outlet.sprinter.es/",
+    d:"sprinter outlet online offers discounted sportswear, shoes and equipment for spain customers looking for clearance deals from sprinter brand.[web:662]",
+    k:"sprinter outlet spain,discount sportswear and shoes,clearance sports equipment deals,online outlet ecommerce,sprinter brand bargains shop",
+    a:""
+  },
+  {
+    i:25,
+    n:"dia.es online shop",
+    c:"food,home,business",
+    u:"https://www.dia.es/compra-online/",
+    d:"dia.es online shop allows customers in spain to order groceries, beverages and household products from dia supermarkets for home delivery or store pickup.[web:662]",
+    k:"dia online spain,supermarket groceries and beverages,household products with delivery or pickup,online shopping dia supermarkets,ecommerce grocery platform spain",
+    a:""
+  }
+];
