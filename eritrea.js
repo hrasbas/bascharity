724bd56ea632baedfebd89903cc7e27a66d0cb55
hrasbas <hrasbasji@gmail.com>
2026-07-15 12:28:58 +0530

    Create eritrea.js with online shop listings
    Added a list of online shops and resources for Eritrean products, including categories like fashion, food, and home goods.

---
+++ eritrea.js
@@ -0,0 +1,229 @@
+// eritrea.js
+window.countryShops = window.countryShops || {};
+window.countryShops["eritrea"] = [
+  {
+    i:1,
+    n:"eritrea online shop facebook",
+    c:"fashion,jewellery,business",
+    u:"https://www.facebook.com/eritreaonline/",
+    d:"eritrea online shop is a small online boutique using facebook to sell clothing, jewellery and accessories to customers in eritrea and abroad via online orders and digital payments.[web:1743]",
+    k:"eritrea online shop facebook,online clothing and jewellery eritrea,small boutique accessories via social media,digital payment fashion shop eritrea",
+    a:""
+  },
+  {
+    i:2,
+    n:"d&d clothing mens clothing eritrea",
+    c:"fashion,business",
+    u:"https://danddclothing.com/pages/mens-clothing-in-eritrea",
+    d:"d&d clothing offers an online mens clothing collection targeted to customers in eritrea with shirts, coats, jackets, shorts, jeans and casual wear that can be ordered internationally.[web:1741]",
+    k:"d&d clothing mens clothing eritrea,online mens fashion shirts coats jackets jeans,eritrea menswear international delivery,danddclothing.com african fashion",
+    a:""
+  },
+  {
+    i:3,
+    n:"habesha store belgium for eritrea",
+    c:"food,home,beauty,books,business",
+    u:"https://www.habeshastore.be/",
+    d:"habesha store in antwerp runs an online shop that ships ethiopian and eritrean products like kitchen accessories, food, spices, drinks, cosmetics, textiles and books to customers across europe, including the eritrean diaspora.[web:1748]",
+    k:"habesha store belgium for eritrea,online ethiopian and eritrean products,kitchen accessories food spices drinks cosmetics textiles books,habeshastore.be diaspora shopping",
+    a:""
+  },
+  {
+    i:4,
+    n:"habesha outlets eritrean products",
+    c:"food,home,beauty,business",
+    u:"https://habeshaoutlets.com/blogs/habeshaupdates/habesha-store-for-ethiopian-eritrean-products-habesha-outlets",
+    d:"habesha outlets is an online habesha marketplace connecting diaspora customers with genuine ethiopian and eritrean products and services such as packaged foods, coffee, spices and cultural items.[web:1749]",
+    k:"habesha outlets eritrean products,online habesha marketplace for ethiopian and eritrean goods,packaged foods coffee spices cultural items,diaspora online shopping habesha outlets",
+    a:""
+  },
+  {
+    i:5,
+    n:"raena shop eritrean products",
+    c:"food,home,beauty,business",
+    u:"https://askai.glarity.app/search/What-are-some-options-for-online-shopping-in-Eritrea",
+    d:"raena shop is mentioned as an online platform specializing in authentic eritrean and ethiopian products, serving customers who want to buy real habesha foods, spices and household items online.[web:1750]",
+    k:"raena shop eritrean products,authentic eritrean and ethiopian items online,habesha foods spices household goods,online marketplace raena shop",
+    a:""
+  },
+  {
+    i:6,
+    n:"adulis online habesha foods",
+    c:"food,home,business",
+    u:"https://adulisonline.nl/",
+    d:"adulis online is a dutch based store offering ethiopian coffee, teff and other habesha products that are popular with eritrean customers in europe who want traditional ingredients shipped to their homes.[web:1746]",
+    k:"adulis online habesha foods,ethiopian coffee teff and products for eritreans,traditional ingredients shipped in europe,online habesha food shop adulisonline.nl",
+    a:""
+  },
+  {
+    i:7,
+    n:"eritrea dress on etsy",
+    c:"fashion,business",
+    u:"https://www.etsy.com/in-en/market/eritrea_dress",
+    d:"the eritrea dress category on etsy features handmade and vintage style eritrean dresses and cultural outfits that people around the world can order from independent designers.[web:1745]",
+    k:"eritrea dress on etsy,handmade and vintage eritrean dresses,cultural outfits from independent designers,online traditional clothing shopping",
+    a:""
+  },
+  {
+    i:8,
+    n:"eritrea traditional clothes aliexpress",
+    c:"fashion,jewellery,business",
+    u:"https://www.aliexpress.com/w/wholesale-eritrea-traditional-clothes.html",
+    d:"aliexpress hosts multiple sellers offering eritrea traditional clothes and jewellery sets so buyers can order affordable cultural fashion and accessories online for worldwide delivery.[web:1747]",
+    k:"eritrea traditional clothes aliexpress,online cultural fashion and jewellery sets,affordable traditional outfits with worldwide delivery,aliexpress eritrea style clothing",
+    a:""
+  },
+  {
+    i:9,
+    n:"d&d clothing habesha wear",
+    c:"fashion,business",
+    u:"https://danddclothing.com/",
+    d:"d&d clothing main site sells african inspired clothing including habesha and eritrean outfits for men and women with custom tailoring and international shipping.[web:1741]",
+    k:"d&d clothing habesha wear,african inspired and eritrean outfits,custom tailoring with international shipping,online african fashion store danddclothing.com",
+    a:""
+  },
+  {
+    i:10,
+    n:"habesha store cosmetics and textiles",
+    c:"beauty,home,business",
+    u:"https://www.habeshastore.be/",
+    d:"habesha store also stocks cosmetics, textiles and small home accessories influenced by ethiopian and eritrean culture, available to order online for gifting or personal use.[web:1748]",
+    k:"habesha store cosmetics and textiles,beauty products and cultural textiles,home accessories with ethiopian and eritrean style,online habesha lifestyle shop",
+    a:""
+  },
+  {
+    i:11,
+    n:"habesha outlets fashion and gifts",
+    c:"fashion,home,beauty,business",
+    u:"https://habeshaoutlets.com/",
+    d:"habesha outlets' main store sells habesha clothing, scarves, accessories, coffee sets and gift boxes focused on ethiopian and eritrean heritage for diaspora shoppers online.[web:1749]",
+    k:"habesha outlets fashion and gifts,habesha clothing scarves accessories coffee sets,gift boxes with ethiopian and eritrean heritage,online diaspora shop habeshaoutlets.com",
+    a:""
+  },
+  {
+    i:12,
+    n:"adulis online kitchenware",
+    c:"home,business",
+    u:"https://adulisonline.nl/",
+    d:"adulis online also offers kitchen accessories and utensils used in eritrean and ethiopian cooking, allowing customers to recreate traditional dishes at home.[web:1746]",
+    k:"adulis online kitchenware,utensils for eritrean and ethiopian cooking,traditional kitchen accessories online,habesha cookware shop adulisonline.nl",
+    a:""
+  },
+  {
+    i:13,
+    n:"eritrea themed jewellery etsy",
+    c:"jewellery,business",
+    u:"https://www.etsy.com/",
+    d:"etsy hosts several sellers making eritrea themed jewellery such as pendants, earrings and bracelets that celebrate the country and can be ordered worldwide.[web:1745]",
+    k:"eritrea themed jewellery etsy,pendants earrings bracelets with eritrea design,handmade accessories from global artisans,online jewellery marketplace etsy",
+    a:""
+  },
+  {
+    i:14,
+    n:"habesha store books and culture",
+    c:"books,business",
+    u:"https://www.habeshastore.be/",
+    d:"habesha store includes a books section with titles about ethiopian and eritrean culture, language and history which customers can buy online.[web:1748]",
+    k:"habesha store books and culture,books on ethiopian and eritrean history and language,online cultural bookstore for habesha diaspora,habeshastore.be books",
+    a:""
+  },
+  {
+    i:15,
+    n:"habesha outlets coffee and spices",
+    c:"food,business",
+    u:"https://habeshaoutlets.com/",
+    d:"habesha outlets sells roasted coffee, spice blends and injera related products that eritreans abroad can purchase online to maintain traditional food habits.[web:1749]",
+    k:"habesha outlets coffee and spices,roasted coffee spice blends injera products,traditional food ingredients for eritreans abroad,online habesha grocery habeshaoutlets.com",
+    a:""
+  },
+  {
+    i:16,
+    n:"adulis online teff products",
+    c:"food,business",
+    u:"https://adulisonline.nl/",
+    d:"adulis online specializes in teff flour and related products that are essential for injera, making it a convenient online source for eritrean and ethiopian staple ingredients.[web:1746]",
+    k:"adulis online teff products,teff flour and injera ingredients,staple food items for eritrean and ethiopian cuisine,online teff store adulisonline.nl",
+    a:""
+  },
+  {
+    i:17,
+    n:"habesha outlets home decor",
+    c:"home,business",
+    u:"https://habeshaoutlets.com/",
+    d:"habesha outlets offers small home decor pieces and cultural art inspired by ethiopia and eritrea that customers can order online to decorate their homes.[web:1749]",
+    k:"habesha outlets home decor,cultural art and decor from ethiopia and eritrea,home accessories for diaspora customers,online habesha decor shop",
+    a:""
+  },
+  {
+    i:18,
+    n:"eritrea t shirts facebook shop",
+    c:"fashion,business",
+    u:"https://www.facebook.com/eritreaonline/",
+    d:"the eritrea online shop facebook page promotes printed eritrea themed t shirts and casual clothing that fans can order via messages and online payment links.[web:1743]",
+    k:"eritrea t shirts facebook shop,eritrea themed printed t shirts online,casual clothing via social media orders,facebook based fashion shop eritrea",
+    a:""
+  },
+  {
+    i:19,
+    n:"eritrea clothing section dan&d",
+    c:"fashion,business",
+    u:"https://danddclothing.com/",
+    d:"on d&d clothing customers can find special eritrean and habesha designs within the wider african clothing collection, suitable for weddings, holidays and cultural events.[web:1741]",
+    k:"eritrea clothing section d&d,eritrean and habesha designs for events,african clothing for weddings and holidays,online special occasion outfits",
+    a:""
+  },
+  {
+    i:20,
+    n:"habesha store gift ideas",
+    c:"fashion,home,beauty,business",
+    u:"https://www.habeshastore.be/",
+    d:"habesha store highlights curated gift ideas combining food, coffee, textiles and beauty items that reflect eritrean and ethiopian culture for online gifting.[web:1748]",
+    k:"habesha store gift ideas,curated gifts with food coffee textiles beauty,eritrean and ethiopian cultural gift boxes,online gifting shop habeshastore.be",
+    a:""
+  },
+  {
+    i:21,
+    n:"habesha outlets kids clothing",
+    c:"fashion,business",
+    u:"https://habeshaoutlets.com/",
+    d:"habesha outlets also stocks habesha style kids clothing that lets eritrean and ethiopian children abroad wear traditional designs purchased online.[web:1749]",
+    k:"habesha outlets kids clothing,habesha style outfits for children,traditional designs for eritrean and ethiopian kids,online kids fashion habesha outlets",
+    a:""
+  },
+  {
+    i:22,
+    n:"adulis online beverages",
+    c:"food,business",
+    u:"https://adulisonline.nl/",
+    d:"adulis online sells traditional drinks and beverage ingredients such as coffee and spices which are common in eritrean households and can be ordered via the webshop.[web:1746]",
+    k:"adulis online beverages,traditional coffee and drink ingredients,eritrean household beverage supplies online,habesha drink shop adulisonline.nl",
+    a:""
+  },
+  {
+    i:23,
+    n:"habesha store textiles and scarves",
+    c:"fashion,home,business",
+    u:"https://www.habeshastore.be/",
+    d:"habesha store textiles and scarves section lists netela, cultural shawls and table textiles inspired by eritrean and ethiopian weaving traditions for online purchase.[web:1748]",
+    k:"habesha store textiles and scarves,netela and cultural shawls table textiles,eritrean and ethiopian weaving traditions,online textile shop habeshastore.be",
+    a:""
+  },
+  {
+    i:24,
+    n:"habesha outlets accessories",
+    c:"fashion,jewellery,business",
+    u:"https://habeshaoutlets.com/",
+    d:"habesha outlets accessories range includes earrings, necklaces, hair accessories and belts that match eritrean and ethiopian outfits and can be ordered online worldwide.[web:1749]",
+    k:"habesha outlets accessories,earrings necklaces hair accessories belts,match eritrean and ethiopian outfits,online accessory store habeshaoutlets.com",
+    a:""
+  },
+  {
+    i:25,
+    n:"online shopping options eritrea overview",
+    c:"software,business",
+    u:"https://askai.glarity.app/search/What-are-some-options-for-online-shopping-in-Eritrea",
+    d:"an online overview of shopping options in eritrea mentions platforms like raena shop and social media based boutiques that help people access eritrean and ethiopian goods through ecommerce.[web:1750][web:1743][web:1749][web:1748]",
+    k:"online shopping options eritrea,raena shop and social media boutiques,access eritrean and ethiopian goods via ecommerce,overview of eritrea online shopping platforms",
+    a:""
+  }
+];
