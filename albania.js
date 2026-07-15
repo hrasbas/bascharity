2bc01cdabd035e17b50324366323f3e91cbd2745
hrasbas <hrasbasji@gmail.com>
2026-07-15 10:29:37 +0530

    Add online shops data for Albania
    This file contains a list of online shops available in Albania, including their categories, URLs, descriptions, and keywords.

---
+++ albania.js
@@ -0,0 +1,229 @@
+// albania.js
+window.countryShops = window.countryShops || {};
+window.countryShops["albania"] = [
+  {
+    i:1,
+    n:"ubuy albania english",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.al/en/",
+    d:"ubuy albania is an international online shopping store where customers in albania can buy imported electronics, mobiles, fashion, beauty, books, sports gear, jewellery, automotive parts and groceries from global brands.[web:2271][web:2273]",
+    k:"ubuy albania,international online shopping albania,imported electronics mobiles fashion and beauty,books sports jewellery automotive and food,global brands via ubuy.al",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy albania albanian",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.al/sq/",
+    d:"the albanian language version of ubuy albania offers the same wide catalog of international electronics, fashion, home goods, beauty items and more with interface and support in shqip.[web:2276][web:2271]",
+    k:"ubuy albania shqip,blere online nderkombetare shqiperi,elektronike mode shtepi dhe kozmetike,faqe shqiptare ubuy.al sq",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy albania italian",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.al/it/",
+    d:"ubuy albania also provides an italian localized site for users who prefer browsing international products such as electronics, apparel, beauty and home goods in italian language.[web:2272][web:2271]",
+    k:"ubuy albania italiano,shopping online albania in italiano,elettronica abbigliamento bellezza e casa,sito ubuy.al it",
+    a:""
+  },
+  {
+    i:4,
+    n:"ubuy tirana city page",
+    c:"electronics,fashion,home,beauty,travel,business",
+    u:"https://www.ubuy.al/en/online-shopping-in-tirana",
+    d:"the ubuy tirana page highlights cross border online shopping options for residents of tirana, listing popular categories like electronics, mobiles, home goods, travel gear and beauty products shipped worldwide.[web:2273]",
+    k:"ubuy tirana,online shopping tirana albania,electronics mobiles home goods travel gear,beauty products cross border ubuy",
+    a:""
+  },
+  {
+    i:5,
+    n:"ubuy albania fashion",
+    c:"fashion,beauty,business",
+    u:"https://www.ubuy.al/en/category/fashion-10169",
+    d:"the fashion category on ubuy albania features global clothing, shoes, bags and accessories plus related beauty items for customers across albania.[web:2280][web:2271]",
+    k:"ubuy albania fashion,online clothing store albania,shoes bags and accessories from global brands,ubuy.al fashion category",
+    a:""
+  },
+  {
+    i:6,
+    n:"ubuy albania snickers",
+    c:"food,business",
+    u:"https://www.ubuy.al/en/brand/snickers",
+    d:"ubuy albania sells snickers chocolates and snacks online, letting sweet lovers in albania order different snickers products with fast delivery.[web:2278][web:2271]",
+    k:"ubuy albania snickers,snickers chocolates online albania,snacks and sweets delivery,ubuy brand page snickers",
+    a:""
+  },
+  {
+    i:7,
+    n:"marks & spencer albania",
+    c:"fashion,home,food,business",
+    u:"https://www.marksandspencer.com/al/",
+    d:"marks & spencer albania site lets shoppers browse and buy womenswear, menswear, kids clothing and some homewares online, reflecting the british retail brand’s presence in albania.[web:2274]",
+    k:"marks and spencer albania,online clothing store albania, womens mens kidswear and homewares,m&s al website shopping",
+    a:""
+  },
+  {
+    i:8,
+    n:"fashion group albania online",
+    c:"fashion,beauty,business",
+    u:"https://fga.al/",
+    d:"fashion group albania is a leading fashion company in the albanian market that runs physical and online stores for multiple clothing and accessories brands under one umbrella.[web:2275]",
+    k:"fashion group albania,online fashion brands albania,clothing and accessories multi brand,fga.al ecommerce",
+    a:""
+  },
+  {
+    i:9,
+    n:"lc waikiki albania online",
+    c:"fashion,business",
+    u:"https://www.lcwaikiki.al/",
+    d:"lc waikiki albania offers an online shop for affordable casual clothing, childrenswear and basics for the local market, complementing its physical stores.[web:2275]",
+    k:"lc waikiki albania,online casual clothing albania,kidswear and basics store,lcwaikiki.al shopping",
+    a:""
+  },
+  {
+    i:10,
+    n:"intersport albania",
+    c:"sports,fashion,business",
+    u:"https://www.intersport.al/",
+    d:"intersport albania runs an ecommerce site with sportswear, running shoes, fitness gear and equipment for many disciplines, serving active customers nationwide.[web:2275]",
+    k:"intersport albania,online sports store albania,sportswear running shoes and equipment,intersport.al shop",
+    a:""
+  },
+  {
+    i:11,
+    n:"megatek albania online",
+    c:"home,electronics,business",
+    u:"https://www.megatek.eu/",
+    d:"megatek eu is a large diy and home improvement retailer in tirana with an online store that offers furniture, decor, tools, lighting and some electronics.[web:2275]",
+    k:"megatek albania,online diy and home store albania,furniture decor tools and lighting,megatek.eu shopping",
+    a:""
+  },
+  {
+    i:12,
+    n:"spar albania online",
+    c:"food,home,business",
+    u:"https://www.sparalbania.com/",
+    d:"spar albania provides information and some online ordering for groceries, fresh food and household essentials from its supermarket network.[web:2275]",
+    k:"spar albania,online supermarket albania,groceries fresh food and essentials,sparalbania.com shop",
+    a:""
+  },
+  {
+    i:13,
+    n:"conad albania",
+    c:"food,home,business",
+    u:"https://www.conad.al/",
+    d:"conad albania is part of the italian supermarket chain and presents its promotions and selected products online for grocery shoppers in albania.[web:2275]",
+    k:"conad albania,supermarket online info albania,italian style groceries and products,conad.al website",
+    a:""
+  },
+  {
+    i:14,
+    n:"neptun albania",
+    c:"electronics,home,business",
+    u:"https://www.neptun.al/",
+    d:"neptun albania is a large electronics and appliances chain with an online store for tvs, audio, phones, computers, white goods and small appliances.[web:2275]",
+    k:"neptun albania,online electronics retailer albania,tvs phones computers and appliances,neptun.al shopping",
+    a:""
+  },
+  {
+    i:15,
+    n:"oranget store albania",
+    c:"electronics,business",
+    u:"https://www.oranget.al/",
+    d:"oranget is an albanian online store focused on computers, components, gaming hardware and other it products for home and business users.[web:2275]",
+    k:"oranget albania,online computer store albania,components gaming hardware and it products,oranget.al tech shop",
+    a:""
+  },
+  {
+    i:16,
+    n:"jysk albania",
+    c:"home,business",
+    u:"https://jysk.al/",
+    d:"jysk albania is part of the scandinavian home retailer offering online shopping for mattresses, furniture, bedding, curtains and home decor items.[web:2275]",
+    k:"jysk albania,online furniture store albania,mattresses bedding and home decor,jysk.al shop",
+    a:""
+  },
+  {
+    i:17,
+    n:"teoren fashion",
+    c:"fashion,business",
+    u:"https://teoren.al/",
+    d:"teoren is an albanian online fashion retailer offering stylish clothing and accessories for men and women with nationwide delivery.[web:2275]",
+    k:"teoren albania,online fashion boutique albania,clothing and accessories store,teoren.al shopping",
+    a:""
+  },
+  {
+    i:18,
+    n:"zeco electronics",
+    c:"electronics,business",
+    u:"https://zeco.al/",
+    d:"zeco is an electronics and it equipment vendor in albania that sells computers, networking gear and accessories through its website and stores.[web:2275]",
+    k:"zeco albania,online it equipment albania,computers networking gear and accessories,zeco.al tech store",
+    a:""
+  },
+  {
+    i:19,
+    n:"europharma albania",
+    c:"health,beauty,business",
+    u:"https://www.europharma.al/",
+    d:"europharma albania operates an online pharmacy catalog where customers can find medicines, supplements, skincare and health products available in its pharmacies.[web:2275]",
+    k:"europharma albania,online pharmacy catalog albania,medicines supplements and skincare,europharma.al health products",
+    a:""
+  },
+  {
+    i:20,
+    n:"kns albania bookstore",
+    c:"books,business",
+    u:"https://kns.al/",
+    d:"kns.al is an online bookstore and educational supplier in albania offering school books, literature, study materials and stationery for students and readers.[web:2275]",
+    k:"kns albania,online bookstore albania,school books literature and stationery,kns.al education shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"dyqan taxi",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.dyqan.taxi/",
+    d:"dyqan taxi is an albanian online marketplace that sells electronics, small appliances, fashion, beauty and some food items with delivery in many cities.[web:2275]",
+    k:"dyqan taxi albania,online marketplace albania,electronics small appliances fashion and beauty,shopping site dyqan.taxi",
+    a:""
+  },
+  {
+    i:22,
+    n:"blerje online shqiperi",
+    c:"fashion,beauty,home,business",
+    u:"https://www.facebook.com/blerjeonlineshqiperi/",
+    d:"blerje online shqiperi is a facebook based shop in albania featuring online sales of women’s and men’s clothing, accessories and various home products.[web:2277]",
+    k:"blerje online shqiperi,online clothing sales albania,facebook shop for fashion and accessories,home products via social commerce",
+    a:""
+  },
+  {
+    i:23,
+    n:"ikea used in albania",
+    c:"home,business",
+    u:"https://www.ikea.com/",
+    d:"many people in albania order ikea furniture, kitchen items and home accessories online using regional ikea sites and shipping or forwarding services.[web:2273]",
+    k:"ikea albania usage,online furniture and home accessories albania,order from regional ikea sites,home shopping ikea.com",
+    a:""
+  },
+  {
+    i:24,
+    n:"amazon used in albania",
+    c:"electronics,books,home,fashion,business",
+    u:"https://www.amazon.it/",
+    d:"customers in albania frequently use amazon italy to buy electronics, books, home products and fashion, shipping purchases to albania via carriers and forwarders.[web:2273]",
+    k:"amazon albania usage,cross border online shopping albania,electronics books home and fashion,amazon.it orders to albania",
+    a:""
+  },
+  {
+    i:25,
+    n:"aliexpress used in albania",
+    c:"electronics,home,fashion,beauty,sports,automotive,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress is a popular cross border marketplace among albanian shoppers for low cost electronics, clothing, beauty items, homeware, sports gear and car accessories.[web:2273]",
+    k:"aliexpress albania usage,cheap online marketplace albania,electronics clothing beauty and home,sports gear and car accessories aliexpress.com",
+    a:""
+  }
+];
