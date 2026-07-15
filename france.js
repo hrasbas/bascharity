d7c46e7100a02c275d505ecba8d5229ecbca05ed
hrasbas <hrasbasji@gmail.com>
2026-07-15 12:36:50 +0530

    Add France country shops data to france.js

---
+++ france.js
@@ -0,0 +1,229 @@
+// france.js
+window.countryShops = window.countryShops || {};
+window.countryShops["france"] = [
+  {
+    i:1,
+    n:"amazon france",
+    c:"electronics,home,fashion,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.amazon.fr/",
+    d:"amazon france is a leading online marketplace in france where customers can buy millions of products including electronics, books, fashion, beauty, home goods, groceries, sports items and automotive accessories with prime delivery options.[web:1681][web:1684][web:1687][web:1690]",
+    k:"amazon france,online shopping france,electronics books fashion beauty home goods,groceries sports items automotive accessories,prime delivery french ecommerce marketplace amazon.fr",
+    a:""
+  },
+  {
+    i:2,
+    n:"cdiscount france",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.cdiscount.com/",
+    d:"cdiscount france is a major french ecommerce marketplace offering electronics, appliances, furniture, fashion and supermarket products with competitive prices and fast delivery across france.[web:1681][web:1684][web:1690]",
+    k:"cdiscount france,french ecommerce marketplace,electronics appliances furniture fashion supermarket products,competitive prices fast delivery cdiscount.com",
+    a:""
+  },
+  {
+    i:3,
+    n:"fnac france",
+    c:"electronics,books,home,business",
+    u:"https://www.fnac.com/",
+    d:"fnac france is a well known retailer selling books, music, films, tech, games and home electronics online with click and collect options in stores across france.[web:1681][web:1685][web:1690]",
+    k:"fnac france,online books music films tech games retailer,home electronics and entertainment,click and collect across france fnac.com",
+    a:""
+  },
+  {
+    i:4,
+    n:"vinted france",
+    c:"fashion,business",
+    u:"https://www.vinted.fr/",
+    d:"vinted france is a peer to peer second hand fashion marketplace where people in france buy and sell used clothing, shoes and accessories through an app and website.[web:1681][web:1685][web:1690]",
+    k:"vinted france,second hand fashion marketplace,buy and sell used clothing shoes accessories,peer to peer fashion app vinted.fr",
+    a:""
+  },
+  {
+    i:5,
+    n:"leroy merlin france",
+    c:"home,sports,business",
+    u:"https://www.leroymerlin.fr/",
+    d:"leroy merlin france is a home improvement and diy retailer offering tools, building materials, garden supplies and furniture through its online shop and stores.[web:1681][web:1687]",
+    k:"leroy merlin france,home improvement and diy retailer,tools building materials garden supplies furniture,online shop leroymerlin.fr",
+    a:""
+  },
+  {
+    i:6,
+    n:"carrefour france online",
+    c:"food,home,fashion,business",
+    u:"https://www.carrefour.fr/",
+    d:"carrefour france online hypermarket sells groceries, fresh food, household essentials and clothing with home delivery and drive pickup services across france.[web:1681][web:1690]",
+    k:"carrefour france online,hypermarket groceries fresh food household essentials clothing,home delivery and drive pickup,online supermarket carrefour.fr",
+    a:""
+  },
+  {
+    i:7,
+    n:"darty france",
+    c:"electronics,home,business",
+    u:"https://www.darty.com/",
+    d:"darty france is an electronics and appliance retailer offering tvs, computers, smartphones, kitchen appliances and home tech online with installation services.[web:1681]",
+    k:"darty france,electronics and appliance retailer,tvs computers smartphones kitchen appliances home tech,online shopping and installation darty.com",
+    a:""
+  },
+  {
+    i:8,
+    n:"boulanger france",
+    c:"electronics,home,business",
+    u:"https://www.boulanger.com/",
+    d:"boulanger france sells household appliances, multimedia devices and consumer electronics through its ecommerce site and nationwide store network.[web:1681]",
+    k:"boulanger france,household appliances multimedia devices consumer electronics,ecommerce site and stores,boulanger.com tech and appliance shop",
+    a:""
+  },
+  {
+    i:9,
+    n:"decathlon france",
+    c:"sports,fashion,business",
+    u:"https://www.decathlon.fr/",
+    d:"decathlon france is a sports retailer with an online shop offering equipment, clothing and footwear for many sports, fitness and outdoor activities.[web:1681]",
+    k:"decathlon france,sports retailer online,equipment clothing footwear for many sports fitness outdoor,decathlon.fr sports ecommerce",
+    a:""
+  },
+  {
+    i:10,
+    n:"zalando france",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.fr/",
+    d:"zalando france is an online fashion platform where customers can buy clothing, shoes, accessories and beauty products from many brands with free returns offers.[web:1681][web:1690]",
+    k:"zalando france,online fashion platform france,clothing shoes accessories beauty products,many brands with free returns zalando.fr",
+    a:""
+  },
+  {
+    i:11,
+    n:"auchan france online",
+    c:"food,home,fashion,business",
+    u:"https://www.auchan.fr/",
+    d:"auchan france online hypermarket provides groceries, fresh produce, home essentials and fashion items with delivery and click and collect services.[web:1681]",
+    k:"auchan france online,groceries fresh produce home essentials fashion,delivery and click and collect auchan.fr,online supermarket france",
+    a:""
+  },
+  {
+    i:12,
+    n:"rakuten france",
+    c:"electronics,home,fashion,books,business",
+    u:"https://fr.shopping.rakuten.com/",
+    d:"rakuten france (formerly priceminister) is a marketplace where individuals and professional sellers offer electronics, books, home goods and fashion with cashback rewards.[web:1681][web:1690]",
+    k:"rakuten france,marketplace for individuals and pro sellers,electronics books home goods fashion,cashback rewards club r fr.shopping.rakuten.com",
+    a:""
+  },
+  {
+    i:13,
+    n:"leboncoin france",
+    c:"electronics,home,automotive,fashion,business",
+    u:"https://www.leboncoin.fr/",
+    d:"leboncoin france is a popular classifieds site where users buy and sell second hand goods such as cars, furniture, electronics and clothing locally across france.[web:1684][web:1687]",
+    k:"leboncoin france,popular classifieds site france,buy and sell cars furniture electronics clothing locally,second hand marketplace leboncoin.fr",
+    a:""
+  },
+  {
+    i:14,
+    n:"temu france access",
+    c:"fashion,home,electronics,beauty,business",
+    u:"https://www.temu.com/fr",
+    d:"temu france access allows french shoppers to buy low priced fashion, homeware, electronics and beauty items from international sellers via cross border ecommerce.[web:1684][web:1690]",
+    k:"temu france access,low priced fashion homeware electronics beauty,cross border ecommerce from international sellers,temu.com fr online shopping",
+    a:""
+  },
+  {
+    i:15,
+    n:"aliexpress france access",
+    c:"electronics,fashion,home,beauty,business",
+    u:"https://fr.aliexpress.com/",
+    d:"aliexpress france access lets customers in france order budget gadgets, clothing, home products and beauty accessories directly from chinese and global sellers.[web:1684][web:1687][web:1690]",
+    k:"aliexpress france access,budget gadgets clothing home products beauty accessories,order from chinese and global sellers,fr.aliexpress.com cross border marketplace",
+    a:""
+  },
+  {
+    i:16,
+    n:"ubuy france",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.fr/en/",
+    d:"ubuy france is an international online shopping store where french customers can buy imported electronics, apparel, mobile phones, beauty products and more from global markets at low prices.[web:1682][web:1683]",
+    k:"ubuy france,international online shopping france,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books sports gear automotive products groceries,global brands delivered to france ubuy",
+    a:""
+  },
+  {
+    i:17,
+    n:"ubuy france french store",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.fr/fr/",
+    d:"ubuy france french store offers the same cross border catalog of electronics, fashion, beauty and household goods with a french language interface and local support.[web:1682][web:1683]",
+    k:"ubuy france french store,french language online shopping,cross border catalog electronics fashion beauty household goods,ubuy.fr fr marketplace france",
+    a:""
+  },
+  {
+    i:18,
+    n:"ubuy france ubay brand store",
+    c:"electronics,home,business",
+    u:"https://www.ubuy.fr/en/brand/ubay",
+    d:"ubuy france ubay brand store sells ubay branded electronics and household products at discounted prices with delivery across major french cities like paris, lyon and marseille.[web:1686]",
+    k:"ubuy france ubay brand store,ubay electronics and household products,discounted prices delivery in paris lyon marseille,ubuy ubay shopping france",
+    a:""
+  },
+  {
+    i:19,
+    n:"e.leclerc drive france",
+    c:"food,home,business",
+    u:"https://www.e.leclerc/",
+    d:"e.leclerc drive france offers online grocery shopping where customers order food, drinks and household products and collect them at drive pickup points or get them delivered.[web:1690]",
+    k:"e.leclerc drive france,online grocery shopping food drinks household products,drive pickup points and home delivery,e.leclerc supermarket online",
+    a:""
+  },
+  {
+    i:20,
+    n:"veepee france",
+    c:"fashion,beauty,home,business",
+    u:"https://www.veepee.fr/",
+    d:"veepee france is a flash sales site offering limited time deals on fashion, beauty, home decor and lifestyle brands, often with significant discounts.[web:1690][web:1685]",
+    k:"veepee france,flash sales site france,limited time deals on fashion beauty home decor lifestyle,discount shopping veepee.fr",
+    a:""
+  },
+  {
+    i:21,
+    n:"showroomprive france",
+    c:"fashion,beauty,home,business",
+    u:"https://www.showroomprive.com/",
+    d:"showroomprive france is a private sales ecommerce site with discounted fashion, beauty products and home items from popular brands for registered members.[web:1681][web:1685]",
+    k:"showroomprive france,private sales ecommerce site,discounted fashion beauty products home items,popular brands for members showroomprive.com",
+    a:""
+  },
+  {
+    i:22,
+    n:"la redoute france",
+    c:"fashion,home,business",
+    u:"https://www.laredoute.fr/",
+    d:"la redoute france is an online catalogue store specializing in french fashion, home textiles and furniture with delivery options throughout france.[web:1681][web:1685]",
+    k:"la redoute france,online catalogue store,french fashion home textiles furniture,delivery throughout france laredoute.fr",
+    a:""
+  },
+  {
+    i:23,
+    n:"manomano france",
+    c:"home,sports,business",
+    u:"https://www.manomano.fr/",
+    d:"manomano france is a specialized marketplace for diy, home improvement and gardening products aimed at both individuals and professionals.[web:1690]",
+    k:"manomano france,specialized marketplace diy home improvement gardening,tools materials and garden products for individuals and pros,manomano.fr ecommerce",
+    a:""
+  },
+  {
+    i:24,
+    n:"sephora france",
+    c:"beauty,health,business",
+    u:"https://www.sephora.fr/",
+    d:"sephora france is a beauty retailer offering makeup, skincare, fragrance and haircare products online from many international and french brands.[web:1685]",
+    k:"sephora france,beauty retailer online france,makeup skincare fragrance haircare products,many international and french brands sephora.fr",
+    a:""
+  },
+  {
+    i:25,
+    n:"top marketplaces france overview",
+    c:"software,business",
+    u:"https://www.channelengine.com/en/blog/top-marketplaces-in-france",
+    d:"top marketplaces france overview article explains key online marketplaces such as amazon, cdiscount, fnac, rakuten and manomano and gives tips for brands selling on the french ecommerce market.[web:1688][web:1681][web:1690]",
+    k:"top marketplaces france overview,amazon cdiscount fnac rakuten manomano,guide for brands selling on french ecommerce market,marketplace overview france",
+    a:""
+  }
+];
