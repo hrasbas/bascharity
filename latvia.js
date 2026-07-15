ba951c9323031791880229d81e482f9e03a3ecde
hrasbas <hrasbasji@gmail.com>
2026-07-15 13:39:29 +0530

    Add online shops data for Latvia
    This file contains a list of online shops in Latvia, including their names, categories, URLs, descriptions, and keywords for each shop.

---
+++ latvia.js
@@ -0,0 +1,229 @@
+// latvia.js
+window.countryShops = window.countryShops || {};
+window.countryShops["latvia"] = [
+  {
+    i:1,
+    n:"ubuy latvia",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.lv/en/",
+    d:"ubuy latvia is an international online shopping store where customers can buy imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and snacks at low prices in latvia.[web:1372][web:1373][web:1374][web:1377]",
+    k:"ubuy latvia,international online shopping latvia,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books toys sports gear automotive products and snacks,cross border ecommerce platform",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy latvia app",
+    c:"software,business",
+    u:"https://www.ubuy.lv/en/download-app",
+    d:"ubuy latvia app lets shoppers in latvia browse products, place orders and track shipments on android and ios for faster and more convenient online shopping.[web:1381]",
+    k:"ubuy latvia app,android and ios shopping app latvia,browse products place orders track shipments,faster convenient mobile ecommerce,ubuy cross border shopping app",
+    a:""
+  },
+  {
+    i:3,
+    n:"220.lv",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://220.lv/",
+    d:"220.lv is a major online marketplace in latvia offering electronics, home goods, fashion, beauty items, sports equipment and household products with nationwide delivery.[web:1376][web:1378]",
+    k:"220.lv latvia,major online marketplace latvia,electronics home goods and fashion,beauty items sports equipment household products,nationwide delivery ecommerce",
+    a:""
+  },
+  {
+    i:4,
+    n:"1a.lv",
+    c:"electronics,home,business",
+    u:"https://www.1a.lv/",
+    d:"1a.lv is a popular online store in latvia focused on electronics, appliances, computer hardware and home products for everyday consumers.[web:1376]",
+    k:"1a.lv latvia,popular electronics and appliances store,computer hardware and home products,everyday consumer online shopping,latvian tech ecommerce site",
+    a:""
+  },
+  {
+    i:5,
+    n:"dateks.lv",
+    c:"electronics,business",
+    u:"https://www.dateks.lv/",
+    d:"dateks.lv is an online computer and electronics shop in latvia selling pcs, laptops, components and accessories.[web:1378]",
+    k:"dateks.lv latvia,online computer and electronics shop,pcs laptops components accessories,tech products shopping latvia,dateks ecommerce platform",
+    a:""
+  },
+  {
+    i:6,
+    n:"euronics latvia",
+    c:"electronics,home,business",
+    u:"https://www.euronics.lv/",
+    d:"euronics latvia is an electronics and home appliances retailer with online shopping for tvs, audio, kitchen appliances and gadgets.[web:1378]",
+    k:"euronics latvia,online electronics and home appliances retailer,tvs audio kitchen appliances gadgets,electronic store ecommerce latvia,euronics.lv shopping",
+    a:""
+  },
+  {
+    i:7,
+    n:"jvrsound.lv",
+    c:"electronics,business",
+    u:"https://www.jvrsound.lv/",
+    d:"jvrsound.lv is a specialized online store in latvia offering audio equipment, speakers and sound systems for homes and events.[web:1378]",
+    k:"jvrsound.lv latvia,audio equipment speakers sound systems,specialized online sound store,home and event audio shopping,latvian electronics retailer",
+    a:""
+  },
+  {
+    i:8,
+    n:"cellbes latvia",
+    c:"fashion,business",
+    u:"https://www.cellbes.lv/",
+    d:"cellbes latvia is an online clothing and footwear store offering fashion for women and men with delivery across latvia.[web:1378]",
+    k:"cellbes latvia,online clothing and footwear store,women and men fashion shopping,delivery across latvia,cellbes fashion ecommerce",
+    a:""
+  },
+  {
+    i:9,
+    n:"euroskor.lv",
+    c:"fashion,business",
+    u:"https://euroskor.lv/",
+    d:"euroskor.lv is a footwear focused online shop in latvia selling shoes and boots for adults and children.[web:1378]",
+    k:"euroskor.lv latvia,online footwear shop shoes and boots,adults and children shoe store,latvian fashion ecommerce,euroskor shoes retailer",
+    a:""
+  },
+  {
+    i:10,
+    n:"eiropasapavi.lv",
+    c:"fashion,business",
+    u:"https://eiropasapavi.lv/",
+    d:"eiropasapavi.lv is a latvian online shoe shop offering european footwear brands for local customers.[web:1378]",
+    k:"eiropasapavi.lv latvia,online shoe shop european brands,footwear for local customers,ecommerce footwear retailer,latvia fashion shoes store",
+    a:""
+  },
+  {
+    i:11,
+    n:"laiks jewellery",
+    c:"jewellery,business",
+    u:"https://laiksjewellery.lv/",
+    d:"laiks jewellery is an online jewelry store in latvia providing gold, silver and designer pieces along with watches.[web:1378]",
+    k:"laiks jewellery latvia,online gold silver designer jewelry,watches and accessories retailer,jewellery ecommerce store latvia,laiksjewellery.lv shopping",
+    a:""
+  },
+  {
+    i:12,
+    n:"zeneva.lv",
+    c:"fashion,jewellery,business",
+    u:"https://www.zeneva.lv/",
+    d:"zeneva.lv is an online fashion and accessories shop in latvia featuring clothes, watches and jewelry items.[web:1378]",
+    k:"zeneva.lv latvia,online fashion and accessories shop,clothes watches and jewelry items,style ecommerce platform,latvian fashion retailer",
+    a:""
+  },
+  {
+    i:13,
+    n:"coverme.lv",
+    c:"fashion,business",
+    u:"https://coverme.lv/",
+    d:"coverme.lv is a latvian online store selling clothing and accessories, including rainwear and everyday fashion pieces.[web:1378]",
+    k:"coverme.lv latvia,online clothing and accessories store,rainwear and everyday fashion pieces,latvia fashion ecommerce,coverme shopping site",
+    a:""
+  },
+  {
+    i:14,
+    n:"douglas latvia",
+    c:"beauty,business",
+    u:"https://www.douglas.lv/",
+    d:"douglas latvia is an online beauty and cosmetics retailer offering perfumes, skincare, makeup and hair care products.[web:1378]",
+    k:"douglas latvia,online beauty and cosmetics retailer,perfumes skincare makeup hair care,latvian beauty ecommerce,douglas.lv shopping",
+    a:""
+  },
+  {
+    i:15,
+    n:"beautyfor.lv",
+    c:"beauty,business",
+    u:"https://beautyfor.lv/",
+    d:"beautyfor.lv is a professional beauty products shop in latvia selling salon supplies, cosmetics and hair products online.[web:1378]",
+    k:"beautyfor.lv latvia,professional beauty products shop,salon supplies cosmetics hair products,online beauty store latvia,beautyfor ecommerce",
+    a:""
+  },
+  {
+    i:16,
+    n:"poetica.lv",
+    c:"beauty,business",
+    u:"https://www.poetica.lv/",
+    d:"poetica.lv is an online cosmetics and skincare store in latvia offering curated beauty brands and personal care items.[web:1378]",
+    k:"poetica.lv latvia,online cosmetics and skincare store,curated beauty brands personal care,latvia beauty ecommerce platform,poetica shopping site",
+    a:""
+  },
+  {
+    i:17,
+    n:"e-menessaptieka",
+    c:"health,business",
+    u:"https://www.e-menessaptieka.lv/",
+    d:"e-menessaptieka is an online pharmacy in latvia providing medicines, health supplements and wellness products with home delivery.[web:1378]",
+    k:"e-menessaptieka latvia,online pharmacy medicines and supplements,health and wellness products,home delivery across latvia,pharmacy ecommerce store",
+    a:""
+  },
+  {
+    i:18,
+    n:"ratini.lv",
+    c:"fashion,business",
+    u:"https://ratini.lv/",
+    d:"ratini.lv is an online baby and kids store in latvia selling strollers, clothing and accessories for children.[web:1378]",
+    k:"ratini.lv latvia,online baby and kids store,strollers clothing and accessories,children products ecommerce,ratini.lv shopping",
+    a:""
+  },
+  {
+    i:19,
+    n:"pirkumins.com",
+    c:"fashion,home,business",
+    u:"https://www.pirkumins.com/",
+    d:"pirkumins.com is a latvian online store offering toys, kids clothing and household items for families.[web:1378]",
+    k:"pirkumins.com latvia,toys kids clothing household items,family oriented online store,latvia ecommerce for children products,pirkumins shopping site",
+    a:""
+  },
+  {
+    i:20,
+    n:"rimcimci.lv",
+    c:"fashion,business",
+    u:"https://www.rimcimci.lv/",
+    d:"rimcimci.lv is an online kids fashion and toy shop in latvia providing clothes, shoes and play items.[web:1378]",
+    k:"rimcimci.lv latvia,online kids fashion and toy shop,children clothes shoes and play items,latvian kids ecommerce,rimcimci shopping",
+    a:""
+  },
+  {
+    i:21,
+    n:"babylands.lv",
+    c:"fashion,home,business",
+    u:"https://www.babylands.lv/",
+    d:"babylands.lv is a baby products store in latvia selling nursery furniture, strollers, clothing and care items online.[web:1378]",
+    k:"babylands.lv latvia,baby products nursery furniture strollers,clothing and care items online,babylands ecommerce store,latvian baby goods retailer",
+    a:""
+  },
+  {
+    i:22,
+    n:"rotallieta.lv",
+    c:"fashion,business",
+    u:"https://rotallieta.lv/",
+    d:"rotallieta.lv is an online toy shop in latvia offering a broad selection of toys and games for children of different ages.[web:1378]",
+    k:"rotallieta.lv latvia,online toy shop toys and games,children entertainment products,latvia kids ecommerce,rotallieta shopping site",
+    a:""
+  },
+  {
+    i:23,
+    n:"rito.lv",
+    c:"home,business",
+    u:"https://rito.lv/",
+    d:"rito.lv is a household goods and kitchenware store in latvia with online shopping for tableware, cookware and home accessories.[web:1378]",
+    k:"rito.lv latvia,household goods and kitchenware store,tableware cookware home accessories,online home shopping latvia,rito ecommerce platform",
+    a:""
+  },
+  {
+    i:24,
+    n:"scand.lv",
+    c:"home,business",
+    u:"https://scand.lv/",
+    d:"scand.lv is an online shop in latvia offering scandinavian style furniture, decor and home accessories.[web:1378]",
+    k:"scand.lv latvia,scandinavian style furniture and decor,home accessories online shop,interior design ecommerce,latvia home retailer",
+    a:""
+  },
+  {
+    i:25,
+    n:"santehnikasveikals.lv",
+    c:"home,business",
+    u:"https://www.santehnikasveikals.lv/",
+    d:"santehnikasveikals.lv is a plumbing and bathroom goods shop in latvia selling fixtures, taps and accessories online.[web:1378]",
+    k:"santehnikasveikals.lv latvia,plumbing and bathroom goods shop,fixtures taps and accessories,online home improvement store,latvian santehnika ecommerce",
+    a:""
+  }
+];
