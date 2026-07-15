1c8fe977d7d1c71cb130f5c85e8315cdd4f88d81
hrasbas <hrasbasji@gmail.com>
2026-07-15 12:30:15 +0530

    Add online shops list for Estonia
    This file contains a list of online shops in Estonia, including their names, categories, URLs, descriptions, and keywords for each shop.

---
+++ estonia.js
@@ -0,0 +1,229 @@
+// estonia.js
+window.countryShops = window.countryShops || {};
+window.countryShops["estonia"] = [
+  {
+    i:1,
+    n:"sellme.ee marketplace",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://sellme.ee/en/",
+    d:"sellme.ee is one of the largest online marketplaces in estonia where customers can buy clothes, furniture, electronics, toys, smartphones, perfumes, cosmetics and more with delivery across the country.[web:1738]",
+    k:"sellme.ee marketplace,online shopping estonia,clothes furniture electronics toys smartphones,perfumes cosmetics and more with delivery,sellme largest online store estonia",
+    a:""
+  },
+  {
+    i:2,
+    n:"kaup24 estonia",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://kaup24.ee/",
+    d:"kaup24 is a popular estonian online store and marketplace selling electronics, home appliances, furniture, fashion, beauty, toys and some grocery products with regular discounts and campaigns.[web:1733][web:1737]",
+    k:"kaup24 estonia,popular online store estonia,electronics home appliances furniture fashion beauty toys,grocery products discounts and campaigns kaup24.ee",
+    a:""
+  },
+  {
+    i:3,
+    n:"1a.ee estonia",
+    c:"electronics,home,business",
+    u:"https://www.1a.ee/",
+    d:"1a.ee is an estonian ecommerce shop offering computers, phones, electronics, household appliances and home goods with convenient delivery and pickup options.[web:1733][web:1737]",
+    k:"1a.ee estonia,online electronics and appliances store,computers phones home goods delivery and pickup,estonia online shopping 1a.ee",
+    a:""
+  },
+  {
+    i:4,
+    n:"euronics estonia",
+    c:"electronics,home,business",
+    u:"https://www.euronics.ee/",
+    d:"euronics estonia is a major electronics and home appliance retailer with an online store selling tvs, laptops, smartphones, kitchen appliances and smart home devices.[web:1737][web:1734]",
+    k:"euronics estonia,online electronics retailer estonia,tvs laptops smartphones kitchen appliances smart home,euronics.ee ecommerce store",
+    a:""
+  },
+  {
+    i:5,
+    n:"arvutitark estonia",
+    c:"electronics,business",
+    u:"https://arvutitark.ee/",
+    d:"arvutitark is an estonian computer and electronics store where gamers and professionals can buy pcs, components, peripherals and accessories online.[web:1733][web:1737]",
+    k:"arvutitark estonia,computer and electronics store,pcs components peripherals accessories for gamers and pros,online tech shop arvutitark.ee",
+    a:""
+  },
+  {
+    i:6,
+    n:"alfashop estonia",
+    c:"electronics,home,business",
+    u:"https://alfashop.ee/",
+    d:"alfashop.ee is an online shop in estonia selling home appliances, electronics and household items including fridges, cookers, washers and small gadgets.[web:1737]",
+    k:"alfashop estonia,online home appliances and electronics shop,fridges cookers washers small gadgets,household items ecommerce alfashop.ee",
+    a:""
+  },
+  {
+    i:7,
+    n:"smartech estonia",
+    c:"electronics,business",
+    u:"https://www.smartech.ee/",
+    d:"smartech.ee is an estonian online electronics shop focusing on phones, accessories, smart devices and tech parts for consumers and small businesses.[web:1737]",
+    k:"smartech estonia,online electronics shop phones accessories smart devices,tech parts for consumers and small businesses,smartech.ee estonia",
+    a:""
+  },
+  {
+    i:8,
+    n:"motonet estonia online",
+    c:"automotive,home,sports,business",
+    u:"https://www.motonet.ee/",
+    d:"motonet estonia online store offers car parts, tools, fishing gear, outdoor equipment and household items for drivers and hobbyists.[web:1737]",
+    k:"motonet estonia online,car parts tools fishing gear outdoor equipment,household items for drivers and hobbyists,motonet.ee estonia ecommerce",
+    a:""
+  },
+  {
+    i:9,
+    n:"auto24 parts estonia",
+    c:"automotive,business",
+    u:"https://eng.auto24.ee/",
+    d:"auto24 parts estonia is an automotive marketplace section where users can browse and buy car parts, tyres and accessories online from various sellers.[web:1737]",
+    k:"auto24 parts estonia,automotive marketplace for car parts tyres accessories,buy from various sellers online,eng.auto24.ee estonia",
+    a:""
+  },
+  {
+    i:10,
+    n:"alvadi estonia",
+    c:"automotive,business",
+    u:"https://alvadi.ee/",
+    d:"alvadi is an online car parts store in estonia providing spare parts, body parts, lights and accessories for many vehicle brands with international shipping options.[web:1737]",
+    k:"alvadi estonia,online car parts store estonia,spare parts body parts lights accessories,multi brand automotive ecommerce alvadi.ee",
+    a:""
+  },
+  {
+    i:11,
+    n:"intercars24 estonia",
+    c:"automotive,business",
+    u:"https://www.intercars24.ee/",
+    d:"intercars24.ee is an online automotive shop in estonia supplying car parts and maintenance products to individual drivers and workshops.[web:1737]",
+    k:"intercars24 estonia,online automotive shop car parts maintenance products,drivers and workshops in estonia,intercars24.ee ecommerce",
+    a:""
+  },
+  {
+    i:12,
+    n:"home4you estonia",
+    c:"home,business",
+    u:"https://www.home4you.ee/",
+    d:"home4you estonia online shop sells furniture, garden furniture, decor and home accessories for apartments, houses and offices.[web:1737][web:1740]",
+    k:"home4you estonia,online furniture and decor store,garden furniture and home accessories,home4you.ee home shopping estonia",
+    a:""
+  },
+  {
+    i:13,
+    n:"zizi.ee estonia",
+    c:"home,business",
+    u:"https://www.zizi.ee/",
+    d:"zizi.ee is an estonian online store offering household goods, storage solutions and home accessories with delivery nationwide.[web:1737]",
+    k:"zizi.ee estonia,household goods storage solutions home accessories,online home store with nationwide delivery,zizi estonia ecommerce",
+    a:""
+  },
+  {
+    i:14,
+    n:"ikea estonia online",
+    c:"home,business",
+    u:"https://www.ikea.ee/",
+    d:"ikea estonia online store lets customers order furniture, kitchen solutions and decor in scandinavian style with pickup and delivery options.[web:1737]",
+    k:"ikea estonia online,furniture kitchen solutions decor scandinavian style,pickup and delivery ikea.ee,home shopping estonia",
+    a:""
+  },
+  {
+    i:15,
+    n:"prisma estonia online",
+    c:"food,home,business",
+    u:"https://www.prisma.ee/",
+    d:"prisma estonia online supermarket allows shoppers to order groceries, fresh food and household essentials for pickup or home delivery.[web:1737]",
+    k:"prisma estonia online,online supermarket groceries fresh food household essentials,pickup or home delivery prisma.ee,food shopping estonia",
+    a:""
+  },
+  {
+    i:16,
+    n:"glami estonia",
+    c:"fashion,business",
+    u:"https://www.glami.ee/",
+    d:"glami estonia is a fashion search and shopping platform where users can discover clothing, shoes and accessories from many estonian and international online stores in one place.[web:1737]",
+    k:"glami estonia,fashion search and shopping platform,clothing shoes accessories from many stores,online fashion hub glami.ee",
+    a:""
+  },
+  {
+    i:17,
+    n:"modivo estonia",
+    c:"fashion,business",
+    u:"https://modivo.ee/",
+    d:"modivo.ee is an online fashion shop in estonia selling clothing, footwear and accessories from well known european brands for men, women and kids.[web:1737][web:1740]",
+    k:"modivo estonia,online fashion shop clothing footwear accessories,european brands for men women kids,modivo.ee estonian fashion ecommerce",
+    a:""
+  },
+  {
+    i:18,
+    n:"about you estonia",
+    c:"fashion,business",
+    u:"https://www.aboutyou.ee/",
+    d:"about you estonia is an online fashion retailer offering trendy clothing, shoes and accessories with personalized recommendations and frequent promotions.[web:1737][web:1740]",
+    k:"about you estonia,online fashion retailer clothing shoes accessories,personalized recommendations promotions,aboutyou.ee fashion estonia",
+    a:""
+  },
+  {
+    i:19,
+    n:"zalando estonia",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.ee/",
+    d:"zalando estonia gives shoppers access to thousands of fashion and beauty brands with a wide range of clothing, footwear, bags and cosmetics delivered to estonia.[web:1737]",
+    k:"zalando estonia,thousands of fashion and beauty brands,clothing footwear bags cosmetics delivered,zalando.ee online fashion estonia",
+    a:""
+  },
+  {
+    i:20,
+    n:"nsking estonia",
+    c:"fashion,sports,business",
+    u:"https://nsking.ee/",
+    d:"nsking estonia is an online store specializing in streetwear fashion, sneakers and sports inspired clothing for younger shoppers.[web:1737]",
+    k:"nsking estonia,streetwear fashion and sneakers,sports inspired clothing online,nsking.ee estonian streetwear shop",
+    a:""
+  },
+  {
+    i:21,
+    n:"watchwear estonia",
+    c:"jewellery,fashion,business",
+    u:"https://watchwear.eu/",
+    d:"watchwear.eu is a baltic online store serving estonia with branded watches, jewellery and fashion accessories for men and women.[web:1737]",
+    k:"watchwear estonia,online store for branded watches jewellery accessories,men and women fashion accessories,watchwear.eu estonia",
+    a:""
+  },
+  {
+    i:22,
+    n:"ideaalkosmeetika estonia",
+    c:"beauty,health,business",
+    u:"https://www.ideaalkosmeetika.ee/",
+    d:"ideaalkosmeetika is an estonian cosmetics chain with an online shop offering makeup, skincare, fragrances and haircare products from many brands.[web:1737]",
+    k:"ideaalkosmeetika estonia,online cosmetics shop makeup skincare fragrances haircare,many beauty brands estonia,ideaalkosmeetika.ee beauty ecommerce",
+    a:""
+  },
+  {
+    i:23,
+    n:"apotheka estonia online",
+    c:"health,beauty,business",
+    u:"https://www.apotheka.ee/",
+    d:"apotheka estonia online pharmacy sells medicines, supplements, skincare and wellness products with pickup at pharmacies or home delivery.[web:1737]",
+    k:"apotheka estonia online,online pharmacy medicines supplements skincare wellness,pickup or home delivery apotheka.ee,health shopping estonia",
+    a:""
+  },
+  {
+    i:24,
+    n:"benu apteek estonia",
+    c:"health,beauty,business",
+    u:"https://www.benu.ee/",
+    d:"benu apteek estonia operates an online pharmacy platform where customers can buy prescription refills, over the counter medicine and personal care products.[web:1737]",
+    k:"benu apteek estonia,online pharmacy platform,prescription refills otc medicine personal care,benu.ee health ecommerce estonia",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy estonia",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.ee/en/",
+    d:"ubuy estonia is an international online shopping platform where customers in estonia can order imported electronics, apparel, mobile phones, beauty products and many other categories from global brands at low prices.[web:1731][web:1739]",
+    k:"ubuy estonia,international online shopping estonia,imported electronics mobiles apparel beauty home goods,books sports gear automotive products groceries,global brands delivered to estonia ubuy.ee",
+    a:""
+  }
+];
