c423b752eb1713d93b7c5d57e269c11a577ae909
hrasbas <hrasbasji@gmail.com>
2026-07-15 10:48:15 +0530

    Add online shops data for Austria
    This file contains a list of online shops available in Austria, including their categories, URLs, descriptions, and keywords for each shop.

---
+++ austria.js
@@ -0,0 +1,229 @@
+// austria.js
+window.countryShops = window.countryShops || {};
+window.countryShops["austria"] = [
+  {
+    i:1,
+    n:"amazon austria (amazon.de)",
+    c:"electronics,home,fashion,beauty,books,food,sports,business",
+    u:"https://www.amazon.de/",
+    d:"amazon serves many customers in austria through amazon.de, offering a huge range of electronics, books, fashion, beauty, homeware, sports items and pantry food with fast shipping options.[web:2190][web:2191]",
+    k:"amazon austria,online shopping austria,electronics books fashion and beauty,homeware sports and groceries amazon.de,marketplace for austrian shoppers",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy austria english",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.co.at/en/",
+    d:"ubuy austria is an international shopping platform where customers can buy imported electronics, mobiles, clothing, beauty products, books, sports gear, jewellery, automotive parts and groceries from global brands.[web:2183]",
+    k:"ubuy austria,international online shopping austria,imported electronics mobiles fashion and beauty,books sports jewellery automotive and food,global brands via ubuy.co.at",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy austria greek",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.co.at/gr/",
+    d:"ubuy austria greek site targets greek speaking users living in austria with the same catalog of imported gadgets, fashion, beauty items, books, sports products, jewellery and car accessories.[web:2189][web:2183]",
+    k:"ubuy austria greek,online shopping austria greek speakers,imported electronics fashion and beauty,books sports jewellery and auto parts ubuy",
+    a:""
+  },
+  {
+    i:4,
+    n:"from austria online shop",
+    c:"food,home,fashion,beauty,books,business",
+    u:"https://www.fromaustria.com/en",
+    d:"from austria is an online shop dedicated to austrian products, selling local food specialities, drinks, home goods, fashion items, beauty products and gifts to customers in austria and abroad.[web:2184]",
+    k:"from austria online shop,austrian products online,local food drinks and delicacies,home goods fashion beauty and gifts,fromaustria.com ecommerce",
+    a:""
+  },
+  {
+    i:5,
+    n:"zalando austria",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.at/",
+    d:"zalando austria is a major online fashion store where shoppers can buy clothing, shoes, sportswear and beauty products from many european and global brands with convenient returns.[web:2187][web:2191]",
+    k:"zalando austria,online fashion store austria,clothing shoes and sportswear,beauty products shopping zalando.at",
+    a:""
+  },
+  {
+    i:6,
+    n:"mediamarkt austria",
+    c:"electronics,home,business",
+    u:"https://www.mediamarkt.at/",
+    d:"mediamarkt austria offers online shopping for consumer electronics such as tvs, laptops, smartphones, gaming consoles and home appliances with pickup and delivery options.[web:2187][web:2191]",
+    k:"mediamarkt austria,online electronics store austria,tvs laptops smartphones and gaming,home appliances mediamarkt.at",
+    a:""
+  },
+  {
+    i:7,
+    n:"ikea austria",
+    c:"home,business",
+    u:"https://www.ikea.com/at/de/",
+    d:"ikea austria lets customers order furniture, kitchens, decor, lighting and storage solutions online with home delivery and click and collect in stores across austria.[web:2191]",
+    k:"ikea austria,online furniture store austria,home decor lighting and storage,ikea.com at de shopping",
+    a:""
+  },
+  {
+    i:8,
+    n:"xxxlutz austria",
+    c:"home,business",
+    u:"https://www.xxxlutz.at/",
+    d:"xxxlutz austria is a large furniture and homeware retailer with an ecommerce shop for sofas, bedrooms, dining furniture, kitchens and decor.[web:2191][web:2185]",
+    k:"xxxlutz austria,online furniture and homeware austria,sofas bedrooms dining and kitchens,home decor shop xxxlutz.at",
+    a:""
+  },
+  {
+    i:9,
+    n:"obi austria",
+    c:"home,business",
+    u:"https://www.obi.at/",
+    d:"obi austria is a diy and home improvement store offering online ordering of tools, building materials, garden products, paint and home accessories.[web:2191][web:2185]",
+    k:"obi austria,online diy store austria,tools building materials and paint,garden products and home accessories obi.at",
+    a:""
+  },
+  {
+    i:10,
+    n:"billa online shop",
+    c:"food,home,business",
+    u:"https://shop.billa.at/",
+    d:"billa online shop allows customers in austria to order groceries, fresh produce, bakery, drinks and household essentials for delivery or pickup.[web:2187][web:2191]",
+    k:"billa online shop,online supermarket austria,groceries fresh food and drinks,household essentials shop.billa.at",
+    a:""
+  },
+  {
+    i:11,
+    n:"hofer online (aldi)",
+    c:"food,home,business",
+    u:"https://www.hofer.at/",
+    d:"hofer, the aldi brand in austria, offers online shopping for non food specials, selected groceries, household goods and seasonal items via its web shop.[web:2190][web:2191]",
+    k:"hofer online austria,aldi austria online shopping,groceries household goods and specials,hofer.at webshop",
+    a:""
+  },
+  {
+    i:12,
+    n:"lidl austria online",
+    c:"food,home,business",
+    u:"https://www.lidl.at/",
+    d:"lidl austria online provides information and ordering for weekly offers, food, beverages and household items, with delivery or in store pickup depending on category.[web:2190][web:2185]",
+    k:"lidl austria online,discount supermarket austria,food beverages and household offers,lidl.at online shopping",
+    a:""
+  },
+  {
+    i:13,
+    n:"dm austria",
+    c:"beauty,health,home,business",
+    u:"https://www.dm.at/",
+    d:"dm austria online shop sells cosmetics, skincare, haircare, health products, baby items and household cleaning goods with home delivery and store pickup.[web:2191][web:2185]",
+    k:"dm austria,online drugstore austria,cosmetics skincare haircare and health products,baby items and cleaning goods dm.at",
+    a:""
+  },
+  {
+    i:14,
+    n:"thalia austria",
+    c:"books,business",
+    u:"https://www.thalia.at/",
+    d:"thalia austria is a leading online bookstore offering german and international books, ebooks, audiobooks, games and stationery.[web:2191][web:2185]",
+    k:"thalia austria,online bookstore austria,german and international books and ebooks,audiobooks games and stationery thalia.at",
+    a:""
+  },
+  {
+    i:15,
+    n:"willhaben",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.willhaben.at/",
+    d:"willhaben is austria’s largest classifieds marketplace where users buy and sell used and new items like electronics, furniture, clothing, cars and property online.[web:2190][web:2185]",
+    k:"willhaben austria,online classifieds marketplace austria,buy and sell electronics furniture and clothes,cars and property willhaben.at",
+    a:""
+  },
+  {
+    i:16,
+    n:"geizhals austria",
+    c:"electronics,home,business",
+    u:"https://geizhals.at/",
+    d:"geizhals is a price comparison and shopping portal in austria that lists electronics, hardware, appliances and other products from many online shops with direct purchase links.[web:2191][web:2185]",
+    k:"geizhals austria,price comparison site austria,electronics hardware and appliances listings,shopping portal geizhals.at",
+    a:""
+  },
+  {
+    i:17,
+    n:"shopping.at",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.shopping.at/",
+    d:"shopping.at is an austrian shopping portal and marketplace that aggregates deals on electronics, fashion, beauty and home products with price comparison and customer reviews.[web:2188]",
+    k:"shopping.at,online marketplace austria,deal portal for electronics fashion and beauty,home products price comparison shopping.at",
+    a:""
+  },
+  {
+    i:18,
+    n:"universal versand",
+    c:"home,fashion,electronics,business",
+    u:"https://www.universal.at/",
+    d:"universal versand is an austrian mail order and online shop offering furniture, home textiles, fashion, shoes and electronics with installment payment options.[web:2185]",
+    k:"universal versand austria,online mail order shop austria,furniture home textiles fashion and shoes,electronics with installment universal.at",
+    a:""
+  },
+  {
+    i:19,
+    n:"ottoversand austria",
+    c:"fashion,home,electronics,business",
+    u:"https://www.ottoversand.at/",
+    d:"ottoversand austria is a general online retailer for clothing, shoes, furniture, decor and electronics with regular promotions and credit options.[web:2185]",
+    k:"ottoversand austria,online retailer austria,clothing shoes furniture and decor,electronics offers ottoversand.at",
+    a:""
+  },
+  {
+    i:20,
+    n:"hervis austria",
+    c:"sports,fashion,business",
+    u:"https://www.hervis.at/",
+    d:"hervis austria is a sports and outdoor ecommerce shop selling sportswear, running shoes, bikes, fitness equipment and winter sports gear.[web:2185]",
+    k:"hervis austria,online sports shop austria,sportswear shoes bikes and fitness gear,winter sports equipment hervis.at",
+    a:""
+  },
+  {
+    i:21,
+    n:"intersport austria",
+    c:"sports,fashion,business",
+    u:"https://www.intersport.at/",
+    d:"intersport austria offers online shopping for sports clothing, footwear and equipment for many disciplines including skiing, running, football and outdoor activities.[web:2185]",
+    k:"intersport austria,online sports equipment austria,skiing running football and outdoor gear,sports clothing and footwear intersport.at",
+    a:""
+  },
+  {
+    i:22,
+    n:"libro",
+    c:"books,home,business",
+    u:"https://www.libro.at/",
+    d:"libro is an austrian retail chain and online shop for books, school supplies, office stationery, toys, multimedia and small household items.[web:2185]",
+    k:"libro austria,online books and stationery austria,school supplies office items and toys,libro.at ecommerce",
+    a:""
+  },
+  {
+    i:23,
+    n:"müller austria",
+    c:"beauty,health,home,books,business",
+    u:"https://www.mueller.at/",
+    d:"müller austria online shop sells perfumes, cosmetics, drugstore products, baby items, household goods and a selection of books and media.[web:2185]",
+    k:"müller austria,online drugstore and perfumery austria,cosmetics health baby and household goods,books and media mueller.at",
+    a:""
+  },
+  {
+    i:24,
+    n:"conrad austria",
+    c:"electronics,business",
+    u:"https://www.conrad.at/",
+    d:"conrad austria is an electronics and technology retailer offering components, tools, measurement devices, smart home products and gadgets for consumers and businesses online.[web:2185]",
+    k:"conrad austria,online electronics components austria,tools measurement and smart home,gadgets for consumers and b2b conrad.at",
+    a:""
+  },
+  {
+    i:25,
+    n:"aliexpress used in austria",
+    c:"electronics,home,fashion,beauty,sports,automotive,business",
+    u:"https://www.aliexpress.com/",
+    d:"many people in austria use aliexpress as a cross border marketplace to import low cost electronics, clothing, homeware, beauty items, sports accessories and car parts from international sellers.[web:2191][web:2187]",
+    k:"aliexpress austria usage,cross border online shopping austria,cheap electronics clothing home and beauty,sports accessories and car parts marketplace",
+    a:""
+  }
+];
