028dd19cc9f1d42314224dab702f1d2473318260
hrasbas <hrasbasji@gmail.com>
2026-07-15 12:39:15 +0530

    Add Gambia online shops data to countryShops

---
+++ gambia.js
@@ -0,0 +1,229 @@
+// gambia.js
+window.countryShops = window.countryShops || {};
+window.countryShops["gambia"] = [
+  {
+    i:1,
+    n:"ebaaba gambia",
+    c:"electronics,fashion,food,home,business",
+    u:"https://www.ebaaba.com/",
+    d:"ebaaba gambia is an online marketplace in the gambia where shoppers can buy clothes, electronics, groceries and home items and get them delivered to their chosen location.[web:1661][web:1668]",
+    k:"ebaaba gambia,online shopping gambia,buy clothes electronics groceries,home items delivery in gambia,gambian ecommerce marketplace ebaaba",
+    a:""
+  },
+  {
+    i:2,
+    n:"ebaaba gambia marketplace",
+    c:"electronics,fashion,food,home,business",
+    u:"https://www.ebaaba.com/?v=1c2903397d88",
+    d:"ebaaba gambia marketplace showcases categories for fashion, electronics, groceries and more with free delivery promotions across the gambia.[web:1668]",
+    k:"ebaaba gambia marketplace,fashion electronics groceries online,free delivery promotions gambia,ecommerce website ebaaba.com",
+    a:""
+  },
+  {
+    i:3,
+    n:"jendal gambia",
+    c:"electronics,fashion,food,business",
+    u:"https://www.jendal.org/",
+    d:"jendal is the gambia's online marketplace where local sellers list groceries, fashion, electronics and everyday products with secure payments and delivery nationwide.[web:1662]",
+    k:"jendal gambia,online marketplace gambia,groceries fashion electronics everyday products,secure payments and delivery,jendal.org shopping site",
+    a:""
+  },
+  {
+    i:4,
+    n:"le-jumbo gambia",
+    c:"electronics,fashion,home,food,business",
+    u:"https://lejumbo.com/",
+    d:"le-jumbo is an online shopping platform in the gambia offering a one stop solution for electronics, fashion, home goods and groceries with doorstep delivery.[web:1663]",
+    k:"le-jumbo gambia,online shopping platform gambia,one stop solution electronics fashion home goods groceries,doorstep delivery lejumbo.com",
+    a:""
+  },
+  {
+    i:5,
+    n:"talabuy gambia app",
+    c:"electronics,fashion,home,beauty,software,business",
+    u:"https://play.google.com/store/apps/details?id=com.talabuy.app",
+    d:"talabuy gambia app is a mobile shopping platform that lets customers browse thousands of products from electronics and fashion to home essentials and beauty and get fast delivery nationwide.[web:1664]",
+    k:"talabuy gambia app,mobile shopping platform gambia,electronics fashion home essentials beauty products,fast reliable delivery nationwide talabuy",
+    a:""
+  },
+  {
+    i:6,
+    n:"ubuy gambia",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.gm/",
+    d:"ubuy gambia is an international online shopping site where customers in the gambia can order imported electronics, apparel, mobile phones, beauty products and more from global markets at low prices.[web:1666]",
+    k:"ubuy gambia,international online shopping gambia,imported electronics mobiles apparel,beauty products home goods sports automotive,global brands delivered to gambia ubuy.gm",
+    a:""
+  },
+  {
+    i:7,
+    n:"ubuy gambia electronics",
+    c:"electronics,business",
+    u:"https://www.ubuy.gm/category/electronics-10171",
+    d:"ubuy gambia electronics section offers gadgets, laptops, cameras, tvs and accessories from top brands like samsung, apple and sony for customers in the gambia.[web:1669]",
+    k:"ubuy gambia electronics,gadgets laptops cameras tvs accessories,top brands samsung apple sony,online electronics shopping gambia ubuy.gm",
+    a:""
+  },
+  {
+    i:8,
+    n:"ubuy gambia 3d printing filament listing",
+    c:"electronics,business",
+    u:"https://www.ubuy.gm/product/AZ106I3PK-spidermaker-spiderflex-matte-finish-flexible-tpe-tpu-3d-printing-filament-shore-75a-1-75mm",
+    d:"ubuy gambia 3d printing filament listing showcases specialist electronics and maker supplies such as flexible tpu filaments that can be shipped into the gambia.[web:1665]",
+    k:"ubuy gambia 3d printing filament,specialist electronics maker supplies,flexible tpu filaments shipped to gambia,online tech accessories ubuy.gm",
+    a:""
+  },
+  {
+    i:9,
+    n:"pricegambia marketplace",
+    c:"food,home,fashion,beauty,business",
+    u:"https://pricegambia.com/",
+    d:"pricegambia is an online marketplace and delivery platform in the gambia that connects local businesses and skilled workers with customers for groceries, supermarket items, fragrances and more delivered by kong kong heroes.[web:1667]",
+    k:"pricegambia marketplace,online shopping and delivery gambia,groceries supermarket items fragrances local markets,kong kong delivery heroes pricegambia.com",
+    a:""
+  },
+  {
+    i:10,
+    n:"pricegambia nduga groceries",
+    c:"food,business",
+    u:"https://pricegambia.com/",
+    d:"pricegambia nduga groceries section lets users shop fruits, vegetables and everyday food products from local markets and supermarkets in the gambia with home delivery.[web:1667]",
+    k:"pricegambia nduga groceries,shop fruits vegetables everyday food products,local markets and supermarkets in gambia,online grocery delivery pricegambia.com",
+    a:""
+  },
+  {
+    i:11,
+    n:"brands brusubi on pricegambia",
+    c:"beauty,business",
+    u:"https://pricegambia.com/",
+    d:"brands brusubi on pricegambia offers authentic fragrances and branded perfumes like chanel, versace and ck to customers in the gambia via online orders.[web:1667]",
+    k:"brands brusubi pricegambia,authentic fragrances and branded perfumes,chanel versace ck in gambia,online beauty shopping pricegambia.com",
+    a:""
+  },
+  {
+    i:12,
+    n:"goxip gambia fashion",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/gm/en",
+    d:"goxip gambia fashion is an online fashion and beauty destination that aggregates trendy clothing, shoes and accessories from over thirty six thousand brands and shops for gambian customers.[web:1670]",
+    k:"goxip gambia fashion,online fashion and beauty destination,trend clothing shoes accessories from many brands,gambia online shopping goxip.com",
+    a:""
+  },
+  {
+    i:13,
+    n:"goxip gambia lifestyle",
+    c:"fashion,beauty,home,business",
+    u:"https://www.goxip.com/gm/en",
+    d:"goxip gambia lifestyle section helps users discover millions of fashion and beauty products plus some lifestyle items from international stores that ship to the gambia.[web:1670]",
+    k:"goxip gambia lifestyle,discover millions of fashion and beauty products,lifestyle items from international stores shipping to gambia,goxip.com gm online shopping",
+    a:""
+  },
+  {
+    i:14,
+    n:"ebaaba electronics gambia",
+    c:"electronics,business",
+    u:"https://www.ebaaba.com/",
+    d:"ebaaba electronics gambia focuses on phones, computers and accessories sold by local merchants through the ebaaba marketplace with delivery services.[web:1661][web:1668]",
+    k:"ebaaba electronics gambia,phones computers accessories local merchants,online electronics shopping gambia,ebaaba.com tech marketplace",
+    a:""
+  },
+  {
+    i:15,
+    n:"ebaaba groceries gambia",
+    c:"food,business",
+    u:"https://www.ebaaba.com/",
+    d:"ebaaba groceries gambia allows shoppers to order supermarket goods, snacks and everyday food products online with free or low cost delivery in many areas.[web:1668]",
+    k:"ebaaba groceries gambia,order supermarket goods snacks food products,online grocery shopping gambia,delivery via ebaaba.com",
+    a:""
+  },
+  {
+    i:16,
+    n:"jendal groceries gambia",
+    c:"food,business",
+    u:"https://www.jendal.org/",
+    d:"jendal groceries gambia connects local grocery stores and food vendors with online customers who can place orders for essentials and have them delivered.[web:1662]",
+    k:"jendal groceries gambia,local grocery stores and food vendors online,order essentials and get delivery,gambia food marketplace jendal.org",
+    a:""
+  },
+  {
+    i:17,
+    n:"jendal fashion gambia",
+    c:"fashion,business",
+    u:"https://www.jendal.org/",
+    d:"jendal fashion gambia lists clothing, shoes and accessories from local boutiques and traders and lets shoppers buy these fashion items online in the gambia.[web:1662]",
+    k:"jendal fashion gambia,clothing shoes accessories from local boutiques,buy fashion items online in gambia,jendal.org fashion marketplace",
+    a:""
+  },
+  {
+    i:18,
+    n:"le-jumbo electronics gambia",
+    c:"electronics,business",
+    u:"https://lejumbo.com/",
+    d:"le-jumbo electronics gambia sells phones, gadgets and small appliances through its online store with nationwide delivery.[web:1663]",
+    k:"le-jumbo electronics gambia,phones gadgets small appliances online,nationwide delivery lejumbo.com,tech shopping in the gambia",
+    a:""
+  },
+  {
+    i:19,
+    n:"le-jumbo home and kitchen gambia",
+    c:"home,food,business",
+    u:"https://lejumbo.com/",
+    d:"le-jumbo home and kitchen gambia provides cookware, home essentials and packaged food items for online shoppers in the gambia.[web:1663]",
+    k:"le-jumbo home and kitchen gambia,cookware home essentials packaged foods,online home and grocery shopping gambia,lejumbo.com marketplace",
+    a:""
+  },
+  {
+    i:20,
+    n:"talabuy electronics gambia",
+    c:"electronics,software,business",
+    u:"https://play.google.com/store/apps/details?id=com.talabuy.app",
+    d:"talabuy electronics gambia category features smartphones, accessories and other gadgets that users can browse and order via the talabuy shopping app.[web:1664]",
+    k:"talabuy electronics gambia,smartphones accessories gadgets via app,online electronics shopping with talabuy,the gambia mobile marketplace",
+    a:""
+  },
+  {
+    i:21,
+    n:"talabuy fashion gambia",
+    c:"fashion,beauty,software,business",
+    u:"https://play.google.com/store/apps/details?id=com.talabuy.app",
+    d:"talabuy fashion gambia showcases clothing, shoes and beauty items in the talabuy app with secure local payment options.[web:1664]",
+    k:"talabuy fashion gambia,clothing shoes beauty items in talabuy app,secure local payment options,the gambia fashion ecommerce",
+    a:""
+  },
+  {
+    i:22,
+    n:"pricegambia restaurants and shops",
+    c:"food,business",
+    u:"https://pricegambia.com/",
+    d:"pricegambia restaurants and shops section lets customers order meals and snacks from favorite local restaurants and shops and get them delivered at home or office in the gambia.[web:1667]",
+    k:"pricegambia restaurants and shops,order meals and snacks from local restaurants,food delivery to home or office in gambia,online food ordering pricegambia.com",
+    a:""
+  },
+  {
+    i:23,
+    n:"ubuy gambia fashion and beauty",
+    c:"fashion,beauty,business",
+    u:"https://www.ubuy.gm/",
+    d:"ubuy gambia fashion and beauty categories offer clothing, footwear, jewellery and cosmetics from overseas brands to gambian shoppers looking for international styles.[web:1666][web:1670]",
+    k:"ubuy gambia fashion and beauty,clothing footwear jewellery cosmetics,overseas brands for gambian shoppers,international style shopping ubuy.gm",
+    a:""
+  },
+  {
+    i:24,
+    n:"goxip gambia beauty",
+    c:"beauty,fashion,business",
+    u:"https://www.goxip.com/gm/en",
+    d:"goxip gambia beauty section brings together makeup, skincare and beauty accessories curated from numerous online fashion and beauty stores that serve the gambia.[web:1670]",
+    k:"goxip gambia beauty,makeup skincare beauty accessories online,curated from many fashion and beauty stores,beauty shopping destination goxip.com gm",
+    a:""
+  },
+  {
+    i:25,
+    n:"gambia online marketplaces overview",
+    c:"software,business",
+    u:"https://pricegambia.com/",
+    d:"gambia online marketplaces overview on pricegambia explains how platforms like ebaaba, jendal, le-jumbo and ubuy contribute to the growing online shopping ecosystem in the gambia.[web:1667][web:1661][web:1662][web:1663][web:1666]",
+    k:"gambia online marketplaces overview,ebaaba jendal le-jumbo ubuy pricegambia platforms,growing online shopping ecosystem in the gambia,business insight gambia ecommerce",
+    a:""
+  }
+];
