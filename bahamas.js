a6e5763fc3689b39e040bc413d1c5ccafa8984d9
hrasbas <hrasbasji@gmail.com>
2026-07-15 10:51:58 +0530

    Add online shopping information for Bahamas

---
+++ bahamas.js
@@ -0,0 +1,229 @@
+// bahamas.js
+window.countryShops = window.countryShops || {};
+window.countryShops["bahamas"] = [
+  {
+    i:1,
+    n:"ubuy bahamas",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.bahamas.ubuy.com/",
+    d:"ubuy bahamas is an international online shopping website where customers can order imported electronics, mobiles, fashion, beauty, books, sports gear, jewellery, automotive parts and groceries from global brands to the bahamas.[web:2163]",
+    k:"ubuy bahamas,international online shopping bahamas,imported electronics mobiles fashion and beauty,books sports jewellery automotive and food,global brands via ubuy bahamas",
+    a:""
+  },
+  {
+    i:2,
+    n:"bahamazon",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.bahamazon.net/",
+    d:"bahamazon is a bahamas based online retail store and marketplace where shoppers can buy a wide variety of products including electronics, home goods, fashion, beauty and grocery items with local delivery.[web:2164]",
+    k:"bahamazon,online shopping bahamas,local marketplace electronics home and fashion,beauty and grocery items bahamazon.net",
+    a:""
+  },
+  {
+    i:3,
+    n:"desertcart bahamas",
+    c:"health,beauty,electronics,fashion,sports,home,business",
+    u:"https://bahamas.desertcart.com/",
+    d:"desertcart bahamas lets customers shop international products online for health care, beauty, baby items, toys, sports, electronics, fashion and more with secure delivery to the bahamas.[web:2165]",
+    k:"desertcart bahamas,international online shopping bahamas,health beauty baby and toys,electronics fashion and sports gear,desertcart bahamas store",
+    a:""
+  },
+  {
+    i:4,
+    n:"kelly's house & home",
+    c:"home,electronics,food,business",
+    u:"https://www.kellysbahamas.com/",
+    d:"kelly's house & home is the bahamas’ largest department store with an online shop for housewares, hardware, lawn and garden, fine china, small appliances and more.[web:2166]",
+    k:"kelly's bahamas,online department store bahamas,housewares hardware lawn and garden,fine china and small appliances,kellysbahamas.com shopping",
+    a:""
+  },
+  {
+    i:5,
+    n:"oasis marketplace",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.oasisbahamas.com/",
+    d:"oasis marketplace is an online shopping platform in the bahamas where users can browse and order a mix of household goods, fashion, electronics, beauty products and groceries from home.[web:2170]",
+    k:"oasis marketplace bahamas,online shopping platform bahamas,household goods fashion and electronics,beauty products and groceries oasisbahamas.com",
+    a:""
+  },
+  {
+    i:6,
+    n:"bahama shop marketplace",
+    c:"fashion,food,home,jewellery,business",
+    u:"https://www.bahamasb2b.com/bahamashop/",
+    d:"bahama shop marketplace is an online mall for authentic bahamian products such as art, antiques, island foods, shell crafts, toys, greeting cards and handmade jewellery.[web:2171]",
+    k:"bahama shop marketplace,online shopping authentic bahamian products,art antiques foods and shell crafts,local jewellery and gifts bahamasb2b bahamashop",
+    a:""
+  },
+  {
+    i:7,
+    n:"cbs bahamas",
+    c:"home,electronics,business",
+    u:"https://www.cbsbahamas.com/",
+    d:"cbs bahamas is a major online hardware and home improvement store offering tools, building materials, hardware, home improvement supplies and some appliances with island wide delivery and smart locker pickup.[web:2172]",
+    k:"cbs bahamas,online hardware store bahamas,tools building materials and home improvement,appliances and smart locker pickup,cbsbahamas.com shopping",
+    a:""
+  },
+  {
+    i:8,
+    n:"buying bahamian",
+    c:"fashion,food,home,jewellery,business",
+    u:"https://buyingbahamian.com/",
+    d:"buying bahamian is a digital platform and marketplace that helps local bahamian businesses sell clothing, crafts, food products, home goods and services online to customers in the bahamas and abroad.[web:2168]",
+    k:"buying bahamian,online marketplace for bahamian businesses,clothing crafts food and home goods,buy local products online bahamas",
+    a:""
+  },
+  {
+    i:9,
+    n:"instagopher bahamas",
+    c:"food,business",
+    u:"https://instagopher.com/",
+    d:"instagopher is a grocery and food delivery ecommerce service in the bahamas where customers can shop online for supermarket items, snacks and beverages and get them delivered to their door or boat.[web:2169]",
+    k:"instagopher bahamas,online grocery delivery bahamas,supermarket items snacks and beverages,instagopher.com food shopping",
+    a:""
+  },
+  {
+    i:10,
+    n:"sysco bahamas food services",
+    c:"food,business",
+    u:"https://www.syscobahamas.com/",
+    d:"sysco bahamas food services runs an online ordering platform mainly for hotels, restaurants and businesses to purchase bulk food, beverages and kitchen supplies across the islands.[web:2169]",
+    k:"sysco bahamas food services,online wholesale food bahamas,bulk food beverages and kitchen supplies,restaurant and hotel ordering platform",
+    a:""
+  },
+  {
+    i:11,
+    n:"bahari bahamas",
+    c:"fashion,business",
+    u:"https://bahari.com/",
+    d:"bahari bahamas is a lifestyle clothing brand with an online store selling bahamas inspired apparel, t shirts, swimwear and accessories for men and women.[web:2169]",
+    k:"bahari bahamas,online clothing store bahamas,bahamas inspired apparel and swimwear,men and women fashion bahari",
+    a:""
+  },
+  {
+    i:12,
+    n:"the sports center bahamas",
+    c:"sports,fashion,business",
+    u:"https://www.thesportscenter242.com/",
+    d:"the sports center is a bahamian sports and fitness retailer offering athletic footwear, sportswear, exercise equipment and accessories through its ecommerce store.[web:2169]",
+    k:"the sports center bahamas,online sports store bahamas,athletic footwear sportswear and equipment,fitness accessories shop online",
+    a:""
+  },
+  {
+    i:13,
+    n:"bristol wines & spirits",
+    c:"food,business",
+    u:"https://www.bristolwine.com/",
+    d:"bristol wines & spirits provides online ordering of wines, spirits and beverages in the bahamas, serving both home consumers and hospitality clients.[web:2169]",
+    k:"bristol wines & spirits bahamas,online liquor store bahamas,wines spirits and beverages,bristolwine.com ordering bahamas",
+    a:""
+  },
+  {
+    i:14,
+    n:"john watling's distillery shop",
+    c:"food,business",
+    u:"https://www.johnwatlings.com/",
+    d:"john watling's distillery runs an online shop for its bahamian rums and branded merchandise so fans can purchase bottles and souvenirs from anywhere.[web:2169]",
+    k:"john watling's distillery,online rum shop bahamas,bahamian rum and branded merchandise,johnwatlings.com store",
+    a:""
+  },
+  {
+    i:15,
+    n:"aliv online shop",
+    c:"electronics,business",
+    u:"https://www.bealiv.com/shop/",
+    d:"aliv online shop lets bahamas customers purchase mobile phones, devices, prepaid plans and accessories directly from the telecom provider’s ecommerce site.[web:2169]",
+    k:"aliv online shop bahamas,buy mobile phones and plans online,devices and accessories bealiv.com,telecom ecommerce bahamas",
+    a:""
+  },
+  {
+    i:16,
+    n:"fusion superplex store",
+    c:"home,electronics,business",
+    u:"https://www.fusionsuperplex.com/store/",
+    d:"fusion superplex in the bahamas sells movie themed merchandise, gifts, snacks and electronics through its online store for cinema fans.[web:2169]",
+    k:"fusion superplex store,online movie merchandise bahamas,gifts snacks and electronics,cinema fan shop fusionsuperplex.com",
+    a:""
+  },
+  {
+    i:17,
+    n:"oasis bahamas bed & bath",
+    c:"home,business",
+    u:"https://www.oasisbahamas.com/",
+    d:"within oasis marketplace, oasis bahamas bed and bath offers bedding, towels and home textiles suitable for island homes and rental properties in the bahamas.[web:2169][web:2170]",
+    k:"oasis bahamas bed and bath,online bedding and towels bahamas,home textiles for island homes,oasisbahamas.com bed bath section",
+    a:""
+  },
+  {
+    i:18,
+    n:"kelly's lawn & garden",
+    c:"home,business",
+    u:"https://www.kellysbahamas.com/",
+    d:"kelly's lawn and garden department lets bahamians order gardening tools, plants, outdoor furniture and irrigation supplies online for delivery or pickup.[web:2166]",
+    k:"kelly's lawn and garden bahamas,online garden store bahamas,tools plants and outdoor furniture,irrigation supplies kellysbahamas.com",
+    a:""
+  },
+  {
+    i:19,
+    n:"cbs bahamas tools",
+    c:"home,business",
+    u:"https://www.cbsbahamas.com/",
+    d:"cbs bahamas tools section focuses on power tools, hand tools and hardware accessories for diy enthusiasts and contractors ordering online.[web:2172]",
+    k:"cbs bahamas tools,online tools store bahamas,power tools hand tools and hardware,contractor supplies cbsbahamas.com",
+    a:""
+  },
+  {
+    i:20,
+    n:"oasis grocery bahamas",
+    c:"food,business",
+    u:"https://www.oasisbahamas.com/",
+    d:"oasis grocery bahamas lists packaged foods, pantry items, snacks and beverages for convenient online grocery shopping inside the oasis marketplace.[web:2170]",
+    k:"oasis grocery bahamas,online grocery shopping bahamas,packaged foods pantry items and snacks,beverages delivery oasisbahamas.com",
+    a:""
+  },
+  {
+    i:21,
+    n:"bahari accessories",
+    c:"fashion,jewellery,business",
+    u:"https://bahari.com/",
+    d:"bahari accessories section offers hats, caps, jewellery and small fashion items that complement bahari bahamas clothing lines and can be ordered online.[web:2169]",
+    k:"bahari accessories bahamas,online fashion accessories bahamas,hats caps and jewellery,bahari.com lifestyle store",
+    a:""
+  },
+  {
+    i:22,
+    n:"buying bahamian food vendors",
+    c:"food,business",
+    u:"https://buyingbahamian.com/",
+    d:"through buying bahamian, local food vendors can list sauces, jams, snacks and other bahamian made food items for online purchase by residents and tourists.[web:2168]",
+    k:"buying bahamian food,online bahamian sauces and jams,snacks and local food products,buyingbahamian.com vendors",
+    a:""
+  },
+  {
+    i:23,
+    n:"bahama shop jewellery",
+    c:"jewellery,business",
+    u:"https://www.bahamasb2b.com/bahamashop/",
+    d:"bahama shop jewellery listings include handmade island themed necklaces, bracelets and accessories crafted by bahamian artisans and sold online worldwide.[web:2171]",
+    k:"bahama shop jewellery,handmade bahamian jewellery online,island themed necklaces and bracelets,bahamashop artisan accessories",
+    a:""
+  },
+  {
+    i:24,
+    n:"aliexpress used in bahamas",
+    c:"electronics,home,fashion,beauty,sports,automotive,business",
+    u:"https://www.aliexpress.com/",
+    d:"many consumers in the bahamas use aliexpress as a cross border marketplace to import affordable electronics, clothing, homeware, beauty items, sports gear and car accessories via freight forwarders.[web:2167]",
+    k:"aliexpress bahamas usage,cross border online shopping bahamas,cheap electronics clothing home and beauty,sports gear and auto accessories marketplace",
+    a:""
+  },
+  {
+    i:25,
+    n:"amazon used in bahamas",
+    c:"electronics,books,home,fashion,beauty,sports,food,business",
+    u:"https://www.amazon.com/",
+    d:"shoppers in the bahamas frequently buy from amazon using us freight forwarders to access electronics, books, home items, fashion, beauty products, sports goods and packaged foods from american sellers.[web:2167]",
+    k:"amazon bahamas usage,online imports from usa to bahamas,electronics books home and fashion,beauty sports and food shopping amazon",
+    a:""
+  }
+];
