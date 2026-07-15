8ec4e1796f7bb3f1777faa28e0bcbbe98f7288dd
hrasbas <hrasbasji@gmail.com>
2026-07-15 13:13:21 +0530

    Add list of online shops in Germany
    This file contains a list of online shops in Germany, including details such as name, category, URL, description, and keywords for each shop.

---
+++ germany.js
@@ -0,0 +1,229 @@
+// germany.js
+window.countryShops = window.countryShops || {};
+window.countryShops["germany"] = [
+  {
+    i:1,
+    n:"amazon germany",
+    c:"electronics,home,fashion,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.amazon.de/",
+    d:"amazon germany is a major online marketplace where customers in germany can buy electronics, books, fashion, beauty, home goods, groceries, automotive parts and more with prime delivery options.[web:1643][web:1647]",
+    k:"amazon germany,online shopping germany,electronics books fashion beauty home goods,groceries automotive parts prime delivery,german ecommerce marketplace amazon.de",
+    a:""
+  },
+  {
+    i:2,
+    n:"otto germany",
+    c:"fashion,home,electronics,business",
+    u:"https://www.otto.de/",
+    d:"otto germany is a general merchandise retailer offering furniture, fashion, electronics and household products through its large ecommerce platform.[web:1643][web:1647]",
+    k:"otto germany,general merchandise retailer,furniture fashion electronics household products,online shopping platform otto.de,german ecommerce otto",
+    a:""
+  },
+  {
+    i:3,
+    n:"zalando germany",
+    c:"fashion,beauty,business",
+    u:"https://www.zalando.de/",
+    d:"zalando germany is a leading fashion e retailer with thousands of brands across apparel, footwear and accessories or beauty for men, women and kids with easy returns.[web:1643][web:1647]",
+    k:"zalando germany,online fashion shopping germany,apparel footwear accessories beauty thousands of brands,easy returns zalando.de marketplace",
+    a:""
+  },
+  {
+    i:4,
+    n:"mediamarkt germany",
+    c:"electronics,home,business",
+    u:"https://www.mediamarkt.de/",
+    d:"mediamarkt germany is a major electronics retailer selling consumer tech, appliances, computers, gaming and media products online alongside its store network.[web:1643][web:1650]",
+    k:"mediamarkt germany,consumer electronics retailer,appliances computers gaming media products,online shopping mediamarkt.de,german tech store",
+    a:""
+  },
+  {
+    i:5,
+    n:"ikea germany online",
+    c:"home,business",
+    u:"https://www.ikea.com/de/de/",
+    d:"ikea germany online store offers furniture, kitchenware, decor and home organization products for german households with delivery and pickup services.[web:1643][web:1646]",
+    k:"ikea germany online,furniture kitchenware decor home organization,german households delivery and pickup,ikea.com de home shopping",
+    a:""
+  },
+  {
+    i:6,
+    n:"lidl germany online shop",
+    c:"food,home,business",
+    u:"https://www.lidl.de/",
+    d:"lidl germany online shop sells groceries, household items and seasonal products at discount prices with home delivery and click and collect options.[web:1643][web:1647]",
+    k:"lidl germany online shop,groceries household items seasonal products,discount prices delivery and click collect,lidl.de supermarket ecommerce",
+    a:""
+  },
+  {
+    i:7,
+    n:"idealo germany",
+    c:"electronics,home,fashion,business",
+    u:"https://www.idealo.de/",
+    d:"idealo germany is a price comparison platform where shoppers compare millions of products from different online retailers and find deals on electronics, home items and fashion.[web:1643][web:1646]",
+    k:"idealo germany,price comparison platform,german shoppers compare millions of products,electronics home items fashion deals,idealo.de shopping tool",
+    a:""
+  },
+  {
+    i:8,
+    n:"ebay germany",
+    c:"electronics,home,fashion,automotive,business",
+    u:"https://www.ebay.de/",
+    d:"ebay germany is a global marketplace for buyers and sellers offering auctions and fixed price items including electronics, fashion, home goods and vehicles.[web:1643]",
+    k:"ebay germany,online marketplace auctions and fixed price items,electronics fashion home goods vehicles,buy and sell on ebay.de germany",
+    a:""
+  },
+  {
+    i:9,
+    n:"ubuy germany",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.de.com/en/",
+    d:"ubuy germany is an international online shopping store where customers in germany can buy imported electronics, beauty products, supplements, apparel, mobile phones and more from global brands.[web:1642][web:1644][web:1629]",
+    k:"ubuy germany,international online shopping germany,imported electronics beauty supplements apparel mobiles,global brands delivered to germany,ubuy.de cross border marketplace",
+    a:""
+  },
+  {
+    i:10,
+    n:"ubuy germany german store",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.de.com/gr/en/",
+    d:"ubuy germany german store provides the same cross border catalog with german language support for customers shopping electronics, fashion, beauty and household goods online.[web:1644][web:1629]",
+    k:"ubuy germany german store,german language online shopping,cross border catalog electronics fashion beauty household goods,ubuy.de gr marketplace germany",
+    a:""
+  },
+  {
+    i:11,
+    n:"online shopping germany app",
+    c:"software,business",
+    u:"https://play.google.com/store/apps/details?id=com.germany.shopping.online.onlineshoppinggermany.store&hl=en",
+    d:"online shopping germany app aggregates popular german shopping websites such as amazon, otto, zalando and mediamarkt into one mobile tool for comparing prices and daily deals up to seventy percent off.[web:1649]",
+    k:"online shopping germany app,aggregates popular german shopping websites,amazon otto zalando mediamarkt in one app,compare prices daily deals up to 70 percent off",
+    a:""
+  },
+  {
+    i:12,
+    n:"kaufland.de marketplace",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.kaufland.de/",
+    d:"kaufland.de marketplace combines supermarket groceries with a wide online marketplace offering electronics, home products and fashion from third party sellers.[web:1650]",
+    k:"kaufland.de marketplace,supermarket groceries and online marketplace,electronics home products fashion third party sellers,german ecommerce kaufland",
+    a:""
+  },
+  {
+    i:13,
+    n:"real.de / kaufland legacy",
+    c:"electronics,home,food,business",
+    u:"https://www.real.de/",
+    d:"real.de legacy platform redirected to kaufland marketplace and still represents online shopping for groceries, electronics and household items in germany.[web:1650]",
+    k:"real.de germany legacy,grocery electronics household items online,kaufland marketplace integration,german online shopping site",
+    a:""
+  },
+  {
+    i:14,
+    n:"hm germany online",
+    c:"fashion,business",
+    u:"https://www2.hm.com/de_de/index.html",
+    d:"hm germany online store sells clothing, shoes and accessories for women, men and children with regular collections and sales.[web:1646]",
+    k:"hm germany online,clothing shoes accessories for women men children,regular collections and sales,h m german fashion ecommerce",
+    a:""
+  },
+  {
+    i:15,
+    n:"c&a germany",
+    c:"fashion,business",
+    u:"https://www.c-and-a.com/de/de/shop",
+    d:"c&a germany offers affordable clothing and basics for the whole family via its online shop with delivery throughout germany.[web:1646]",
+    k:"c&a germany,affordable clothing and basics for family,online shop delivery throughout germany,c-and-a.com german fashion store",
+    a:""
+  },
+  {
+    i:16,
+    n:"decathlon germany",
+    c:"sports,fashion,business",
+    u:"https://www.decathlon.de/",
+    d:"decathlon germany is a sports retailer with an online shop for fitness equipment, outdoor gear, sportswear and footwear covering many disciplines.[web:1646]",
+    k:"decathlon germany,sports retailer fitness equipment outdoor gear,sportswear and footwear many disciplines,decathlon.de sports ecommerce",
+    a:""
+  },
+  {
+    i:17,
+    n:"action germany online catalogue",
+    c:"home,food,sports,business",
+    u:"https://www.action.com/de-de/",
+    d:"action germany offers a low cost assortment of household items, tools, cleaning products, snacks and small sports accessories with weekly online catalogues.[web:1646]",
+    k:"action germany,low cost household items tools cleaning products snacks sports accessories,weekly online catalogues,action.com german discount store",
+    a:""
+  },
+  {
+    i:18,
+    n:"rossmann germany online",
+    c:"health,beauty,business",
+    u:"https://www.rossmann.de/",
+    d:"rossmann germany online shop sells drugstore and healthcare products including cosmetics, personal care, baby goods and household cleaners with promotions and delivery.[web:1646][web:1647]",
+    k:"rossmann germany online,drugstore and healthcare products cosmetics personal care baby goods household cleaners,promotions and delivery rossmann.de",
+    a:""
+  },
+  {
+    i:19,
+    n:"dm germany online",
+    c:"health,beauty,business",
+    u:"https://www.dm.de/",
+    d:"dm germany online store offers beauty, wellness, pharmacy and household products with click and collect and shipping options.[web:1647]",
+    k:"dm germany online,beauty wellness pharmacy household products,click and collect and shipping options,dm.de drugstore ecommerce germany",
+    a:""
+  },
+  {
+    i:20,
+    n:"shopapo germany",
+    c:"health,business",
+    u:"https://www.shopapo.de/",
+    d:"shopapo germany is an online pharmacy platform used to order medications and health products with home delivery.[web:1646][web:1647]",
+    k:"shopapo germany,online pharmacy platform,order medications and health products,home delivery shopapo.de",
+    a:""
+  },
+  {
+    i:21,
+    n:"vinted germany",
+    c:"fashion,business",
+    u:"https://www.vinted.de/",
+    d:"vinted germany is a marketplace app and site for buying and selling second hand clothing, shoes and accessories between individuals in germany.[web:1646]",
+    k:"vinted germany,second hand clothing shoes accessories marketplace,peer to peer fashion trading,german users on vinted.de",
+    a:""
+  },
+  {
+    i:22,
+    n:"moebel.de furniture marketplace",
+    c:"home,business",
+    u:"https://www.moebel.de/",
+    d:"moebel.de is a furniture marketplace that aggregates offers from many online stores and lets german customers compare and buy furniture and home decor.[web:1646]",
+    k:"moebel.de furniture marketplace,aggregates offers from many online stores,compare and buy furniture and home decor,german home shopping moebel.de",
+    a:""
+  },
+  {
+    i:23,
+    n:"home24 germany",
+    c:"home,business",
+    u:"https://www.home24.de/",
+    d:"home24 germany is an online furniture and home decor retailer offering sofas, beds, tables, lighting and accessories delivered across germany.[web:1646]",
+    k:"home24 germany,online furniture and home decor retailer,sofas beds tables lighting accessories,delivery across germany home24.de",
+    a:""
+  },
+  {
+    i:24,
+    n:"jysk germany online",
+    c:"home,business",
+    u:"https://www.jysk.de/",
+    d:"jysk germany online shop sells mattresses, furniture and home accessories with a focus on scandinavian style designs.[web:1646]",
+    k:"jysk germany online,matrices furniture home accessories,scandinavian style designs,jysk.de home ecommerce germany",
+    a:""
+  },
+  {
+    i:25,
+    n:"galeria germany",
+    c:"fashion,home,beauty,business",
+    u:"https://www.galeria.de/",
+    d:"galeria germany department store site offers fashion, footwear, beauty products, home goods and gifts via its online shop.[web:1646]",
+    k:"galeria germany,department store online shop,fashion footwear beauty products home goods gifts,galeria.de shopping germany",
+    a:""
+  }
+];
