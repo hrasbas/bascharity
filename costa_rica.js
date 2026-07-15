002ea901cc32e0e2e88a62e5603d6b96f6b5a4ab
hrasbas <hrasbasji@gmail.com>
2026-07-15 11:58:01 +0530

    Update Costa Rica shop entries with new details

--- costa_rica.js
+++ costa_rica.js
@@ -1,86 +1,229 @@
-// costa_rica.js
-
+ // costa_rica.js
 window.countryShops = window.countryShops || {};
 window.countryShops["costa_rica"] = [
   {
     i:1,
     n:"ubuy costa rica",
-    c:"electronics,home,fashion,beauty,health,sports,books,business",
-    u:"https://www.ubuy.cr/es/",
-    d:"ubuy costa rica is an international online shopping website delivering imported electronics, apparel, mobile phones, beauty products, sports gear, books and home goods from global brands at low prices.[web:483][web:489]",
-    k:"ubuy costa rica,cross border shopping,imported electronics and mobiles,apparel and fashion,beauty and health items,sports gear and books,home goods store costa rica",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.cr/en/",
+    d:"ubuy costa rica is an international online shopping platform where customers can order imported electronics, apparel, mobile phones, beauty products and many other categories with delivery across costa rica.[web:1888]",
+    k:"ubuy costa rica,international online shopping costa rica,imported electronics mobiles apparel beauty and home,books sports gear groceries automotive,global brands delivered to costa rica ubuy",
     a:""
   },
   {
     i:2,
-    n:"ubuy costa rica luxury shopping deals",
+    n:"goxip costa rica",
     c:"fashion,beauty,business",
-    u:"https://www.ubuy.cr/es/deals/luxury-shopping-store",
-    d:"ubuy costa rica luxury shopping deals page showcases premium fashion, accessories and beauty collections for customers seeking luxury brands online.[web:490][web:483]",
-    k:"ubuy costa rica luxury shopping,premium fashion brands,luxury accessories and beauty,online luxury store costa rica",
+    u:"https://www.goxip.com/cr/en",
+    d:"goxip costa rica is an online fashion and lifestyle shopping destination where users can browse millions of clothing and beauty items from over 36,000 brands and online stores.[web:1893]",
+    k:"goxip costa rica,online fashion shopping costa rica,clothing shoes and beauty products,goxip lifestyle marketplace cr",
     a:""
   },
   {
     i:3,
-    n:"ubuy costa rica japan imports",
-    c:"electronics,home,fashion,beauty,business",
-    u:"https://www.ubuy.cr/en/shop-import-products-from-japan",
-    d:"ubuy costa rica japan imports page focuses on genuine japanese electronics, beauty items, home goods and fashion products shipped to costa rica.[web:492][web:483]",
-    k:"ubuy costa rica japan imports,japanese electronics store,imported beauty items japan,home goods and fashion from japan,cross border shopping costa rica",
+    n:"unimart costa rica",
+    c:"electronics,home,business",
+    u:"https://unimart.com/",
+    d:"unimart is an online store in costa rica that sells laptops, cell phones, electronics, office supplies, appliances and more with fast shipping nationwide.[web:1889][web:1897]",
+    k:"unimart costa rica,online electronics and appliances shop costa rica,laptops cell phones office supplies,fast delivery unimart.com cr",
     a:""
   },
   {
     i:4,
-    n:"ubuy costa rica cayenne pepper supplements",
-    c:"health,food,business",
-    u:"https://www.ubuy.cr/en/product/TTWUEE36Y-kyrebcc-elorita-cayenne-pepper-softgels-elorita-s-cayenne-pepper-softgels-1pcs",
-    d:"ubuy costa rica cayenne pepper supplements page sells elorita cayenne pepper softgels and similar nutrition products that are not easily found locally.[web:484][web:483]",
-    k:"ubuy costa rica cayenne pepper supplements,elorita cayenne pepper softgels,nutrition supplements online,health and wellness products costa rica",
+    n:"hola compras",
+    c:"electronics,home,fashion,business",
+    u:"https://holacompras.com/",
+    d:"hola compras is an ecommerce platform used by shoppers in costa rica to buy electronics, home items and fashion products online from a curated catalog.[web:1889]",
+    k:"hola compras,online shopping costa rica,electronics home and fashion products,hola compras ecommerce platform",
     a:""
   },
   {
     i:5,
-    n:"ubuy costa rica body shop fuji green tea butter",
-    c:"beauty,health,business",
-    u:"https://www.ubuy.cr/en/product/7TZ8NL5ZC-new-listing-the-body-shop-fuji-green-tea-replenishing-body-butter-6-9-oz-196-g-new-x-2",
-    d:"ubuy costa rica body shop fuji green tea butter page offers hydrating body butter from the body shop for costa rican shoppers looking for imported skincare.[web:486][web:483]",
-    k:"ubuy costa rica body shop fuji green tea butter,imported skincare products,body shop body butter,costa rica beauty shopping online",
+    n:"pacifiko costa rica",
+    c:"electronics,home,fashion,business",
+    u:"https://www.pacifiko.com/cr",
+    d:"pacifiko costa rica is an online marketplace offering electronics, household goods and lifestyle products with home delivery and secure payment methods.[web:1889]",
+    k:"pacifiko costa rica,online marketplace costa rica,electronics household goods and lifestyle,secure ecommerce pacifiko cr",
     a:""
   },
   {
     i:6,
-    n:"ubuy costa rica cafe leyenda coffee",
-    c:"food,business",
-    u:"https://www.ubuy.cr/en/product/1AQS2T8-cafe-leyenda-gourmet-costa-rica-dark-roast-coffee-4-pack-8-8oz-ground-100-arabica-coffee",
-    d:"ubuy costa rica cafe leyenda coffee page sells gourmet costa rica dark roast coffee made from 100 percent arabica beans for international and local buyers.[web:488][web:483]",
-    k:"ubuy costa rica cafe leyenda coffee,gourmet costa rica dark roast,100 percent arabica beans,coffee lovers online shop",
+    n:"siman costa rica",
+    c:"fashion,home,beauty,business",
+    u:"https://cr.siman.com/",
+    d:"siman costa rica is the online department store of almacenes siman, selling fashion, beauty, home decor and small appliances to costa rican shoppers.[web:1889]",
+    k:"siman costa rica,online department store costa rica,fashion beauty and home decor,siman.com cr ecommerce",
     a:""
   },
   {
     i:7,
-    n:"ubuy costa rica app",
-    c:"electronics,software,business",
-    u:"https://www.ubuy.cr/es/download-app",
-    d:"ubuy costa rica app lets users browse products, check out quickly and track orders on android and ios devices for easier mobile shopping.[web:485][web:491]",
-    k:"ubuy costa rica app,mobile shopping app,browse and order products,quick checkout and order tracking,android and ios ecommerce costa rica",
+    n:"automercado online",
+    c:"food,home,business",
+    u:"https://automercado.cr/",
+    d:"automercado online lets customers in costa rica order supermarket groceries, fresh food and household essentials for pickup or delivery from automercado stores.[web:1889]",
+    k:"automercado online,online supermarket costa rica,grocery and household delivery automercado,food shopping automercado.cr",
     a:""
   },
   {
     i:8,
-    n:"aftership top 100 online stores costa rica",
-    c:"business",
-    u:"https://www.aftership.com/store-list/top-100-cr-ecommerces-stores",
-    d:"aftership top 100 online stores costa rica lists leading ecommerce brands such as gollo costa rica, tienda monge and automercado with platform and revenue details for market benchmarking.[web:489]",
-    k:"aftership top 100 online stores costa rica,ecommerce ranking list,gollo costa rica tienda monge,automercado online,platform and revenue benchmarks",
+    n:"cemaco costa rica",
+    c:"home,electronics,business",
+    u:"https://www.cemaco.co.cr/",
+    d:"cemaco costa rica is a home improvement and household store with an online shop for furniture, decor, kitchenware, tools and selected electronics.[web:1889]",
+    k:"cemaco costa rica,home improvement online store costa rica,furniture decor kitchenware and tools,cemaco.co.cr ecommerce",
     a:""
   },
   {
     i:9,
-    n:"semrush most visited retail sites costa rica",
-    c:"business",
-    u:"https://www.semrush.com/trending-websites/cr/retail",
-    d:"semrush most visited retail sites costa rica report shows aliexpress, ebay and walmart costa rica among the top retail sites by traffic for the country.[web:487]",
-    k:"semrush most visited retail sites costa rica,aliexpress ebay walmart costa rica,retail traffic report,ecommerce trends costa rica",
+    n:"pricesmart costa rica",
+    c:"food,home,electronics,business",
+    u:"https://www.pricesmart.com/site/cr/es",
+    d:"pricesmart costa rica offers a membership based online store where customers can buy bulk groceries, household goods, electronics and more and pick up at club locations.[web:1889]",
+    k:"pricesmart costa rica,warehouse club online shopping costa rica,bulk groceries household goods and electronics,pricesmart.com cr membership ecommerce",
+    a:""
+  },
+  {
+    i:10,
+    n:"gollo costa rica",
+    c:"electronics,home,business",
+    u:"https://www.gollotienda.com/",
+    d:"gollo is a leading electronics and appliance retailer in costa rica with an online store selling mobiles, tvs, white goods, furniture and more at competitive prices.[web:1897]",
+    k:"gollo costa rica,online electronics and appliance store costa rica,mobiles tvs white goods and furniture,gollotienda.com ecommerce",
+    a:""
+  },
+  {
+    i:11,
+    n:"tienda monge",
+    c:"electronics,home,business",
+    u:"https://www.tiendamonge.com/",
+    d:"tienda monge operates an online shop in costa rica offering a wide range of electronics, appliances, furniture and personal tech with financing options.[web:1897]",
+    k:"tienda monge,online electronics appliances and furniture costa rica,personal tech and financing options,monge ecommerce store cr",
+    a:""
+  },
+  {
+    i:12,
+    n:"epa en linea costa rica",
+    c:"home,business",
+    u:"https://www.epaenlinea.com/",
+    d:"epa en linea is the online hardware and home improvement store of ferreteria epa in costa rica, selling construction materials, bathroom, kitchen and decor items.[web:1897]",
+    k:"epa en linea costa rica,online hardware and home improvement store,construction materials bathroom kitchen decor,epaenlinea.com ecommerce cr",
+    a:""
+  },
+  {
+    i:13,
+    n:"walmart costa rica online",
+    c:"food,home,electronics,business",
+    u:"https://www.walmart.co.cr/",
+    d:"walmart costa rica online lets shoppers buy groceries, household products, electronics and more from walmart stores and schedule delivery or pickup.[web:1897]",
+    k:"walmart costa rica online,online supermarket and general store costa rica,groceries household and electronics,walmart.co.cr ecommerce",
+    a:""
+  },
+  {
+    i:14,
+    n:"intelec costa rica",
+    c:"electronics,software,business",
+    u:"https://www.intelec.co.cr/",
+    d:"intelec is a costa rican online retailer focused on computers, servers, tablets, software, cctv, consoles and phones with technical support for customers nationwide.[web:1897]",
+    k:"intelec costa rica,online computer and server store costa rica,tablets software cctv consoles and phones,tech retailer with support intelec.co.cr",
+    a:""
+  },
+  {
+    i:15,
+    n:"cafe britt online",
+    c:"food,business",
+    u:"https://www.cafebritt.com/",
+    d:"cafe britt runs an online store that ships gourmet costa rican coffee, gifts and chocolates from costa rica to customers around the world and domestically.[web:1897]",
+    k:"cafe britt online,gourmet costa rica coffee shop,coffee gifts and chocolates from costa rica,order coffee beans online cafe britt",
+    a:""
+  },
+  {
+    i:16,
+    n:"tabacon resort shop",
+    c:"travel,home,beauty,business",
+    u:"https://www.tabacon.com/",
+    d:"tabacon grand spa and resort in costa rica operates an online shop for spa products, gift cards and resort experiences for travelers planning their stay.[web:1897]",
+    k:"tabacon resort shop,online spa and resort store costa rica,spa products and gift cards,travel experiences booking tabacon",
+    a:""
+  },
+  {
+    i:17,
+    n:"unimart office and school",
+    c:"home,books,business",
+    u:"https://unimart.com/",
+    d:"unimart also sells office supplies, school materials and stationery online so costa rica customers can get pens, paper, printers and accessories delivered.[web:1897][web:1889]",
+    k:"unimart office and school,online office supplies costa rica,school materials and stationery unimart,home and business essentials ecommerce",
+    a:""
+  },
+  {
+    i:18,
+    n:"automercado express",
+    c:"food,business",
+    u:"https://automercado.cr/",
+    d:"automercado express service allows fast online ordering of fresh produce, meat, bakery items and pantry staples for same day supermarket delivery in costa rica.[web:1889]",
+    k:"automercado express,online grocery delivery costa rica,fresh produce meat bakery and pantry,fast supermarket service automercado",
+    a:""
+  },
+  {
+    i:19,
+    n:"cemaco baby and kids",
+    c:"home,fashion,business",
+    u:"https://www.cemaco.co.cr/",
+    d:"cemaco baby and kids section online includes furniture, decor, toys and clothing for infants and children with delivery options throughout costa rica.[web:1897]",
+    k:"cemaco baby and kids,baby furniture and decor costa rica,online toys and kids clothing,cemaco.co.cr niños tienda en linea",
+    a:""
+  },
+  {
+    i:20,
+    n:"pricesmart foods costa rica",
+    c:"food,business",
+    u:"https://www.pricesmart.com/site/cr/es",
+    d:"pricesmart foods in costa rica lets members order bulk food, snacks, beverages and frozen items online for warehouse pickup or scheduled delivery.[web:1897]",
+    k:"pricesmart foods costa rica,bulk food and snacks online,warehouse grocery shopping costa rica,pricesmart.com club delivery or pickup",
+    a:""
+  },
+  {
+    i:21,
+    n:"gollo mobile store",
+    c:"electronics,business",
+    u:"https://www.gollotienda.com/celulares",
+    d:"gollo mobile store section offers smartphones, feature phones and accessories for costa rican users who prefer to purchase devices online with promotions and plans.[web:1897]",
+    k:"gollo mobile store,online smartphones shop costa rica,phones and accessories gollotienda,mobile deals and promotions cr",
+    a:""
+  },
+  {
+    i:22,
+    n:"tienda monge computers",
+    c:"electronics,business",
+    u:"https://www.tiendamonge.com/",
+    d:"monge computers category sells laptops, desktops and tablets along with accessories, giving costa rica customers financing and delivery options on tech purchases.[web:1897]",
+    k:"tienda monge computers,online laptops and desktops costa rica,tablets and accessories monge,financing options tech store cr",
+    a:""
+  },
+  {
+    i:23,
+    n:"epa garden and outdoor",
+    c:"home,sports,business",
+    u:"https://www.epaenlinea.com/",
+    d:"epa en linea garden and outdoor section features tools, plants, garden furniture and barbecues for costa rican homeowners planning outdoor projects via online shopping.[web:1897]",
+    k:"epa garden and outdoor,online garden store costa rica,tools plants furniture and barbecues,home and outdoor improvement epaenlinea",
+    a:""
+  },
+  {
+    i:24,
+    n:"ubuy costa rica fashion",
+    c:"fashion,business",
+    u:"https://www.ubuy.cr/en/category/fashion-10169",
+    d:"ubuy costa rica fashion category offers clothing, shoes and accessories from top global brands to customers in san jose and other regions with secure checkout.[web:1895][web:1888]",
+    k:"ubuy costa rica fashion,online clothing and shoes costa rica,global brand accessories ubuy,fashion ecommerce category cr",
+    a:""
+  },
+  {
+    i:25,
+    n:"ubuy costa rica coffee selection",
+    c:"food,business",
+    u:"https://www.ubuy.cr/en/product/227YB07C-nestle-tofina-cevada-chicoria-e-cafe-instant-coffee-mix-2-pack-total-of-400g",
+    d:"ubuy costa rica coffee selection includes imported instant coffee mixes and specialty blends like nestle tofina that customers can order online when local stores do not stock them.[web:1892][web:1894]",
+    k:"ubuy costa rica coffee selection,instant coffee mix online costa rica,specialty coffee products from abroad,coffee lovers ecommerce ubuy cr",
     a:""
   }
 ];
