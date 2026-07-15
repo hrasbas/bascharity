2dac4a564cf34ce2d65e8aee95673404f9b940cb
hrasbas <hrasbasji@gmail.com>
2026-07-15 13:19:09 +0530

    Add online shopping directory for Guinea-Bissau
    This file contains a directory of online shopping platforms available in Guinea-Bissau, including descriptions, categories, and keywords for each platform.

---
+++ guinea-bissau.js
@@ -0,0 +1,229 @@
+// guinea-bissau.js
+window.countryShops = window.countryShops || {};
+window.countryShops["guinea-bissau"] = [
+  {
+    i:1,
+    n:"ubuy guinea-bissau",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.gw/en/",
+    d:"ubuy guinea-bissau is an international online shopping store where customers in guinea-bissau can buy imported electronics, apparel, mobile phones, beauty products, books, sports gear, jewellery, automotive accessories and grocery items from global brands.[web:1582][web:1588]",
+    k:"ubuy guinea-bissau,international online shopping guinea-bissau,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books sports gear automotive accessories groceries,cross border ecommerce platform guinea-bissau",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy guinea-bissau portuguese store",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.gw/pt/",
+    d:"ubuy guinea-bissau portuguese store offers the same global brands and imported products with a portuguese language interface for shoppers buying electronics, fashion, beauty, books and groceries online.[web:1584]",
+    k:"ubuy guinea-bissau portuguese store,portuguese language online shopping guinea-bissau,global brands electronics fashion beauty books groceries,ubuy.gw pt cross border marketplace",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy bissau cross border page",
+    c:"electronics,fashion,home,beauty,travel,business",
+    u:"https://www.ubuy.gw/en/online-shopping-in-bissau",
+    d:"ubuy bissau cross border page highlights international shopping for electronics, fashion, beauty, travel accessories and home goods delivered from overseas brands to bissau and other areas of guinea-bissau.[web:1588]",
+    k:"ubuy bissau cross border page,international shopping electronics fashion beauty travel home goods,overseas brands delivered to bissau guinea-bissau,ubuy.gw online shopping guide",
+    a:""
+  },
+  {
+    i:4,
+    n:"ikuma online",
+    c:"food,electronics,home,business",
+    u:"https://www.ikuma.online/",
+    d:"ikuma online is the first online supermarket and marketplace in guinea-bissau offering groceries, mobile phones, electronics, appliances and computer products with local delivery.[web:1586]",
+    k:"ikuma online guinea-bissau,first online supermarket and marketplace,groceries mobile phones electronics appliances informática,local delivery shopping ikuma.online",
+    a:""
+  },
+  {
+    i:5,
+    n:"guine bissau market",
+    c:"food,home,fashion,business",
+    u:"https://guinebissaumarket.com/",
+    d:"guine bissau market is a large online marketplace where people in guinea-bissau can buy and sell food, clothing, home goods and everyday products in a secure and easy way.[web:1585]",
+    k:"guine bissau market,mercado online da guiné-bissau,secure and easy buy and sell platform,food clothing home goods everyday products,guinebissaumarket.com ecommerce",
+    a:""
+  },
+  {
+    i:6,
+    n:"goxip guinea-bissau",
+    c:"fashion,beauty,business",
+    u:"https://www.goxip.com/gw/en",
+    d:"goxip guinea-bissau is an online fashion and beauty shopping destination allowing customers to browse millions of trendy clothing and beauty products from over thirty six thousand brands and stores.[web:1589]",
+    k:"goxip guinea-bissau,online fashion and beauty shopping,millions of trendy clothing and beauty products,thousands of brands and stores,goxip.com gw fashion marketplace",
+    a:""
+  },
+  {
+    i:7,
+    n:"facebook marketplace guinea-bissau general",
+    c:"electronics,home,fashion,books,sports,automotive,food,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace in guinea-bissau works as a broad online marketplace where residents list and buy electronics, fashion, home and garden, books and media, vehicles, sports gear and second hand items.[web:1587]",
+    k:"facebook marketplace guinea-bissau,local online marketplace for residents,electronics fashion home and garden books media vehicles sports second hand,peer to peer shopping guinea-bissau",
+    a:""
+  },
+  {
+    i:8,
+    n:"facebook marketplace guinea-bissau food & drinks",
+    c:"food,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau food and drinks section features sellers offering groceries, snacks and beverages with informal online ordering and local pickup or delivery.[web:1587]",
+    k:"facebook marketplace guinea-bissau food and drinks,groceries snacks beverages sellers,informal online ordering local pickup delivery,community food marketplace",
+    a:""
+  },
+  {
+    i:9,
+    n:"facebook marketplace guinea-bissau casa & jardim",
+    c:"home,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau casa e jardim section lists furniture, decor, garden tools and household items for online discovery and messaging based sales.[web:1587]",
+    k:"facebook marketplace guinea-bissau casa e jardim,furniture decor garden tools household items,online discovery and messaging based sales,home products marketplace guinea-bissau",
+    a:""
+  },
+  {
+    i:10,
+    n:"facebook marketplace guinea-bissau eletronicos",
+    c:"electronics,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau electronics category includes phones, computers, accessories and other tech devices advertised by individuals and small shops.[web:1587]",
+    k:"facebook marketplace guinea-bissau eletronicos,phones computers accessories tech devices,individuals and small shops ads,online electronics trading guinea-bissau",
+    a:""
+  },
+  {
+    i:11,
+    n:"facebook marketplace guinea-bissau moda",
+    c:"fashion,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau fashion category allows local sellers to showcase clothing, shoes and bags for men, women and kids.[web:1587]",
+    k:"facebook marketplace guinea-bissau moda,clothing shoes bags for men women kids,local sellers showcase fashion items,community fashion ecommerce",
+    a:""
+  },
+  {
+    i:12,
+    n:"facebook marketplace guinea-bissau livros & midia",
+    c:"books,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau livros e midia section features books, magazines and media items for sale from individuals and small businesses.[web:1587]",
+    k:"facebook marketplace guinea-bissau livros e midia,books magazines media items for sale,individuals and small businesses listings,online books trading guinea-bissau",
+    a:""
+  },
+  {
+    i:13,
+    n:"facebook marketplace guinea-bissau sports & outdoor",
+    c:"sports,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau sports and outdoor category offers sports equipment, bicycles and camping gear available through local listings.[web:1587]",
+    k:"facebook marketplace guinea-bissau sports and outdoor,sports equipment bicycles camping gear,local listings online,active lifestyle marketplace guinea-bissau",
+    a:""
+  },
+  {
+    i:14,
+    n:"facebook marketplace guinea-bissau vehicles",
+    c:"automotive,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace guinea-bissau vehicles category lists cars, motorbikes and other vehicles along with spare parts offered by local sellers.[web:1587]",
+    k:"facebook marketplace guinea-bissau vehicles,cars motorbikes other vehicles spare parts,local sellers automotive marketplace,online vehicle trading guinea-bissau",
+    a:""
+  },
+  {
+    i:15,
+    n:"instagram shops guinea-bissau moda",
+    c:"fashion,business",
+    u:"https://www.instagram.com/",
+    d:"instagram shops in guinea-bissau moda segment showcase clothing and accessories from local boutiques using shoppable posts and direct messaging for orders.[web:1587]",
+    k:"instagram shops guinea-bissau moda,clothing and accessories from local boutiques,shoppable posts direct messaging orders,fashion ecommerce via instagram",
+    a:""
+  },
+  {
+    i:16,
+    n:"instagram shops guinea-bissau saude & beleza",
+    c:"beauty,health,business",
+    u:"https://www.instagram.com/",
+    d:"instagram shops guinea-bissau saude e beleza promote skincare, haircare, cosmetics and wellness products with online engagement and delivery arrangements.[web:1587]",
+    k:"instagram shops guinea-bissau saude e beleza,skincare haircare cosmetics wellness products,online engagement and delivery arrangements,beauty and health ecommerce via instagram",
+    a:""
+  },
+  {
+    i:17,
+    n:"guinea-bissau online stores directory",
+    c:"software,business",
+    u:"https://ecommerce.aftership.com/countries/gw/stores",
+    d:"guinea-bissau online stores directory is a research tool listing local ecommerce websites with information about their traffic, revenue, domain ranking and platforms used.[web:1583]",
+    k:"guinea-bissau online stores directory,ecommerce research tool listing local websites,traffic revenue domain ranking platform data,aftership ecommerce directory gw",
+    a:""
+  },
+  {
+    i:18,
+    n:"shoprank guinea-bissau report",
+    c:"software,business",
+    u:"https://shoprank.com/countries/guinea-bissau",
+    d:"shoprank guinea-bissau report analyzes more than eighty ecommerce stores, showing platform market share, payment provider adoption and shipping trends in the country.[web:1590]",
+    k:"shoprank guinea-bissau report,analysis of ecommerce stores,platform market share payment provider adoption shipping trends,country level ecommerce insights",
+    a:""
+  },
+  {
+    i:19,
+    n:"guinea-bissau online shopping mall facebook",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.facebook.com/groups/guineabissauwa/",
+    d:"guinea-bissau's online shopping mall facebook group is part of an omnichannel marketplace ecosystem serving a growing community of online buyers and sellers in guinea-bissau.[web:1591]",
+    k:"guinea-bissau online shopping mall facebook,omnichannel marketplace ecosystem,growing community of online buyers and sellers,group based ecommerce guinea-bissau",
+    a:""
+  },
+  {
+    i:20,
+    n:"ikuma supermarket groceries",
+    c:"food,business",
+    u:"https://www.ikuma.online/",
+    d:"ikuma supermarket groceries section provides packaged foods, fresh items and household staples for online shoppers in guinea-bissau.[web:1586]",
+    k:"ikuma supermarket groceries,packaged foods fresh items household staples,online shoppers in guinea-bissau,ikuma.online grocery shopping",
+    a:""
+  },
+  {
+    i:21,
+    n:"ikuma phones & electronics",
+    c:"electronics,business",
+    u:"https://www.ikuma.online/",
+    d:"ikuma phones and electronics category sells smartphones, mobile phones, accessories and other consumer electronics for customers in guinea-bissau.[web:1586]",
+    k:"ikuma phones and electronics,smartphones mobile phones accessories consumer electronics,online tech shopping guinea-bissau,ikuma.online electronics store",
+    a:""
+  },
+  {
+    i:22,
+    n:"ikuma appliances & informática",
+    c:"electronics,home,business",
+    u:"https://www.ikuma.online/",
+    d:"ikuma appliances and informática section offers home appliances, computer equipment and related technology products with online ordering and delivery.[web:1586]",
+    k:"ikuma appliances and informática,home appliances computer equipment technology products,online ordering and delivery guinea-bissau,ikuma.online tech and home shop",
+    a:""
+  },
+  {
+    i:23,
+    n:"guine bissau market fashion",
+    c:"fashion,business",
+    u:"https://guinebissaumarket.com/",
+    d:"guine bissau market fashion listings contain clothing, shoes and accessories from local merchants using the marketplace for online visibility.[web:1585]",
+    k:"guine bissau market fashion,clothing shoes accessories local merchants,marketplace online visibility,guinebissaumarket.com fashion shopping",
+    a:""
+  },
+  {
+    i:24,
+    n:"guine bissau market home goods",
+    c:"home,business",
+    u:"https://guinebissaumarket.com/",
+    d:"guine bissau market home goods section offers kitchenware, decor and household items aimed at families in guinea-bissau shopping online.[web:1585]",
+    k:"guine bissau market home goods,kitchenware decor household items,families in guinea-bissau shopping online,guinebissaumarket.com home products",
+    a:""
+  },
+  {
+    i:25,
+    n:"shopbycountries guinea-bissau guide",
+    c:"software,business",
+    u:"https://www.shopbycountries.com/pt/country/gw",
+    d:"shopbycountries guinea-bissau guide lists trusted shopping platforms such as facebook marketplace, instagram shops and aliexpress with notes on availability and categories in guinea-bissau.[web:1587]",
+    k:"shopbycountries guinea-bissau guide,lists trusted shopping platforms,facebook marketplace instagram shops aliexpress availability and categories,online shopping information guinea-bissau",
+    a:""
+  }
+];
