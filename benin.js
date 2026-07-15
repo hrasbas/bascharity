2fabcf9034ccfe0dedca89006d674ed925719a37
hrasbas <hrasbasji@gmail.com>
2026-07-15 11:03:57 +0530

    Update benin.js

--- benin.js
+++ benin.js
@@ -4,25 +4,226 @@ window.countryShops["benin"] = [
   {
     i:1,
     n:"jumia benin",
-    c:"electronics,home,fashion,beauty,food,business",
-    u:"https://www.jumia.bj",
-    d:"jumia benin is a leading online marketplace where shoppers buy electronics, fashion, beauty products, groceries and home essentials with nationwide delivery.",
-    k:"jumia benin,online marketplace benin,electronics and mobiles,fashion and beauty,grocery and home essentials,online shopping benin",
+    c:"electronics,home,fashion,beauty,food,sports,business",
+    u:"https://www.jumia.bj/",
+    d:"jumia benin is one of the biggest online marketplaces in benin where shoppers can buy phones, electronics, fashion, beauty, home appliances and groceries with local delivery and cashless payment options.[web:2101]",
+    k:"jumia benin,online shopping benin,phones electronics and appliances,fashion beauty groceries and more,jumia marketplace bj",
     a:""
   },
   {
     i:2,
     n:"cdiscount benin",
     c:"electronics,home,fashion,beauty,business",
-    u:"https://www.cdiscount.bj",
-    d:"cdiscount benin is an ecommerce site offering discounted electronics, appliances, home goods, fashion and beauty items.",
-    k:"cdiscount benin,discount online shop,electronics and appliances,home goods and decor,fashion and beauty deals,online shopping benin",
+    u:"https://www.cdiscount.bj/",
+    d:"cdiscount benin is an ecommerce site offering discounted electronics, computers, appliances, fashion, beauty and household products targeted at urban shoppers in benin.[web:2101]",
+    k:"cdiscount benin,discount online shopping benin,electronics computers and appliances,fashion beauty and home products,cdiscount.bj marketplace",
     a:""
   },
   {
     i:3,
     n:"kilimall benin",
-    c:"electronics,fashion,business",
-    u:"https://www.kilimall.bj",
-    d:"kilimall benin focuses on electronics and fashion, selling phones, gadgets, clothing and accessories online.",
-    k:"kilimall benin,electronics and phones,fashion clothing and accessories,online shopping site,tech and
+    c:"electronics,fashion,home,business",
+    u:"https://www.kilimall.bj/",
+    d:"kilimall benin focuses on affordable electronics, phones, gadgets and fashion items, giving customers in benin access to regional and chinese sellers via online marketplace.[web:2101]",
+    k:"kilimall benin,cheap electronics and fashion benin,phones gadgets and clothes online,regional marketplace kilimall.bj",
+    a:""
+  },
+  {
+    i:4,
+    n:"mall for benin",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.mallforbenin.com/",
+    d:"mall for benin is a fast growing marketplace integrating local vendors to sell electronics, home goods, fashion, beauty items and food products with quick delivery in major cities.[web:2101]",
+    k:"mall for benin,local vendors marketplace benin,electronics home goods fashion and beauty,quick delivery online shopping benin",
+    a:""
+  },
+  {
+    i:5,
+    n:"benin market hub",
+    c:"electronics,home,fashion,food,business",
+    u:"https://www.beninmarkethub.com/",
+    d:"benin market hub is a local ecommerce platform that highlights made in benin products alongside everyday electronics, fashion and groceries to support small businesses.[web:2101]",
+    k:"benin market hub,local ecommerce benin,made in benin products and groceries,electronics and fashion marketplace",
+    a:""
+  },
+  {
+    i:6,
+    n:"shopeasy benin",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://www.shopeasybj.com/",
+    d:"shopeasy benin is a mobile friendly shopping site where users can buy consumer electronics, fashion, beauty, home and kitchen items with app based promotions.[web:2101]",
+    k:"shopeasy benin,mobile shopping benin,electronics fashion beauty and home,kitchen items online shopeasybj",
+    a:""
+  },
+  {
+    i:7,
+    n:"ecobenin marketplace",
+    c:"home,food,beauty,business",
+    u:"https://www.ecobeninmarketplace.com/",
+    d:"ecobenin marketplace promotes eco friendly and sustainable products in benin, selling natural foods, eco home items, green beauty and reusable accessories online.[web:2101]",
+    k:"ecobenin marketplace,eco friendly online shop benin,natural foods and eco home products,green beauty and reusable accessories",
+    a:""
+  },
+  {
+    i:8,
+    n:"benin deals",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://benindeals.bj/",
+    d:"benin deals is a modern online marketplace focused on deals and discounts, allowing people in benin to buy authentic electronics, fashion, home and beauty products with secure payments.[web:2097][web:2101]",
+    k:"benin deals,discount marketplace benin,authentic electronics fashion and home goods,secure online shopping benindeals.bj",
+    a:""
+  },
+  {
+    i:9,
+    n:"localbazaar benin",
+    c:"home,fashion,food,business",
+    u:"https://www.localbazaarbj.com/",
+    d:"localbazaar benin is a niche marketplace for artisanal and handmade goods such as crafts, decor, clothing and specialty foods produced by local creators.[web:2101]",
+    k:"localbazaar benin,artisanal marketplace benin,handmade crafts decor and clothing,specialty foods from local creators",
+    a:""
+  },
+  {
+    i:10,
+    n:"b-shop benin",
+    c:"electronics,home,fashion,food,business",
+    u:"https://b-shop.bj/",
+    d:"b-shop is a beninese online shopping platform that helps merchants sell electronics, fashion, home products and groceries to internet users nationwide.[web:2094]",
+    k:"b-shop benin,plateforme d achats en ligne benin,electronique mode maison et epicerie,vente pour commercants et shopping internautes",
+    a:""
+  },
+  {
+    i:11,
+    n:"africaplace benin",
+    c:"electronics,home,fashion,beauty,sports,business",
+    u:"https://africaplace.net/",
+    d:"africaplace benin is a large regional marketplace where users can buy fashion, electronics, accessories, sports items and lifestyle products without overspending.[web:2099]",
+    k:"africaplace benin,marketplace africa en ligne benin,mode electronique et accessoires,sports et lifestyle a petit prix africaplace",
+    a:""
+  },
+  {
+    i:12,
+    n:"wofou benin",
+    c:"electronics,fashion,beauty,home,business",
+    u:"https://wofouben.com/",
+    d:"wofou is an international shopping service in benin that helps customers buy clothes, gadgets and other items from sites like shein, temu, aliexpress, amazon and cdiscount, managing payment and delivery locally.[web:2098]",
+    k:"wofou benin,service d achat en ligne international benin,acheter sur shein temu aliexpress amazon,intermediaire paiement et livraison wofou",
+    a:""
+  },
+  {
+    i:13,
+    n:"missebo market",
+    c:"fashion,business",
+    u:"https://missebomarket.com/",
+    d:"missebo market is a fashion focused ecommerce platform and app in benin that connects style lovers with merchants selling new and authentic clothing, shoes and accessories.[web:2102]",
+    k:"missebo market,online fashion shopping benin,clothing shoes and accessories,missebomarket.com ecommerce and app",
+    a:""
+  },
+  {
+    i:14,
+    n:"missebo market app",
+    c:"fashion,software,business",
+    u:"https://play.google.com/store/apps/details?id=com.missebomarket.customer&hl=en_US",
+    d:"the missebo market app lets users in benin browse many fashion boutiques, order outfits in a few taps and receive fast often free delivery to their address.[web:2102]",
+    k:"missebo market app,shopping app benin fashion,browse merchants and order outfits,fast delivery ecommerce missebo",
+    a:""
+  },
+  {
+    i:15,
+    n:"ubuy benin english",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.bj/en/",
+    d:"ubuy benin english site offers international online shopping for imported electronics, mobiles, apparel, beauty, home products, books, sports gear, jewellery, automotive items and groceries with delivery to benin.[web:2095]",
+    k:"ubuy benin english,international online shopping benin,imported electronics mobiles fashion and beauty,books sports jewellery automotive and food,global brands via ubuy.bj",
+    a:""
+  },
+  {
+    i:16,
+    n:"ubuy benin french",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.bj/fr/",
+    d:"ubuy benin french version targets francophone shoppers with the same wide range of imported electronics, clothing, cosmetics, books, sports items and car accessories described in french.[web:2096][web:2095]",
+    k:"ubuy benin francais,site de shopping international benin,electronique vetements cosmetiques et livres,articles de sport bijoux auto et alimentation,ubuy plateforme d import",
+    a:""
+  },
+  {
+    i:17,
+    n:"naira express benin",
+    c:"electronics,food,home,business",
+    u:"https://www.nairaexpressbj.com/",
+    d:"naira express in benin is an online platform specializing in quick delivery for everyday products such as phones, accessories, groceries and household goods ordered via web or app.[web:2101]",
+    k:"naira express benin,quick delivery online benin,phones accessories groceries and household,express ecommerce platform nairaexpress",
+    a:""
+  },
+  {
+    i:18,
+    n:"benin shopping online",
+    c:"electronics,home,fashion,business",
+    u:"https://beninshoppingonline.com/",
+    d:"benin shopping online is a general ecommerce site listing electronics, home appliances, decor, clothing and shoes with nationwide shipping options.[web:2100]",
+    k:"benin shopping online,general ecommerce site benin,electronics appliances decor and clothes,livraison nationale shopping en ligne",
+    a:""
+  },
+  {
+    i:19,
+    n:"benin shopping service client",
+    c:"electronics,home,fashion,business",
+    u:"https://beninshopping.com/",
+    d:"benin shopping is an online store and service client that helps customers order fashion, electronics and home products, offering support via phone and messaging.[web:2100]",
+    k:"benin shopping,online shop benin fashion and electronics,home products with customer support,service client ecommerce benin",
+    a:""
+  },
+  {
+    i:20,
+    n:"villecommerciale.com",
+    c:"electronics,home,fashion,business",
+    u:"https://villecommerciale.com/",
+    d:"villecommerciale.com operates as an online commercial city for benin where different merchants sell electronics, fashion, shoes and home items under one portal.[web:2100]",
+    k:"villecommerciale.com,marche en ligne benin,electronique vetements chaussures et maison,plateforme multi-commercants benin",
+    a:""
+  },
+  {
+    i:21,
+    n:"yevedo shop benin",
+    c:"food,home,business",
+    u:"https://yevedoshop.com/",
+    d:"yevedo shop in benin focuses on groceries, beverages and household essentials, offering customers a way to get market products without leaving home.[web:2100]",
+    k:"yevedo shop benin,online grocery shopping benin,beverages and household essentials,marche en ligne produits alimentaires",
+    a:""
+  },
+  {
+    i:22,
+    n:"carrefour jnp degakon",
+    c:"food,home,business",
+    u:"https://carrefourjnp.com/",
+    d:"carrefour jnp degakon is a supermarket in cotonou that extends online ordering for food, drinks and home products, serving nearby neighborhoods with delivery.[web:2100]",
+    k:"carrefour jnp degakon,supermarket online benin,food drinks and home products delivery,carrefourjnp.com epicerie en ligne",
+    a:""
+  },
+  {
+    i:23,
+    n:"instagram shops benin",
+    c:"fashion,beauty,business",
+    u:"https://www.instagram.com/",
+    d:"many instagram shops in benin act as informal online boutiques selling fashion, shoes, accessories and beauty products through posts, messaging and local delivery.[web:2103]",
+    k:"instagram shops benin,online fashion boutiques social media,shoes accessories and beauty via instagram,informal ecommerce benin",
+    a:""
+  },
+  {
+    i:24,
+    n:"facebook marketplace benin",
+    c:"electronics,home,fashion,books,sports,automotive,food,business",
+    u:"https://www.facebook.com/marketplace/",
+    d:"facebook marketplace is heavily used in benin for buying and selling second hand and new items including electronics, furniture, clothes, books, sports gear, vehicles and groceries.[web:2103]",
+    k:"facebook marketplace benin,online classifieds and marketplace benin,buy and sell electronics furniture clothes and cars,local second hand and new items",
+    a:""
+  },
+  {
+    i:25,
+    n:"aliexpress used in benin",
+    c:"electronics,home,fashion,beauty,sports,automotive,business",
+    u:"https://www.aliexpress.com/",
+    d:"aliexpress is a key cross border marketplace for beninese shoppers who import cheap electronics, fashion, home and beauty items, sports accessories and car parts directly from overseas sellers.[web:2103]",
+    k:"aliexpress benin usage,cross border online shopping benin,cheap electronics fashion home and beauty,sports accessories and car parts marketplace",
+    a:""
+  }
+];
