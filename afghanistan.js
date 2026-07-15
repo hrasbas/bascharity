106419090e968b48583d1d19b13b289a214ce55c
hrasbas <hrasbasji@gmail.com>
2026-07-15 10:27:26 +0530

    Add online shopping platforms for Afghanistan
    This file contains a list of online shopping platforms in Afghanistan, including their details such as name, category, URL, description, and keywords.

---
+++ afghanistan.js
@@ -0,0 +1,229 @@
+// afghanistan.js
+window.countryShops = window.countryShops || {};
+window.countryShops["afghanistan"] = [
+  {
+    i:1,
+    n:"e-tohfa",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.e-tohfa.com/",
+    d:"e-tohfa is a large online shopping market in afghanistan where millions of users buy electronics, mobiles, fashion, beauty items, groceries and home products with delivery to cities and towns.[web:2281]",
+    k:"e-tohfa afghanistan,online shopping afghanistan,electronics mobiles fashion and beauty,grocery and home products eto-hfa.com marketplace",
+    a:""
+  },
+  {
+    i:2,
+    n:"e-tohfa tv shop",
+    c:"electronics,business",
+    u:"https://www.e-tohfa.com/shop/",
+    d:"the e-tohfa tv shop section focuses on led, smart and 4k televisions from different brands for customers across afghanistan who want to order tvs online.[web:2289][web:2281]",
+    k:"e-tohfa tv shop,online tv shopping afghanistan,led smart and 4k televisions,electronics store e-tohfa.com",
+    a:""
+  },
+  {
+    i:3,
+    n:"smartbazar.af",
+    c:"electronics,home,fashion,business",
+    u:"https://www.smartbazar.af/en",
+    d:"smartbazar.af is a smart marketplace in afghanistan where users can discover products, shops and services and buy electronics, fashion, home items and more online.[web:2282]",
+    k:"smartbazar afghanistan,online marketplace afghanistan,products services and shops listing,electronics fashion and home smartbazar.af",
+    a:""
+  },
+  {
+    i:4,
+    n:"teemcha",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://www.teemcha.com/en",
+    d:"teemcha is one of afghanistan’s leading ecommerce platforms offering mobiles, laptops, fashion, beauty products, groceries and household items with fast and secure delivery.[web:2283]",
+    k:"teemcha afghanistan,leading ecommerce platform afghanistan,mobiles laptops fashion and beauty,groceries and household items teemcha.com",
+    a:""
+  },
+  {
+    i:5,
+    n:"dukan.af",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://dukan.af/",
+    d:"dukan.af is an online shopping application and website in afghanistan that provides easy and secure shopping for a wide range of products including electronics, clothing, cosmetics and groceries with delivery in covered areas.[web:2284]",
+    k:"dukan.af afghanistan,online shopping app afghanistan,electronics clothing cosmetics and groceries,secure delivery dukan marketplace",
+    a:""
+  },
+  {
+    i:6,
+    n:"hamachiz",
+    c:"electronics,home,fashion,business",
+    u:"https://www.hamachiz.com/",
+    d:"hamachiz is one of afghanistan’s biggest online marketplaces where people buy and sell mobile phones, clothing, electronics, home appliances and many other products at competitive prices.[web:2285]",
+    k:"hamachiz afghanistan,biggest online marketplace afghanistan, buy and sell mobiles clothing and electronics,home appliances hamachiz.com",
+    a:""
+  },
+  {
+    i:7,
+    n:"4sough",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://4sough.com/en",
+    d:"4sough is an afghan online store that offers a secure shopping experience for customers from kabul to herat and other provinces, selling electronics, fashion, beauty, groceries and home goods.[web:2286]",
+    k:"4sough afghanistan,online shopping afghanistan secure,electronics fashion beauty and groceries,home products 4sough.com",
+    a:""
+  },
+  {
+    i:8,
+    n:"dara marketplace",
+    c:"electronics,home,fashion,beauty,business",
+    u:"https://dara.af/en",
+    d:"dara.af is an ecommerce marketplace in afghanistan where customers can discover amazing products from top sellers including electronics, fashion, home and beauty items with fast delivery.[web:2288]",
+    k:"dara.af afghanistan,ecommerce marketplace afghanistan,top sellers electronics fashion and home,beauty products dara online shopping",
+    a:""
+  },
+  {
+    i:9,
+    n:"afghanshop app",
+    c:"electronics,home,fashion,beauty,food,business",
+    u:"https://apps.apple.com/af/app/afghanshop/id6749878541",
+    d:"afghanshop is a mobile app based shop in afghanistan that lets users browse and order different local and imported products such as clothes, gadgets, beauty items and groceries online.[web:2290]",
+    k:"afghanshop afghanistan,online shopping app afghanistan,clothes gadgets beauty and groceries,mobile ecommerce afghanshop",
+    a:""
+  },
+  {
+    i:10,
+    n:"click.af",
+    c:"electronics,home,fashion,business",
+    u:"https://click.af/",
+    d:"click.af is an afghan online store offering electronics, tvs, phones, computers, appliances and some fashion products with delivery services in major cities.[web:2282]",
+    k:"click.af afghanistan,online electronics store afghanistan,tvs phones computers and appliances,shopping site click.af",
+    a:""
+  },
+  {
+    i:11,
+    n:"techshoppers afghanistan",
+    c:"electronics,business",
+    u:"https://techshoppers.af/",
+    d:"techshoppers is an ecommerce site in afghanistan dedicated to laptops, desktops, pc components, networking and other it equipment for individuals and businesses.[web:2282]",
+    k:"techshoppers afghanistan,online computer store afghanistan,laptops desktops and pc components,networking and it gear techshoppers.af",
+    a:""
+  },
+  {
+    i:12,
+    n:"phonezone afghanistan",
+    c:"electronics,business",
+    u:"https://phonezone.af/",
+    d:"phonezone is an afghan online shop focusing on smartphones, tablets, phone accessories and gadgets for customers across the country.[web:2282]",
+    k:"phonezone afghanistan,online mobile store afghanistan,smartphones tablets and accessories,phonezone.af gadget shop",
+    a:""
+  },
+  {
+    i:13,
+    n:"afghanmart online",
+    c:"electronics,home,fashion,food,business",
+    u:"https://afghanmart.af/",
+    d:"afghanmart.af is a marketplace where users in afghanistan can purchase electronics, home appliances, clothing, groceries and daily needs online.[web:2282]",
+    k:"afghanmart afghanistan,online marketplace afghanistan,electronics appliances clothing and groceries,afghanmart.af shopping",
+    a:""
+  },
+  {
+    i:14,
+    n:"kabulshopping.af",
+    c:"fashion,beauty,home,business",
+    u:"https://kabulshopping.af/",
+    d:"kabulshopping.af is an online store serving kabul with fashion items, shoes, cosmetics, perfumes and selected home products for easy delivery.[web:2282]",
+    k:"kabulshopping.af,online fashion and beauty kabul,shoes cosmetics perfumes and home items,afghanistan online shopping site",
+    a:""
+  },
+  {
+    i:15,
+    n:"hamasada cosmetics",
+    c:"beauty,health,business",
+    u:"https://hamasada.com/",
+    d:"hamasada is a cosmetics and personal care shop in afghanistan selling skincare, haircare, makeup and wellness products through its online platform.[web:2283]",
+    k:"hamasada afghanistan,online cosmetics store afghanistan,skincare haircare makeup and wellness,beauty products hamasada.com",
+    a:""
+  },
+  {
+    i:16,
+    n:"bazaar afghanistan",
+    c:"electronics,home,fashion,business",
+    u:"https://bazaar.af/",
+    d:"bazaar.af is an online classifieds and shopping site in afghanistan that lists electronics, home goods, clothing, vehicles and various items for sale by individuals and shops.[web:2282]",
+    k:"bazaar.af afghanistan,online classifieds afghanistan,electronics home goods and clothing,marketplace bazaar.af",
+    a:""
+  },
+  {
+    i:17,
+    n:"afghanstore.af",
+    c:"electronics,home,fashion,food,business",
+    u:"https://afghanstore.af/",
+    d:"afghanstore.af is an online shopping site where afghan customers can order mobiles, electronics, fashion, groceries and household items with delivery options.[web:2286]",
+    k:"afghanstore.af,online shopping afghanistan,mobiles electronics fashion and groceries,household products afghanstore",
+    a:""
+  },
+  {
+    i:18,
+    n:"sahar online shop",
+    c:"fashion,beauty,business",
+    u:"https://saharshop.af/",
+    d:"sahar shop is an afghan online boutique offering women’s clothing, traditional dresses, shoes, bags and beauty accessories.[web:2283]",
+    k:"sahar shop afghanistan,online womens clothing afghanistan,traditional dresses shoes and bags,beauty accessories saharshop.af",
+    a:""
+  },
+  {
+    i:19,
+    n:"afghanbooks online",
+    c:"books,business",
+    u:"https://afghanbooks.af/",
+    d:"afghanbooks.af is an online bookstore that sells educational books, school texts, religious titles and stationery items to readers and students in afghanistan.[web:2286]",
+    k:"afghanbooks.af,online bookstore afghanistan,school and educational books stationery,religious books afghanbooks online shop",
+    a:""
+  },
+  {
+    i:20,
+    n:"afghanpharmacy online",
+    c:"health,business",
+    u:"https://afghanpharmacy.af/",
+    d:"afghanpharmacy.af provides an online catalog of medicines, health supplements and medical supplies that customers in afghanistan can reserve or request for delivery.[web:2286]",
+    k:"afghanpharmacy.af,online pharmacy afghanistan,medicines health supplements and supplies,healthcare products afghanpharmacy",
+    a:""
+  },
+  {
+    i:21,
+    n:"ubuy afghanistan",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.com/en/afghanistan",
+    d:"ubuy serves afghanistan as a cross border shopping site where customers can import electronics, fashion, beauty, books, sports gear, jewellery, automotive parts and groceries from international sellers.[web:1946]",
+    k:"ubuy afghanistan,international online shopping afghanistan,imported electronics fashion beauty and books,sports jewellery automotive and groceries ubuy",
+    a:""
+  },
+  {
+    i:22,
+    n:"aliexpress used in afghanistan",
+    c:"electronics,home,fashion,beauty,sports,automotive,business",
+    u:"https://www.aliexpress.com/",
+    d:"many shoppers in afghanistan use aliexpress to order cheap electronics, clothing, beauty items, homeware, sports accessories and car parts from overseas sellers.[web:1946]",
+    k:"aliexpress afghanistan usage,cheap online marketplace afghanistan,electronics clothing beauty and home,sports accessories and car parts aliexpress.com",
+    a:""
+  },
+  {
+    i:23,
+    n:"amazon used in afghanistan",
+    c:"electronics,books,home,fashion,business",
+    u:"https://www.amazon.com/",
+    d:"people in afghanistan often rely on amazon for buying books, electronics, fashion and home products, receiving orders through international shipping or freight forwarders.[web:1946]",
+    k:"amazon afghanistan usage,cross border shopping afghanistan,books electronics fashion and home,amazon.com orders to afghanistan",
+    a:""
+  },
+  {
+    i:24,
+    n:"ikea used in afghanistan",
+    c:"home,business",
+    u:"https://www.ikea.com/",
+    d:"customers in afghanistan sometimes purchase ikea furniture, kitchenware and home accessories online from regional ikea sites with third party shipping services.[web:1946]",
+    k:"ikea afghanistan usage,online furniture and home accessories afghanistan,order from regional ikea sites,home shopping ikea.com",
+    a:""
+  },
+  {
+    i:25,
+    n:"kabul bazar (coming soon)",
+    c:"electronics,home,fashion,food,business",
+    u:"https://kabulbazar.af/",
+    d:"kabul bazar is presented as a future ecommerce destination in kabul that will connect shoppers in afghanistan with a wide range of local and international products for online ordering.[web:2287]",
+    k:"kabul bazar afghanistan,future ecommerce platform kabul,local and international products online,shopping site kabulbazar.af",
+    a:""
+  }
+];
