ca732df4815d42fdc1b0fdd6adbd8aaae96ff958
hrasbas <hrasbasji@gmail.com>
2026-07-15 12:10:44 +0530

    Add online shops data for Denmark

---
+++ denmark.js
@@ -0,0 +1,229 @@
+// denmark.js
+window.countryShops = window.countryShops || {};
+window.countryShops["denmark"] = [
+  {
+    i:1,
+    n:"ubuy denmark",
+    c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
+    u:"https://www.ubuy.dk/en/",
+    d:"ubuy denmark is an international online shopping platform where customers in denmark can buy imported electronics, apparel, mobile phones, beauty products and many other categories from global brands at competitive prices.[web:1818][web:1826]",
+    k:"ubuy denmark,international online shopping denmark,imported electronics mobiles apparel beauty and home goods,books sports gear automotive products groceries,global brands delivered to denmark ubuy.dk",
+    a:""
+  },
+  {
+    i:2,
+    n:"ubuy denmark ubay brand",
+    c:"electronics,home,business",
+    u:"https://www.ubuy.dk/da/brand/ubay",
+    d:"the ubay brand page on ubuy denmark highlights ubay products such as electronics and home items that shoppers across danish cities can order online at discounted prices.[web:1820]",
+    k:"ubuy denmark ubay brand,ubay products online denmark,electronics and home goods at discount prices,shop ubay via ubuy.dk",
+    a:""
+  },
+  {
+    i:3,
+    n:"ubuy denmark label printer",
+    c:"electronics,business",
+    u:"https://www.ubuy.dk/da/product/3EIUT0-rollo-label-printer-commercial-grade-direct-thermal-high-speed-printer-compatible-with-ets",
+    d:"this ubuy denmark product page sells a commercial grade rollo usb shipping label printer suitable for danish ecommerce businesses that need high speed thermal label printing.[web:1821]",
+    k:"ubuy denmark rollo label printer,commercial grade shipping label printer denmark,high speed thermal 4x6 label printer for ecommerce,order rollo printer online ubuy",
+    a:""
+  },
+  {
+    i:4,
+    n:"elgiganten denmark",
+    c:"electronics,home,business",
+    u:"https://www.elgiganten.dk/",
+    d:"elgiganten is one of the most popular online stores in denmark for consumer electronics, selling mobiles, computers, gaming, tv and appliances with pickup and delivery options.[web:1819]",
+    k:"elgiganten denmark,online electronics store denmark,mobiles computers gaming tv and appliances,click and collect or delivery elgiganten.dk",
+    a:""
+  },
+  {
+    i:5,
+    n:"zalando denmark",
+    c:"fashion,business",
+    u:"https://www.zalando.dk/",
+    d:"zalando denmark is a large online fashion retailer offering shoes, clothing and accessories for women, men and kids from many european and global brands.[web:1819]",
+    k:"zalando denmark,online fashion shopping denmark,shoes clothing and accessories for all,eu brands fashion store zalando.dk",
+    a:""
+  },
+  {
+    i:6,
+    n:"boozt denmark",
+    c:"fashion,business",
+    u:"https://www.boozt.com/dk/da",
+    d:"boozt runs an ecommerce platform in denmark selling stylish fashion, footwear and accessories from scandinavian and international brands at competitive prices.[web:1819]",
+    k:"boozt denmark,online fashion store denmark,scandinavian and international clothing and shoes,boozt.com dk ecommerce",
+    a:""
+  },
+  {
+    i:7,
+    n:"bilka online",
+    c:"food,home,electronics,fashion,sports,business",
+    u:"https://www.bilka.dk/",
+    d:"bilka is a danish hypermarket chain whose online shop sells groceries, home and garden products, electronics, fashion, sports items and more with nationwide delivery or pickup.[web:1819][web:1824]",
+    k:"bilka online,hypermarket online shopping denmark,groceries home garden electronics fashion sports,click and collect or delivery bilka.dk",
+    a:""
+  },
+  {
+    i:8,
+    n:"nemlig.com",
+    c:"food,home,business",
+    u:"https://www.nemlig.com/",
+    d:"nemlig.com is a popular online supermarket in denmark where customers can order groceries, fresh produce and household essentials with flexible delivery slots.[web:1824]",
+    k:"nemlig.com,online supermarket denmark,grocery delivery fresh food and household essentials,danish online grocery shopping platform",
+    a:""
+  },
+  {
+    i:9,
+    n:"coop.dk",
+    c:"home,food,electronics,business",
+    u:"https://www.coop.dk/",
+    d:"coop.dk is the ecommerce site of coop denmark selling kitchen appliances, furniture, bikes, kids toys and many consumer goods along with links to grocery services.[web:1819]",
+    k:"coop.dk,online store coop denmark,kitchen appliances furniture bikes and toys,danish consumer goods ecommerce site",
+    a:""
+  },
+  {
+    i:10,
+    n:"ikea denmark",
+    c:"home,business",
+    u:"https://www.ikea.dk/",
+    d:"ikea denmark offers an online shop for furniture, storage, lighting and kitchen accessories, allowing danish customers to plan rooms and order products for home delivery.[web:1819]",
+    k:"ikea denmark,online furniture and homeware denmark,sofas beds shelving and kitchen accessories,room planning and delivery ikea.dk",
+    a:""
+  },
+  {
+    i:11,
+    n:"apple online store denmark",
+    c:"electronics,software,business",
+    u:"https://www.apple.com/dk/",
+    d:"apple online store denmark sells iphones, ipads, macs, apple watch and accessories directly to danish customers with financing, trade in and support options.[web:1819]",
+    k:"apple online store denmark,iphones ipads macs and apple watch,official apple ecommerce denmark,financing and support apple.com dk",
+    a:""
+  },
+  {
+    i:12,
+    n:"dba.dk",
+    c:"electronics,home,automotive,fashion,business",
+    u:"https://www.dba.dk/",
+    d:"dba.dk is a leading danish classifieds marketplace where users buy and sell used and new items including electronics, furniture, cars, fashion and more across denmark.[web:1822]",
+    k:"dba.dk,danish classifieds marketplace,buy and sell used electronics furniture cars fashion,most visited retail website denmark dba",
+    a:""
+  },
+  {
+    i:13,
+    n:"pricerunner denmark",
+    c:"electronics,home,fashion,business",
+    u:"https://www.pricerunner.dk/",
+    d:"pricerunner.dk is a price comparison site in denmark helping shoppers find the best offers on electronics, home products, fashion and more before buying online.[web:1822]",
+    k:"pricerunner denmark,price comparison site dk,compare prices on electronics home and fashion,find best online deals pricerunner.dk",
+    a:""
+  },
+  {
+    i:14,
+    n:"harald nyborg online",
+    c:"home,sports,business",
+    u:"https://www.harald-nyborg.dk/",
+    d:"harald nyborg is a danish retailer whose online shop sells tools, hardware, garden equipment, camping gear and home improvement products at low prices.[web:1822]",
+    k:"harald nyborg online,tools hardware and garden equipment denmark,camping and home improvement products,discount retailer harald-nyborg.dk",
+    a:""
+  },
+  {
+    i:15,
+    n:"wolt denmark",
+    c:"food,software,business",
+    u:"https://wolt.com/da/dnk",
+    d:"wolt denmark is a food delivery platform where users order meals, groceries and convenience items from local restaurants and stores through the app and website.[web:1824]",
+    k:"wolt denmark,food delivery app denmark,order meals and groceries from local restaurants and stores,online food courier service wolt",
+    a:""
+  },
+  {
+    i:16,
+    n:"saxo.com",
+    c:"books,business",
+    u:"https://www.saxo.com/dk",
+    d:"saxo.com is one of the largest online bookstores in denmark offering millions of titles, ebooks and audiobooks along with subscription options and used book sales.[web:1819]",
+    k:"saxo.com,online bookstore denmark,millions of books ebooks and audiobooks,danish bookshop with memberships saxo",
+    a:""
+  },
+  {
+    i:17,
+    n:"sportmaster denmark",
+    c:"sports,fashion,business",
+    u:"https://www.sportmaster.dk/",
+    d:"sportmaster is a major danish sports retailer with an online shop selling sportswear, running shoes, fitness gear and equipment for many activities.[web:1819]",
+    k:"sportmaster denmark,online sports shop denmark,sportswear running shoes and fitness equipment,sportmaster.dk sporting goods",
+    a:""
+  },
+  {
+    i:18,
+    n:"intersport danmark",
+    c:"sports,fashion,business",
+    u:"https://www.intersport.dk/",
+    d:"intersport danmark ecommerce site offers branded sports clothing, shoes and gear for football, running, training and outdoor activities.[web:1819]",
+    k:"intersport danmark,online sports clothing and shoes denmark,football running training and outdoor gear,intersport.dk webshop",
+    a:""
+  },
+  {
+    i:19,
+    n:"matas online",
+    c:"beauty,health,business",
+    u:"https://www.matas.dk/",
+    d:"matas is a well known danish chain whose online store sells beauty products, skincare, haircare, perfumes and health supplements with click and collect options.[web:1819]",
+    k:"matas online,beauty and health store denmark,skincare haircare perfumes and supplements,matas.dk webshop click and collect",
+    a:""
+  },
+  {
+    i:20,
+    n:"apopro online pharmacy",
+    c:"health,business",
+    u:"https://www.apopro.dk/",
+    d:"apopro is a danish online pharmacy where customers can buy prescription medicines, over the counter drugs and healthcare products with professional guidance and delivery.[web:1819]",
+    k:"apopro online pharmacy,danish internet pharmacy,prescription and otc medicine with advice,healthcare products delivery apopro.dk",
+    a:""
+  },
+  {
+    i:21,
+    n:"føtex online",
+    c:"food,home,business",
+    u:"https://www.foetex.dk/",
+    d:"føtex online offers groceries, personal care and selected home products for danish shoppers, combining supermarket convenience with delivery and pickup services.[web:1819]",
+    k:"føtex online,online supermarket denmark,grocery personal care and home items,delivery and click collect foetex.dk",
+    a:""
+  },
+  {
+    i:22,
+    n:"netto plus denmark",
+    c:"food,business",
+    u:"https://netto.dk/",
+    d:"netto danmark provides information and digital offers and in some areas online ordering for discount groceries, snacks and daily essentials.[web:1819]",
+    k:"netto plus denmark,discount grocery chain denmark,online offers and limited ordering for food and essentials,netto.dk digital supermarket",
+    a:""
+  },
+  {
+    i:23,
+    n:"whiteaway denmark",
+    c:"home,electronics,business",
+    u:"https://www.whiteaway.com/dk/",
+    d:"whiteaway is an online retailer in denmark specializing in large and small home appliances like washing machines, dishwashers, ovens and vacuum cleaners with nationwide delivery.[web:1819]",
+    k:"whiteaway denmark,online appliance store washing machines dishwashers ovens,vacuum cleaners and kitchen appliances delivery,whiteaway.com dk webshop",
+    a:""
+  },
+  {
+    i:24,
+    n:"computersalg.dk",
+    c:"electronics,home,business",
+    u:"https://www.computersalg.dk/",
+    d:"computersalg.dk is a danish online shop selling computers, components, electronics, office equipment and household items to private and business customers.[web:1819]",
+    k:"computersalg.dk,online computer and electronics shop denmark,components office equipment and household items,tech and more ecommerce computersalg",
+    a:""
+  },
+  {
+    i:25,
+    n:"proshop denmark",
+    c:"electronics,business",
+    u:"https://www.proshop.dk/",
+    d:"proshop is a major danish ecommerce site for electronics, offering pcs, components, gaming gear, smart home products and accessories with fast shipping.[web:1819]",
+    k:"proshop denmark,online electronics retailer pcs components gaming,smart home products and accessories,fast shipping tech store proshop.dk",
+    a:""
+  }
+];
