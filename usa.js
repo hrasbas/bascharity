3038f259cdbcda7ac5a8248dbfa104ce58e3df62
hrasbas <hrasbasji@gmail.com>
2026-07-25 00:44:05 +0530

    Fix formatting in usa.js for foot locker entry

--- usa.js
+++ usa.js
@@ -8455,7 +8455,7 @@ d:"calvin klein is a usa online fashion store for underwear, apparel and accesso
 k:"calvin klein,online shopping usa,usa fashion,apparel,underwear,accessories,online retail,minimalist style",
 a:""
 },
-{
+/*{
 i:940,
 n:"foot locker",
 c:"business,fashion,sports",
