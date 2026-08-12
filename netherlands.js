7f47b3098038c3285660fb45db46a3c1b124cde7
hrasbas <hrasbasji@gmail.com>
2026-08-12 22:01:54 +0530

    Add AliExpress and Alibaba entries to netherlands.js

--- netherlands.js
+++ netherlands.js
@@ -11,6 +11,25 @@ window.countryShops["netherlands"] = [
     k:"ubuy netherlands,international online shopping netherlands,imported electronics mobiles fashion beauty health,home kitchen goods books sports travel gear,groceries jewellery and automotive accessories cross border shopping netherlands",
     a:""
   },
+
+  {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   {
     i:2,
     n:"bol.com netherlands online shop",
