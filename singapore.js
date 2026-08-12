cc6d14bd684e6416190a4204a485c00b5f1d767f
hrasbas <hrasbasji@gmail.com>
2026-08-12 22:11:37 +0530

    Add AliExpress and Alibaba entries to singapore.js

--- singapore.js
+++ singapore.js
@@ -10,6 +10,25 @@ window.countryShops["singapore"] = [
     k:"shopee singapore,online marketplace singapore,electronics mobiles gadgets deals,fashion beauty and home products,grocery and sports gear shopping app",
     a:""
   },
+{
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
+  
   {
     i:2,
     n:"lazada singapore",
