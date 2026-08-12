27a459c641e50f068e7e26fdd638bcaaab1c0dd6
hrasbas <hrasbasji@gmail.com>
2026-08-12 21:55:24 +0530

    Add AliExpress and Alibaba entries to france.js

--- france.js
+++ france.js
@@ -10,6 +10,25 @@ window.countryShops["france"] = [
     k:"amazon france,online shopping france,electronics books fashion beauty home goods,groceries sports items automotive accessories,prime delivery french ecommerce marketplace amazon.fr",
     a:""
   },
+{
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
+  
   {
     i:2,
     n:"cdiscount france",
