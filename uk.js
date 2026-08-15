26abf952c7741d144dd3311db5fd063ee5d0bcec
hrasbas <hrasbasji@gmail.com>
2026-08-15 17:40:55 +0530

    Change Amazon UK affiliate link
    Updated Amazon UK affiliate link for better tracking.

--- uk.js
+++ uk.js
@@ -8,7 +8,8 @@ c:"electronics,home,fashion,beauty,health,food,sports,travel,books,software,busi
 u:"https://www.amazon.co.uk",
 d:"amazon uk is a leading online marketplace in the united kingdom offering electronics, fashion, home essentials, books and groceries with fast delivery options.",
 k:"amazon uk,online shopping uk,electronics and fashion uk,amazon prime uk,buy books and groceries uk",
-a:"https://www.amazon.co.uk/?tag=vmmbaslinks03-21"
+a:"https://www.amazon.co.uk/?tag=bascharity02-21"
+
 },
  {
 i:"1a",
