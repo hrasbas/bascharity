e8635de11ebb9d05105007e6094fb3242781eda1
hrasbas <hrasbasji@gmail.com>
2026-08-16 23:24:13 +0530

    Add Body Lab entry to italy.js

--- italy.js
+++ italy.js
@@ -30,6 +30,15 @@ k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufa
 a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
 },
   {
+  i:"1c",
+  n:"Body Lab",
+  c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+  u:"https://www.bodylab.com/",
+  d:"Body Lab is an online shopping destination offering a range of products across health, fitness, beauty and everyday lifestyle categories.",
+  k:"body lab,online shopping,health,fitness,beauty,lifestyle,wellness",
+  a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.bodylab.com/"
+},
+  {
     i:2,
     n:"ubuy italy",
     c:"electronics,fashion,home,beauty,books,sports,jewellery,automotive,food,business",
