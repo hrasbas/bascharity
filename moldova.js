d2d9b1cd2b3d8d4a9ed5fc452f56dc45b40fde91
hrasbas <hrasbasji@gmail.com>
2026-08-17 17:56:09 +0530

    Add AliExpress and Alibaba entries to moldova.js

--- moldova.js
+++ moldova.js
@@ -10,5 +10,23 @@ window.countryShops["moldova"] = [
     d:"ubuy moldova is an international online shopping website where customers across moldova can buy imported electronics, clothing, mobile phones, beauty products, groceries, home and kitchen goods, books and automotive accessories from global brands at low prices.[web:1174][web:1175][web:1177][web:1178]",
     k:"ubuy moldova,international online shopping moldova,imported electronics clothing mobiles beauty,grocery home kitchen goods online store,books and automotive accessories cross border shopping moldova",
     a:""
-  }
+  },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
 ];
