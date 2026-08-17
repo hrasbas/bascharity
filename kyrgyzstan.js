83526dd09ff83a48adea4e189979c6c2761e3330
hrasbas <hrasbasji@gmail.com>
2026-08-17 17:46:15 +0530

    Add AliExpress and Alibaba entries to kyrgyzstan.js

--- kyrgyzstan.js
+++ kyrgyzstan.js
@@ -10,6 +10,24 @@ window.countryShops["kyrgyzstan"] = [
     k:"ubuy kyrgyzstan,international online shopping kyrgyzstan,imported electronics mobiles and gadgets,apparel jewellery beauty home kitchen,books toys sports gear automotive products and snacks,cross border ecommerce platform",
     a:""
   },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   {
     i:2,
     n:"ubuy kyrgyzstan app",
