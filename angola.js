714e414db7ba8ab62f91a1fae1c2a9932c79a429
hrasbas <hrasbasji@gmail.com>
2026-08-17 11:37:31 +0530

    Add AliExpress and Alibaba entries to angola.js

--- angola.js
+++ angola.js
@@ -9,7 +9,24 @@ window.countryShops["angola"] = [
     d:"ubuy angola is an international online shopping store where customers can order imported electronics, mobiles, fashion, beauty, books, sports gear, jewellery, automotive parts and groceries from global brands.[web:2242]",
     k:"ubuy angola,international online shopping angola,imported electronics mobiles fashion and beauty,books sports jewellery automotive and food,global brands via ubuy.co.ao",
     a:""
-  },
+  },{
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   {
     i:2,
     n:"ubuy angola luxury brands",
