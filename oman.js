e1b530711947b09b02239091c1865c3b22ce26f6
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:18:16 +0530

    Add AliExpress and Alibaba to oman.js
    Added new entries for AliExpress and Alibaba with details.

--- oman.js
+++ oman.js
@@ -3,6 +3,25 @@ countryShops.oman = [
   { id: 1, n: "lulu hypermarket muscat", c: "grocery, general, electronics", u: "https://www.luluhypermarket.com", a: "" },
   { id: 2, n: "carrefour muscat city centre", c: "grocery, general, electronics", u: "https://www.carrefouroman.com", a: "" },
   { id: 3, n: "sharaf dg muscat grand mall", c: "electronics", u: "https://oman.sharafdg.com", a: "" },
+
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   { id: 4, n: "extra muscat ghala electronics", c: "electronics", u: "https://www.extra.com/en-om", a: "" },
   { id: 5, n: "e-max muscat grand mall", c: "electronics", u: "https://www.landmarkgroup.com", a: "" },
   { id: 6, n: "mi store muscat mall of oman", c: "electronics", u: "https://www.mi.com/om", a: "" },
