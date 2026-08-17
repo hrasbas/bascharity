b2cd786d51495bdaf5db5ead264ea31a35297004
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:14:28 +0530

    Update north_macedonia.js

--- north_macedonia.js
+++ north_macedonia.js
@@ -10,5 +10,23 @@ window.countryShops["north_macedonia"] = [
     d:"ubuy north macedonia is an international online shopping website where customers in north macedonia can buy imported electronics, computers, mobile phones, fashion, beauty and grooming products, home and kitchen goods, books, sports gear, travel accessories, groceries, jewellery and automotive parts from global brands at low prices.[web:1326][web:1328][web:1329][web:1331][web:1333]",
     k:"ubuy north macedonia,international online shopping north macedonia,imported electronics computers mobiles fashion,beauty grooming home kitchen goods books,sports gear travel accessories groceries jewellery automotive parts",
     a:""
-  }
+  },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
 ];
