afff990a7bf9b1766cacb468dcdc0beb061c52c4
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:24:03 +0530

    Add AliExpress and Alibaba entries to paraguay.js

--- paraguay.js
+++ paraguay.js
@@ -10,5 +10,22 @@ window.countryShops["paraguay"] = [
     d:"ubuy paraguay is an international online shopping website where customers in paraguay can buy imported electronics, mobiles and tablets, fashion and footwear, beauty and personal care products, home and kitchen goods, books, toys, sports gear, travel accessories, groceries, jewellery and automotive parts from global brands at low prices.[web:1392][web:1393][web:1394][web:1396][web:1397][web:1398][web:1399][web:1401]",
     k:"ubuy paraguay,international online shopping paraguay,imported electronics mobiles tablets fashion footwear,beauty personal care home kitchen goods books,toys sports gear travel accessories groceries jewellery automotive parts",
     a:""
-  }
+  },  {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
 ];
