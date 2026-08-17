635d694fe379a68b81aeb61165c4ada2764f57b2
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:11:00 +0530

    Add AliExpress and Alibaba entries to niger.js

--- niger.js
+++ niger.js
@@ -10,5 +10,23 @@ window.countryShops["niger"] = [
     d:"ubuy niger is an international online shopping website where customers in niger can buy imported electronics, mobiles and tablets, home goods, fashion, beauty and health items, books, sports and outdoor gear, groceries, jewellery and automotive accessories from global brands at low prices.[web:1301][web:1303][web:1307]",
     k:"ubuy niger,international online shopping niger,imported electronics mobiles tablets home goods,fashion beauty health items books sports gear,groceries jewellery and automotive accessories cross border shopping niger",
     a:""
-  }
+  },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
 ];
