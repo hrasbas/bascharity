9d489f62cd3c4fc80116bbb7ff63e203103e3216
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:07:25 +0530

    Add AliExpress and Alibaba entries to nauru.js

--- nauru.js
+++ nauru.js
@@ -10,5 +10,23 @@ window.countryShops["nauru"] = [
     d:"ubuy nauru is an international online shopping website where customers in nauru can order over one hundred million imported products from global brands including electronics, fashion, beauty and health items, home goods, books, groceries, jewellery and automotive accessories at low prices.[web:1252][web:1254][web:1257]",
     k:"ubuy nauru,international online shopping nauru,imported electronics fashion beauty health,home goods books groceries online store,jewellery and automotive accessories cross border shopping nauru",
     a:""
-  }
+  },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+}
 ];
