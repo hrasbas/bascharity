172b14ef4e8f96f87b7a584e1be47acf3bc247a4
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:04:27 +0530

    Add AliExpress and Alibaba entries to mozambique.js

--- mozambique.js
+++ mozambique.js
@@ -10,5 +10,23 @@ window.countryShops["mozambique"] = [
     d:"ubuy mozambique is an international online shopping website where customers in mozambique can buy imported electronics, apparel, mobile phones, beauty products, home and kitchen goods, books, groceries, jewellery and automotive accessories from global brands at low prices.[web:1226][web:1225][web:1228][web:1230][web:1231]",
     k:"ubuy mozambique,international online shopping mozambique,imported electronics apparel mobiles beauty,home kitchen goods books groceries online store,jewellery and automotive accessories cross border shopping mozambique",
     a:""
-  }
+  },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+}
 ];
