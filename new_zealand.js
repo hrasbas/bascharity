4b06ff55efec07db9a614390b1cd65ce5bcc266a
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:09:16 +0530

    Add AliExpress and Alibaba entries to new_zealand.js

--- new_zealand.js
+++ new_zealand.js
@@ -20,6 +20,24 @@ window.countryShops["new_zealand"] = [
     k:"trade me new zealand online marketplace,nz auction and classifieds site,buy and sell new and used items,property jobs motors electronics fashion,home garden books sports gear travel services",
     a:""
   },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   {
     i:3,
     n:"themarket new zealand shopping destination",
