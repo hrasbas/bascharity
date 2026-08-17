8af86cc68979fef6a9ff59475ac6e329f8b78cf1
hrasbas <hrasbasji@gmail.com>
2026-08-17 18:08:10 +0530

    Add AliExpress and Alibaba entries to nepal.js

--- nepal.js
+++ nepal.js
@@ -11,6 +11,24 @@ window.countryShops["nepal"] = [
     k:"ubuy nepal,international online shopping nepal,imported electronics fashion beauty health supplements,home kitchen goods books sports travel gear,groceries jewellery and automotive accessories cross border shopping nepal",
     a:""
   },
+    {
+i:"1a",
+n:"AliExpress",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.aliexpress.com/",
+d:"AliExpress is a global online shopping marketplace with a huge variety of products including electronics, fashion, home items, beauty products and everyday purchases.",
+k:"aliexpress,online shopping,global marketplace,electronics shopping,fashion shopping,home goods,general shopping,online bazaar",
+a:"https://rzekl.com/g/1e8d114494cf9d224bea16525dc3e8/?ulp=https%3A%2F%2Fwww.aliexpress.com%2F"
+},
+{
+i:"1b",
+n:"Alibaba",
+c:"business,electronics,home,fashion,beauty,health,food,sports,books,jewellery,automotive,travel,software",
+u:"https://www.alibaba.com/",
+d:"Alibaba.com is a global online marketplace for wholesale products, business sourcing, manufacturing and a wide range of commercial goods.",
+k:"alibaba,online shopping,wholesale,business sourcing,global marketplace,manufacturing,ecommerce",
+a:"https://rzekl.com/g/pm1aev55clcf9d224bea219aa26f6f/"
+},
   {
     i:2,
     n:"daraz nepal online shopping",
